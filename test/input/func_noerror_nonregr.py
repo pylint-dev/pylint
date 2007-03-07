a829aa890ba096afdbf2139c6c918c4dd7964d93
Sylvain Thénault <sylvain.thenault@logilab.fr>
2007-03-07 10:18:50 +0100

    blah

---
+++ test/input/func_noerror_nonregr.py
@@ -0,0 +1,13 @@
+# pylint: disable-msg=W0104
+"""snippets of codes which have at some point made pylint crash"""
+
+__revision__ = 1
+
+def function1(cbarg = lambda: None):
+    """
+  File "/usr/lib/python2.4/site-packages/logilab/astng/scoped_nodes.py", line
+391, in mularg_class
+    i = self.argnames.index(argname)
+ValueError: list.index(x): x not in list
+    """
+    cbarg().x
