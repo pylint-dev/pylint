7e01bd6da35c9ba6ff619729f537ee3f68686b69
Sylvain Thénault <sylvain.thenault@logilab.fr>
2007-03-09 08:59:36 +0100

    defaultSuite is not working with testlib.unittest_main, go back to unittest.main

--- test/func_test_rpython.py
+++ test/func_test_rpython.py
@@ -106,6 +106,6 @@ if __name__=='__main__':
     if len(sys.argv) > 1:            
         FILTER_RGX = sys.argv[1]
         del sys.argv[1]
-    testlib.unittest_main(defaultTest='suite')
+    unittest.main(defaultTest='suite')
 
 
