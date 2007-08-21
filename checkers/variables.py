460a7875f8660c901c3adbfadf9c8f7ffaf00ea1
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2007-08-21 09:36:16 +0200

    NameError

--- checkers/variables.py
+++ checkers/variables.py
@@ -356,7 +356,7 @@ builtins. Remember that you should avoid to define new builtins when possible.'
         for name, _ in node.names:
             parts = name.split('.')
             try:
-                module = node.infer_name_module(name_parts[0]).next()
+                module = node.infer_name_module(parts[0]).next()
             except astng.ResolveError:
                 continue
             self._check_module_attrs(node, module, parts[1:])
