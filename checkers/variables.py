af04a907a2a084ec18cbb8e28552ceafd7dbb95c
Sylvain Thénault <sylvain.thenault@logilab.fr>
2009-03-23 18:43:47 +0100

    cleanup

--- checkers/variables.py
+++ checkers/variables.py
@@ -354,8 +354,6 @@ builtins. Remember that you should avoid to define new builtins when possible.'
                 continue
             else:
                 # checks for use before assigment
-                # FIXME: the last condition should just check attribute access
-                # is protected by a try: except NameError: (similar to #9219)
                 defnode = assign_parent(to_consume[name][0])
                 if defnode is not None:
                     defstmt = defnode.statement()
@@ -373,6 +371,9 @@ builtins. Remember that you should avoid to define new builtins when possible.'
                         # defined in global or builtin scope
                         if defframe.root().lookup(name)[1]:
                             maybee0601 = False
+                    # FIXME: the last condition should just check attribute
+                    # access is protected by a try: except NameError: (similar
+                    # to #9219), we simply test using are_exclusive
                     if (maybee0601
                         and stmt.fromlineno <= defstmt.fromlineno
                         and not is_defined_before(node)
