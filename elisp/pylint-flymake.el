6a770636cfec2491c47e42079231496399cd8cad
Sylvain Thénault <sylvain.thenault@logilab.fr>
2009-08-06 10:24:42 +0200

    improved flymake code and doc provided by Derek Harland

--- elisp/pylint-flymake.el
+++ elisp/pylint-flymake.el
@@ -1,11 +1,16 @@
+
+;; Configure flymake for python
+(setq pylint "epylint")
 (when (load "flymake" t)
   (defun flymake-pylint-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
+                       'flymake-create-temp-inplace))
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "epylint" (list local-file))))
-
+      (list (expand-file-name pylint "") (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pylint-init)))
+
+;; Set as a minor mode for python
+(add-hook 'python-mode-hook '(lambda () (flymake-mode)))
