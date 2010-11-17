dabe7b1b00814ec358412e36684ccfc86c0c3358
Emile Anclin <emile.anclin@logilab.fr>
2010-11-17 14:35:58 +0100

    py3k: update more tests

--- test/input/func_noerror_defined_and_used_on_same_line.py
+++ test/input/func_noerror_defined_and_used_on_same_line.py
@@ -6,6 +6,8 @@ __revision__ = None
 print [index
        for index in range(10)]
 
+print((index
+       for index in range(10)))
 
 FILTER_FUNC = lambda x: not x
 
@@ -21,3 +23,8 @@ j = 4; LAMB = lambda x: x+j
 
 FUNC4 = lambda a, b : a != b
 FUNC3 = lambda (a, b) : a != b
+
+# test http://www.logilab.org/ticket/6954:
+
+with open('f') as f: print(f.read())
+
--- test/input/func_noerror_defined_and_used_on_same_line_py24.py
+++
@@ -1,7 +0,0 @@
-#pylint: disable=C0111,C0321
-"""pylint complains about 'index' being used before definition"""
-
-__revision__ = None
-
-print (index
-       for index in range(10))
