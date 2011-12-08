e10f513f345f0bf6be6825e334a6b0defe97b0b6
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-12-08 15:28:29 +0100

    0.25.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -18,7 +18,7 @@
 
 modname = distname = 'pylint'
 
-numversion = (0, 25, 0)
+numversion = (0, 25, 1)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = ['logilab-common >= 0.53.0', 'logilab-astng >= 0.21.1']
