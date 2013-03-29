dfa5084e115d85ec933b267317f669733251044c
Sylvain Thénault <sylvain.thenault@logilab.fr>
2013-03-29 16:54:58 +0100

    add test for #124337, fixed in logilab-astng

---
+++ test/input/func_noerror_crash_124337.py
@@ -0,0 +1,11 @@
+# pylint: disable=C0121
+"""http://www.logilab.org/ticket/124337"""
+
+import gtk
+
+def print_some_constant(arg=gtk.BUTTONS_OK):
+    """crash because gtk.BUTTONS_OK, a gtk enum type, is returned by astng as a
+    constant
+    """
+    print arg
+
