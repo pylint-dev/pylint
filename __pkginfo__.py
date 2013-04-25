88cb4cd5d6597c302d2426b59d1daa59570150eb
Sylvain Thénault <sylvain.thenault@logilab.fr>
2013-04-25 13:34:37 +0200

    [pkginfo] update project's url to pylint.org and kill deprecated ftp/copyright

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -24,10 +24,8 @@ version = '.'.join([str(num) for num in numversion])
 install_requires = ['logilab-common >= 0.53.0', 'logilab-astng >= 0.24.3']
 
 license = 'GPL'
-copyright = 'Logilab S.A.'
 description = "python code static checker"
-web = "http://www.logilab.org/project/%s" % distname
-ftp = "ftp://ftp.logilab.org/pub/%s" % modname
+web = 'http://www.pylint.org'
 mailinglist = "mailto://python-projects@lists.logilab.org"
 author = 'Logilab'
 author_email = 'python-projects@lists.logilab.org'
