3dd9ed7ee2259b7b76dec15b63723651e464af86
Julien Cristau <julien.cristau@logilab.fr>
2013-06-18 19:00:40 +0200

    test: split func_excess_escapes
    Part of it is spethial in python3 (2to3 doesn't convert byte strings to
    byte strings)

--- test/input/func_excess_escapes.py
+++ test/input/func_excess_escapes.py
@@ -28,20 +28,3 @@ ESCAPE_UNICODE = "\\\\n"
 
 You shouldn't have ambiguous text like: C:\Program Files\alpha
 """
-
-# Would be valid in Unicode, but probably not what you want otherwise
-BAD_UNICODE = '\u0042'
-BAD_LONG_UNICODE = '\U00000042'
-BAD_NAMED_UNICODE = '\N{GREEK SMALL LETTER ALPHA}'
-
-GOOD_UNICODE = u'\u0042'
-GOOD_LONG_UNICODE = u'\U00000042'
-GOOD_NAMED_UNICODE = u'\N{GREEK SMALL LETTER ALPHA}'
-
-
-# Valid raw strings
-RAW_BACKSLASHES = r'raw'
-RAW_UNICODE = ur"\u0062\n"
-
-# In a comment you can have whatever you want: \ \\ \n \m
-# even things that look like bad strings: "C:\Program Files"
