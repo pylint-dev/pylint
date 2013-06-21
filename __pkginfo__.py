9f000f8eeb402899856bd681b320a4166debd31f
Sylvain Thénault <sylvain.thenault@logilab.fr>
2013-06-21 07:59:31 +0200

    fix a few places where astng was still referenced instead of astroid + bad logilab-astroid in pkginfo dependencies

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -21,7 +21,7 @@ modname = distname = 'pylint'
 numversion = (0, 28, 0)
 version = '.'.join([str(num) for num in numversion])
 
-install_requires = ['logilab-common >= 0.53.0', 'logilab-astroid >= 0.24.3']
+install_requires = ['logilab-common >= 0.53.0', 'astroid >= 0.24.3']
 
 license = 'GPL'
 description = "python code static checker"
