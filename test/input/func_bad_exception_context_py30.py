2c3bbfa9da050e9b8bfd4cbaa97e16abde174637
Claudiu Popa <pcmanticore@gmail.com>
2013-12-28 15:14:03 +0200

    Add bad-exception-context warning for checking that `raise ... from ...` sets a proper exception context.
    --HG--
    branch : exception_context

---
+++ test/input/func_bad_exception_context_py30.py
@@ -0,0 +1,17 @@
+"""Check that raise ... from .. uses a proper exception context """
+
+# pylint: disable=unreachable
+
+__revision__ = 0
+
+class ExceptionSubclass(Exception):
+    """ subclass """
+
+def test():
+    """ docstring """
+    raise IndexError from 1
+    raise IndexError from None
+    raise IndexError from ZeroDivisionError
+    raise IndexError from object()
+    raise IndexError from ExceptionSubclass
+
