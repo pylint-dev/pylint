0499a1b0ce7e3925e89d23eb44b7841df6e59be6
Holger Peters <email@holger-peters.de>
2014-04-11 12:16:09 +0200

    Add more tests for TypeChecker's getattr check and ignored_modules

--- test/test_type.py
+++ test/test_type.py
@@ -1,27 +1,53 @@
 """Unittest for the type checker."""
 
-import re
-
 from astroid import test_utils
 from pylint.checkers import typecheck
 from pylint.testutils import CheckerTestCase, Message, set_config
 
-
 class TypeCheckerTest(CheckerTestCase):
+    "Tests for pylint.checkers.typecheck"
     CHECKER_CLASS = typecheck.TypeChecker
-    CONFIG = {
-        'bad_names': set(),
-        }
 
-#    @set_config(include_naming_hint=True,
-#            const_name_hint='CONSTANT')
-    def test_naming_hint_configured_hint(self):
-        const = test_utils.extract_node("""
+    def test_no_member_in_getattr(self):
+        """Make sure that a module attribute access is checked by pylint.
+        """
+
+        node = test_utils.extract_node("""
         import argparse
         argparse.THIS_does_not_EXIST 
         """)
-        with self.assertAddsMessages(Message('no-member', node=const, args=('Module', 'argparse', 'THIS_does_not_EXIST'))):
-            self.checker.visit_getattr(const)
+        with self.assertAddsMessages(
+                Message(
+                    'no-member',
+                    node=node,
+                    args=('Module', 'argparse', 'THIS_does_not_EXIST'))):
+            self.checker.visit_getattr(node)
+
+    def test_no_member_in_getattr_ignored(self):
+        """Make sure that a module attribute access check is omitted with a
+        module that is configured to be ignored.
+        """
+
+        node = test_utils.extract_node("""
+        import optparse
+        optparse.THIS_does_not_EXIST 
+        """)
+        with self.assertNoMessages():
+            self.checker.visit_getattr(node)
+
+
+    @set_config(ignored_modules=('argparse',))
+    def test_no_member_in_getattr_ignored_cust_config(self):
+        """Make sure that a module can be added to the ignored_modules list and
+        a no-member message will not be raised upon attribute access.
+        """
+
+        node = test_utils.extract_node("""
+        import argparse
+        argparse.THIS_does_not_EXIST 
+        """)
+        with self.assertNoMessages():
+            self.checker.visit_getattr(node)
 
 
 if __name__ == '__main__':
