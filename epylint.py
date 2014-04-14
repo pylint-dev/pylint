db303eb0c3ef4ed77ba01de7e1193028961590b8
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-04-14 16:25:34 -0400

    Fix merge error.  Closes #133.

--- epylint.py
+++ epylint.py
@@ -87,8 +87,9 @@ def lint(filename, options=None):
     from pylint import lint as lint_mod
     lint_path = lint_mod.__file__
     options = options or ['--disable=C,R,I']
-    cmd = [sys.executable, lint_path] + options + ['--msg-template',
-            '{path}:{line}: [{symbol}, {obj}] {msg}', '-r', 'n', child_path]
+    cmd = [sys.executable, lint_path] + options + [
+        '--msg-template', '{path}:{line}: {category} ({msg_id}, {symbol}, {obj}) {msg}',
+        '-r', 'n', child_path]
     process = Popen(cmd, stdout=PIPE, cwd=parent_path, env=_get_env(),
                     universal_newlines=True)
 
