1ab3a5b65b0b61f63ea153e6f9a0ed0b555e155b
Alexandru Coman <fcoman@bitdefender.com>
2014-07-09 01:44:35 +0300

    Fixing Issue #149 (W0511 false positive)

--- test/input/func_fixme.py
+++ test/input/func_fixme.py
@@ -1,9 +1,11 @@
 """docstring"""
-
+# pylint: disable=W0612
 __revision__ = ''
 
 # FIXME: beep
 
+
 def function():
     '''XXX:bop'''
-
+    variable = "FIXME: Ignore me!"
+    test = "text"  # FIXME: Valid test
