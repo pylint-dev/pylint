eaa59f6f26841d45ed2c6efff7d8dcaf4889f423
Claudiu Popa <pcmanticore@gmail.com>
2014-08-24 22:13:07 +0300

    1.3.1.
    --HG--
    branch : pylint-1.3

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -19,14 +19,14 @@ import sys
 
 modname = distname = 'pylint'
 
-numversion = (1, 3, 0)
+numversion = (1, 3, 1)
 version = '.'.join([str(num) for num in numversion])
 
 if sys.version_info < (2, 6):
-    install_requires = ['logilab-common >= 0.53.0', 'astroid >= 1.2',
+    install_requires = ['logilab-common >= 0.53.0', 'astroid >= 1.2.1',
                         'StringFormat']
 else:
-    install_requires = ['logilab-common >= 0.53.0', 'astroid >= 1.2']
+    install_requires = ['logilab-common >= 0.53.0', 'astroid >= 1.2.1']
 
 license = 'GPL'
 description = "python code static checker"
