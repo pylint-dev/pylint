55815119ab0fb84d295f2d7852c2fbee7b6d3d89
Claudiu Popa <pcmanticore@gmail.com>
2014-09-16 15:14:23 +0300

    Add test for issue #338.
    The problem was fixed in astroid, with changeset 2359964.

--- test/functional/string_formatting.py
+++ test/functional/string_formatting.py
@@ -127,3 +127,19 @@ def issue322():
     '{0}{1[FOO]}'.format(123, {'FOO': 456})
     '{0}{1[FOO]}'.format(123, {'FOO': 456}, 321) # [too-many-format-args]
     '{0}{1[FOO]}'.format(123) # [too-few-format-args]
+
+def issue338():
+    """
+    Check that using a namedtuple subclass doesn't crash when
+    trying to infer EmptyNodes (resulted after mocking the
+    members of namedtuples).
+    """
+    from collections import namedtuple
+
+    class Crash(namedtuple("C", "foo bar")):
+        """ Looking for attributes in __str__ will crash,
+        because EmptyNodes can't be infered.
+        """
+        def __str__(self):
+            return "{0.foo}: {0.bar}".format(self)
+    return Crash
