a8f764450c9a79570d1e08ab576b8be73625c7e1
Claudiu Popa <cpopa@cloudbasesolutions.com>
2014-12-06 13:41:43 +0200

    Minimize the except block, by catching only what will raise NoSuchArgumentError.

--- checkers/stdlib.py
+++ checkers/stdlib.py
@@ -138,15 +138,17 @@ class StdlibChecker(BaseChecker):
     def _check_open_mode(self, node):
         """Check that the mode argument of an open or file call is valid."""
         try:
-            mode_arg = utils.get_argument_from_call(node, position=1, keyword='mode')
-            if mode_arg:
-                mode_arg = utils.safe_infer(mode_arg)
-                if (isinstance(mode_arg, astroid.Const)
-                        and not _check_mode_str(mode_arg.value)):
-                    self.add_message('bad-open-mode', node=node,
-                                     args=(mode_arg.value))
-        except (utils.NoSuchArgumentError, TypeError):
-            pass
+            mode_arg = utils.get_argument_from_call(node, position=1,
+                                                    keyword='mode')
+        except utils.NoSuchArgumentError:
+            return
+        if mode_arg:
+            mode_arg = utils.safe_infer(mode_arg)
+            if (isinstance(mode_arg, astroid.Const)
+                    and not _check_mode_str(mode_arg.value)):
+                self.add_message('bad-open-mode', node=node,
+                                 args=mode_arg.value)
+
 
 def register(linter):
     """required method to auto register this checker """
