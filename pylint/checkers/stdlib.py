7a10fa91dd8a24040d346979cdb0661e1d33bd7f
Chris Rebert <code@rebertia.com>
2015-02-21 01:36:09 -0800

    add missing spaces between words in W1503's docs
    --HG--
    branch : missing-spaces

--- pylint/checkers/stdlib.py
+++ pylint/checkers/stdlib.py
@@ -96,8 +96,8 @@ class StdlibChecker(BaseChecker):
         'W1503': ('Redundant use of %s with constant '
                   'value %r',
                   'redundant-unittest-assert',
-                  'The first argument of assertTrue and assertFalse is'
-                  'a condition. If a constant is passed as parameter, that'
+                  'The first argument of assertTrue and assertFalse is '
+                  'a condition. If a constant is passed as parameter, that '
                   'condition will be always true. In this case a warning '
                   'should be emitted.')
     }
