e0f2c50566a2d18383e31d0fe1ff57096bf56c50
Mihai Balint <balint.mihai@gmail.com>
2015-02-28 17:13:21 +0200

    start removing checks from disabled list, fix code to make sure these pass: C0102,C0112,C0121,C0202,C0203,C0204,C0301

--- pylint/epylint.py
+++ pylint/epylint.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8; mode: python; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=python:et:sw=4:ts=4:sts=4
+# -*- coding: utf-8; mode: python; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4
+# -*- vim:fenc=utf-8:ft=python:et:sw=4:ts=4:sts=4
 # Copyright (c) 2003-2013 LOGILAB S.A. (Paris, FRANCE).
 # http://www.logilab.fr/ -- mailto:contact@logilab.fr
 #
