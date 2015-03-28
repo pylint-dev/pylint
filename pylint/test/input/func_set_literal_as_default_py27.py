929b815499e1bc498f9b501d4b3ffb7d6a8ee5b6
Claudiu Popa <cpopa@cloudbasesolutions.com>
2015-03-28 14:43:53 +0200

    Port more tests from the old test style to the new test style.

--- pylint/test/input/func_set_literal_as_default_py27.py
+++
@@ -1,7 +0,0 @@
-"""docstring"""
-from __future__ import print_function
-__revision__ = ''
-
-def function1(value={1}):
-    """set is mutable and dangerous."""
-    print(value)
--- pylint/test/input/func_w0103.py
+++
@@ -1,8 +0,0 @@
-"""test max branch
-"""
-from __future__ import print_function
-__revision__ = ''
-
-def stupid_function(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9):
-    """reallly stupid function"""
-    print(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
