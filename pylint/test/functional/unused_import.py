2c933470b87a6eccf28090b2160dabf0e4d38af4
Radu Ciorba <radu@devrandom.ro>
2015-03-28 15:26:42 +0200

    add functional tests for unsed import false positive #475
    --HG--
    branch : unused-import

--- pylint/test/functional/unused_import.py
+++ pylint/test/functional/unused_import.py
@@ -1,12 +1,15 @@
 """unused import"""
-# pylint: disable=undefined-all-variable, import-error, no-absolute-import
+# pylint: disable=undefined-all-variable, import-error, no-absolute-import, too-few-public-methods, missing-docstring
 import xml.etree  # [unused-import]
 import xml.sax  # [unused-import]
 import os.path as test  # [unused-import]
 from sys import argv as test2  # [unused-import]
 from sys import flags  # [unused-import]
 # +1:[unused-import,unused-import]
-from collections import deque, OrderedDict, Counter
+from collections import deque, OrderedDict, Counter, defaultdict
 DATA = Counter()
+
+class SomeClass(object):
+    attr = defaultdict
 
 from never import __all__
