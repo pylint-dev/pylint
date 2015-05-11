67dd07bb7269c93b8b9385e3edf2ba5bc3ed95d3
Claudiu Popa <cpopa@cloudbasesolutions.com>
2015-05-11 23:46:13 +0300

    Port attribute-defined-outside-init to the new test style format.

---
+++ pylint/test/functional/attribute_defined_outside_init.py
@@ -0,0 +1,54 @@
+# pylint: disable=missing-docstring,too-few-public-methods,invalid-name
+
+class A(object):
+
+    def __init__(self):
+        self.x = 0
+        self.setUp()
+
+    def set_y(self, y):
+        self.y = y
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_z(self, z):
+        self.z = z # [attribute-defined-outside-init]
+
+    def setUp(self):
+        self.x = 0
+        self.y = 0
+
+
+class B(A):
+
+    def test(self):
+        self.z = 44 # [attribute-defined-outside-init]
+
+
+class C(object):
+
+    def __init__(self):
+        self._init()
+
+    def _init(self):
+        self.z = 44
+
+
+class D(object):
+
+    def setUp(self):
+        self.set_z()
+
+    def set_z(self):
+        self.z = 42
+
+
+class E(object):
+
+    def __init__(self):
+        i = self._init
+        i()
+
+    def _init(self):
+        self.z = 44
