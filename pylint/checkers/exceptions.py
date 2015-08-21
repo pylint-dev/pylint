b3559ea5dd0d87bd1531373242ee660fa6683842
Rene Zhang <rz99@cornell.edu>
2015-08-21 11:33:30 -0700

    More idiomatic python

--- pylint/checkers/exceptions.py
+++ pylint/checkers/exceptions.py
@@ -194,8 +194,7 @@ class ExceptionsChecker(BaseChecker):
               isinstance(expr, (astroid.List, astroid.Dict, astroid.Tuple,
                                 astroid.Module, astroid.Function))):
             emit = True
-            if (not PY3K and isinstance(expr, astroid.Tuple) and
-                len(expr.elts) > 0):
+            if not PY3K and isinstance(expr, astroid.Tuple) and expr.elts:
                 # On Python 2, using the following is not an error:
                 #    raise (ZeroDivisionError, None)
                 #    raise (ZeroDivisionError, )
