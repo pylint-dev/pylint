c79413de026d27f3ef57e5b3b8587ed584d4a0ab
Claudiu Popa <pcmanticore@gmail.com>
2015-09-18 23:57:11 +0000

    Get the sections sorted in options_by_section. This means the generated rcfile always has a consistent order. Closes issue #602.

--- pylint/config.py
+++ pylint/config.py
@@ -756,7 +756,7 @@ class OptionsProviderMixIn(object):
                 (optname, optdict, self.option_value(optname)))
         if None in sections:
             yield None, sections.pop(None)
-        for section, options in sections.items():
+        for section, options in sorted(sections.items()):
             yield section.upper(), options
 
     def options_and_values(self, options=None):
