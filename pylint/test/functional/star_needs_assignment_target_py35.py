2dd49c31140270448eb85a2fdc155ffe8de36dc4
Cezar <celnazli@bitdefender.com>
2015-10-04 14:53:19 +0300

    More PEP 448 tests

--- pylint/test/functional/star_needs_assignment_target_py35.py
+++ pylint/test/functional/star_needs_assignment_target_py35.py
@@ -4,4 +4,7 @@ https://www.python.org/dev/peps/pep-0448/
 """
 UNPACK_TUPLE = (*range(4), 4)
 UNPACK_LIST = [*range(4), 4]
-UNPACK_DICT = {*range(4), 4}
+UNPACK_SET = {*range(4), 4}
+UNPACK_DICT = {'a': 1, **{'b': '2'}}
+UNPACK_DICT2 = {**UNPACK_DICT, "x": 1, "y": 2}
+UNPACK_DICT3 = {**{'a': 1}, 'a': 2, **{'a': 3}}
