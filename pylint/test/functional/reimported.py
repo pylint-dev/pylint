69e6f6cbe8cda80a465a5607fd7f8f01d8bcb1bb
Claudiu Popa <pcmanticore@gmail.com>
2015-12-01 10:33:33 +0200

    Refactor things through the imports checker
    This patch transforms some public functions / methods to private
    and moves some blocks of code into their own functions. Through
    the latter, a couple of new messages are now emitted even though
    the module couldn't be imported, such as reimported, which doesn't
    make sense to not emit in this case.

---
+++ pylint/test/functional/reimported.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-docstring,unused-import,import-error
+
+from time import sleep, sleep # [reimported]
+from lala import missing, missing # [reimported]
+
+import missing1
+import missing1 # [reimported]
