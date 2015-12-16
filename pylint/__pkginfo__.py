45780992de1fe5b55c756a2f279052ba8d36ad5f
Claudiu Popa <pcmanticore@gmail.com>
2015-12-16 17:48:28 +0200

    master pylint is now 1.6.0

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -23,7 +23,7 @@ from os.path import join
 
 modname = distname = 'pylint'
 
-numversion = (1, 5, 0)
+numversion = (1, 6, 0)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = [
