9e8e2ae9ad7636af3e81b0418e7de959efcfda94
Claudiu Popa <pcmanticore@gmail.com>
2015-12-21 02:15:22 +0200

    Prepare 1.5.2 release.

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -23,7 +23,7 @@ from os.path import join
 
 modname = distname = 'pylint'
 
-numversion = (1, 5, 1)
+numversion = (1, 5, 2)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = [
