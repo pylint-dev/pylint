47a988fc662c1cf902a30123bd208d1cfc734937
Kevin Phillips <thefriendlycoder@gmail.com>
2016-03-02 23:15:40 -0400

    Fixed Windows startup scripts so they redirect to the executable files generated by setuptools.

--- bin/pyreverse.bat
+++ bin/pyreverse.bat
@@ -2,4 +2,4 @@
 rem Use python to execute the python script having the same name as this batch
 rem file, but without any extension, located in the same directory as this
 rem batch file
-python "%~dpn0" %*
+"%~dpn0" %*
