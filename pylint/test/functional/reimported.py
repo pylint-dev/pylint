e36fa297bae2346d42690191708973c9ca49e3a2
Claudiu Popa <pcmanticore@gmail.com>
2016-03-25 11:56:48 +0000

    Ignore reimports when an alias is used.

--- pylint/test/functional/reimported.py
+++ pylint/test/functional/reimported.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-docstring,unused-import,import-error, wildcard-import,unused-wildcard-import,redefined-builtin,no-name-in-module
+# pylint: disable=missing-docstring,unused-import,import-error, wildcard-import,unused-wildcard-import,redefined-builtin,no-name-in-module,ungrouped-imports
 
 from time import sleep, sleep # [reimported]
 from lala import missing, missing # [reimported]
@@ -8,6 +8,9 @@ import missing1 # [reimported]
 
 from collections import deque
 from itertools import deque # [reimported]
+
+from collections import OrderedDict
+from itertools import OrderedDict as NotOrderedDict
 
 from itertools import *
 from os import *
