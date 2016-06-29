7a9b26b401752845b03b84aee479c370d4cbf951
Claudiu Popa <pcmanticore@gmail.com>
2016-06-29 18:19:21 +0100

    Make sure that astroid's master is installed, instead of relying on a unpublished version.

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -15,10 +15,14 @@ numversion = (2, 0, 0)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = [
-    'astroid >= 1.5.0,<1.6.0',
+    'astroid',
     'six',
     'isort >= 4.2.5',
     'mccabe',
+]
+
+dependency_links = [
+    'https://github.com/PyCQA/astroid/tarball/master#egg=astroid-master-1.5'
 ]
 
 if sys.platform == 'win32':
