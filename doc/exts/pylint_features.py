35019b87f2f12e3684870040810fa87bc2991a10
Claudiu Popa <pcmanticore@gmail.com>
2016-07-06 17:51:32 +0100

    Add a Sphinx hook for generating the features file of Pylint's documentation
    Currently we can generate this locally using the makefiles, but, unfortunately,
    on ReadThedocs we are running only sphinx-build, which means that we have no
    pre-generated features.rst file before actually building the documentation.
    This commit just adds one, which does the same thing as the makefiles.

---
+++ doc/exts/pylint_features.py
@@ -0,0 +1,29 @@
+# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# For details: https://github.com/PyCQA/pylint/blob/master/COPYING
+
+"""Script used to generate the features file before building the actual documentation."""
+
+import os
+import subprocess
+import sys
+
+import sphinx
+
+
+def builder_inited(app):
+    popen = subprocess.Popen([sys.executable, "-m", "pylint", "--full-documentation"],
+                             stdout=subprocess.PIPE)
+    output, _ = popen.communicate()
+
+    if not output:
+        print("Pylint might not be available.")
+        return
+
+    features = os.path.join(os.path.dirname('.'), 'features.rst')
+    with open(features, 'wb') as stream:
+        stream.write(output)
+
+
+def setup(app):
+    app.connect('builder-inited', builder_inited)
+    return {'version': sphinx.__display_version__}
