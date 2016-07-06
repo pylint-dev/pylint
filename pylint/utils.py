c280a1ef191146c8f5a73e4a090b90b7a7807bda
Claudiu Popa <pcmanticore@gmail.com>
2016-07-06 15:11:06 +0100

    Remove the deprecation message from inside the closure and pass it as an argument instead.

--- pylint/utils.py
+++ pylint/utils.py
@@ -987,11 +987,10 @@ def get_global_option(checker, option, default=None):
     return default
 
 
-def deprecated_option(shortname=None, opt_type=None, help_msg=None):
+def deprecated_option(shortname=None, opt_type=None, help_msg=None, deprecation_msg=None):
     def _warn_deprecated(option, optname, *args): # pylint: disable=unused-argument
-        msg = ("Warning: option %s is obsolete and "
-               "it is slated for removal in Pylint 1.6.\n")
-        sys.stderr.write(msg % (optname,))
+        if deprecation_msg:
+            sys.stderr.write(deprecation_msg % (optname,))
 
     option = {
         'help': help_msg,
