d13c17873809d388d76649ec795a31acfb9288b5
Claudiu Popa <pcmanticore@gmail.com>
2016-07-15 17:05:56 +0300

    Split the tests into 3.5+ tests and <3.5 tests for useless-super-delegation.

--- pylint/test/functional/useless_super_delegation_py3.py
+++ pylint/test/functional/useless_super_delegation_py3.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-docstring, no-member, unused-argument, invalid-name,unused-variable
-
+# pylint: disable=too-few-public-methods
 
 class NotUselessSuper(object):
 
@@ -10,14 +10,7 @@ class NotUselessSuper(object):
         return super(NotUselessSuper, self).passing_keyword_only_with_modifications(
             first, second + 1)
 
-    def not_passing_all_params(self, first, *args, second=None, **kwargs):
-        return super(NotUselessSuper, self).not_passing_all_params(*args, second, **kwargs)
-
-
 class UselessSuper(object):
 
-    def useless(self, first, *, second=None, **kwargs): # [useless-super-delegation]
-        return super(UselessSuper, self).useless(first, second=second, **kwargs)
-
-    def useless_1(self, *, first): # [useless-super-delegation]
-        super(UselessSuper, self).useless_1(first=first)
+    def useless(self, *, first): # [useless-super-delegation]
+        super(UselessSuper, self).useless(first=first)
