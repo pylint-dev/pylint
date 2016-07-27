803a2cc6c7a2c786e715f7a364d4ee195b0c5757
Claudiu Popa <pcmanticore@gmail.com>
2016-07-27 22:47:00 +0300

    Move the recommendation checkers into refactoring, since it belongs there.

--- pylint/checkers/base.py
+++ pylint/checkers/base.py
@@ -1458,98 +1458,6 @@ class LambdaForComprehensionChecker(_BasicChecker):
             self.add_message('deprecated-lambda', node=node)
 
 
-class RecommandationChecker(_BasicChecker):
-    msgs = {'C0200': ('Consider using enumerate instead of iterating with range and len',
-                      'consider-using-enumerate',
-                      'Emitted when code that iterates with range and len is '
-                      'encountered. Such code can be simplified by using the '
-                      'enumerate builtin.'),
-            'C0201': ('Consider iterating the dictionary directly instead of calling .keys()',
-                      'consider-iterating-dictionary',
-                      'Emitted when the keys of a dictionary are iterated through the .keys() '
-                      'method. It is enough to just iterate through the dictionary itself, as '
-                      'in "for key in dictionary".'),
-           }
-
-    @staticmethod
-    def _is_builtin(node, function):
-        inferred = utils.safe_infer(node)
-        if not inferred:
-            return False
-        return utils.is_builtin_object(inferred) and inferred.name == function
-
-    @utils.check_messages('consider-iterating-dictionary')
-    def visit_call(self, node):
-        inferred = utils.safe_infer(node.func)
-        if not inferred:
-            return
-
-        if not isinstance(inferred, astroid.BoundMethod):
-            return
-        if not isinstance(inferred.bound, astroid.Dict) or inferred.name != 'keys':
-            return
-
-        # Check if the statement is what we're expecting to have.
-        statement = node.statement()
-        if isinstance(statement, astroid.Expr):
-            statement = statement.value
-
-        if isinstance(statement, astroid.For) or utils.is_comprehension(statement):
-            self.add_message('consider-iterating-dictionary', node=node)
-
-    @utils.check_messages('consider-using-enumerate')
-    def visit_for(self, node):
-        """Emit a convention whenever range and len are used for indexing."""
-        # Verify that we have a `range(len(...))` call and that the object
-        # which is iterated is used as a subscript in the body of the for.
-
-        # Is it a proper range call?
-        if not isinstance(node.iter, astroid.Call):
-            return
-        if not self._is_builtin(node.iter.func, 'range'):
-            return
-        if len(node.iter.args) != 1:
-            return
-
-        # Is it a proper len call?
-        if not isinstance(node.iter.args[0], astroid.Call):
-            return
-        second_func = node.iter.args[0].func
-        if not self._is_builtin(second_func, 'len'):
-            return
-        len_args = node.iter.args[0].args
-        if not len_args or len(len_args) != 1:
-            return
-        iterating_object = len_args[0]
-        if not isinstance(iterating_object, astroid.Name):
-            return
-
-        # Verify that the body of the for loop uses a subscript
-        # with the object that was iterated. This uses some heuristics
-        # in order to make sure that the same object is used in the
-        # for body.
-        for child in node.body:
-            for subscript in child.nodes_of_class(astroid.Subscript):
-                if not isinstance(subscript.value, astroid.Name):
-                    continue
-                if not isinstance(subscript.slice, astroid.Index):
-                    continue
-                if not isinstance(subscript.slice.value, astroid.Name):
-                    continue
-                if subscript.slice.value.name != node.target.name:
-                    continue
-                if iterating_object.name != subscript.value.name:
-                    continue
-                if subscript.value.scope() != node.scope():
-                    # Ignore this subscript if it's not in the same
-                    # scope. This means that in the body of the for
-                    # loop, another scope was created, where the same
-                    # name for the iterating object was used.
-                    continue
-                self.add_message('consider-using-enumerate', node=node)
-                return
-
-
 def _is_one_arg_pos_call(call):
     """Is this a call with exactly 1 argument,
     where that argument is positional?
@@ -1885,5 +1793,4 @@ def register(linter):
     linter.register_checker(PassChecker(linter))
     linter.register_checker(LambdaForComprehensionChecker(linter))
     linter.register_checker(ComparisonChecker(linter))
-    linter.register_checker(RecommandationChecker(linter))
     linter.register_checker(ElifChecker(linter))
--- pylint/checkers/refactoring.py
+++ pylint/checkers/refactoring.py
@@ -16,7 +16,8 @@ from pylint.interfaces import IAstroidChecker
 from pylint.checkers import BaseChecker
 from pylint.checkers.utils import (
     check_messages, is_builtin_object, safe_infer,
-    is_none, node_type)
+    node_type)
+from pylint.checkers import utils
 
 
 
@@ -88,6 +89,100 @@ class RefactoringChecker(BaseChecker):
                              args=(duplicated_name, ', '.join(names)))
 
 
+class RecommandationChecker(BaseChecker):
+    __implements__ = (IAstroidChecker, )
+    name = 'refactoring'
+    msgs = {'C0200': ('Consider using enumerate instead of iterating with range and len',
+                      'consider-using-enumerate',
+                      'Emitted when code that iterates with range and len is '
+                      'encountered. Such code can be simplified by using the '
+                      'enumerate builtin.'),
+            'C0201': ('Consider iterating the dictionary directly instead of calling .keys()',
+                      'consider-iterating-dictionary',
+                      'Emitted when the keys of a dictionary are iterated through the .keys() '
+                      'method. It is enough to just iterate through the dictionary itself, as '
+                      'in "for key in dictionary".'),
+           }
+
+    @staticmethod
+    def _is_builtin(node, function):
+        inferred = utils.safe_infer(node)
+        if not inferred:
+            return False
+        return utils.is_builtin_object(inferred) and inferred.name == function
+
+    @utils.check_messages('consider-iterating-dictionary')
+    def visit_call(self, node):
+        inferred = utils.safe_infer(node.func)
+        if not inferred:
+            return
+
+        if not isinstance(inferred, astroid.BoundMethod):
+            return
+        if not isinstance(inferred.bound, astroid.Dict) or inferred.name != 'keys':
+            return
+
+        # Check if the statement is what we're expecting to have.
+        statement = node.statement()
+        if isinstance(statement, astroid.Expr):
+            statement = statement.value
+
+        if isinstance(statement, astroid.For) or utils.is_comprehension(statement):
+            self.add_message('consider-iterating-dictionary', node=node)
+
+    @utils.check_messages('consider-using-enumerate')
+    def visit_for(self, node):
+        """Emit a convention whenever range and len are used for indexing."""
+        # Verify that we have a `range(len(...))` call and that the object
+        # which is iterated is used as a subscript in the body of the for.
+
+        # Is it a proper range call?
+        if not isinstance(node.iter, astroid.Call):
+            return
+        if not self._is_builtin(node.iter.func, 'range'):
+            return
+        if len(node.iter.args) != 1:
+            return
+
+        # Is it a proper len call?
+        if not isinstance(node.iter.args[0], astroid.Call):
+            return
+        second_func = node.iter.args[0].func
+        if not self._is_builtin(second_func, 'len'):
+            return
+        len_args = node.iter.args[0].args
+        if not len_args or len(len_args) != 1:
+            return
+        iterating_object = len_args[0]
+        if not isinstance(iterating_object, astroid.Name):
+            return
+
+        # Verify that the body of the for loop uses a subscript
+        # with the object that was iterated. This uses some heuristics
+        # in order to make sure that the same object is used in the
+        # for body.
+        for child in node.body:
+            for subscript in child.nodes_of_class(astroid.Subscript):
+                if not isinstance(subscript.value, astroid.Name):
+                    continue
+                if not isinstance(subscript.slice, astroid.Index):
+                    continue
+                if not isinstance(subscript.slice.value, astroid.Name):
+                    continue
+                if subscript.slice.value.name != node.target.name:
+                    continue
+                if iterating_object.name != subscript.value.name:
+                    continue
+                if subscript.value.scope() != node.scope():
+                    # Ignore this subscript if it's not in the same
+                    # scope. This means that in the body of the for
+                    # loop, another scope was created, where the same
+                    # name for the iterating object was used.
+                    continue
+                self.add_message('consider-using-enumerate', node=node)
+                return
+
+
 class NotChecker(BaseChecker):
     """checks for too many not in comparison expressions
 
@@ -152,3 +247,4 @@ def register(linter):
     """Required method to auto register this checker."""
     linter.register_checker(RefactoringChecker(linter))
     linter.register_checker(NotChecker(linter))
+    linter.register_checker(RecommandationChecker(linter))
