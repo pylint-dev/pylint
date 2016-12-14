ea4273cdff22cb5c0bc5fd8a408cd03b454c43bf
Erik <erik.eriksson@yahoo.com>
2016-12-14 17:56:49 +0100

    Extracted overlapping exceptions checker into extension

--- pylint/test/functional/invalid_exceptions_caught.py
+++ pylint/test/functional/invalid_exceptions_caught.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-docstring, too-few-public-methods
-# pylint: disable=too-many-ancestors, no-absolute-import, import-error, multiple-imports,wrong-import-position, overlapping-except
+# pylint: disable=too-many-ancestors, no-absolute-import, import-error, multiple-imports,wrong-import-position
 from __future__ import print_function
 
 import socket, binascii, abc, six
