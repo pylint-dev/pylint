4a3abc540063a06818015f76fe16f534ef731965
JesusZapata <jesus@vauxoo.com>
2017-01-02 21:19:18 +0000

    [FIX] pylint: Delete unnecessary code

--- pylint/test/functional/except_pass.py
+++ pylint/test/functional/except_pass.py
@@ -1,17 +1,6 @@
 # coding: utf-8
 """Test Except Pass usage"""
-# pylint: disable=too-few-public-methods, no-self-use, continue-in-finally
-# pylint: disable=undefined-variable, bare-except, continue-in-finally
-# pylint: disable=bad-except-order
-# pylint: disable=import-error
-# pylint: disable=ungrouped-imports, redefine-in-handler
-# pylint: disable=too-many-nested-blocks, misplaced-bare-raise
-# pylint: disable=redefined-argument-from-local
-# pylint: disable=no-name-in-module, unnecessary-pass
-# pylint: disable=redefined-argument-from-local
-# pylint: disable=undefined-variable, bare-except, continue-in-finally
-# pylint: disable=bad-except-order, no-member, wrong-import-position
-# pylint: disable=undefined-variable
+# pylint: disable=too-few-public-methods, no-self-use
 
 
 class TestExceptPass(object):
