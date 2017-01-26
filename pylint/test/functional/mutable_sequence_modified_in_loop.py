e70d1e26bd0fc95967aee7e179e1b41d194478fc
JesusZapata <jesus@vauxoo.com>
2017-01-26 21:21:39 +0000

    [FIX] Fix case and test file for mutable_sequence_modified_in_loop

--- pylint/test/functional/mutable_sequence_modified_in_loop.py
+++ pylint/test/functional/mutable_sequence_modified_in_loop.py
@@ -1,12 +1,16 @@
-items = range(1, 6)
-for item in items:
-    print(item)
-    items.remove(item)
+""" The mutable sequence was modified inside the loop """
+
+# pylint:disable=superfluous-parens,using-constant-test,invalid-name,missing-docstring
 
 items = range(1, 6)
 for item in items:
     print(item)
-    items.pop(item)
+    items.remove(item)  # [mutable-sequence-modified-in-loop]
+
+items = range(1, 6)
+for item in items:
+    print(item)
+    items.pop(item)  # [mutable-sequence-modified-in-loop]
 
 items = range(1, 6)
 for item in list(items):
@@ -17,12 +21,12 @@ for item in list(items):
 items = range(1, 6)
 for item in items:
     print(item)
-    items.remove(item)
+    items.remove(item)  # [mutable-sequence-modified-in-loop]
 
 items = range(1, 6)
 for item in items:
     if True:
-        items.remove(item)
+        items.remove(item)  # [mutable-sequence-modified-in-loop]
         if True:
-            items.pop()
+            items.pop()  # [mutable-sequence-modified-in-loop]
     print(item)
