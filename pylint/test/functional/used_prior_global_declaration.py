cab2a941d702f23d82b52fdd91758545dafdf98a
Claudiu Popa <pcmanticore@gmail.com>
2017-01-28 14:09:04 +0200

    Added a new error, 'used-prior-global-declaration'.
    This is emitted when a name is used prior a global declaration in a function.
    This causes a SyntaxError in Python 3.6
    
    Close #1257

---
+++ pylint/test/functional/used_prior_global_declaration.py
@@ -0,0 +1,22 @@
+# pylint: disable=missing-docstring, pointless-statement, global-variable-not-assigned, global-statement
+
+
+CONST = 1
+
+
+def test():
+    CONST  # [used-prior-global-declaration]
+
+    global CONST
+
+
+def other_test():
+    CONST
+
+    global SOMETHING
+
+
+def other_test_1():
+    global SOMETHING
+
+    CONST
