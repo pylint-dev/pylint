e80ed4c721c39779c7aaa1e4d6b6acdbbd9ffdee
Łukasz Rogalski <rogalski.91@gmail.com>
2017-02-09 21:34:57 +0100

    Add failing test case for missing module

--- pylint/test/functional/wildcard_import_allowed.py
+++ pylint/test/functional/wildcard_import_allowed.py
@@ -1,3 +1,4 @@
 # pylint: disable=missing-docstring,unused-wildcard-import,redefined-builtin
 from csv import *
 from abc import *  # [wildcard-import]
+from UNINFERABLE import *  # [wildcard-import]
