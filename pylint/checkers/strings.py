0d3b4a72b585b34cc12cb95eaf62ca3d13173a9e
Łukasz Rogalski <rogalski.91@gmail.com>
2017-02-19 12:24:42 +0100

    Understand starred expression used in string formatting RHS tuple (#1299)
    Fixes #957

--- pylint/checkers/strings.py
+++ pylint/checkers/strings.py
@@ -304,7 +304,10 @@ class StringFormatChecker(BaseChecker):
             # the % operator matches the number required by the format
             # string.
             if isinstance(args, astroid.Tuple):
-                num_args = len(args.elts)
+                rhs_tuple = utils.safe_infer(args)
+                num_args = None
+                if rhs_tuple not in (None, astroid.Uninferable):
+                    num_args = len(rhs_tuple.elts)
             elif isinstance(args, OTHER_NODES + (astroid.Dict, astroid.DictComp)):
                 num_args = 1
             else:
