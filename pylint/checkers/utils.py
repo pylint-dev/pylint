56daa5c3a0d2d6592c673c2932c354b68679997f
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2017-03-01 04:59:08 -0600

    Use a lru_cache for unimplemented_abstract_methods and safe_infer
    This change should speed up Pylint a bit.

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -18,6 +18,10 @@ try:
 except ImportError:
     # pylint: disable=import-error
     from singledispatch import singledispatch as singledispatch
+try:
+    from functools import lru_cache
+except ImportError:
+    from backports.functools_lru_cache import lru_cache
 import itertools
 import re
 import sys
@@ -515,6 +519,7 @@ def decorated_with(func, qnames):
     return False
 
 
+@lru_cache(maxsize=1024)
 def unimplemented_abstract_methods(node, is_abstract_cb=None):
     """
     Get the unimplemented abstract methods for the given *node*.
@@ -756,6 +761,7 @@ def supports_delitem(value):
 
 
 # TODO(cpopa): deprecate these or leave them as aliases?
+@lru_cache(maxsize=1024)
 def safe_infer(node, context=None):
     """Return the inferred value for the given node.
 
