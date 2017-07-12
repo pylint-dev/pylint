e7fa7ae62527bd335da973f8b696fb2a5927f271
Ashley Whetter <ashley@awhetter.co.uk>
2017-07-12 04:48:06 +0100

    Slotted in IniFileParser and Configuration

--- pylint/lint.py
+++ pylint/lint.py
@@ -1453,13 +1453,16 @@ group are mutually exclusive.",
         linter.read_config_file(verbose=self.verbose)
         config_parser = linter.cfgfile_parser
         # run init hook, if present, before loading plugins
-        if config_parser.has_option("MASTER", "init-hook"):
+        if config_parser._parser.has_option("MASTER", "init-hook"):
             cb_init_hook(
-                "init-hook", utils._unquote(config_parser.get("MASTER", "init-hook"))
+                "init-hook",
+                utils._unquote(config_parser._parser.get("MASTER", "init-hook")),
             )
         # is there some additional plugins in the file configuration, in
-        if config_parser.has_option("MASTER", "load-plugins"):
-            plugins = utils._splitstrip(config_parser.get("MASTER", "load-plugins"))
+        if config_parser._parser.has_option("MASTER", "load-plugins"):
+            plugins = utils._splitstrip(
+                config_parser._parser.get("MASTER", "load-plugins")
+            )
             linter.load_plugin_modules(plugins)
         # now we can load file config and command line, plugins (which can
         # provide options) have been registered
