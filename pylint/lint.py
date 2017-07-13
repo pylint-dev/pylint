7160a33956b113238ece08cac8c48a25d9c62c7d
Ashley Whetter <ashley@awhetter.co.uk>
2017-07-13 04:17:38 +0100

    Slotted in argparse with CLIParser

--- pylint/lint.py
+++ pylint/lint.py
@@ -293,13 +293,13 @@ class PyLinter(config.OptionsManagerMixIn,
     @staticmethod
     def make_options():
         return (('ignore',
-                 {'type' : 'csv', 'metavar' : '<file>[,<file>...]',
+                 {'type' : 'csv', 'metavar' : '<file>,...',
                   'dest' : 'black_list', 'default' : ('CVS',),
                   'help' : 'Add files or directories to the blacklist. '
                            'They should be base names, not paths.'}),
 
                 ('ignore-patterns',
-                 {'type' : 'regexp_csv', 'metavar' : '<pattern>[,<pattern>...]',
+                 {'type' : 'regexp_csv', 'metavar' : '<pattern>,...',
                   'dest' : 'black_list_re', 'default' : (),
                   'help' : 'Add files or directories matching the regex patterns to the'
                            ' blacklist. The regex matches against base names, not paths.'}),
@@ -410,7 +410,7 @@ class PyLinter(config.OptionsManagerMixIn,
                            ' may run arbitrary code.')}),
 
                 ('extension-pkg-whitelist',
-                 {'type': 'csv', 'metavar': '<pkg[,pkg]>', 'default': [],
+                 {'type': 'csv', 'metavar': '<pkg>,...', 'default': [],
                   'help': ('A comma-separated list of package or module names'
                            ' from where C extensions may be loaded. Extensions are'
                            ' loading into the active Python interpreter and may run'
