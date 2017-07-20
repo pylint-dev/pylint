24eef43e00991e7b992bd40c5b59c7a3d1a3ac68
guillaume2 <guillaume.peillex@gmail.col>
2017-07-20 20:56:55 +0200

    Add of a check that warns if a + operator is used inside call of logging module and if one and only one of the operant is a litteral string

--- pylint/checkers/logging.py
+++ pylint/checkers/logging.py
@@ -174,12 +174,25 @@ class LoggingChecker(checkers.BaseChecker):
         else:
             return
 
-        if isinstance(node.args[format_pos], astroid.BinOp) and node.args[format_pos].op == '%':
-            self.add_message('logging-not-lazy', node=node)
+        if isinstance(node.args[format_pos], astroid.BinOp):
+            binop = node.args[format_pos]
+            if (binop.op == '%' or binop.op == '+' and
+                    len([_operand for _operand in (binop.left, binop.right)
+                         if self._is_operand_literal_str(_operand)]) == 1):
+                self.add_message('logging-not-lazy', node=node)
         elif isinstance(node.args[format_pos], astroid.Call):
             self._check_call_func(node.args[format_pos])
         elif isinstance(node.args[format_pos], astroid.Const):
             self._check_format_string(node, format_pos)
+
+    @staticmethod
+    def _is_operand_literal_str(operand):
+        """
+        Return True if the operand in argument is a literal string
+        """
+        if isinstance(operand, astroid.Const) and operand.name == 'str':
+            return True
+        return False
 
     def _check_call_func(self, node):
         """Checks that function call is not format_string.format().
