309b4035d8389dd984a47f008f1287f0a5b3f16a
Ashley Whetter <ashley@awhetter.co.uk>
2017-09-09 20:53:57 +0100

    Started using more specific exceptions in config

--- pylint/exceptions.py
+++ pylint/exceptions.py
@@ -20,3 +20,6 @@ class EmptyReportError(Exception):
 
 class InvalidReporterError(Exception):
     """raised when selected reporter is invalid (e.g. not found)"""
+
+class ConfigurationError(Exception):
+    """Raised when the configuration is invalid."""
