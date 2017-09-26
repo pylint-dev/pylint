b365a9f8869d2b178b6e2b717c1e3dfd365915bd
hippo91 <guillaume.peillex@gmail.com>
2017-09-26 10:20:16 +0200

    Removing redundant comment

--- pylint/checkers/base.py
+++ pylint/checkers/base.py
@@ -673,8 +673,6 @@ class BasicErrorChecker(_BasicChecker):
         """check for redefinition of a function / method / class name"""
         defined_self = node.parent.frame()[node.name]
         if defined_self is not node and not astroid.are_exclusive(node, defined_self):
-            # before emitting a message check that function/method name doesn't not
-            # correspond to dummy names.
             dummy_variables_rgx = lint_utils.get_global_option(
                 self, 'dummy-variables-rgx', default=None)
             if dummy_variables_rgx and dummy_variables_rgx.match(defined_self.name):
