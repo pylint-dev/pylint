e5da571cbe5b9ef124bda23d5a0c03c36daa20ca
Claudiu Popa <pcmanticore@gmail.com>
2017-09-29 11:45:21 +0200

    Launch 1.7.3

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -18,7 +18,7 @@ from setuptools import __version__ as setuptools_version
 
 modname = distname = 'pylint'
 
-numversion = (1, 7, 2)
+numversion = (1, 7, 3)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = [
