dbb2f3e65b9a452cf981e1bda318ec019b43bd8c
Jacques Kvam <jwkvam@gmail.com>
2017-12-24 10:46:23 -0600

    Use the proper node to get the name for redefined functions (#1792)
    Close #1774

--- pylint/checkers/base.py
+++ pylint/checkers/base.py
@@ -714,7 +714,7 @@ class BasicErrorChecker(_BasicChecker):
         if defined_self is not node and not astroid.are_exclusive(node, defined_self):
             dummy_variables_rgx = lint_utils.get_global_option(
                 self, 'dummy-variables-rgx', default=None)
-            if dummy_variables_rgx and dummy_variables_rgx.match(defined_self.name):
+            if dummy_variables_rgx and dummy_variables_rgx.match(node.name):
                 return
             self.add_message('function-redefined', node=node,
                              args=(redeftype, defined_self.fromlineno))
