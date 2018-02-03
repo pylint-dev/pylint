22b94ad866a82496fcc800db5e6f7d5fa2f741a7
Ashley Whetter <ashley@awhetter.co.uk>
2018-02-03 15:27:53 -0800

    Removed command line parsing from Linter

--- pylint/utils.py
+++ pylint/utils.py
@@ -1207,29 +1207,36 @@ class PyLintASTWalker(object):
 
 PY_EXTS = ('.py', '.pyc', '.pyo', '.pyw', '.so', '.dll')
 
-def register_plugins(linter, directory):
-    """load all module and package in the given directory, looking for a
-    'register' function in each one, used to register pylint checkers
+def register_plugins(registry, directory):
+    """Load plugins from all modules and packages in the given directory.
+
+    Args:
+        registry (CheckerRegistry): The registry to register the checkers with.
+        directory (str): The directory to search for plugins.
     """
-    imported = {}
+    imported = {'__init__', '__pycache__'}
     for filename in os.listdir(directory):
-        base, extension = splitext(filename)
-        if base in imported or base == '__pycache__':
+        base, extension = os.path.splitext(filename)
+        if base in imported:
             continue
-        if extension in PY_EXTS and base != '__init__' or (
-                not extension and isdir(join(directory, base))):
+
+        package_dir = os.path.join(directory, base)
+        if (extension in PY_EXTS
+                or (not extension and os.path.isdir(package_dir))):
+            file_path = os.path.join(directory, filename)
             try:
-                module = modutils.load_module_from_file(join(directory, filename))
+                module = modutils.load_module_from_file(file_path)
             except ValueError:
-                # empty module name (usually emacs auto-save files)
+                # Empty module name
                 continue
             except ImportError as exc:
                 print("Problem importing module %s: %s" % (filename, exc),
                       file=sys.stderr)
             else:
                 if hasattr(module, 'register'):
-                    module.register(linter)
-                    imported[base] = 1
+                    module.register(registry)
+                    imported.add(base)
+
 
 def get_global_option(checker, option, default=None):
     """ Retrieve an option defined by the given *checker* or
