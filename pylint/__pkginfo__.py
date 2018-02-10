88d336ec29ad046f65c64f56f68637c321d744b2
Claudiu Popa <pcmanticore@gmail.com>
2018-02-10 10:45:32 +0100

    Revert "Use a dependency link for astroid 2.0 until release"
    This reverts commit 5603ddaf38920889aac3a7c65574ab1f9b0c62fc.
    Apparently astroid 2.0 has some changes that causes some pylint tests to fail.

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -27,16 +27,13 @@ numversion = (2, 0, 0)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = [
-    'astroid',
+    'astroid<2.0',
     'six',
     'isort >= 4.2.5',
     'mccabe',
 ]
 
-dependency_links = [
-    'https://github.com/PyCQA/astroid/tarball/master#egg=astroid-master-2.0'
-
-]
+dependency_links = []
 
 extras_require = {}
 extras_require[':sys_platform=="win32"'] = ['colorama']
