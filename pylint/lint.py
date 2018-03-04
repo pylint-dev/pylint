af7f5f391dfb9991e1a01d390fa8bbd3b0746c40
Fergal Hainey <fergal.hainey@skyscanner.net>
2018-03-04 20:52:13 +0000

    Add InvalidCheckerError

--- pylint/lint.py
+++ pylint/lint.py
@@ -944,7 +944,8 @@ class PluginRegistry(object):
         :param checker: The checker to register.
         :type checker: BaseChecker
 
-        :raises ValueError: If the priority of the checker is invalid.
+        :raises InvalidCheckerError: If the priority of the checker is
+            invalid.
         """
         if checker.name in self._checkers:
             # TODO: Raise if classes are the same
@@ -954,9 +955,8 @@ class PluginRegistry(object):
                 warnings.warn(msg)
 
         if checker.priority > 0:
-            # TODO: Use a custom exception
              msg = '{}.priority must be <= 0'.format(checker.__class__)
-             raise ValueError(msg)
+             raise exceptions.InvalidCheckerError(msg)
 
         self._checkers[checker.name].append(checker)
 
