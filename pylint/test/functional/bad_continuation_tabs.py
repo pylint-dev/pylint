5d4708a7aa31b9650558072024268cfb3b7fba3c
Andreas Freimuth <andreas.freimuth@united-bits.de>
2018-03-24 10:20:37 +0100

    Add some documentation

--- pylint/test/functional/bad_continuation_tabs.py
+++ pylint/test/functional/bad_continuation_tabs.py
@@ -16,30 +16,37 @@ MY_LIST = [
 # Issue 1148
 class Abc(object):
 	def b(self, c):
+		# bock indentation
 		self.d(
-			c)
+			c)  # (is: tabs only)
 
 	def d(self, e):
+		# bad hanging indentation
 		self.b(
-		       e)  # [bad-continuation]
+		       e)  # [bad-continuation] (is: 2 tabs + 7 spaces)
 
 	def f(self):
+		# block indentiation (is: all tabs only)
 		return [
 			self.b,
 			self.d
 		]
 
 	def g(self):
+		# bad hanging indentation
+		# the closing ] requires 7 - 8 more spcaes; see h(), i()
 		return [self.b,
-		        self.d
-		]  # [bad-continuation]
+		        self.d  # (is: 2 tabs + 8 spaces)
+		]  # [bad-continuation] (is: tabs only)
 
 	def h(self):
+		# hanging indentation: all lined up with first token 'self.b'
 		return [self.b,
-		        self.d
-		        ]
+		        self.d # (is: 2 tabs + 8 spaces)
+		        ] # (is: 2 tabs + 8 spaces)
 
 	def i(self):
+		# hangin identation: closing ] lined up with opening [
 		return [self.b,
-		        self.d
-		       ]
+		        self.d   # (2 tabs + 8 spaces)
+		       ]  # (2 tabs + 7 spaces)
