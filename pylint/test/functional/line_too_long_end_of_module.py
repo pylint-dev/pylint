30de25391a28203d879ce89bb13642a71ccc1a91
Claudiu Popa <pcmanticore@gmail.com>
2018-04-17 09:53:27 +0200

    Revert "Proposal of a fix for bug #1950"

--- pylint/test/functional/line_too_long_end_of_module.py
+++
@@ -1,7 +0,0 @@
-#pylint: disable=missing-docstring, line-too-long
-
-# Hey this is a very long commented line just to trigger pylint's line-too-long message. It is just above a dummy function with a very long code line.
-def dummy_function():
-    print("What a beautiful dummy function with such a very long line inside that it should trigger pylint's line-too-long message ")
-
-# Hey this is a very long commented line at the end of the module. It is just here to trigger pylint's line-too-long message.
