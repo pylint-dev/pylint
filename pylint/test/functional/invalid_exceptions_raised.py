49e9dea4bfc3410b88fe26f3b8387bc854789920
Bradley Wogsland <bradley@popresearch.com>
2018-05-14 10:43:56 -0500

    #1896 fixed final failing test

--- pylint/test/functional/invalid_exceptions_raised.py
+++ pylint/test/functional/invalid_exceptions_raised.py
@@ -33,6 +33,7 @@ def good_case3():
 
 def bad_case0():
     """raise"""
+    # +2:<3.0:[nonstandard-exception]
     # +1:>=3.0:[raising-non-exception]
     raise OldStyleClass('hop')
 
@@ -42,6 +43,7 @@ def bad_case1():
 
 def bad_case2():
     """raise"""
+    # +2:<3.0:[nonstandard-exception]
     # +1:>=3.0:[raising-non-exception]
     raise OldStyleClass('hop')
 
