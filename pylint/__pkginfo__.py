235c6e5b9bcac6b07944c3d88c9e626aeb99fdd0
Ashley Whetter <ashley@awhetter.co.uk>
2018-05-16 11:25:37 -0700

    Prepare 1.9.1

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -27,7 +27,7 @@ from setuptools import __version__ as setuptools_version
 
 modname = distname = 'pylint'
 
-numversion = (1, 9, 0)
+numversion = (1, 9, 1)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = [
