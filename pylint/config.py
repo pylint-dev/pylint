e886f126dcb7bb2f94de6fb953d012290ce0e804
Fergal Hainey <fergal.hainey@skyscanner.net>
2018-06-09 11:32:28 +0100

    Add back MASTER config section over DEFAULT

--- pylint/config.py
+++ pylint/config.py
@@ -422,10 +422,10 @@ class CLIParser(ConfigParser):
             group, args, kwargs = self._convert_definition(option, definition)
             option_groups[group].append((args, kwargs))
 
-        for args, kwargs in option_groups['DEFAULT']:
+        for args, kwargs in option_groups['MASTER']:
             self._parser.add_argument(*args, **kwargs)
 
-        del option_groups['DEFAULT']
+        del option_groups['MASTER']
 
         for group, arguments in option_groups.items():
             self._option_groups.add(group)
@@ -479,7 +479,7 @@ class CLIParser(ConfigParser):
         if definition.get('hide'):
             kwargs['help'] = argparse.SUPPRESS
 
-        group = definition.get('group', 'DEFAULT').upper()
+        group = definition.get('group', 'MASTER').upper()
         return group, args, kwargs
 
     def parse(self, to_parse, config):
@@ -563,6 +563,7 @@ class IniFileParser(FileParser):
         super(IniFileParser, self).__init__()
         self._parser = configparser.ConfigParser(
             inline_comment_prefixes=('#', ';'),
+            default_section='MASTER',
         )
 
     def add_option_definitions(self, option_definitions):
@@ -579,7 +580,7 @@ class IniFileParser(FileParser):
                     self._option_groups.add(group)
 
             if default is not None:
-                self._parser['DEFAULT'].update(default)
+                self._parser['MASTER'].update(default)
 
     @staticmethod
     def _convert_definition(option, definition):
@@ -599,7 +600,7 @@ class IniFileParser(FileParser):
             default_value = unvalidator(definition['default'])
             default = {option: default_value}
 
-        group = definition.get('group', 'DEFAULT').upper()
+        group = definition.get('group', 'MASTER').upper()
         return group, default
 
     def parse(self, to_parse, config):
