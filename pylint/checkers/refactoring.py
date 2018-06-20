06f12916c5bf1a4cc940f7a7b3a9b0aac35648a2
Konstantin <Github@pheanex.de>
2018-06-20 07:59:42 +0200

    Add new checker 'consider-using-dict-comprehension' (#2204)
    A new check is emitted for using old style dict initializations with list
    comprehensions instead of dict-comprehensions, which are faster
    and easier to read.

--- pylint/checkers/refactoring.py
+++ pylint/checkers/refactoring.py
@@ -159,7 +159,14 @@ class RefactoringChecker(checkers.BaseTokenChecker):
                   'chained-comparison',
                   'This message is emitted when pylint encounters boolean operation like'
                   '"a < b and b < c", suggesting instead to refactor it to "a < b < c"',
-                  ),
+                 ),
+        'R1717': ('Consider using a dictionary comprehension',
+                  'consider-using-dict-comprehension',
+                  'Although there is nothing syntactically wrong with this code, '
+                  'it is hard to read and can be simplified to a dict comprehension.'
+                  'Also it is faster since you don\'t need to create another '
+                  'transient list',
+                 ),
     }
     options = (('max-nested-blocks',
                 {'default': 5, 'type': 'int', 'metavar': '<int>',
@@ -434,9 +441,18 @@ class RefactoringChecker(checkers.BaseTokenChecker):
         stopiteration_qname = '{}.StopIteration'.format(utils.EXCEPTIONS_MODULE)
         return any(_class.qname() == stopiteration_qname for _class in exc.mro())
 
-    @utils.check_messages('stop-iteration-return')
+    def _check_consider_using_dict_comprehension(self, node):
+        if (isinstance(node.func, astroid.Name) and
+                node.func.name == 'dict' and
+                node.args and
+                isinstance(node.args[0], astroid.ListComp)):
+            self.add_message('consider-using-dict-comprehension', node=node)
+
+    @utils.check_messages('stop-iteration-return',
+                          'consider-using-dict-comprehension')
     def visit_call(self, node):
         self._check_raising_stopiteration_in_generator_next_call(node)
+        self._check_consider_using_dict_comprehension(node)
 
     def _check_raising_stopiteration_in_generator_next_call(self, node):
         """Check if a StopIteration exception is raised by the call to next function
