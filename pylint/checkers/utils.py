7a22911f3cd28adc5efd0274986749087a5b8c30
Bryce Guinta <bryce.paul.guinta@gmail.com>
2018-07-11 19:01:34 -0600

    Fix false positive unused-variable in lambda default arguments
    The NameConsumer to_consume frame needed to be backed out by one so that
    for lambda bound default argument variables (closure) could consume the
    name in a higher scope (just like function default arguments)
    
    Close #1921
    Close #1552
    Close #1099
    Close #210

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -243,17 +243,18 @@ def is_defined_before(var_node):
         _node = _node.previous_sibling()
     return False
 
-def is_func_default(node):
-    """return true if the given Name node is used in function default argument's
-    value
+def is_default_argument(node):
+    """return true if the given Name node is used in function or lambda
+    default argument's value
     """
     parent = node.scope()
-    if isinstance(parent, astroid.FunctionDef):
+    if isinstance(parent, (astroid.FunctionDef, astroid.Lambda)):
         for default_node in parent.args.defaults:
             for default_name_node in default_node.nodes_of_class(astroid.Name):
                 if default_name_node is node:
                     return True
     return False
+
 
 def is_func_decorator(node):
     """return true if the name is used in function decorator"""
