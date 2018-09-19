388301f1af6bd3cf76ca92d0bfa8e6eb0c330745
bengineer19 <benjames1999@hotmail.co.uk>
2018-09-19 19:08:24 +0100

    Fix C0200 bug: don't suggest enumerate when defining __iter__ (closes #2477)

--- pylint/checkers/refactoring.py
+++ pylint/checkers/refactoring.py
@@ -1078,6 +1078,9 @@ class RecommandationChecker(checkers.BaseChecker):
         iterating_object = len_args[0]
         if not isinstance(iterating_object, astroid.Name):
             return
+        # If we're defining __iter__ on self enumerate won't work
+        if iterating_object.name == 'self' and node.parent.name == '__iter__':
+            return
 
         # Verify that the body of the for loop uses a subscript
         # with the object that was iterated. This uses some heuristics
