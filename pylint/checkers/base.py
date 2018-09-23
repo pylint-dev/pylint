0f3fb65ed680b9318d9fd83ec5e3130b8b61faf3
Claudiu Popa <pcmanticore@gmail.com>
2018-09-23 17:09:17 +0200

    Emit ``too-many-starred-assignment`` only when the number of Starred nodes is per assignment elements
    Nested assignment tuples can each have a single starred assignment, as in the
    following example:
    
       ((a, *b), *c) = ...
    
    The check was previously unflattening the occurrences of Starred nodes,
    resulting in a total number of 2 Starred nodes for the given
    assignment tuple, while in fact there is just one starred node
    for the first tuple, followed by a separate second starred node.
    
    Close #2513

--- pylint/checkers/base.py
+++ pylint/checkers/base.py
@@ -535,12 +535,21 @@ class BasicErrorChecker(_BasicChecker):
 
     @utils.check_messages("too-many-star-expressions", "invalid-star-assignment-target")
     def visit_assign(self, node):
-        starred = list(node.targets[0].nodes_of_class(astroid.Starred))
-        if len(starred) > 1:
-            self.add_message("too-many-star-expressions", node=node)
+        target = node.targets[0]
+
+        if isinstance(target, astroid.Tuple):
+            itered_elements = target.itered()
+            starred_count = 0
+            for elem in itered_elements:
+                if isinstance(elem, astroid.Starred):
+                    starred_count += 1
+                elif sum(1 for _ in elem.nodes_of_class(astroid.Starred)) > 1:
+                    self.add_message("too-many-star-expressions", node=node)
+            if starred_count > 1:
+                self.add_message("too-many-star-expressions", node=node)
 
         # Check *a = b
-        if isinstance(node.targets[0], astroid.Starred):
+        if isinstance(target, astroid.Starred):
             self.add_message("invalid-star-assignment-target", node=node)
 
     @utils.check_messages("star-needs-assignment-target")
--- pylint/test/functional/too_many_star_expressions.py
+++ pylint/test/functional/too_many_star_expressions.py
@@ -1,4 +1,5 @@
 """Test for too-many-star-expressions."""
 
 *FIRST, *SECOND = [1, 2, 3] # [too-many-star-expressions]
+(FIRST, *SECOND), *THIRD = ((1, 2, 3), 4, 5)
 *FIRST_1, SECOND = (1, 2, 3)
