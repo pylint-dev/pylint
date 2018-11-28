55c638537ac956c463b79b16e73a0505ae544dfe
Claudiu Popa <pcmanticore@gmail.com>
2018-11-28 09:40:28 +0100

    Prepare 2.2.2

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -25,7 +25,7 @@ from os.path import join
 
 modname = distname = "pylint"
 
-numversion = (2, 2, 1)
+numversion = (2, 2, 2)
 dev_version = None
 string_version = ".".join(str(num) for num in numversion)
 
