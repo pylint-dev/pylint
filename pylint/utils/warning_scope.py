3444f5736430eb8f27f647fddeb5b1a85aaf3669
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2018-12-09 14:35:39 +0100

    Refactor - Create a file for each class in utils

---
+++ pylint/utils/warning_scope.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# For details: https://github.com/PyCQA/pylint/blob/master/COPYING
+
+
+class WarningScope:
+    LINE = "line-based-msg"
+    NODE = "node-based-msg"
