4dc496b46716966ce80a7fd6bd147d2248fed1bd
Justin Li <justinnhli@gmail.com>
2018-12-18 15:37:40 -0800

    Add testcases for generalized detection of potential chained comparisons

--- pylint/test/functional/simplify_chained_comparison.py
+++ pylint/test/functional/simplify_chained_comparison.py
@@ -1,6 +1,6 @@
-"""Test that some boolean expression  statement tests can be simplified."""
+"""Test that some boolean expression statement tests can be simplified."""
 
-# pylint: disable=missing-docstring, invalid-name, no-else-return
+# pylint: disable=missing-docstring, invalid-name, no-else-return, too-many-branches
 
 
 def test_simplify_chained_comparison_1():
@@ -19,6 +19,7 @@ def test_simplify_chained_comparison_3():
     a = 1
     b = 2
     c = 3
+    d = 4
     if a < 10 and a > 1: # [chained-comparison]
         pass
     elif a > 1 and a < 10: # [chained-comparison]
@@ -43,12 +44,23 @@ def test_simplify_chained_comparison_3():
         pass
     elif a < b and b < 0 and c == 786: # [chained-comparison]
         pass
+    elif a < b and c == 786 and b < 0: # [chained-comparison]
+        pass
+    elif c == 786 and b < 0 and a < b: # [chained-comparison]
+        pass
+    elif a < b < c and c < d: # [chained-comparison]
+        pass
+    elif b < c < d and a < b: # [chained-comparison]
+        pass
+    elif a < b < c and a < b and b < c: # [chained-comparison]
+        pass
 
 
 def test_not_simplify_chained_comparison_1():
     a = 1
     b = 2
     c = 3
+    d = 4
     if a < 10 and b > 1:
         pass
     elif a > 1 and b < 10:
@@ -66,4 +78,12 @@ def test_not_simplify_chained_comparison_1():
     elif a < b and a == 1 and a < c:
         pass
     elif a < b and a < c and c == 786:
+        pass
+    elif a < b < c and b < d:
+        pass
+    elif a < b < c and a < d:
+        pass
+    elif b < c < d and a < c:
+        pass
+    elif b < c < d and a < d:
         pass
