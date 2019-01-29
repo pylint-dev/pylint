9c026683df60543952dd12bbaf8c5766c0bd0e7d
Svetoslav Neykov <svet@hyperscience.com>
2019-01-29 12:00:54 +0200

    Fixes for linting of new style logging format.
    The number of arguments was not handled properly, leading to an always successful check. See new tests for specific cases this fixes.

--- pylint/test/unittest_checker_logging.py
+++ pylint/test/unittest_checker_logging.py
@@ -63,15 +63,63 @@ class TestLoggingModuleDetection(CheckerTestCase):
         with self.assertAddsMessages(Message("logging-not-lazy", node=stmts[1])):
             self.checker.visit_call(stmts[1])
 
-    @set_config(logging_format_style="new")
-    def test_brace_format_style(self):
+    def _assert_brace_format_no_messages(self, stmt):
         stmts = astroid.extract_node(
             """
         import logging #@
-        logging.error('{}', 1) #@
-        """
+        logging.error<placeholder> #@
+        """.replace(
+                "<placeholder>", stmt
+            )
         )
         self.checker.visit_module(None)
         self.checker.visit_import(stmts[0])
         with self.assertNoMessages():
             self.checker.visit_call(stmts[1])
+
+    def _assert_brace_format_message(self, msg, stmt):
+        stmts = astroid.extract_node(
+            """
+        import logging #@
+        logging.error<placeholder> #@
+        """.replace(
+                "<placeholder>", stmt
+            )
+        )
+        self.checker.visit_module(None)
+        self.checker.visit_import(stmts[0])
+        with self.assertAddsMessages(Message(msg, node=stmts[1])):
+            self.checker.visit_call(stmts[1])
+
+    def _assert_brace_format_too_few_args(self, stmt):
+        self._assert_brace_format_message("logging-too-few-args", stmt)
+
+    def _assert_brace_format_too_many_args(self, stmt):
+        self._assert_brace_format_message("logging-too-many-args", stmt)
+
+    @set_config(logging_format_style="new")
+    def test_brace_format_style_matching_arguments(self):
+        self._assert_brace_format_no_messages("('constant string')")
+        self._assert_brace_format_no_messages("('{}')")
+        self._assert_brace_format_no_messages("('{}', 1)")
+        self._assert_brace_format_no_messages("('{0}', 1)")
+        self._assert_brace_format_no_messages("('{named}', {'named': 1})")
+        self._assert_brace_format_no_messages("('{} {named}', 1, {'named': 1})")
+        self._assert_brace_format_no_messages("('{0} {named}', 1, {'named': 1})")
+
+    @set_config(logging_format_style="new")
+    def test_brace_format_style_too_few_args(self):
+        self._assert_brace_format_too_few_args("('{}, {}', 1)")
+        self._assert_brace_format_too_few_args("('{0}, {1}', 1)")
+        self._assert_brace_format_too_few_args("('{named1}, {named2}', {'named1': 1})")
+        self._assert_brace_format_too_few_args("('{0}, {named}', 1)")
+        self._assert_brace_format_too_few_args("('{}, {named}', {'named': 1})")
+        self._assert_brace_format_too_few_args("('{0}, {named}', {'named': 1})")
+
+    @set_config(logging_format_style="new")
+    def test_brace_format_style_not_enough_arguments(self):
+        self._assert_brace_format_too_many_args("('constant string', 1, 2)")
+        self._assert_brace_format_too_many_args("('{}', 1, 2)")
+        self._assert_brace_format_too_many_args("('{0}', 1, 2)")
+        self._assert_brace_format_too_many_args("('{}, {named}', 1, 2, {'named': 1})")
+        self._assert_brace_format_too_many_args("('{0}, {named}', 1, 2, {'named': 1})")
