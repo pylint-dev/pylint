24a439cc76bdfd1dafefdb13069101e3e21b6671
hippo91 <guillaume.peillex@gmail.com>
2019-02-16 18:26:05 +0100

    Completes the unit tests

--- pylint/test/functional/missing_parentheses_for_call_in_test.py
+++ pylint/test/functional/missing_parentheses_for_call_in_test.py
@@ -1,6 +1,6 @@
 """Verify if call to function or method inside tests are missing parentheses."""
-# pylint: disable=invalid-name, missing-docstring,too-few-public-methods
-# pylint: disable=no-init,expression-not-assigned, useless-object-inheritance
+# pylint: disable=using-constant-test, missing-docstring, useless-object-inheritance
+# pylint: disable=invalid-name, expression-not-assigned
 
 import collections
 
@@ -23,8 +23,6 @@ class Class(object):
         return 42
 
 
-instance = Class()
-
 if collections:
     pass
 
@@ -33,3 +31,43 @@ if bool_function: # [missing-parentheses-for-call-in-test]
 
 if not bool_function():
     pass
+
+if nonbool_function:
+    pass
+
+if nonbool_function() != 42:
+    pass
+
+instance = Class()
+
+if instance.bool_method:  # [missing-parentheses-for-call-in-test]
+    pass
+
+if not instance.bool_method():
+    pass
+
+if not instance.nonbool_method:
+    pass
+elif instance.bool_method:  # [missing-parentheses-for-call-in-test]
+    pass
+
+bool_lambda = lambda: True
+
+if bool_lambda:  # [missing-parentheses-for-call-in-test]
+    pass
+
+if not bool_lambda():
+    pass
+
+nonbool_lambda = lambda: 42
+
+if nonbool_lambda:
+    pass
+
+if not nonbool_lambda():
+    pass
+
+MY_VALUE = 42 if bool_function else -1  # [missing-parentheses-for-call-in-test]
+MY_2ND_VALUE = 42 if not bool_function() else -1
+MY_THIRD_VALUE = 42 if bool_lambda else -1  # [missing-parentheses-for-call-in-test]
+MY_FOURTH_VALUE = 42 if nonbool_lambda else -1
