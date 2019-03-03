01738a2deb098d50f798f08d74318a0c9cce31c5
Claudiu Popa <pcmanticore@gmail.com>
2019-03-03 13:41:52 +0100

    Prepare 2.2.3

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -25,7 +25,7 @@ from os.path import join
 
 modname = distname = "pylint"
 
-numversion = (2, 2, 2)
+numversion = (2, 2, 3)
 dev_version = None
 string_version = ".".join(str(num) for num in numversion)
 
@@ -34,7 +34,7 @@ if dev_version:
 else:
     version = string_version
 
-install_requires = ["astroid>=2.0.0", "isort >= 4.2.5", "mccabe"]
+install_requires = ["astroid>=2.0,<2.2.0", "isort >= 4.2.5", "mccabe"]
 
 dependency_links = []  # type: ignore
 
