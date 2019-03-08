21954deb237d6eac162a1360f969f1b58ea354a7
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-03-08 20:05:32 +0100

    Refactor - Move test_create_invalid_message_type in pylint.message test

--- pylint/test/message/unittest_message.py
+++ pylint/test/message/unittest_message.py
@@ -11,3 +11,16 @@ from pylint.message import MessagesStore, MessageDefinition
 from pylint.checkers.utils import check_messages, get_node_last_lineno
 from pylint.exceptions import InvalidMessageError
 import pytest
+
+
+@pytest.mark.parametrize(
+    "msgid,expected",
+    [
+        ("Q1234", "Bad message type Q in 'Q1234'"),
+        ("W12345", "Invalid message id 'W12345'"),
+    ],
+)
+def test_create_invalid_message_type(msgid, expected):
+    with pytest.raises(InvalidMessageError) as cm:
+        MessageDefinition("checker", msgid, "msg", "descr", "symbol", "scope")
+    assert str(cm.value) == expected
