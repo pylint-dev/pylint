5d0c0fa73bfc1c95f59b8d6e746117777681b260
Ashley Whetter <AWhetter@users.noreply.github.com>
2019-05-20 23:49:29 -0700

    Fixed pragmas on their own line after a backlash being ignored (#2923)
    Closes #199

---
+++ pylint/test/functional/pragma_after_backslash.py
@@ -0,0 +1,10 @@
+"""Test that a pragma has an effect when separated by a backslash."""
+# pylint: disable=too-few-public-methods
+
+class Foo:
+    """block-disable test"""
+
+    def meth3(self):
+        """test one line disabling"""
+        print(self.bla) \
+           # pylint: disable=E1101
