b7aa7b78a7e11ba3b2d0bcef549c95cc83c7a9fd
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2019-06-09 18:20:21 +0200

    Feat - Permit to get message definition by msgid in Checkers

--- pylint/checkers/base_checker.py
+++ pylint/checkers/base_checker.py
@@ -98,6 +98,14 @@ class BaseChecker(OptionsProviderMixIn):
 
     # dummy methods implementing the IChecker interface
 
+    def get_message_definition(self, msgid):
+        for message_definition in self.messages:
+            if message_definition.msgid == msgid:
+                return message_definition
+        error_msg = "MessageDefinition for '{}' does not exists".format(msgid)
+        error_msg += "Choose from {}".format([m.msgid for m in self.messages])
+        raise InvalidMessageError(error_msg)
+
     def open(self):
         """called before visiting project (i.e set of modules)"""
 
