b1211bd3f696e3ccb7fe6735d9af036c2337896e
hippo91 <guillaume.peillex@gmail.com>
2019-10-12 12:25:27 +0200

    Adds a specific exception in case the pragma is not valid. Throw this exception if an assignment is missing or at the wrong place. Adds another matched group in the regex to retrieve only the part of the pragma that comes after pylint keyword.

--- pylint/utils/pragma_parser.py
+++ pylint/utils/pragma_parser.py
@@ -14,12 +14,13 @@ OPTION_RGX = r"""
     \s*                # Any number of whithespace
     \#?                # One or zero hash
     .*                 # Anything (as much as possible)
-    (\s*               # Any number of whitespaces
+    (\s*               # Beginning of first matched group and any number of whitespaces
     \#                 # Beginning of comment
     .*?                # Anything (as little as possible)
     \bpylint:          # pylint word and column
     \s*                # Any number of whitespaces
-    [^;#\n]+)          # Anything except semicolon or hash or newline
+    ([^;#\n]+))        # Anything except semicolon or hash or newline (it is the second matched group) 
+                       # and end of the first matched group
     [;#]{0,1}"""       # From 0 to 1 repetition of semicolon or hash
 OPTION_PO = re.compile(OPTION_RGX, re.VERBOSE)
 
@@ -27,25 +28,52 @@ OPTION_PO = re.compile(OPTION_RGX, re.VERBOSE)
 PragmaRepresenter = namedtuple('PragmaRepresenter', "action messages")
 
 
-def parse_pragma(pylint_pragma: str) -> List[PragmaRepresenter]:
-    token_specification = [
-        ('KEYWORD', r"\b(disable-all|skip-file|disable-msg|enable-msg|disable|enable)\b"),
-        ('MESSAGE_STRING', r'[A-Za-z\-]{2,}'),  # Identifiers
-        ('ASSIGN', r'='),      # Assignment operator
-        ('MESSAGE_NUMBER', r'[CREIWF]{1}\d{4}'),
-    ]
-    tok_regex = '|'.join('(?P<{:s}>{:s})'.format(token_name, token_rgx) for token_name, token_rgx in token_specification)
+TOKEN_SPECIFICATION = [
+    ('KEYWORD', r"\b(disable-all|skip-file|disable-msg|enable-msg|disable|enable)\b"),
+    ('MESSAGE_STRING', r'[A-Za-z\-]{2,}'),  # Identifiers
+    ('ASSIGN', r'='),      # Assignment operator
+    ('MESSAGE_NUMBER', r'[CREIWF]{1}\d{4}'),
+]
+TOK_REGEX = '|'.join('(?P<{:s}>{:s})'.format(token_name, token_rgx) for token_name, token_rgx in TOKEN_SPECIFICATION)
 
+
+class PragmaParserError(Exception):
+    """
+    A class for exceptions thrown by pragma_parser module
+    """
+    def __init__(self, message, token):
+        """
+        :args message: explain the reason why the exception has been thrown
+        :args token: token concerned by the exception
+        """
+        self.message = message
+        self.token = token
+
+
+def parse_pragma(pylint_pragma: str) -> List[PragmaRepresenter]:
     action = None
-    messages = list()
-    for mo in re.finditer(tok_regex, pylint_pragma):
+    messages = []
+    requiring_assignment = (False, '')
+    for mo in re.finditer(TOK_REGEX, pylint_pragma):
         kind = mo.lastgroup
         value = mo.group()
+        if kind == 'ASSIGN' and not requiring_assignment[0]:
+            raise PragmaParserError('The assignment operator = should '
+                                    'not be present right after the token', requiring_assignment[1])
+        elif requiring_assignment[0] and kind != 'ASSIGN':
+            raise PragmaParserError('The assignment operator = is '
+                                    'missing right after the token', requiring_assignment[1])
+        else:
+            requiring_assignment = (False, requiring_assignment[1])
         if kind == 'KEYWORD':
             if action:
                 yield PragmaRepresenter(action, messages)
             action = value
             messages = list()
+            if value in ('disable-msg', 'enable-msg', 'disable', 'enable'):
+                requiring_assignment = (True, value)
+            else:
+                requiring_assignment = (False, value)
         elif kind in ('MESSAGE_STRING', 'MESSAGE_NUMBER'):
             messages.append(value)
     if action:
