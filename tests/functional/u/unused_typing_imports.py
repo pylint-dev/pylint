7946f13c27a422d8c55c553c670d4062b47f7bc0
Claudiu Popa <pcmanticore@gmail.com>
2019-11-13 08:34:04 +0100

    Relax type import detection for names that do not come from the ``typing`` module
    Close #3191

--- tests/functional/u/unused_typing_imports.py
+++ tests/functional/u/unused_typing_imports.py
@@ -7,6 +7,7 @@ which means we were never processing them.
 
 import re
 import typing
+from collections import defaultdict
 from datetime import datetime
 from typing import (
     Any,
@@ -70,3 +71,8 @@ def magic(alpha, beta, gamma):
     # type: (str, Optional[str], Optional[datetime]) -> Any
     """going strong"""
     return alpha, beta, gamma
+
+
+def unused_assignment_import():
+    foo_or_bar = defaultdict(int)  # type: defaultdict
+    return foo_or_bar
