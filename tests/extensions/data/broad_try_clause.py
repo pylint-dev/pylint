594b4a0089786c333e1f0896d049e34503e5172e
Tyler Thieding <tyler@thieding.com>
2020-02-05 02:31:55 -0500

    Fixed broad_try_clause extension to check try/finally statements and to check for nested statements. (#3374)
    Previously, the broad_try_except extension did not check nested statements. For example, an if/else statement and its body inside of a try clause did not result in a message. Now, the extension checks within sub-nodes (i.e., if, for, while, and with) and counts their statements too. Also, the extension also checks try/finally statements now.

--- tests/extensions/data/broad_try_clause.py
+++ tests/extensions/data/broad_try_clause.py
@@ -5,6 +5,41 @@ MY_DICTIONARY = {"key_one": 1, "key_two": 2, "key_three": 3}
 try:  # [max-try-statements]
     value = MY_DICTIONARY["key_one"]
     value += 1
+    print("This one has an except clause only.")
+except KeyError:
+    pass
+
+try:  # [max-try-statements]
+    value = MY_DICTIONARY["key_one"]
+    value += 1
+    print("This one has an finally clause only.")
+finally:
+    pass
+
+try:  # [max-try-statements]
+    value = MY_DICTIONARY["key_one"]
+    value += 1
+    print("This one has an except clause...")
+    print("and also a finally clause!")
+except KeyError:
+    pass
+finally:
+    pass
+
+try:  # [max-try-statements]
+    if "key_one" in MY_DICTIONARY:
+        entered_if_body = True
+        print("This verifies that content inside of an if statement is counted too.")
+    else:
+        entered_if_body = False
+
+    while False:
+        print("This verifies that content inside of a while loop is counted too.")
+
+    for item in []:
+        print("This verifies that content inside of a for loop is counted too.")
+
+
 except KeyError:
     pass
 
