9aa7c6fa169193b1cb30bccf6be5623808c6ddf5
anubh-v <anubhav@u.nus.edu>
2020-02-05 16:20:03 +0800

    Remove hardcoding of extension names

--- pylint/extensions/__init__.py
+++ pylint/extensions/__init__.py
@@ -1,15 +1,8 @@
-import importlib
+import os
 
+EXTENSIONS_DIRECTORY = "pylint/extensions"
 extension_names = [
-    "bad_builtin",
-    "broad_try_clause",
-    "comparetozero",
-    "docparams",
-    "emptystring",
+    filename
+    for filename in os.listdir(EXTENSIONS_DIRECTORY)
+    if filename.endswith(".py") and not filename.startswith("_")
 ]
-
-
-def get_extension_documentation(extension_name):
-    indent = "  "
-    base_path = "pylint.extensions."
-    return indent + importlib.import_module(base_path + extension_name).__doc__
