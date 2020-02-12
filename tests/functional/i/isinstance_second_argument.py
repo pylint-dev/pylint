034fc1acfd57c1345a4a198862820cce2d497073
anubh-v <anubhav@u.nus.edu>
2020-02-12 20:41:38 +0800

    Update tests

--- tests/functional/i/isinstance_second_argument.py
+++ tests/functional/i/isinstance_second_argument.py
@@ -7,12 +7,18 @@ class A:
 class B(A):
     pass
 
+isinstance(A(), A)
+isinstance(A(), B)
+
 isinstance(-9999, int)
 isinstance(True and False, bool)
 isinstance("a 'string'", float)
 isinstance(3.123213, set)
-isinstance(A(), A)
-isinstance(A(), B)
+isinstance(foo, (int, str))
+isinstance("a string", ((int, bool), (float, set), str))
 
 # Negative test cases
-isinstance({a:1}, any)
+isinstance({a:1}, hash) # [isinstance-second-argument-not-valid-type]
+isinstance(64, hex) # [isinstance-second-argument-not-valid-type]
+isinstance({b: 100}, (hash, dict)) # [isinstance-second-argument-not-valid-type]
+isinstance("string", ((dict, iter), str, (int, bool))) # [isinstance-second-argument-not-valid-type]
