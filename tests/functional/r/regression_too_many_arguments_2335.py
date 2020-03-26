641a707cabb4dede99098c1264675454d501c512
Claudiu Popa <pcmanticore@gmail.com>
2020-03-26 10:08:47 +0100

    Add regression test for #2335

---
+++ tests/functional/r/regression_too_many_arguments_2335.py
@@ -0,0 +1,11 @@
+"""Test that `abc.ABCMeta.__new__` does not trigger too-many-function-arguments when referred
+
+https://github.com/PyCQA/pylint/issues/2335
+"""
+# pylint: disable=missing-class-docstring
+from abc import ABCMeta
+
+
+class NodeCheckMetaClass(ABCMeta):
+    def __new__(cls, name, bases, namespace, **kwargs):
+        return ABCMeta.__new__(cls, name, bases, namespace)
