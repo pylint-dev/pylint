9dd214bb07cfc8a5e342c3cdb58d6a8f32390e4d
Claudiu Popa <pcmanticore@gmail.com>
2020-03-26 09:28:38 +0100

    Add regression test for #1078

---
+++ tests/functional/r/regression_no_member_1078.py
@@ -0,0 +1,14 @@
+"""Make sure no-member is not emitted when modifying __doc__ via augmented assignment
+
+https://github.com/PyCQA/pylint/issues/1078
+"""
+# pylint: disable=too-few-public-methods,missing-class-docstring
+class Cls:
+    def test(self):
+        "a"
+
+    test.__doc__ += "b"
+
+
+print(Cls().test.__doc__)
+print(Cls.test.__doc__)
