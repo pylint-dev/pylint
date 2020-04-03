392b062ac053fec2722481b9c01178a707244eb7
Claudiu Popa <pcmanticore@gmail.com>
2020-04-03 14:54:21 +0200

    Squash TestRunners into a single parametrized test

--- tests/test_pylint_runners.py
+++ tests/test_pylint_runners.py
@@ -1,47 +1,20 @@
-""" Here we test the executable iterfaces shipped with pylint """
+# pylint: disable=missing-module-docstring, missing-function-docstring
 import os
 import sys
-import unittest
 from unittest.mock import patch
 
-from pylint import run_pylint, run_epylint, run_pyreverse, run_symilar
+import pytest
+
+from pylint import run_epylint, run_pylint, run_pyreverse, run_symilar
 
 
-class TestRunners(unittest.TestCase):
-    """ A tester to ensure that the runners we ship execute with minimal params """
-
-    def test_run_pylint(self):
-        """ Tests basic invocation of the pylint setup via the runner func """
-        filepath = os.path.abspath(__file__)
-        testargs = ["", filepath]
-        with patch.object(sys, "argv", testargs):
-            with self.assertRaises(SystemExit) as err:
-                run_pylint()
-            self.assertEqual(0, err.exception.code)
-
-    def test_run_epylint(self):
-        """ Tests basic invocation of the epylint setup via its runner func """
-        filepath = os.path.abspath(__file__)
-        testargs = ["", filepath]
-        with patch.object(sys, "argv", testargs):
-            with self.assertRaises(SystemExit) as err:
-                run_epylint()
-            self.assertEqual(0, err.exception.code)
-
-    def test_run_pyreverse(self):
-        """ Tests basic invocation of pyreverse """
-        filepath = os.path.abspath(__file__)
-        testargs = ["", filepath]
-        with patch.object(sys, "argv", testargs):
-            with self.assertRaises(SystemExit) as err:
-                run_pyreverse()
-            self.assertEqual(0, err.exception.code)
-
-    def test_run_symilar(self):
-        """ Tests basic invocation of the similar utilities via its runner func  """
-        filepath = os.path.abspath(__file__)
-        testargs = ["", filepath]
-        with patch.object(sys, "argv", testargs):
-            with self.assertRaises(SystemExit) as err:
-                run_symilar()
-            self.assertEqual(0, err.exception.code)
+@pytest.mark.parametrize(
+    "runner", [run_pylint, run_epylint, run_pyreverse, run_symilar]
+)
+def test_runner(runner):
+    filepath = os.path.abspath(__file__)
+    testargs = ["", filepath]
+    with patch.object(sys, "argv", testargs):
+        with pytest.raises(SystemExit) as err:
+            runner()
+        assert err.value.code == 0
