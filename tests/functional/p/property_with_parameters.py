dcb5148ef1456296b58d98d0fa83ab7b16dab372
Claudiu Popa <pcmanticore@gmail.com>
2020-05-14 08:30:03 +0200

    `property-with-parameters` properly handles abstract properties
    Close #3600

--- tests/functional/p/property_with_parameters.py
+++ tests/functional/p/property_with_parameters.py
@@ -1,7 +1,21 @@
 # pylint: disable=missing-docstring, too-few-public-methods
+from abc import ABCMeta, abstractproperty
 
 
 class Cls:
     @property
     def attribute(self, param, param1): # [property-with-parameters]
         return param + param1
+
+
+class MyClassBase(metaclass=ABCMeta):
+    """MyClassBase."""
+
+    @abstractproperty
+    def example(self):
+        """Getter."""
+
+    @abstractproperty
+    @example.setter
+    def example(self, value):
+        """Setter."""
