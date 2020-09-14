11c3710ad2614bce68ccf9485794a08de5e9e7ef
David Cain <davidjosephcain@gmail.com>
2020-09-13 20:10:13 -0700

    Fix `super-with-arguments` if no `__class__` cell
    In certain cases, it's not actually possible to use Python 3's `super()`
    without any arguments. A simple example would be a function that is
    meant to decorate a class and therefore does not have a known
    `__class__` cell. This decorator must instead be explicitly given a
    `cls`.
    
    This fixes an error with the `super-with-arguments` check, introduced in
    version 2.6.0.
    
    The root cause is that the return type of `node_frame_class()` is
    `Optional`, and we don't check for the null case.
    
    After searching recent pylint issues, it does appear that somebody else
    already noticed & applied the same fix. See:
    https://github.com/PyCQA/pylint/pull/3810
    
    This includes a small test case, though. The original author should
    receive credit, but hopefully this is helpful.
    
    ------------------------------------------------------------------------
    
    This test passes with:
    ```
    python3 -m pytest "tests/test_functional.py::test_functional[super_with_arguments]"
    ```
    
    (the test also fails without this change)

--- tests/functional/s/super_with_arguments.py
+++ tests/functional/s/super_with_arguments.py
@@ -25,3 +25,11 @@ class NotSuperCall(Foo):
 class InvalidSuperCall(Foo):
     def __init__(self):
         super(InvalidSuperCall.__class__, self).__init__()
+
+
+def method_accepting_cls(cls, self):
+    # Using plain `super()` is not valid here, since there's no `__class__` cell found
+    # (Exact exception would be 'RuntimeError: super(): __class__ cell not found')
+    # Instead, we expect to *not* see a warning about `super-with-arguments`.
+    # Explicitly passing `cls`, and `self` to `super()` is what's required.
+    super(cls, self).__init__()
