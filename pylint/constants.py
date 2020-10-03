c15d3f06128de9f902228dc3db11fb1b1e77d388
wtracy <afishionado@gmail.com>
2020-10-03 10:51:17 -0700

    Remove unused regular expressions import.

--- pylint/constants.py
+++ pylint/constants.py
@@ -1,7 +1,6 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/master/COPYING
 
-import re
 import sys
 
 from astroid.__pkginfo__ import version as astroid_version
