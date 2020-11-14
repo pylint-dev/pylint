3969153bb6ba6d608124cadc0e21f642dc3cc9ee
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2020-11-14 16:13:37 +0100

    Remove redundant character escape '\]' in _EXPECTED_RE

--- pylint/testutils.py
+++ pylint/testutils.py
@@ -355,7 +355,7 @@ _MESSAGE = {"msg": r"[a-z][a-z\-]+"}
 _EXPECTED_RE = re.compile(
     r"\s*#\s*(?:(?P<line>[+-]?[0-9]+):)?"
     r"(?:(?P<op>[><=]+) *(?P<version>[0-9.]+):)?"
-    r"\s*\[(?P<msgs>%(msg)s(?:,\s*%(msg)s)*)\]" % _MESSAGE
+    r"\s*\[(?P<msgs>%(msg)s(?:,\s*%(msg)s)*)]" % _MESSAGE
 )
 
 
