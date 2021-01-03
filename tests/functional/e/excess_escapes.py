525a47aebe3eec4295e6891e358de3bc262b46a9
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-01-03 22:14:12 +0100

    Move excess escape functional test to new framework

--- tests/functional/e/excess_escapes.py
+++ tests/functional/e/excess_escapes.py
@@ -4,17 +4,18 @@
 __revision__ = '$Id$'
 
 # Bad escape sequences, which probably don't do what you expect.
-A = "\[\]\\"
-assert '\/' == '\\/'
-ESCAPE_BACKSLASH = '\`'
+A = "\[\]\\"  # [anomalous-backslash-in-string,anomalous-backslash-in-string]
+assert '\/' == '\\/' # [anomalous-backslash-in-string]
+ESCAPE_BACKSLASH = '\`'  # [anomalous-backslash-in-string]
 
 # Valid escape sequences.
 NEWLINE = "\n"
 OLD_ESCAPES = '\a\b\f\n\t\r\v'
 HEX = '\xad\x0a\x0d'
+# +1:[anomalous-backslash-in-string,anomalous-backslash-in-string]
 FALSE_OCTAL = '\o123\o000'  # Not octal in Python
 OCTAL = '\123\000'
-NOT_OCTAL = '\888\999'
+NOT_OCTAL = '\888\999'  # [anomalous-backslash-in-string,anomalous-backslash-in-string]
 NUL = '\0'
 UNICODE = u'\u1234'
 HIGH_UNICODE = u'\U0000abcd'
@@ -24,7 +25,7 @@ LITERAL_NEWLINE = '\
 ESCAPE_UNICODE = "\\\\n"
 
 # Bad docstring
-"""Even in a docstring
+"""Even in a docstring # [anomalous-backslash-in-string]
 
 You shouldn't have ambiguous text like: C:\Program Files\alpha
 """
