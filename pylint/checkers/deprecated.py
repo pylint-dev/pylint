5aa8f18b6681711f683e5d56c0f569a2a4145571
Matus Valo <matusvalo@gmail.com>
2021-01-29 06:36:51 +0100

    Introduced DeprecatedMixin

---
+++ pylint/checkers/deprecated.py
@@ -0,0 +1,49 @@
+# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# For details: https://github.com/PyCQA/pylint/blob/master/COPYING
+
+"""Checker mixin for deprecated functionality."""
+
+import abc
+
+import astroid
+
+ACCEPTABLE_NODES = (
+    astroid.BoundMethod,
+    astroid.UnboundMethod,
+    astroid.FunctionDef,
+)
+
+
+class DeprecatedMixin(metaclass=abc.ABCMeta):
+    """A mixin implementing logic for checking deprecated symbols.
+    A class imlementing mixin must define "deprecated-method" Message.
+    """
+
+    @abc.abstractmethod
+    def deprecated_methods(self):
+        """Callback returning the deprecated methods/functions.
+
+        Returns:
+            collections.abc.Container of deprecated function/method names.
+        """
+
+    def check_deprecated_method(self, node, inferred):
+        """Executes the checker for the given node. This method should
+        be called from the checker implementing this mixin.
+        """
+
+        # Reject nodes which aren't of interest to us.
+        if not isinstance(inferred, ACCEPTABLE_NODES):
+            return
+
+        if isinstance(node.func, astroid.Attribute):
+            func_name = node.func.attrname
+        elif isinstance(node.func, astroid.Name):
+            func_name = node.func.name
+        else:
+            # Not interested in other nodes.
+            return
+
+        qname = inferred.qname()
+        if any(name in self.deprecated_methods() for name in (qname, func_name)):
+            self.add_message("deprecated-method", node=node, args=(func_name,))
---
+++ tests/checkers/unittest_deprecated.py
@@ -0,0 +1,99 @@
+import astroid
+
+from pylint.checkers import BaseChecker, DeprecatedMixin, utils
+from pylint.interfaces import UNDEFINED, IAstroidChecker
+from pylint.testutils import CheckerTestCase, Message
+
+
+class _DeprecatedChecker(BaseChecker, DeprecatedMixin):
+    __implements__ = (IAstroidChecker,)
+    name = "deprecated"
+
+    msgs = {
+        "W1505": (
+            "Using deprecated method %s()",
+            "deprecated-method",
+            "The method is marked as deprecated and will be removed in "
+            "a future version of Python. Consider looking for an "
+            "alternative in the documentation.",
+        )
+    }
+
+    @utils.check_messages(
+        "deprecated-method",
+    )
+    def visit_call(self, node):
+        """Visit a Call node."""
+        try:
+            for inferred in node.func.infer():
+                self.check_deprecated_method(node, inferred)
+        except astroid.InferenceError:
+            return
+
+    def deprecated_methods(self):
+        return {"deprecated_func", ".Deprecated.deprecated_method"}
+
+
+class TestDeprecatedChecker(CheckerTestCase):
+    CHECKER_CLASS = _DeprecatedChecker
+
+    def test_deprecated_function(self):
+        # Tests detecting deprecated function
+        node = astroid.extract_node(
+            """
+        def deprecated_func():
+            pass
+
+        deprecated_func()
+        """
+        )
+        with self.assertAddsMessages(
+            Message(
+                msg_id="deprecated-method",
+                args=("deprecated_func",),
+                node=node,
+                confidence=UNDEFINED,
+            )
+        ):
+            self.checker.visit_call(node)
+
+    def test_deprecated_method(self):
+        # Tests detecting deprecated method
+        node = astroid.extract_node(
+            """
+        class Deprecated:
+            def deprecated_method():
+                pass
+
+        d = Deprecated()
+        d.deprecated_method()
+        """
+        )
+        with self.assertAddsMessages(
+            Message(
+                msg_id="deprecated-method",
+                args=("deprecated_method",),
+                node=node,
+                confidence=UNDEFINED,
+            )
+        ):
+            self.checker.visit_call(node)
+
+    def test_no_message(self):
+        # Tests not raising error when no deprecated functions/methods are present.
+        node = astroid.extract_node(
+            """
+        class MyClass:
+            def mymethod():
+                pass
+
+        MyClass().mymethod()
+
+        def myfunc():
+            pass
+
+        myfunc()
+        """
+        )
+        with self.assertNoMessages():
+            self.checker.visit_call(node)
