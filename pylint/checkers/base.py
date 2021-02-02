pylint-dev/pylint

commit 3422e4adc718c60a467d66ab0dedf4a7032c4fc5

No longer emit ``invalid-name`` for non-constants found at module level.
Pylint was taking the following statement from PEP-8 too far, considering
all module level variables as constants, which is not what the statement is saying:

`Constants are usually defined on a module level and written in
all capital letters with underscores separating words.`

Close #3111
Close #3132

pylint/checkers/base.py
@@ -1886,9 +1886,11 @@ class NameChecker(_BasicChecker):
                 if isinstance(utils.safe_infer(assign_type.value), astroid.ClassDef):
                     self._check_name("class", node.name, node)
                 else:
-                    if not _redefines_import(node):
-                        # Don't emit if the name redefines an import
-                        # in an ImportError except handler.
+                    # Don't emit if the name redefines an import
+                    # in an ImportError except handler.
+                    if not _redefines_import(node) and isinstance(
+                        utils.safe_infer(assign_type.value), astroid.Const
+                    ):
                         self._check_name("const", node.name, node)
             elif isinstance(assign_type, astroid.ExceptHandler):
                 self._check_name("variable", node.name, node)

tests/functional/n/name_styles.py
@@ -134,6 +134,9 @@ def _private_scope_function_with_long_descriptive_name():
     return 12
 
 LONG_CONSTANT_NAME_IN_PUBLIC_SCOPE_ARE_OKAY = True
+# We don't emit for non-const nodes
+good_name_for_funcs = lambda: None
+good_name_for_lists = [1, 2, 3]
 
 class _AnExceptionalExceptionThatOccursVeryVeryRarely(Exception):
     """A very exceptional exception with a nice descriptive name"""

commit a1f27c42fd4c93bf2653bf2fddaa81dda9dcf9ff

[blacklisted-name] Allow emission on non-constants
Change:
- Fix a regression introduced in
  3422e4adc718c60a467d66ab0dedf4a7032c4fc5 where blacklisted-name
  (emitted by _check_name()) would only be emitted for constants.
- The blacklisted-name check has been pulled out into its own helper
  function, and is now called instead of _check_name() when we are NOT
  at a constant.
- Fix a similar bug (not regression) for non-ascii-name in the same
  scenario.
- Changelog updated.

Test Plan:
- New functional tests added

Tickets:
- Fixes #3701

Signed-off-by: Rick Elrod <rick@elrod.me>

pylint/checkers/base.py
@@ -1972,10 +1972,15 @@ class NameChecker(_BasicChecker):
                     self._check_name("class", node.name, node)
                 # Don't emit if the name redefines an import
                 # in an ImportError except handler.
-                elif not _redefines_import(node) and isinstance(
-                    utils.safe_infer(assign_type.value), astroid.Const
-                ):
-                    self._check_name("const", node.name, node)
+                elif not _redefines_import(node):
+                    if isinstance(utils.safe_infer(assign_type.value), astroid.Const):
+                        # We can (potentially) emit invalid-name if we are at a constant.
+                        self._check_name("const", node.name, node)
+                    else:
+                        # If we're not at a constant, we can still potentially emit
+                        # blacklisted-name and non-ascii-name.
+                        self._check_non_ascii_name("const", node.name, node)
+                        self._check_name_listed("const", node.name, node)
             elif isinstance(assign_type, astroid.ExceptHandler):
                 self._check_name("variable", node.name, node)
         elif isinstance(frame, astroid.FunctionDef):
@@ -2028,15 +2033,35 @@ class NameChecker(_BasicChecker):
             pattern.match(name) for pattern in self._bad_names_rgxs_compiled
         )
 
-    def _check_name(self, node_type, name, node, confidence=interfaces.HIGH):
-        """check for a name using the type's regexp"""
+    def _check_name_listed(self, node_type, name, node):
+        """
+        emits blacklisted-name if the name is blacklisted.
+        returns True if the name is whitelisted or blacklisted, False otherwise.
+        """
 
+        if self._name_valid_due_to_whitelist(name=name):
+            return True
+
+        if self._name_invalid_due_to_blacklist(name=name):
+            self.stats["badname_" + node_type] += 1
+            self.add_message("blacklisted-name", node=node, args=name)
+            return True
+
+        return False
+
+    def _check_non_ascii_name(self, node_type, name, node, confidence=interfaces.HIGH):
         non_ascii_match = self._non_ascii_rgx_compiled.match(name)
 
         if non_ascii_match is not None:
             self._raise_name_warning(
                 node, node_type, name, confidence, warning="non-ascii-name"
             )
+
+
+    def _check_name(self, node_type, name, node, confidence=interfaces.HIGH):
+        """check for a name using the type's regexp"""
+
+        self._check_non_ascii_name(node_type, name, node, confidence)
 
         def _should_exempt_from_invalid_name(node):
             if node_type == "variable":
@@ -2049,12 +2074,10 @@ class NameChecker(_BasicChecker):
             clobbering, _ = utils.clobber_in_except(node)
             if clobbering:
                 return
-        if self._name_valid_due_to_whitelist(name=name):
+
+        if self._check_name_listed(node_type, name, node):
             return
-        if self._name_invalid_due_to_blacklist(name=name):
-            self.stats["badname_" + node_type] += 1
-            self.add_message("blacklisted-name", node=node, args=name)
-            return
+
         regexp = self._name_regexps[node_type]
         match = regexp.match(name)
 

tests/functional/b/blacklisted_name.py
@@ -2,3 +2,6 @@
 
 def baz(): # [blacklisted-name]
     pass
+
+foo = {}.keys()  # [blacklisted-name]
+foo = None  # [blacklisted-name]

tests/functional/n/non_ascii_name.py
@@ -1,5 +1,6 @@
 """ Tests for non-ascii-name checker. """
 
+áéíóú = {}.keys() # [non-ascii-name]
 áéíóú = 4444 # [non-ascii-name]
 
 def úóíéá(): # [non-ascii-name]