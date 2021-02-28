3dfecb0b269f3594b2e211032fbfa54ee80f6aaa
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-02-28 22:34:02 +0100

    Change version for 2.7.2 release

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -31,8 +31,8 @@
 from os.path import join
 
 # For an official release, use dev_version = None
-numversion = (2, 8, 0)
-dev_version = 1
+numversion = (2, 7, 2)
+dev_version = None
 
 version = ".".join(str(num) for num in numversion)
 if dev_version is not None:
