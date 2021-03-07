b16d72f8184c4352052b984acffaa1ab42dd5fa7
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-03-07 21:12:22 +0100

    Migrate func_e0204.py to new functional tests

--- tests/functional/n/no/no_self_argument.py
+++ tests/functional/n/no/no_self_argument.py
@@ -1,20 +1,18 @@
-"""check for method without self as first argument
-"""
+"""Check for method without self as first argument"""
 # pylint: disable=useless-object-inheritance
 from __future__ import print_function
-__revision__ = 0
 
-
-class Abcd(object):
+class NoSelfArgument(object):
     """dummy class"""
 
-    def __init__(truc):
+    def __init__(truc):  # [no-self-argument]
         """method without self"""
         print(1)
 
-    def abdc(yoo):
+    def abdc(yoo):  # [no-self-argument]
         """another test"""
         print(yoo)
+
     def edf(self):
         """just another method"""
         print('yapudju in', self)
