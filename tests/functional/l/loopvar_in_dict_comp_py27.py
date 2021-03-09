d805d97ca46cd9fee0067eccae45154d3836714e
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-03-09 20:25:11 +0100

    Migrate func_loopvar_in_dict_comp_py27.py to new functional tests

--- tests/functional/l/loopvar_in_dict_comp_py27.py
+++ tests/functional/l/loopvar_in_dict_comp_py27.py
@@ -5,4 +5,4 @@ __revision__ = 0
 
 def bad_case():
     """Loop variable from dict comprehension."""
-    return {x: lambda: x for x in range(10)}
+    return {x: lambda: x for x in range(10)} # [cell-var-from-loop]
