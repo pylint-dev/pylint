55a1991d8a694bd70e38bd49bc8d69075660ee64
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-03-09 20:30:12 +0100

    Migrate func_no_dummy_redefined.py to new functional tests

--- tests/functional/n/no/no_dummy_redefined.py
+++ tests/functional/n/no/no_dummy_redefined.py
@@ -4,11 +4,11 @@ from __future__ import print_function
 
 _, INTERESTING = 'a=b'.split('=')
 
-value = 10
+value = 10  # [invalid-name]
 
 
 def clobbering():
     """Clobbers a dummy name from the outer scope."""
-    value = 9
+    value = 9  # [redefined-outer-name]
     for _ in range(7):
         print(value)
