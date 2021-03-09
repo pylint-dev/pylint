c41daa7d229823e18ce6106bef85e071d6910be1
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-03-09 20:44:38 +0100

    Migrate func_dotted_ancestor.py to new functional tests

--- tests/functional/d/dotted_ancestor.py
+++ tests/functional/d/dotted_ancestor.py
@@ -1,11 +1,11 @@
 """bla"""
 # pylint: disable=no-absolute-import
 
-from input import func_w0233
+from . import func_w0233
 
 __revision__ = 'yo'
 
-class Aaaa(func_w0233.AAAA):
+class Aaaa(func_w0233.AAAA):  # [too-few-public-methods]
     """test dotted name in ancestors"""
     def __init__(self):
         func_w0233.AAAA.__init__(self)
