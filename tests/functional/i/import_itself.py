258f26c02655d162bc3afe6fe9edefbecb3c08a4
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-03-09 22:07:20 +0100

    Migrate func_w406.py to new functional tests

--- tests/functional/i/import_itself.py
+++ tests/functional/i/import_itself.py
@@ -1,10 +1,10 @@
 """test module importing itself"""
 # pylint: disable=no-absolute-import,using-constant-test
 from __future__ import print_function
-from . import func_w0406
+from . import import_itself  # [import-self]
 
 __revision__ = 0
 
 
 if __revision__:
-    print(func_w0406)
+    print(import_itself)
