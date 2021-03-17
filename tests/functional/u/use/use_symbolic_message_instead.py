fe5ecc0a5b0dd2d14484e7ea608f270c011957a6
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-03-17 21:14:19 +0100

    Merge the two tests for use-symbolic-message-instead
    Keep the more explicit name.

--- tests/functional/u/use/use_symbolic_message_instead.py
+++ tests/functional/u/use/use_symbolic_message_instead.py
@@ -1,11 +1,25 @@
-"""Use symbolic message instead are also tested in messages_managed_by_id.py"""
-
 # pylint: disable=C0111,R0903,T1234  # [bad-option-value,use-symbolic-message-instead,use-symbolic-message-instead]
 # pylint: enable=C0111   # [use-symbolic-message-instead]
 
-def myfunction(arg):  # [missing-function-docstring]
+def my_function(arg):  # [missing-function-docstring]
     return arg or True
 
 # pylint: disable=C0111  # [use-symbolic-message-instead]
 # pylint: enable=R0903  # [use-symbolic-message-instead]
 # pylint: disable=R0903  # [use-symbolic-message-instead]
+
+
+def foo():  # pylint: disable=C0102  # [use-symbolic-message-instead]
+    return 1
+
+
+def toto():  # pylint: disable=C0102,R1711  # [use-symbolic-message-instead,use-symbolic-message-instead]
+    return
+
+
+def test_enabled_by_id_msg():  # pylint: enable=C0111  # [use-symbolic-message-instead,missing-function-docstring]
+    pass
+
+
+def baz():  # pylint: disable=blacklisted-name
+    return 1
