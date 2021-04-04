949d6679e39ff215e28734b9dede7e3d86be6d25
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-04-04 21:38:52 +0200

    Use setup.cfg for existing hard coded values

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from pathlib import Path
 from typing import Any, Dict
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 HERE = Path(__file__).parent
 
@@ -33,55 +33,12 @@ for requirement_file in DEV_REQUIREMENTS_FILES:
 
 if __name__ == "__main__":
     setup(
-        name="pylint",
         version=__pkginfo__["__version__"],
-        license="GPL-2.0-or-later",
-        description="python code static checker",
         long_description=long_description,
-        author="Python Code Quality Authority",
-        author_email="code-quality@python.org",
-        url="https://github.com/PyCQA/pylint",
-        project_urls={"What's New": "https://pylint.pycqa.org/en/latest/whatsnew/"},
-        classifiers=[
-            "Development Status :: 6 - Mature",
-            "Environment :: Console",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: GNU General Public License (GPL)",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3 :: Only",
-            "Programming Language :: Python :: Implementation :: CPython",
-            "Programming Language :: Python :: Implementation :: PyPy",
-            "Topic :: Software Development :: Debuggers",
-            "Topic :: Software Development :: Quality Assurance",
-            "Topic :: Software Development :: Testing",
-        ],
-        packages=find_packages(),
-        python_requires="~=3.6",
-        setup_requires=[],
         tests_require=test_requires,
-        install_requires=[
-            "astroid>=2.5.2,<2.7",
-            "isort>=4.2.5,<6",
-            "mccabe>=0.6,<0.7",
-            "toml>=0.7.1",
-        ],
         extras_require={
             ':sys_platform=="win32"': ["colorama"],
             "docs": doc_extra_requires,
             "dev": dev_extra_requires,
-        },
-        entry_points={
-            "console_scripts": [
-                "pylint = pylint:run_pylint",
-                "epylint = pylint:run_epylint",
-                "pyreverse = pylint:run_pyreverse",
-                "symilar = pylint:run_symilar",
-            ]
         },
     )
