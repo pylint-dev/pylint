d0591ba2a097312c41d544e4269eda5b809c47a0
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2021-04-09 21:13:14 +0200

    Enable loading of plugins in functional tests (Fix 4331) (#4332)
    * Load plugins if defined in option file of functional test.
    * Fix functional test which now fails after optional plugins are now loaded correctly
    * Added functional test for bad_builtins from #4291

--- pylint/testutils/lint_module_test.py
+++ pylint/testutils/lint_module_test.py
@@ -22,6 +22,7 @@ from pylint.testutils.functional_test_file import (
 )
 from pylint.testutils.output_line import OutputLine
 from pylint.testutils.reporter_for_tests import FunctionalTestReporter
+from pylint.utils import utils
 
 
 class LintModuleTest:
@@ -38,6 +39,11 @@ class LintModuleTest:
         self._linter.disable("useless-suppression")
         try:
             self._linter.read_config_file(test_file.option_file)
+            if self._linter.cfgfile_parser.has_option("MASTER", "load-plugins"):
+                plugins = utils._splitstrip(
+                    self._linter.cfgfile_parser.get("MASTER", "load-plugins")
+                )
+                self._linter.load_plugin_modules(plugins)
             self._linter.load_config_file()
         except NoFileError:
             pass
