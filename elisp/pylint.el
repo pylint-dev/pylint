b5436013dcfe4f0b6ce91295b502aaf883c98f5f
ramiro050 <ramiroleal050@gmail.com>
2021-04-11 09:23:41 -0700

    Undid if-statement fix for parsing tramp file

--- elisp/pylint.el
+++ elisp/pylint.el
@@ -202,9 +202,9 @@ output buffer, to go to the lines where pylint found matches.
 
   (save-some-buffers (not pylint-ask-about-save) nil)
   (let* ((filename (buffer-file-name))
-         (filename (if (tramp-tramp-file-p filename)
-                       (tramp-file-local-name filename)
-                     filename))
+         (filename (or (and (tramp-tramp-file-p filename)
+                         (aref (tramp-dissect-file-name filename) 3))
+                      filename))
          (filename (shell-quote-argument filename))
          (pylint-command (if arg
                              pylint-alternate-pylint-command
