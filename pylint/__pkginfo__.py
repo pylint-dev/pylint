78acd527220fe7f67b4f6ff4a757fc88ab623f21
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-04-25 10:08:58 +0200

    Add scm_setuptools for packaging

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -1,19 +1,12 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/master/LICENSE
 
-from typing import Optional
+from pkg_resources import DistributionNotFound, get_distribution
 
-__version__ = "2.8.1"
-# For an official release, use 'alpha_version = False' and 'dev_version = None'
-alpha_version: bool = False  # Release will be an alpha version if True (ex: '1.2.3a6')
-dev_version: Optional[int] = None
-
-if dev_version is not None:
-    if alpha_version:
-        __version__ += f"a{dev_version}"
-    else:
-        __version__ += f".dev{dev_version}"
-
+try:
+    __version__ = get_distribution("pylint").version
+except DistributionNotFound:
+    __version__ = "2.8.2+"
 
 # Kept for compatibility reason, see https://github.com/PyCQA/pylint/issues/4399
 numversion = tuple(__version__.split("."))
