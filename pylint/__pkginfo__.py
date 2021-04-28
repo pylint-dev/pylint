3481d200b11fabb3e69cddc376836b62b117aea6
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-04-28 21:58:38 +0200

    Fix retrocompatbility of numversion
    Closes #4420

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -1,5 +1,6 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/master/LICENSE
+from typing import Tuple
 
 from pkg_resources import DistributionNotFound, get_distribution
 
@@ -8,5 +9,27 @@ try:
 except DistributionNotFound:
     __version__ = "2.8.2+"
 
-# Kept for compatibility reason, see https://github.com/PyCQA/pylint/issues/4399
-numversion = tuple(__version__.split("."))
+
+def get_numversion_from_version(v: str) -> Tuple:
+    """Kept for compatibility reason
+
+    See https://github.com/PyCQA/pylint/issues/4399
+    https://github.com/PyCQA/pylint/issues/4420,
+    """
+    v = v.replace("pylint-", "")
+    version = []
+    for n in v.split(".")[0:3]:
+        try:
+            version.append(int(n))
+        except ValueError:
+            num = ""
+            for c in n:
+                if c.isdigit():
+                    num += c
+                else:
+                    break
+            version.append(int(num))
+    return tuple(version)
+
+
+numversion = get_numversion_from_version(__version__)
