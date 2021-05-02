e02be21c25a4124d612acb7621ea94bb1f3cc89e
Dr. Nick <das-intensity@users.noreply.github.com>
2021-05-02 13:14:03 -0400

    Add --fail-on option (#4227) (#4398)
    * Add --fail-on option (#4227)
    
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_self.py
+++ tests/test_self.py
@@ -741,6 +741,69 @@ class TestRunTC:
             code=22,
         )
 
+    @pytest.mark.parametrize(
+        "fu_score,fo_msgs,fname,out",
+        [
+            # Essentially same test cases as --fail-under, but run with/without a detected issue code
+            # missing-function-docstring (C0116) is issue in both files
+            # --fail-under should be irrelevant as missing-function-docstring is hit
+            (-10, "missing-function-docstring", "fail_under_plus7_5.py", 16),
+            (6, "missing-function-docstring", "fail_under_plus7_5.py", 16),
+            (7.5, "missing-function-docstring", "fail_under_plus7_5.py", 16),
+            (7.6, "missing-function-docstring", "fail_under_plus7_5.py", 16),
+            (-11, "missing-function-docstring", "fail_under_minus10.py", 22),
+            (-10, "missing-function-docstring", "fail_under_minus10.py", 22),
+            (-9, "missing-function-docstring", "fail_under_minus10.py", 22),
+            (-5, "missing-function-docstring", "fail_under_minus10.py", 22),
+            # --fail-under should guide whether error code as missing-function-docstring is not hit
+            (-10, "broad-except", "fail_under_plus7_5.py", 0),
+            (6, "broad-except", "fail_under_plus7_5.py", 0),
+            (7.5, "broad-except", "fail_under_plus7_5.py", 0),
+            (7.6, "broad-except", "fail_under_plus7_5.py", 16),
+            (-11, "broad-except", "fail_under_minus10.py", 0),
+            (-10, "broad-except", "fail_under_minus10.py", 0),
+            (-9, "broad-except", "fail_under_minus10.py", 22),
+            (-5, "broad-except", "fail_under_minus10.py", 22),
+            # Enable by message id
+            (-10, "C0116", "fail_under_plus7_5.py", 16),
+            # Enable by category
+            (-10, "C", "fail_under_plus7_5.py", 16),
+            (-10, "fake1,C,fake2", "fail_under_plus7_5.py", 16),
+            # Ensure entire category not enabled by any msg id
+            (-10, "C0115", "fail_under_plus7_5.py", 0),
+        ],
+    )
+    def test_fail_on(self, fu_score, fo_msgs, fname, out):
+        self._runtest(
+            [
+                "--fail-under",
+                f"{fu_score:f}",
+                f"--fail-on={fo_msgs}",
+                "--enable=all",
+                join(HERE, "regrtest_data", fname),
+            ],
+            code=out,
+        )
+
+    @pytest.mark.parametrize(
+        "opts,out",
+        [
+            # Special case to ensure that disabled items from category aren't enabled
+            (["--disable=C0116", "--fail-on=C"], 0),
+            # Ensure order does not matter
+            (["--fail-on=C", "--disable=C0116"], 0),
+            # Ensure --fail-on takes precedence over --disable
+            (["--disable=C0116", "--fail-on=C0116"], 16),
+            # Ensure order does not matter
+            (["--fail-on=C0116", "--disable=C0116"], 16),
+        ],
+    )
+    def test_fail_on_edge_case(self, opts, out):
+        self._runtest(
+            opts + [join(HERE, "regrtest_data", "fail_under_plus7_5.py")],
+            code=out,
+        )
+
     @staticmethod
     def test_modify_sys_path() -> None:
         @contextlib.contextmanager
