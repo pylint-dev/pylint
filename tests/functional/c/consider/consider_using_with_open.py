f9df028c23ec5458c80d1dd62a31b217a11ce44f
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2021-05-09 14:19:04 +0200

    Issue 4430 false positive consider-using-with R1732 (#4453)
    * Suppress consider-using-with if used inside context manager
    
    * Added ChangeLog entry

--- tests/functional/c/consider/consider_using_with_open.py
+++ tests/functional/c/consider/consider_using_with_open.py
@@ -1,10 +1,15 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, invalid-name, import-outside-toplevel
+# pylint: disable=missing-class-docstring, too-few-public-methods, unused-variable
 """
 The functional test for the standard ``open()`` function has to be moved in a separate file,
 because PyPy has to be excluded for the tests as the ``open()`` function is uninferable in PyPy.
 However, all remaining checks for consider-using-with work in PyPy, so we do not want to exclude
 PyPy from ALL functional tests.
 """
+from contextlib import contextmanager
+
+
+myfile = open("test.txt")  # [consider-using-with]
 
 
 def test_open():
@@ -13,3 +18,24 @@ def test_open():
 
     with open("test.txt") as fh:  # must not trigger
         fh.read()
+
+
+def test_open_in_enter():
+    """Message must not trigger if the resource is allocated in a context manager."""
+    class MyContextManager:
+        def __init__(self):
+            self.file_handle = None
+
+        def __enter__(self):
+            self.file_handle = open("foo.txt", "w")  # must not trigger
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self.file_handle.close()
+
+
+@contextmanager
+def test_open_in_with_contextlib():
+    """Message must not trigger if the resource is allocated in a context manager."""
+    file_handle = open("foo.txt", "w")  # must not trigger
+    yield file_handle
+    file_handle.close()
