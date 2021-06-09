3c65e6b2f80673c8323f01b813e544bcea05304d
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-06-09 23:20:47 +0200

    Move to new extension CodeStyleChecker

---
+++ pylint/extensions/code_style.py
@@ -0,0 +1,58 @@
+from typing import Union
+
+import astroid
+
+from pylint.checkers import BaseChecker
+from pylint.checkers.utils import check_messages
+from pylint.interfaces import IAstroidChecker
+from pylint.lint import PyLinter
+
+
+class CodeStyleChecker(BaseChecker):
+    """Checkers that can improve code consistency.
+
+    As such they don't necessarily provide a performance benefit and
+    are often times opinionated.
+    """
+
+    __implements__ = (IAstroidChecker,)
+
+    name = "code_style"
+    priority = -1
+    msgs = {
+        "R6102": (
+            "Consider using an in-place tuple%s",
+            "consider-using-tuple",
+            "Emitted when an in-place defined list or set can be "
+            "replaced by a slightly faster tuple.",
+        ),
+    }
+
+    def __init__(self, linter: PyLinter) -> None:
+        """Initialize checker instance."""
+        super().__init__(linter=linter)
+
+    @check_messages("consider-using-tuple")
+    def visit_for(self, node: astroid.For) -> None:
+        self._check_inplace_defined_list_set(node)
+
+    @check_messages("consider-using-tuple")
+    def visit_comprehension(self, node: astroid.Comprehension) -> None:
+        self._check_inplace_defined_list_set(node)
+
+    def _check_inplace_defined_list_set(
+        self, node: Union[astroid.For, astroid.Comprehension]
+    ) -> None:
+        """Check if inplace defined list / set can be replaced by a tuple."""
+        if isinstance(node.iter, (astroid.List, astroid.Set)) and not any(
+            isinstance(item, astroid.Starred) for item in node.iter.elts
+        ):
+            self.add_message(
+                "consider-using-tuple",
+                node=node.iter,
+                args=(f" instead of {node.iter.__class__.__qualname__.lower()}"),
+            )
+
+
+def register(linter: PyLinter) -> None:
+    linter.register_checker(CodeStyleChecker(linter))
