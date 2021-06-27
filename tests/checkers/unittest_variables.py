3d6389b97e8f9416304185a1babfe49e8a0dc348
Sergei Lebedev <185856+superbobry@users.noreply.github.com>
2021-06-27 19:11:54 +0100

    VariableChecker now accounts for attribute lookups in type comments (#4604)
    * VariableChecker now accounts for attribute lookups in type comments
    
    Prior to this commit VariableChecker did not recurse into attribute lookups
    in type comments. This lead to false positive unused-import messages in e.g.
    
        import collections
        d = ...  # type: collections.OrderedDict
    
    Fixes #4603

--- tests/checkers/unittest_variables.py
+++ tests/checkers/unittest_variables.py
@@ -21,11 +21,13 @@
 import os
 import re
 import sys
+import unittest
 from pathlib import Path
 
 import astroid
 
 from pylint.checkers import variables
+from pylint.constants import IS_PYPY
 from pylint.interfaces import UNDEFINED
 from pylint.testutils import CheckerTestCase, Message, linter, set_config
 
@@ -186,6 +188,24 @@ class TestVariablesChecker(CheckerTestCase):
                 pass
             def my_method(self) -> MyType:
                 pass
+        """
+        )
+        with self.assertNoMessages():
+            self.walk(module)
+
+    @unittest.skipIf(IS_PYPY, "PyPy does not parse type comments")
+    def test_attribute_in_type_comment(self):
+        """Ensure attribute lookups in type comments are accounted for.
+
+        https://github.com/PyCQA/pylint/issues/4603
+        """
+        module = astroid.parse(
+            """
+        import foo
+        from foo import Bar, Boo
+        a = ... # type: foo.Bar
+        b = ... # type: foo.Bar[Boo]
+        c = ... # type: Bar.Boo
         """
         )
         with self.assertNoMessages():
