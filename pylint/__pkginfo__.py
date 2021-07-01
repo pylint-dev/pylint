7c85b0d6ce12fa7f86885cfbdba19587127f7395
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-07-01 10:25:52 +0200

    Move back to a dev version following 2.9.2 release

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -2,7 +2,7 @@
 # For details: https://github.com/PyCQA/pylint/blob/master/LICENSE
 from typing import Tuple
 
-__version__ = "2.9.2"
+__version__ = "2.9.3-dev0"
 
 
 def get_numversion_from_version(v: str) -> Tuple:
