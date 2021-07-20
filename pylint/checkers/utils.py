867d61b2345a1322474ad2a9772c9e1ef3e218c3
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-07-20 19:12:25 +0200

    Bump pylint to 2.9.4, update changelog

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -42,6 +42,7 @@
 # Copyright (c) 2020 Slavfox <slavfoxman@gmail.com>
 # Copyright (c) 2020 Anthony Sottile <asottile@umich.edu>
 # Copyright (c) 2021 Marc Mueller <30130371+cdce8p@users.noreply.github.com>
+# Copyright (c) 2021 Matus Valo <matusvalo@users.noreply.github.com>
 # Copyright (c) 2021 Lorena B <46202743+lorena-b@users.noreply.github.com>
 # Copyright (c) 2021 yushao2 <36848472+yushao2@users.noreply.github.com>
 
