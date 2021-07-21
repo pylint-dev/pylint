dbfcc07cf1b83c77d689c59f38d27e5ba10ef896
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-07-21 09:14:16 +0200

    [4692 regression] Test won't fail if the real click is installed

--- tests/functional/i/inference_crash_4692.py
+++ tests/functional/i/inference_crash_4692.py
@@ -1,7 +1,9 @@
 """Regression test for https://github.com/PyCQA/pylint/issues/4692."""
 
-import click  # [import-error]
+# We can't use click like in the issue because the crash
+# does not appear if click is installed (astroid can analyse it)
+import notclick  # [import-error]
 
 
-for name, item in click.__dict__.items():
-    _ = isinstance(item, click.Command) and item != 'foo'
+for name, item in notclick.__dict__.items():
+    _ = isinstance(item, notclick.Command) and item != 'foo'
