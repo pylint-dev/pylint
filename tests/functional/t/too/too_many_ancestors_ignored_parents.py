09f0fc9fec90c481ab6b2f8c009b884f8e87711b
Rebecca Turner <rturner@starry.com>
2021-07-27 15:21:48 -0400

    Add comment/diagrams to functional test

--- tests/functional/t/too/too_many_ancestors_ignored_parents.py
+++ tests/functional/t/too/too_many_ancestors_ignored_parents.py
@@ -1,5 +1,26 @@
 # pylint: disable=missing-docstring, too-few-public-methods, invalid-name
 
+# Inheritance diagram:
+#       F
+#      /
+#  D  E
+#   \/
+#    B  C
+#     \/
+#      A
+#
+# Once `E` is pruned from the tree, we have:
+#  D
+#   \
+#    B  C
+#     \/
+#      A
+#
+# By setting `max-parents=2`, we're able to check that tree-pruning works
+# correctly; in the new diagram, `B` has only 1 parent, so it doesn't raise a
+# message, and `A` has 3, so it does raise a message with the specific number
+# of parents.
+
 class F:
     """0 parents"""
 
