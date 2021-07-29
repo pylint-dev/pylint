4dc23dd5b11ec4243bcfef4ce1f4f935f6cca093
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-07-29 21:59:46 +0200

    Fix tests based on review

--- tests/functional/f/forgotten_debug_statement_py37.py
+++ tests/functional/f/forgotten_debug_statement_py37.py
@@ -1,10 +1,10 @@
 # pylint: disable=missing-docstring
 
+import pdb
 import sys
 
 breakpoint()  # [forgotten-debug-statement]
 sys.breakpointhook()  # [forgotten-debug-statement]
+pdb.set_trace()  # [forgotten-debug-statement]
 b = breakpoint
 b()  # [forgotten-debug-statement]
-bsys = sys.breakpointhook
-bsys()  # [forgotten-debug-statement]
