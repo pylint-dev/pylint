6e99924995a640bf19681d7d5a32501d9a4d9be7
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2021-08-09 06:37:03 +0200

    Update license link to point to main, not master

--- tests/pyreverse/test_printer.py
+++ tests/pyreverse/test_printer.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2021 Andreas Finkler <andi.finkler@gmail.com>
 
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# For details: https://github.com/PyCQA/pylint/blob/master/LICENSE
+# For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
 
 from typing import Type
 
