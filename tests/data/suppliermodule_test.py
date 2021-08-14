09808ba6fa71368895aeca9e54b6e1fd77c5fb06
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2021-08-14 17:40:22 +0200

    Add new test class to check type annotations

--- tests/data/suppliermodule_test.py
+++ tests/data/suppliermodule_test.py
@@ -10,3 +10,11 @@ class Interface:
 class DoNothing: pass
 
 class DoNothing2: pass
+
+class DoSomething:
+    def __init__(self, a_string: str, optional_int: int = None):
+        self.my_string = a_string
+        self.my_int = optional_int
+
+    def do_it(self, new_int: int) -> int:
+        return self.my_int + new_int
