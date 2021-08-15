254824ee04d4d32c0570c82c0621a83381595263
David Liu <david@cs.toronto.edu>
2021-08-15 13:08:45 -0400

    Additional tests for PyCQA/astroid#1126

--- tests/functional/t/too/too_many_instance_attributes.py
+++ tests/functional/t/too/too_many_instance_attributes.py
@@ -1,4 +1,6 @@
 # pylint: disable=missing-docstring, too-few-public-methods, useless-object-inheritance
+from dataclasses import dataclass, InitVar
+
 
 class Aaaa(object): # [too-many-instance-attributes]
 
@@ -24,3 +26,20 @@ class Aaaa(object): # [too-many-instance-attributes]
         self._iiii = 9
         self._jjjj = 10
         self.tomuch = None
+
+
+# InitVars should not count as instance attributes (see issue #3754)
+# Default max_instance_attributes is 7
+@dataclass
+class Hello:
+    a_1: int
+    a_2: int
+    a_3: int
+    a_4: int
+    a_5: int
+    a_6: int
+    a_7: int
+    a_8: InitVar[int]
+
+    def __post_init__(self, a_8):
+        self.a_1 += a_8
