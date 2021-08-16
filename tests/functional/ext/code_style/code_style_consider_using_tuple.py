1b41bc1e4c62b3d8574aeef8b9ff55aaffc6fb48
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-08-16 14:48:48 +0200

    Add new test cases

--- tests/functional/ext/code_style/code_style_consider_using_tuple.py
+++ tests/functional/ext/code_style/code_style_consider_using_tuple.py
@@ -29,3 +29,31 @@ for x in [2, *var]:  # [consider-using-tuple]
 # Don't emit warning for sets as this is handled by builtin checker
 (x for x in {1, 2, 3})  # [use-sequence-for-iteration]
 [x for x in {*var, 2}]  # [use-sequence-for-iteration]
+
+
+# -----
+# Suggest tuple for `in` comparisons
+x in var
+x in {1, 2, 3}
+x in (1, 2, 3)
+x in [1, 2, 3]  # [consider-using-tuple]
+
+if x in var:
+    pass
+if x in {1, 2, 3}:
+    pass
+if x in (1, 2, 3):
+    pass
+if x in [1, 2, 3]:  # [consider-using-tuple]
+    pass
+
+42 if x in [1, 2, 3] else None  # [consider-using-tuple]
+assert x in [1, 2, 3]  # [consider-using-tuple]
+(x for x in var if x in [1, 2, 3])  # [consider-using-tuple]
+while x in [1, 2, 3]:  # [consider-using-tuple]
+    break
+
+# Stacked operators, rightmost pair is evaluated first
+# Doesn't make much sense in practice since `in` will only return `bool`
+True == x in [1, 2, 3]  # [consider-using-tuple]  # noqa: E712
+1 >= x in [1, 2, 3]  # [consider-using-tuple]  # noqa: E712
