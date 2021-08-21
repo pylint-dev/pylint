54a9d5b76ab4deffc6066b3bd24af1effb00bd1d
Ashley Whetter <ashley@awhetter.co.uk>
2021-08-20 21:11:32 -0700

    Bump pylint to 2.10.2-dev0, update changelog

--- tests/pyreverse/test_utils.py
+++ tests/pyreverse/test_utils.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2021 Ashley Whetter <ashley@awhetter.co.uk>
 # Copyright (c) 2021 Pierre Sassoulas <pierre.sassoulas@gmail.com>
 # Copyright (c) 2021 Marc Mueller <30130371+cdce8p@users.noreply.github.com>
 # Copyright (c) 2021 Mark Byrne <31762852+mbyrnepr2@users.noreply.github.com>
