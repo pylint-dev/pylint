3915b6c68a25c617faf1d16d72553f14ab9c5d93
Ashley Whetter <ashley@awhetter.co.uk>
2021-08-20 20:56:36 -0700

    Bump pylint to 2.10.1, update changelog

--- pylint/pyreverse/dot_printer.py
+++ pylint/pyreverse/dot_printer.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2021 Pierre Sassoulas <pierre.sassoulas@gmail.com>
 # Copyright (c) 2021 Nick Drozd <nicholasdrozd@gmail.com>
 # Copyright (c) 2021 Marc Mueller <30130371+cdce8p@users.noreply.github.com>
 # Copyright (c) 2021 Andreas Finkler <andi.finkler@gmail.com>
