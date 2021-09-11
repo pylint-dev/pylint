7390b6fd0b2f80a1d3dc09327f959bffff5a94b0
SupImDos <62866982+SupImDos@users.noreply.github.com>
2021-09-11 13:31:12 +0800

    Fix no-self-use and docparams extension for async functions/methods (#4986)
    * Fixed 'no-self-check' for async functions.
    
    * fixed 'docparams' for async functions.
    
    * Added 'no-self-use' to the pylint disable comment in broken test.
    
    * Added small changelog entry to Pylint 2.11.0 milestone.
    
    * Added test: 'no-self-use' for async method.
    
    * Added test: revamped 'docparams' functional tests to test all missing documentation.
    
    Co-authored-by: Marc Mueller <30130371+cdce8p@users.noreply.github.com>
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- pylint/checkers/classes.py
+++ pylint/checkers/classes.py
@@ -1430,6 +1430,8 @@ a metaclass class method.",
             ):
                 self.add_message("no-self-use", node=node)
 
+    leave_asyncfunctiondef = leave_functiondef
+
     def visit_attribute(self, node: nodes.Attribute) -> None:
         """check if the getattr is an access to a class member
         if so, register it. Also check for access to protected
