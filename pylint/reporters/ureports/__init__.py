365128acf0e7bb67bace85512eca61d53265bcc1
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-09-14 15:54:20 +0200

    Update pylint/reporters/ureports/__init__.py
    Co-authored-by: Marc Mueller <30130371+cdce8p@users.noreply.github.com>

--- pylint/reporters/ureports/__init__.py
+++ pylint/reporters/ureports/__init__.py
@@ -1,6 +1,6 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
 
-__all__ = ["BaseWriter"]
+__all__ = ("BaseWriter",)
 
 from pylint.reporters.ureports.base_writer import BaseWriter
