0eb8cb5a4c4704451eb217eb004b9a9c348244ee
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-09-14 10:31:58 +0200

    Upgrade tbump and the documentation for 3.0.0 alpha release

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -9,7 +9,7 @@ It's updated via tbump, do not modify.
 
 from __future__ import annotations
 
-__version__ = "2.15.0-dev0"
+__version__ = "3.0.0-a5"
 
 
 def get_numversion_from_version(v: str) -> tuple[int, int, int]:
