76a487fa5abca6f5c166b41df9d64b66ef6ee75a
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-09-16 22:44:42 +0200

    Bump pylint to 2.11.1, update changelog

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -2,7 +2,7 @@
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
 from typing import Tuple
 
-__version__ = "2.11.1-dev0"
+__version__ = "2.11.1"
 
 
 def get_numversion_from_version(v: str) -> Tuple:
