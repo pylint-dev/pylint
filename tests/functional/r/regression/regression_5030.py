3dd1818d8f630c94ca34a918d7ceddcdf7c6e468
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-09-17 16:39:19 +0200

    Add tests for ``ClassDef`` inference regression Closes #5030 Closes #5036

---
+++ tests/functional/r/regression/regression_5030.py
@@ -0,0 +1,30 @@
+"""Regression in astroid on ClassDef inference with two test cases.
+Fixed in https://github.com/PyCQA/astroid/pull/1181"""
+
+from typing import Tuple, Type
+from typing import Dict, List, Any
+from dataclasses import dataclass, field
+
+# Example 1
+def is_type_list(f_type: Type) -> bool:
+    """just here to show the issue"""
+    return f_type == list
+
+assert not is_type_list(Tuple)
+
+# Example 2
+@dataclass
+class SomeData:
+    """A dataclass."""
+    a_dict: Dict[str, List[str]] = field(default_factory=dict)
+
+
+@dataclass
+class SubSomeData(SomeData):
+    """A subclass of a dataclass."""
+
+    def __init__(self, **kwargs: Dict[str, Any]) -> None:
+        """Subclass init func."""
+        super().__init__(**kwargs)
+        if "test" in self.a_dict:
+            print(self.a_dict["test"])
