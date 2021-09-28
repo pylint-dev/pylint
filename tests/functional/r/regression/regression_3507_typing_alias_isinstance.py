1cb10a9d57f55f8a4bdd1660e7a3ca0d265ff1d1
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2021-09-28 22:52:46 +0200

    Add regression test for issue 3507
    * https://github.com/PyCQA/pylint/issues/3507

---
+++ tests/functional/r/regression/regression_3507_typing_alias_isinstance.py
@@ -0,0 +1,24 @@
+"""
+https://github.com/PyCQA/pylint/issues/3507
+False-positive 'isinstance-second-argument-not-valid-type'
+for typing aliases in 'isinstance' calls.
+"""
+import collections
+import collections.abc
+import typing
+
+isinstance(42, typing.Dict)
+isinstance(42, typing.Counter)
+isinstance(42, typing.Collection)
+isinstance(42, typing.Iterator)
+isinstance(42, typing.Tuple)
+isinstance(42, typing.Callable)
+
+
+# For comparison - also valid calls
+isinstance(42, dict)
+isinstance(42, collections.Counter)
+isinstance(42, collections.abc.Collection)
+isinstance(42, collections.abc.Iterator)
+isinstance(42, tuple)
+isinstance(42, collections.abc.Callable)
