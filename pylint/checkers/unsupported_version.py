6bfa6c0753842596285b7e2087f3db514e6e7934
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-10-13 11:46:57 +0200

    Code review

--- pylint/checkers/unsupported_version.py
+++ pylint/checkers/unsupported_version.py
@@ -12,6 +12,7 @@ from astroid import nodes
 from pylint.checkers import BaseChecker
 from pylint.checkers.utils import check_messages
 from pylint.interfaces import IAstroidChecker
+from pylint.lint import PyLinter
 from pylint.utils import get_global_option
 
 
@@ -34,24 +35,15 @@ class UnsupportedVersionChecker(BaseChecker):
     def open(self) -> None:
         """Initialize visit variables and statistics."""
         py_version = get_global_option(self, "py-version")
-        self._py35_plus = py_version >= (3, 5)
         self._py36_plus = py_version >= (3, 6)
-        self._py37_plus = py_version >= (3, 7)
-        self._py37_plus = py_version >= (3, 8)
-        self._py39_plus = py_version >= (3, 9)
-        self._py310_plus = py_version >= (3, 10)
 
     @check_messages("using-f-string-in-unsupported-version")
     def visit_joinedstr(self, node: nodes.JoinedStr) -> None:
         """Check f-strings"""
-        self._check_unsupported_version(node)
-
-    def _check_unsupported_version(self, node: nodes.JoinedStr) -> None:
-        """Check if f-strings are used while < 3.6 is supported"""
         if not self._py36_plus:
             self.add_message("using-f-string-in-unsupported-version", node=node)
 
 
 def register(linter: PyLinter) -> None:
-    """required method to auto register this checker"""
+    """Required method to auto register this checker"""
     linter.register_checker(UnsupportedVersionChecker(linter))
