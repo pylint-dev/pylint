a65ce1cfd955edd003df93e22e75982736d4d713
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-10-17 09:55:40 +0200

    Fix expected output in functional tests following disable

--- tests/functional/b/bad_reversed_sequence_py37.py
+++ tests/functional/b/bad_reversed_sequence_py37.py
@@ -1,9 +1,12 @@
 """ Dictionaries are reversible starting on python 3.8"""
 
 # pylint: disable=missing-docstring
+
 reversed({'a': 1, 'b': 2}) # [bad-reversed-sequence]
+
 
 class InheritDict(dict):
     pass
+
 
 reversed(InheritDict({'a': 1, 'b': 2})) # [bad-reversed-sequence]
