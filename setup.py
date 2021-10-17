384b93ad04934f09e1e36c2391ed2ced949692b4
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-10-17 11:38:07 +0200

    Apply suggestions from code review
    Co-authored-by: Marc Mueller <30130371+cdce8p@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 
 class PylintIncompatiblePythonError(Exception):
-    def __init__(self, version_info: "sys._version_info" = sys.version_info) -> None:
+    def __init__(self) -> None:
         super().__init__(
             "The last version compatible with Python <= 3.6.2 is pylint '2.9.3'. "
-            f"You're using {'.'.join([str(v) for v in version_info[:3]])}. "
+            f"You're using {'.'.join([str(v) for v in sys.version_info[:3]])}. "
             "Please install pylint 2.9.3 explicitly or upgrade your python interpreter "
             "to at least 3.6.2. Remember that Python 3.6 end life is December 2021. "
             "See https://github.com/PyCQA/pylint/issues/5065 for more detail."
