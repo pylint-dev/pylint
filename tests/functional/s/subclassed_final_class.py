4f1df3367a4dc553f006a7803a48d3ef7626d35b
Mark Byrne <mbyrnepr2@gmail.com>
2021-10-17 10:56:45 +0200

    Typo - Add comma

--- tests/functional/s/subclassed_final_class.py
+++ tests/functional/s/subclassed_final_class.py
@@ -1,7 +1,7 @@
 """A class decorated with typing.final cannot be subclassed """
 
 # pylint: disable=no-init, import-error, invalid-name, using-constant-test, useless-object-inheritance
-# pylint: disable=missing-docstring, too-few-public-methods, no-name-in-module using-final-in-unsupported-version
+# pylint: disable=missing-docstring, too-few-public-methods, no-name-in-module, using-final-in-unsupported-version
 
 from typing import final
 
