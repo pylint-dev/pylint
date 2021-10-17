25272fd88f0884dce319559726b8daf15cda51fd
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-10-17 11:28:03 +0200

    Apply suggestions from code review
    Co-authored-by: Marc Mueller <30130371+cdce8p@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ from setuptools import setup
 
 
 class PylintIncompatiblePythonError(Exception):
-    def __init__(self, major, minor, patch):
+    def __init__(self, version_info: sys._version_info) -> None:
         super().__init__(
-            f"The last version compatible with python <= 3.6.2 is pylint '2.9.3'."
-            f"You're using {major}.{minor}.{patch}. "
+            "The last version compatible with Python <= 3.6.2 is pylint '2.9.3'. "
+            f"You're using {'.'.join(map(str, version_info[:3]))}. "
             "Please install pylint 2.9.3 explicitly or upgrade your python interpreter "
-            "to at least 3.6.2. Remember that python 3.6 end life is December 2021."
+            "to at least 3.6.2. Remember that Python 3.6 end life is December 2021. "
             "See https://github.com/PyCQA/pylint/issues/5065 for more detail."
         )
 
 
-version = sys.version_info[:3]
-if version < (3, 6, 2):
-    raise PylintIncompatiblePythonError(*version)
+if sys.version_info < (3, 6, 2):
+    raise PylintIncompatiblePythonError(sys.version_info)
 
 setup()
