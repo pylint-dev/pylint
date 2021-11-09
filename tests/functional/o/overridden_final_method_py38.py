57515ae304c9e4bae6a94f08ea7ef2d6fadc61ce
Mark Byrne <31762852+mbyrnepr2@users.noreply.github.com>
2021-11-09 12:51:34 +0100

    Add checker `using-final-decorator-in-unsupported-version` (#5165)
    * Add checker `using-final-in-unsupported-version`
    
    This is one of the tasks in issue: #5134
    
    Also:
    - Ensure the existing checkers for `typing.final` are used irrespective of Python
      version
    
    * Emit `using-final-in-unsupported-version` warning when Python version < 3.8
    and none of the other `typing.final`-related warnings
    
    * Add `uninferable_final_decorators`
    
     Return any `typing.final` decorators for a given `Decorators` node.
     Used to determine if this decorator is used with a version of Python in
     which it is unsupported.
    
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- tests/functional/o/overridden_final_method_py38.py
+++ tests/functional/o/overridden_final_method_py38.py
@@ -1,8 +1,7 @@
 """Since Python version 3.8, a method decorated with typing.final cannot be
 overridden"""
 
-# pylint: disable=no-init, import-error, invalid-name, using-constant-test, useless-object-inheritance
-# pylint: disable=missing-docstring, too-few-public-methods
+# pylint: disable=no-init, useless-object-inheritance, missing-docstring, too-few-public-methods
 
 from typing import final
 
