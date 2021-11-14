38ca701befe32de73347b8f4edc550083d76bd08
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-14 16:58:10 +0200

    Update tests/functional/m/misplaced_comparison_constant.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- tests/functional/m/misplaced_comparison_constant.py
+++ tests/functional/m/misplaced_comparison_constant.py
@@ -40,6 +40,8 @@ def double_comparison():
     for i in range(10):
         if i == 1 == 2:
             pass
+        if 2 <= i <= 8:
+            print("Between 2 and 8 inclusive")
 
 def const_comparison():
     """Check that we return early for comparison of two constants"""
