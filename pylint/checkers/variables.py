c1c727577fdd2c7db6309a53f1894a37fe315753
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-26 10:29:24 +0100

    De-dent an else statement

--- pylint/checkers/variables.py
+++ pylint/checkers/variables.py
@@ -1199,32 +1199,29 @@ class VariablesChecker(BaseChecker):
                     )
                     return (VariableVisitConsumerAction.CONSUME, found_nodes)
 
-            if base_scope_type == "lambda":
-                # E0601 can occur in class-level scope in lambdas, as in
-                # the following example:
-                #   class A:
-                #      x = lambda attr: f + attr
-                #      f = 42
-                if isinstance(frame, nodes.ClassDef) and node.name in frame.locals:
-                    if isinstance(node.parent, nodes.Arguments):
-                        if stmt.fromlineno <= defstmt.fromlineno:
-                            # Doing the following is fine:
-                            #   class A:
-                            #      x = 42
-                            #      y = lambda attr=x: attr
-                            self.add_message(
-                                "used-before-assignment",
-                                args=node.name,
-                                node=node,
-                            )
-                    else:
+            # E0601 can occur in class-level scope in lambdas, as in
+            # the following example:
+            #   class A:
+            #      x = lambda attr: f + attr
+            #      f = 42
+            if isinstance(frame, nodes.ClassDef) and node.name in frame.locals:
+                if isinstance(node.parent, nodes.Arguments):
+                    if stmt.fromlineno <= defstmt.fromlineno:
+                        # Doing the following is fine:
+                        #   class A:
+                        #      x = 42
+                        #      y = lambda attr=x: attr
                         self.add_message(
-                            "undefined-variable", args=node.name, node=node
+                            "used-before-assignment",
+                            args=node.name,
+                            node=node,
                         )
-                        return (VariableVisitConsumerAction.CONSUME, found_nodes)
-                elif current_consumer.scope_type == "lambda":
-                    self.add_message("undefined-varibale", args=node.name, node=node)
+                else:
+                    self.add_message("undefined-variable", args=node.name, node=node)
                     return (VariableVisitConsumerAction.CONSUME, found_nodes)
+            elif current_consumer.scope_type == "lambda":
+                self.add_message("undefined-varibale", args=node.name, node=node)
+                return (VariableVisitConsumerAction.CONSUME, found_nodes)
 
         elif self._is_only_type_assignment(node, defstmt):
             self.add_message("undefined-variable", args=node.name, node=node)
