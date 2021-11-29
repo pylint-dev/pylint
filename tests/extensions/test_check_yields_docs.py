89380a8c4d7e2ce434671bd600e0612fab80db5e
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-29 16:56:58 +0100

    Move test 3

--- tests/extensions/test_check_yields_docs.py
+++ tests/extensions/test_check_yields_docs.py
@@ -15,8 +15,6 @@
 `DocstringChecker` in :mod:`pylint.extensions.check_docs`
 """
 
-# pylint: disable=too-many-public-methods
-
 import astroid
 
 from pylint.extensions.docparams import DocstringParameterChecker
@@ -27,17 +25,6 @@ class TestDocstringCheckerYield(CheckerTestCase):
     """Tests for pylint_plugin.RaiseDocChecker"""
 
     CHECKER_CLASS = DocstringParameterChecker
-
-    def test_ignores_unknown_style(self) -> None:
-        yield_node = astroid.extract_node(
-            '''
-        def my_func(self):
-            """This is a docstring."""
-            yield False #@
-        '''
-        )
-        with self.assertNoMessages():
-            self.checker.visit_yield(yield_node)
 
     @set_config(accept_no_yields_doc="no")
     def test_warn_partial_sphinx_yields(self) -> None:
