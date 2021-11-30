38cd0761884d886becd20526cf98d3a6981c1c9c
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-30 09:16:09 +0100

    Move test

--- tests/functional/ext/docparams/raise/missing_raises_doc_Google.py
+++ tests/functional/ext/docparams/raise/missing_raises_doc_Google.py
@@ -1,6 +1,6 @@
 """Tests for missing-raises-doc and missing-raises-type-doc for Google style docstrings"""
 # pylint: disable=function-redefined, invalid-name, undefined-variable, missing-function-docstring
-# pylint: disable=unused-argument
+# pylint: disable=unused-argument, import-outside-toplevel
 
 
 def test_find_missing_google_raises(self):  # [missing-raises-doc]
@@ -11,3 +11,14 @@ def test_find_missing_google_raises(self):  # [missing-raises-doc]
     """
     raise RuntimeError("hi")
     raise NameError("hi")  # [unreachable]
+
+
+def test_find_google_attr_raises_exact_exc(self):
+    """This is a google docstring.
+
+    Raises:
+        re.error: Sometimes
+    """
+    import re
+
+    raise re.error("hi")
