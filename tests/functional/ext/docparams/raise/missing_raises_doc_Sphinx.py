4adb3546226effb45389e12184f92bea81423be5
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-30 09:21:54 +0100

    Move test

--- tests/functional/ext/docparams/raise/missing_raises_doc_Sphinx.py
+++ tests/functional/ext/docparams/raise/missing_raises_doc_Sphinx.py
@@ -10,3 +10,14 @@ def test_find_missing_sphinx_raises(self):  # [missing-raises-doc]
     """
     raise RuntimeError("hi")
     raise NameError("hi")  # [unreachable]
+
+
+def test_ignore_spurious_sphinx_raises(self):
+    """This is a docstring.
+
+    :raises RuntimeError: Always
+    :except NameError: Never
+    :raise OSError: Never
+    :exception ValueError: Never
+    """
+    raise RuntimeError("Blah")
