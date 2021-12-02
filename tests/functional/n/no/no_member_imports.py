e4b23c1ce829b08563d2e6d151b8812b8749da1c
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-12-02 17:51:55 +0100

    Do not use collecitons for check

--- tests/functional/n/no/no_member_imports.py
+++ tests/functional/n/no/no_member_imports.py
@@ -5,9 +5,9 @@
 
 def test_no_member_in_getattr():
     """Make sure that a module attribute access is checked by pylint."""
-    import collections
+    import math
 
-    collections.THIS_does_not_EXIST  # [no-member]
+    math.THIS_does_not_EXIST  # [no-member]
 
 
 def test_no_member_in_getattr_ignored() -> None:
