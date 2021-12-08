c638e65feb43f8783db69907f8cef55bcc5f2eb7
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-12-08 15:03:35 +0100

    Create files before move

---
+++ tests/functional/ext/docparams/parameter/missing_param_doc_required_Numpy.py
@@ -0,0 +1,6 @@
+"""Tests for missing-param-doc and missing-type-doc for Numpy style docstrings
+with accept-no-param-doc = no
+"""
+# pylint: disable=invalid-name, unused-argument, undefined-variable
+# pylint: disable=line-too-long, too-few-public-methods, missing-class-docstring
+# pylint: disable=missing-function-docstring, function-redefined, inconsistent-return-statements
