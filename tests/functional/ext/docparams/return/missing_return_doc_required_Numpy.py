f8b23eb9cccf4bda452bf56dc1c71b8a046e571b
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-12-12 23:26:43 +0100

    Move ``Numpy`` tests from ``TestParamDocChecker`` to functional tests (#5507)

--- tests/functional/ext/docparams/return/missing_return_doc_required_Numpy.py
+++ tests/functional/ext/docparams/return/missing_return_doc_required_Numpy.py
@@ -1,7 +1,7 @@
 """Tests for missing-return-doc and missing-return-type-doc for Numpy style docstrings
 with accept-no-returns-doc = no"""
 # pylint: disable=function-redefined, invalid-name, undefined-variable, missing-function-docstring
-# pylint: disable=unused-argument
+# pylint: disable=unused-argument, too-few-public-methods
 
 
 def my_func(self, doc_type):  # [missing-return-doc]
@@ -38,3 +38,60 @@ def my_func(self):  # [missing-return-doc]
         list(:class:`mymodule.Class`)
     """
     return [mymodule.Class()]
+
+
+class Foo:
+    """test_finds_missing_property_return_type_numpy
+    Example of a property having return documentation in
+    a numpy style docstring
+    """
+
+    @property
+    def foo_prop(self):  # [missing-return-type-doc]
+        """docstring ...
+
+        Raises
+        ------
+        RuntimeError
+            Always
+        """
+        raise RuntimeError()
+        return 10  # [unreachable]
+
+
+class Foo:
+    """test_ignores_non_property_return_type_numpy
+    Example of a class function trying to use `type` as return
+    documentation in a numpy style docstring
+    """
+
+    def foo_method(self):  # [missing-return-doc, missing-return-type-doc]
+        """int: docstring ...
+
+        Raises
+        ------
+        RuntimeError
+            Always
+        """
+        print(self)
+        raise RuntimeError()
+        return 10  # [unreachable]
+
+
+class Foo:
+    """test_non_property_annotation_return_type_numpy
+    Example of a class function trying to use `type` as return
+    documentation in a numpy style docstring
+    """
+
+    def foo_method(self) -> int:  # [missing-return-doc]
+        """int: docstring ...
+
+        Raises
+        ------
+        RuntimeError
+            Always
+        """
+        print(self)
+        raise RuntimeError()
+        return 10  # [unreachable]
