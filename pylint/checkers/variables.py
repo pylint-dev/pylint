0996ee9e541e13c4817274968e43505389746dce
Jacob Walls <jacobtylerwalls@gmail.com>
2021-12-12 19:36:21 -0500

    Don't promise changes from other PR

--- pylint/checkers/variables.py
+++ pylint/checkers/variables.py
@@ -408,8 +408,7 @@ MSGS = {
         "Emitted when a local variable is accessed before its assignment took place. "
         "Assignments in try blocks are assumed not to have occurred when evaluating "
         "associated except/finally blocks. Assignments in except blocks are assumed "
-        "not to have occurred, except when the associated try block contains a return "
-        "statement.",
+        "not to have occurred when evaluating statements outside the block.",
     ),
     "E0602": (
         "Undefined variable %r",
