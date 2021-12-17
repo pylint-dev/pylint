ddd2123e09f97ef91d5745e9790d6f24a5493156
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-12-17 23:57:45 +0100

    Fix crash on uninferable decorators on Python 3.6 and 3.7 (#5549)
    Co-authored-by: Mark Byrne <31762852+mbyrnepr2@users.noreply.github.com>

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -876,7 +876,11 @@ def uninferable_final_decorators(
             except AttributeError:
                 continue
         elif isinstance(decorator, nodes.Name):
-            import_node = decorator.lookup(decorator.name)[1][0]
+            lookup_values = decorator.lookup(decorator.name)
+            if lookup_values[1]:
+                import_node = lookup_values[1][0]
+            else:
+                continue  # pragma: no cover # Covered on Python < 3.8
         else:
             continue
 
