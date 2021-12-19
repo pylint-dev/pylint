8f06aed9ccb56199b0ff29c4f521af923a72c354
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-12-19 23:48:37 +0100

    Patch copy under python < 3.8
    See https://stackoverflow.com/a/56935186

--- pylint/testutils/configuration_test.py
+++ pylint/testutils/configuration_test.py
@@ -5,17 +5,24 @@
 import copy
 import json
 import logging
+import re
 import unittest
 from pathlib import Path
 from typing import Any, Dict, List, Tuple, Union
 from unittest.mock import Mock
 
+from pylint.constants import PY38_PLUS
 from pylint.lint import Run
 
 # We use Any in this typing because the configuration contains real objects and constants
 # that could be a lot of things.
 ConfigurationValue = Any
 PylintConfiguration = Dict[str, ConfigurationValue]
+
+
+if not PY38_PLUS:
+    # See https://stackoverflow.com/a/56935186
+    copy._deepcopy_dispatch[type(re.compile(""))] = lambda r, _: r  # type: ignore[attr-defined]
 
 
 def get_expected_or_default(
