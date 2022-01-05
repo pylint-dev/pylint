29997f81b05b60b6c9749f50e445de2b94e25ed4
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-01-06 00:03:41 +0100

    Move ``HUMAN_READABLE_TYPES`` to ``constants`` (#5642)
    Ref #5311
    
    Co-authored-by: Carli* Freudenberg <carli.freudenberg@energymeteo.de>

--- pylint/constants.py
+++ pylint/constants.py
@@ -55,3 +55,17 @@ class WarningScope:
 full_version = f"""pylint {__version__}
 astroid {astroid.__version__}
 Python {sys.version}"""
+
+HUMAN_READABLE_TYPES = {
+    "module": "module",
+    "const": "constant",
+    "class": "class",
+    "function": "function",
+    "method": "method",
+    "attr": "attribute",
+    "argument": "argument",
+    "variable": "variable",
+    "class_attribute": "class attribute",
+    "class_const": "class constant",
+    "inlinevar": "inline iteration",
+}
