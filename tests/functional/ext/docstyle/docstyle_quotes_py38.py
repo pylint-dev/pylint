afc07b89da4d7aca95c9d249becef3c449631dc8
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-01-11 20:06:23 +0100

    Update tests

--- tests/functional/ext/docstyle/docstyle_quotes_py38.py
+++ tests/functional/ext/docstyle/docstyle_quotes_py38.py
@@ -1,5 +1,5 @@
 """Checks of Dosctrings 'bad-docstring-quotes'"""
-# pylint: disable=docstring-first-line-empty,missing-class-docstring
+# pylint: disable=docstring-first-line-empty,missing-class-docstring, undefined-variable
 
 
 class FFFF:
@@ -16,3 +16,17 @@ class FFFF:
 
     def method4(self):  # [bad-docstring-quotes]
         ' """bad docstring 3 '
+
+    @check_messages("bad-open-mode", "redundant-unittest-assert", "deprecated-module")
+    def method5(self):
+        """Test OK 1 with decorators"""
+
+    def method6(self):
+        r"""Test OK 2 with raw string"""
+
+    def method7(self):
+        u"""Test OK 3 with unicode string"""
+
+
+def function2():
+    """Test Ok"""
