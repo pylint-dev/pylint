af974aa5498071baa73344a8ca84075b2e76305a
Jacob Walls <jacobtylerwalls@gmail.com>
2022-01-12 11:18:43 -0500

    Fix #5586: False positive for `used-before-assignment` with homonyms in filtered comprehensions and except blocks (#5666)
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- pylint/checkers/variables.py
+++ pylint/checkers/variables.py
@@ -1316,6 +1316,13 @@ class VariablesChecker(BaseChecker):
             if utils.is_func_decorator(current_consumer.node) or not (
                 current_consumer.scope_type == "comprehension"
                 and self._has_homonym_in_upper_function_scope(node, consumer_level)
+                # But don't catch homonyms against the filter of a comprehension,
+                # (like "if x" in "[x for x in expr() if x]")
+                # https://github.com/PyCQA/pylint/issues/5586
+                and not (
+                    isinstance(node.parent.parent, nodes.Comprehension)
+                    and node.parent in node.parent.parent.ifs
+                )
             ):
                 self._check_late_binding_closure(node)
                 self._loopvar_name(node)
