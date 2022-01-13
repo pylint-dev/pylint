faf0c849fd3c8da5ed8fd46e80a024ce4b668073
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-01-13 20:38:41 +0100

    Fix crash on properties & inherited methods in ``implicit-booleaness`` (#5652)

--- pylint/checkers/refactoring/implicit_booleaness_checker.py
+++ pylint/checkers/refactoring/implicit_booleaness_checker.py
@@ -1,9 +1,9 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
-from typing import List
+from typing import List, Union
 
 import astroid
-from astroid import nodes
+from astroid import bases, nodes
 
 from pylint import checkers, interfaces
 from pylint.checkers import utils
@@ -107,7 +107,7 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
         except astroid.InferenceError:
             # Probably undefined-variable, abort check
             return
-        mother_classes = self.base_classes_of_node(instance)
+        mother_classes = self.base_names_of_instance(instance)
         affected_by_pep8 = any(
             t in mother_classes for t in ("str", "tuple", "list", "set")
         )
@@ -165,7 +165,7 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
                 target_instance = utils.safe_infer(target_node)
                 if target_instance is None:
                     continue
-                mother_classes = self.base_classes_of_node(target_instance)
+                mother_classes = self.base_names_of_instance(target_instance)
                 is_base_comprehension_type = any(
                     t in mother_classes for t in ("tuple", "list", "dict", "set")
                 )
@@ -209,9 +209,13 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
                     )
 
     @staticmethod
-    def base_classes_of_node(instance: nodes.ClassDef) -> List[str]:
-        """Return all the classes names that a ClassDef inherit from including 'object'."""
-        try:
-            return [instance.name] + [x.name for x in instance.ancestors()]
-        except TypeError:
-            return [instance.name]
+    def base_names_of_instance(
+        node: Union[bases.Uninferable, bases.Instance]
+    ) -> List[str]:
+        """Return all names inherited by a class instance or those returned by a function.
+
+        The inherited names include 'object'.
+        """
+        if isinstance(node, bases.Instance):
+            return [node.name] + [x.name for x in node.ancestors()]
+        return []
