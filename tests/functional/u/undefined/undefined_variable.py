a6a10e50fa63e46bbb649627f3891834458afab9
Jacob Walls <jacobtylerwalls@gmail.com>
2022-01-16 14:16:38 -0500

    Fix #5568: Allow nested classes as return annotations (#5688)
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- tests/functional/u/undefined/undefined_variable.py
+++ tests/functional/u/undefined/undefined_variable.py
@@ -425,3 +425,18 @@ def typing_and_value_assignment_with_tuple_assignment():
     var_one, var_two = 1, 1
     print(var_one)
     print(var_two)
+
+
+def nested_class_as_return_annotation():
+    """A namedtuple as a class attribute is used as a return annotation
+
+    Taken from https://github.com/PyCQA/pylint/issues/5568"""
+    from collections import namedtuple
+
+    class MyObject:
+        Coords = namedtuple('Point', ['x', 'y'])
+
+        def my_method(self) -> Coords:
+            pass
+
+    print(MyObject)
