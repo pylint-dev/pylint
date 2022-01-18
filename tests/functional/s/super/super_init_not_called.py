89915e1332c6aabf47890d93e72ba8219c077623
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-01-18 17:04:15 +0100

    Apply suggestions from code review
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- tests/functional/s/super/super_init_not_called.py
+++ tests/functional/s/super/super_init_not_called.py
@@ -8,11 +8,12 @@ from typing import Protocol
 from typing_extensions import Protocol as ExtensionProtocol
 
 
-# This should not emit a super-init-not-called warning. It previously did this, because
-# ``next(node.infer())`` was used in that checker's logic and the first inferred node
-# was an Uninferable object, leading to this false positive.
 class Foo(ctypes.BigEndianStructure):
-    """A class"""
+    """This class should not emit a super-init-not-called warning.
+    
+    It previously did, because ``next(node.infer())`` was used in that checker's logic
+    and the first inferred node was an Uninferable object, leading to this false positive.
+    """
 
     def __init__(self):
         ctypes.BigEndianStructure.__init__(self)
