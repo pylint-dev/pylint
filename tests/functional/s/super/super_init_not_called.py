6977e48ab9399ce9d953ea86fb480d362d9cb6db
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-01-24 20:19:17 +0100

    Fix ``super-init-not-called`` if parent or ``self`` is a ``Protocol`` (#5697)
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- tests/functional/s/super/super_init_not_called.py
+++ tests/functional/s/super/super_init_not_called.py
@@ -1,13 +1,22 @@
-"""This should not emit a super-init-not-called warning. It previously did this, because
-``next(node.infer())`` was used in that checker's logic and the first inferred node
-was an Uninferable object, leading to this false positive."""
+"""Tests for super-init-not-called."""
 # pylint: disable=too-few-public-methods
 
 import ctypes
 
 
 class Foo(ctypes.BigEndianStructure):
-    """A class"""
+    """This class should not emit a super-init-not-called warning.
+
+    It previously did, because ``next(node.infer())`` was used in that checker's logic
+    and the first inferred node was an Uninferable object, leading to this false positive.
+    """
 
     def __init__(self):
         ctypes.BigEndianStructure.__init__(self)
+
+
+class UninferableChild(UninferableParent):  # [undefined-variable]
+    """An implementation that test if we don't crash on uninferable parents."""
+
+    def __init__(self):
+        ...
