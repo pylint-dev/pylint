8c7c38d782102f31f250cc1bf0d4b13fcba92c8d
Jacob Walls <jacobtylerwalls@gmail.com>
2022-01-27 10:58:48 -0500

    Fix crash when providing None to mode arg of open()

--- pylint/checkers/stdlib.py
+++ pylint/checkers/stdlib.py
@@ -671,7 +671,7 @@ class StdlibChecker(DeprecatedMixin, BaseChecker):
         if (
             not mode_arg
             or isinstance(mode_arg, nodes.Const)
-            and "b" not in mode_arg.value
+            and (not mode_arg.value or "b" not in mode_arg.value)
         ):
             encoding_arg = None
             try:
