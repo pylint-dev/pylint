59f5524dc4e045ed15073e99df3671744aee8067
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-01-27 09:45:25 +0100

    Update tests/extensions/test_check_docs_utils.py

--- tests/extensions/test_check_docs_utils.py
+++ tests/extensions/test_check_docs_utils.py
@@ -12,7 +12,7 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
 
-"""Unit tests utils functions in :mod:`pylint.extensions._check_docs_utils`."""
+"""Unit tests for utils functions in :mod:`pylint.extensions._check_docs_utils`."""
 import astroid
 import pytest
 
