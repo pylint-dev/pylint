ef7085a199f793c0f7d3d91afcf4a9dbee81746b
Jacob Walls <jacobtylerwalls@gmail.com>
2022-02-04 12:14:54 -0500

    Remove Return True

--- pylint/checkers/variables.py
+++ pylint/checkers/variables.py
@@ -756,10 +756,11 @@ scope_type : {self._atomic.scope_type}
                     if isinstance(target, nodes.AssignName) and target.name == name:
                         return True
                     if isinstance(target, (nodes.Tuple, nodes.List)):
-                        for elt in utils.get_all_elements(target):
-                            if isinstance(elt, nodes.Const) and elt.value == name:
-                                return True
-                return True
+                        if any(
+                            isinstance(elt, nodes.Const) and elt.value == name
+                            for elt in utils.get_all_elements(target)
+                        ):
+                            return True
             if isinstance(stmt, nodes.If):
                 if (
                     isinstance(stmt.test, nodes.NamedExpr)
