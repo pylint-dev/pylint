d2cf57eb0c4fe79f5593bec31566668ec491bb62
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-02-26 12:06:26 +0100

    Take review into account

--- tests/message/test_no_removed_msgid_or_symbol_used.py
+++ tests/message/test_no_removed_msgid_or_symbol_used.py
@@ -1,14 +1,11 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
-from typing import TYPE_CHECKING
 
 from pylint.constants import DELETED_MESSAGES
-
-if TYPE_CHECKING:
-    from pylint.lint import PyLinter
+from pylint.lint import PyLinter
 
 
-def test_no_removed_msgid_or_symbol_used(linter: "PyLinter") -> None:
+def test_no_removed_msgid_or_symbol_used(linter: PyLinter) -> None:
     """Tests that we're not using deleted msgid or symbol.
 
     This could cause occasional bugs, but more importantly confusion and inconsistencies
