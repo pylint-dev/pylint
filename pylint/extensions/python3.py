c946208be4072c0478ddce8989b9d845d1bdc195
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-02-26 13:24:56 +0100

    Adaptation for python3 and add typing and changelog

--- pylint/extensions/python3.py
+++ pylint/extensions/python3.py
@@ -7,33 +7,34 @@ are still useful in python3 after all.
 See https://github.com/PyCQA/pylint/issues/5025
 """
 
+from astroid import nodes
 
 from pylint import checkers, interfaces
 from pylint.checkers import utils
+from pylint.lint import PyLinter
 
 
 class Python3Checker(checkers.BaseChecker):
 
     __implements__ = interfaces.IAstroidChecker
-    enabled = True
+    enabled = False
     name = "python3"
 
     msgs = {
         "W1641": (
             "Implementing __eq__ without also implementing __hash__",
             "eq-without-hash",
-            "Used when a class implements __eq__ but not __hash__.  In Python 2, objects "
-            "get object.__hash__ as the default implementation, in Python 3 objects get "
+            "Used when a class implements __eq__ but not __hash__. Objects get "
             "None as their default __hash__ implementation if they also implement __eq__.",
         ),
     }
 
     @utils.check_messages("eq-without-hash")
-    def visit_classdef(self, node):
+    def visit_classdef(self, node: nodes.ClassDef) -> None:
         locals_and_methods = set(node.locals).union(x.name for x in node.mymethods())
         if "__eq__" in locals_and_methods and "__hash__" not in locals_and_methods:
             self.add_message("eq-without-hash", node=node)
 
 
-def register(linter):
+def register(linter: PyLinter) -> None:
     linter.register_checker(Python3Checker(linter))
