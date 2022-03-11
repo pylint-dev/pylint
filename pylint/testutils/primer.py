52d15e4fe370f2114d813b8ba260896fc58819c2
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-03-11 17:12:10 +0100

    Upgrade mypy to 0.940

--- pylint/testutils/primer.py
+++ pylint/testutils/primer.py
@@ -87,7 +87,7 @@ class PackageToLint:
                 "depth": 1,
             }
             logging.info("Directory does not exists, cloning: %s", options)
-            git.Repo.clone_from(**options)
+            git.Repo.clone_from(**options)  # type: ignore[arg-type]
             return
 
         remote_sha1_commit = (
