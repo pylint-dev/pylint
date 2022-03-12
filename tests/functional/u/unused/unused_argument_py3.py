134157407a051d4d083d69fc23d94fd8092376a9
Jacob Walls <jacobtylerwalls@gmail.com>
2022-03-12 08:22:49 -0500

    Fix `unused-argument` false positive for nonlocals

--- tests/functional/u/unused/unused_argument_py3.py
+++ tests/functional/u/unused/unused_argument_py3.py
@@ -7,3 +7,12 @@ def func(first, *, second): # [unused-argument, unused-argument]
 def only_raises(first, second=42): # [unused-argument]
     if first == 24:
         raise ValueError
+
+
+def increment_factory(initial):
+
+    def increment():
+        nonlocal initial
+        initial += 1
+
+    return increment
