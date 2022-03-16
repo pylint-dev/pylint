ada25b59b6d45c4a4c69dead6ac576ce1c33adb4
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-03-16 10:02:38 +0100

    Use short copyright notice in ``doc`` and ``pylint`` directories
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/exts/pylint_extensions.py
+++ doc/exts/pylint_extensions.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
+# Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
 
 """Script used to generate the extensions file before building the actual documentation."""
 
