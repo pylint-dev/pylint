114700002c26b1eb3eab58cbe5a241f7924735e9
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-03-24 21:54:42 +0100

    Update doc/data/messages/a/arguments-differ/good.py

--- doc/data/messages/a/arguments-differ/good.py
+++ doc/data/messages/a/arguments-differ/good.py
@@ -5,7 +5,7 @@ class Rectangle:
 
 
 class Square(Rectangle):
-    def __init__(self, length, width):
+    def __init__(self, length, width=None):
         self.length = length
         self.width = width
 
