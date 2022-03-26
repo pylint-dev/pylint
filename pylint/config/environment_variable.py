e0c346470be7eaf039aaa9dd915ddb8ab493ecb1
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-03-26 09:31:51 +0100

    [refactor] Create an env_variable file in pylint.config

---
+++ pylint/config/environment_variable.py
@@ -0,0 +1,7 @@
+# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
+# Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
+
+from pylint.config.find_default_config_files import find_pylintrc
+
+PYLINTRC = find_pylintrc()
