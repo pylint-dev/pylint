8e1d562a1cc56567afd5ba5c8bf4dc1df6188235
Joe Young <80432516+jpy-git@users.noreply.github.com>
2022-03-26 23:02:55 +0000

    Update doc/data/messages/m/manual-magic-methods/bad.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/m/manual-magic-methods/bad.py
+++ doc/data/messages/m/manual-magic-methods/bad.py
@@ -1,1 +1,6 @@
-var_str = var.__str__() # [manual-magic-methods]
+three = 3.0.__str__()  # [manual-magic-methods]
+twelve = "1".__add__("2")  # [manual-magic-methods]
+
+
+def is_bigger_than_two(x):
+    return x.__gt__(2)  # [manual-magic-methods]
