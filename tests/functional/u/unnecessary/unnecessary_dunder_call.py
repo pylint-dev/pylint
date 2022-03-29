ffee2235d2753b38810759dda82daf9a69707903
jpy-git <josephyoung.jpy@gmail.com>
2022-03-29 21:37:09 +0100

    Fix unit test

--- tests/functional/u/unnecessary/unnecessary_dunder_call.py
+++ tests/functional/u/unnecessary/unnecessary_dunder_call.py
@@ -1,5 +1,6 @@
 """Checks for unnecessary-dunder-call."""
 # pylint: disable=too-few-public-methods, undefined-variable, useless-object-inheritance
+# pylint: disable=missing-class-docstring, missing-function-docstring
 
 # Test includelisted dunder methods raise lint when manually called.
 num_str = some_num.__str__() # [unnecessary-dunder-call]
