b9b81bec7b74ffeba170e809289c337b5de4e6bc
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-04-01 19:53:24 +0200

    Some refactors of configuration file discovery (#6052)
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- pylint/config/options_provider_mixin.py
+++ pylint/config/options_provider_mixin.py
@@ -67,8 +67,7 @@ class OptionsProviderMixIn:
                 if isinstance(value, (list, tuple)):
                     _list = value
                 elif value is not None:
-                    _list = []
-                    _list.append(value)
+                    _list = [value]
                 setattr(self.config, optname, _list)
             elif isinstance(_list, tuple):
                 setattr(self.config, optname, _list + (value,))
@@ -108,4 +107,4 @@ class OptionsProviderMixIn:
         if options is None:
             options = self.options
         for optname, optdict in options:
-            yield (optname, optdict, self.option_value(optname))
+            yield optname, optdict, self.option_value(optname)
