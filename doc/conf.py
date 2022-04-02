19e0acf787445bb7ecd3a166e26274c002685240
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-04-02 12:33:12 +0200

    Apply suggestions from code review

--- doc/conf.py
+++ doc/conf.py
@@ -108,8 +108,8 @@ html_theme = "furo"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+# Currently we use the default Furo configuration
 # html_theme_options = {}
-# Use Furo theme default config
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
