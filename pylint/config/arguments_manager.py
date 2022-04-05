edf81a364fe0872febc6130e573d27f93c366813
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-04-05 10:12:21 +0200

    Finalize ``_parse_command_line_configuration``

--- pylint/config/arguments_manager.py
+++ pylint/config/arguments_manager.py
@@ -5,7 +5,8 @@
 """Arguments manager class used to handle command-line arguments and options."""
 
 import argparse
-from typing import TYPE_CHECKING, Dict, List
+import sys
+from typing import TYPE_CHECKING, Dict, List, Optional
 
 from pylint.config.argument import (
     _Argument,
@@ -145,15 +146,17 @@ class _ArgumentsManager:
         # TODO: This should parse_args instead of parse_known_args
         self.namespace = self._arg_parser.parse_known_args(arguments, self.namespace)[0]
 
-    def _parse_command_line_configuration(self, arguments: List[str]) -> None:
+    def _parse_command_line_configuration(
+        self, arguments: Optional[List[str]] = None
+    ) -> List[str]:
         """Parse the arguments found on the command line into the namespace."""
-        # pylint: disable-next=fixme
-        # TODO: This should parse_args instead of parse_known_args
-        self.namespace = self._arg_parser.parse_known_args(arguments, self.namespace)[0]
+        arguments = sys.argv[1:] if arguments is None else arguments
 
-        # pylint: disable-next=fixme
-        # TODO: This should return a list of arguments with the option arguments removed
-        # just as PyLinter.load_command_line_configuration does
+        self.namespace, parsed_args = self._arg_parser.parse_known_args(
+            arguments, self.namespace
+        )
+
+        return parsed_args
 
     def _parse_plugin_configuration(self) -> None:
         # pylint: disable-next=fixme
