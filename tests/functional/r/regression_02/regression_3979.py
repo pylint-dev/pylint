141d8f00e17f05f5db480fa5277c87fc8f1fdfa8
Jacob Walls <jacobtylerwalls@gmail.com>
2022-04-09 12:35:08 -0400

    cosmetic

--- tests/functional/r/regression_02/regression_3979.py
+++ tests/functional/r/regression_02/regression_3979.py
@@ -6,8 +6,8 @@ import os
 from typing import TYPE_CHECKING, Any, Union
 
 if TYPE_CHECKING:
-    BasePathLike = os.PathLike[Any]  # <-- that is where pylint identifies E1136
+    BasePathLike = os.PathLike[Any]  # <-- pylint used to emit E1136 here
 else:
     BasePathLike = os.PathLike
 
-foo : Union[str, BasePathLike] = "bar"
+foo: Union[str, BasePathLike] = "bar"
