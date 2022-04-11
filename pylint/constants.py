75618740974336bd3e07b6409151cb1eb7220b6d
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-04-11 21:49:31 +0200

    Remove removed messages from the tests

--- pylint/constants.py
+++ pylint/constants.py
@@ -176,6 +176,10 @@ DELETED_MESSAGES = [
     ),
     # https://github.com/PyCQA/pylint/pull/3571
     DeletedMessage("C0330", "bad-continuation"),
+    # No PR
+    DeletedMessage("R0921", "abstract-class-not-used"),
+    # https://github.com/PyCQA/pylint/pull/3577
+    DeletedMessage("C0326", "abstract-class-not-used"),
 ]
 
 
