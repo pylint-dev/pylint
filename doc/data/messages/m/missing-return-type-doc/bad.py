b11184939358e7c0ba96ad65ec53ac2817b7acf2
Matus Valo <matusvalo@gmail.com>
2022-04-15 16:16:07 +0200

    Added missing-return-type-doc message example

---
+++ doc/data/messages/m/missing-return-type-doc/bad.py
@@ -0,0 +1,7 @@
+def integer_sum(a, b):  # [missing-return-type-doc]
+    """Returns sum of two integers
+    :param a: first integer
+    :type a: int
+    :return: sum of parameters a and b
+    """
+    return a + b
