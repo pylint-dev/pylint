aaa998c971b42e422a64b6b061d4fe09b9f8c7f8
Matus Valo <matusvalo@gmail.com>
2022-04-15 16:17:25 +0200

    Added missing-yield-doc message example

---
+++ doc/data/messages/m/missing-yield-doc/good.py
@@ -0,0 +1,12 @@
+def even_number_under(n: int):
+    """Prints even numbers smaller than n.
+    Args:
+        n (int): Upper limit of even numbers.
+
+    Yields:
+        int: even numbers
+    """
+    for i in range(n):
+        if i % 2 == 1:
+            continue
+        yield i
