4580f8735b4595a226a90513bbc02420a4b854b8
Matus Valo <matusvalo@gmail.com>
2022-04-15 17:35:53 +0200

    Fix missing parameter in example

--- doc/data/messages/m/missing-return-doc/good.py
+++ doc/data/messages/m/missing-return-doc/good.py
@@ -2,6 +2,8 @@ def integer_sum(a, b):
     """Returns sum of two integers
     :param a: first integer
     :type a: int
+    :param b: second integer
+    :type b: int
     :return: sum of parameters a and b
     :rtype: int
     """
