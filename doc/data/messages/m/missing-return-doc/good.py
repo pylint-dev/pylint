4cac77fb8409e5c3ee45abd18cf5f155ddaba1ef
Matus Valo <matusvalo@gmail.com>
2022-04-15 16:15:00 +0200

    Added missing-return-doc message example

---
+++ doc/data/messages/m/missing-return-doc/good.py
@@ -0,0 +1,8 @@
+def integer_sum(a, b):
+    """Returns sum of two integers
+    :param a: first integer
+    :type a: int
+    :return: sum of parameters a and b
+    :rtype: int
+    """
+    return a + b
