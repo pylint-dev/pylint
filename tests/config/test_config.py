63173f8f3a21671155c9a3122c5ecaa8a66ce0b5
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-04-15 12:56:16 +0200

    Improve coverage of ``argparse`` (#6332)
    * Fix coverage of ``pylint/config/config_file_parser.py``
    * Fix coverage of ``pylint/config/argument.py``
    
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
+import pytest
 from pytest import CaptureFixture
 
 from pylint.lint import Run
@@ -58,3 +59,27 @@ def test_unknown_message_id(capsys: CaptureFixture) -> None:
     Run([str(EMPTY_MODULE), "--disable=12345"], exit=False)
     output = capsys.readouterr()
     assert "Command line:1:0: E0012: Bad option value for --disable." in output.out
+
+
+def test_unknown_confidence(capsys: CaptureFixture) -> None:
+    """Check that we correctly error an unknown confidence value."""
+    with pytest.raises(SystemExit):
+        Run([str(EMPTY_MODULE), "--confidence=UNKNOWN_CONFIG"], exit=False)
+    output = capsys.readouterr()
+    assert "argument --confidence: UNKNOWN_CONFIG should be in" in output.err
+
+
+def test_unknown_yes_no(capsys: CaptureFixture) -> None:
+    """Check that we correctly error on an unknown yes/no value."""
+    with pytest.raises(SystemExit):
+        Run([str(EMPTY_MODULE), "--reports=maybe"], exit=False)
+    output = capsys.readouterr()
+    assert "Invalid yn value 'maybe', should be in " in output.err
+
+
+def test_unknown_py_version(capsys: CaptureFixture) -> None:
+    """Check that we correctly error on an unknown python-version."""
+    with pytest.raises(SystemExit):
+        Run([str(EMPTY_MODULE), "--py-version=the-newest"], exit=False)
+    output = capsys.readouterr()
+    assert "the-newest has an invalid format, should be a version string." in output.err
