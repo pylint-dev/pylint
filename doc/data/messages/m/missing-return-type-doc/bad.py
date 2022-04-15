0640abab940573edea628152509aa9195497ef81
Matus Valo <matusvalo@gmail.com>
2022-04-15 17:34:17 +0200

    Fix bad.py file

--- doc/data/messages/m/missing-return-type-doc/bad.py
+++ doc/data/messages/m/missing-return-type-doc/bad.py
@@ -2,6 +2,8 @@ def integer_sum(a, b):  # [missing-return-type-doc]
     """Returns sum of two integers
     :param a: first integer
     :type a: int
+    :param b: second integer
+    :type b: int
     :return: sum of parameters a and b
     """
     return a + b
