658269daabd50af86aca2310bb0501386e055f3e
Matus Valo <matusvalo@gmail.com>
2022-04-15 16:11:10 +0200

    Added missing-type-doc message example

---
+++ doc/data/messages/m/missing-type-doc/bad.py
@@ -0,0 +1,7 @@
+def integer_sum(a, b):  # [missing-type-doc]
+    """Returns sum of two integers
+    :param a: first integer
+    :type a: int
+    :param b: second integer
+    """
+    return a + b
---
+++ doc/data/messages/m/missing-type-doc/good.py
@@ -0,0 +1,8 @@
+def integer_sum(a, b):
+    """Returns sum of two integers
+    :param a: first integer
+    :type a: int
+    :param b: second integer
+    :type b: int
+    """
+    return a + b
