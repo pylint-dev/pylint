b80cb2daec9be49b9e338fb0320967ca75976e06
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-04-17 22:09:36 +0200

    Remove the 'no-init' message tat was not emitted anyway
    See https://github.com/PyCQA/pylint/issues/2409#issuecomment-1100952171
    
    Closes #2409

--- tests/functional/i/init_not_called.py
+++ tests/functional/i/init_not_called.py
@@ -1,19 +1,26 @@
-# pylint: disable=too-few-public-methods,import-error,missing-docstring,wrong-import-position,useless-super-delegation, useless-object-inheritance, unnecessary-pass
-"""test for __init__ not called
-"""
+# pylint: disable=too-few-public-methods, import-error, missing-docstring, wrong-import-position
+# pylint: disable=useless-super-delegation, useless-object-inheritance, unnecessary-pass
+
 from __future__ import print_function
+
+from typing import overload
+
+from missing import Missing
+
 
 class AAAA:
     """ancestor 1"""
 
     def __init__(self):
-        print('init', self)
+        print("init", self)
+
 
 class BBBB:
     """ancestor 2"""
 
     def __init__(self):
-        print('init', self)
+        print("init", self)
+
 
 class CCCC:
     """ancestor 3"""
@@ -25,55 +32,49 @@ class ZZZZ(AAAA, BBBB, CCCC):
     def __init__(self):  # [super-init-not-called]
         AAAA.__init__(self)
 
+
 class NewStyleA(object):
     """new style class"""
+
     def __init__(self):
         super().__init__()
-        print('init', self)
+        print("init", self)
+
 
 class NewStyleB(NewStyleA):
     """derived new style class"""
+
     def __init__(self):
         super().__init__()
 
-class NoInit(object):
-    """No __init__ defined"""
-
-class Init(NoInit):
-    """Don't complain for not calling the super __init__"""
-
-    def __init__(self, arg):
-        self.arg = arg
 
 class NewStyleC(object):
     """__init__ defined by assignment."""
+
     def xx_init(self):
         """Initializer."""
         pass
 
     __init__ = xx_init
 
+
 class AssignedInit(NewStyleC):
     """No init called."""
+
     def __init__(self):  # [super-init-not-called]
         self.arg = 0
 
-from missing import Missing
 
 class UnknownBases(Missing):
-    """Don't emit no-init if the bases aren't known."""
+    """No false positives if the bases aren't known."""
 
-
-from typing import overload  # pylint: disable=wrong-import-order
 
 class Parent:
-
     def __init__(self, num: int):
         self.number = num
 
 
 class Child(Parent):
-
     @overload
     def __init__(self, num: int):
         ...
