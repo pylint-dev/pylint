8bc8a4640e1843931e9baf08296768e5d606bec0
Matus Valo <matusvalo@gmail.com>
2022-04-19 22:49:26 +0200

    Migrated to type annotations

--- doc/data/messages/m/missing-return-type-doc/bad.py
+++ doc/data/messages/m/missing-return-type-doc/bad.py
@@ -1,9 +1,7 @@
-def integer_sum(a, b):  # [missing-return-type-doc]
+def integer_sum(a: int, b: int):  # [missing-return-type-doc]
     """Returns sum of two integers
     :param a: first integer
-    :type a: int
     :param b: second integer
-    :type b: int
     :return: sum of parameters a and b
     """
     return a + b
--- doc/data/messages/m/missing-return-type-doc/good.py
+++ doc/data/messages/m/missing-return-type-doc/good.py
@@ -1,10 +1,7 @@
-def integer_sum(a, b):
+def integer_sum(a: int, b: int) -> int:
     """Returns sum of two integers
     :param a: first integer
-    :type a: int
     :param b: second integer
-    :type b: int
     :return: sum of parameters a and b
-    :rtype: int
     """
     return a + b
