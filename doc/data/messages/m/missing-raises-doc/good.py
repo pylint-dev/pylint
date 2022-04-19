13988ff89833887d0307b7c7558804237c7471f2
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-04-19 23:10:00 +0200

    Update doc/data/messages/m/missing-raises-doc/good.py

--- doc/data/messages/m/missing-raises-doc/good.py
+++ doc/data/messages/m/missing-raises-doc/good.py
@@ -2,7 +2,7 @@ def integer_sum(a: int, b: int):
     """Returns sum of two integers
     :param a: first integer
     :param b: second integer
-    :raises ValueError: One of parameters is not an integer.
+    :raises ValueError: One of the parameters is not an integer.
     """
     if not (isinstance(a, int) and isinstance(b, int)):
         raise ValueError('Function supports only integer parameters.')
