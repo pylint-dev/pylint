c91e7903c444ea61689abb96d70cfc6de62025bb
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-04-21 13:55:52 +0200

    Finish typing of ``pylint/testutils/decorator.py``

--- pylint/testutils/decorator.py
+++ pylint/testutils/decorator.py
@@ -6,35 +6,29 @@ from __future__ import annotations
 
 import functools
 from collections.abc import Callable
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 from pylint.testutils.checker_test_case import CheckerTestCase
 
-if TYPE_CHECKING:
-    from pylint.checkers.base_checker import BaseChecker
 
-
-def set_config(**kwargs: Any) -> Callable:
+def set_config(**kwargs: Any) -> Callable[[Callable[..., None]], Callable[..., None]]:
     """Decorator for setting an option on the linter.
 
     Passing the args and kwargs back to the test function itself
     allows this decorator to be used on parametrized test cases.
     """
 
-    def _wrapper(fun: Callable) -> Callable:
+    def _wrapper(fun: Callable[..., None]) -> Callable[..., None]:
         @functools.wraps(fun)
         def _forward(
-            self: BaseChecker | CheckerTestCase, *args: Any, **test_function_kwargs: Any
+            self: CheckerTestCase, *args: Any, **test_function_kwargs: Any
         ) -> None:
             """Set option via argparse."""
             for key, value in kwargs.items():
                 self.linter.set_option(key, value)
 
             # Reopen checker in case, it may be interested in configuration change
-            if isinstance(self, CheckerTestCase):
-                self.checker.open()
-            else:
-                self.open()
+            self.checker.open()
 
             fun(self, *args, **test_function_kwargs)
 
