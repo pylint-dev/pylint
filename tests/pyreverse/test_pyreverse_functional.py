b84759d2da471dc0051a182ddd8d35c7b26ec1ca
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2022-04-24 19:35:50 +0200

    Move logic to `testutils` and implement parsing of per-test configuration

--- tests/pyreverse/test_pyreverse_functional.py
+++ tests/pyreverse/test_pyreverse_functional.py
@@ -1,35 +1,16 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
-from __future__ import annotations
-
 from pathlib import Path
-from typing import NamedTuple
 
 import pytest
 from py._path.local import LocalPath  # type: ignore[import]
 
 from pylint.pyreverse.main import Run
-
-
-class FunctionalPyreverseTestfile(NamedTuple):
-    """Named tuple containing the test file and the expected output."""
-
-    source: Path
-    expectation: Path
-
-
-def get_functional_test_files(
-    root_directory: Path,
-) -> list[FunctionalPyreverseTestfile]:
-    """Get all functional test files from the given directory."""
-    return [
-        FunctionalPyreverseTestfile(
-            source=test_file, expectation=test_file.with_suffix(".mmd")
-        )
-        for test_file in root_directory.rglob("*.py")
-    ]
-
+from pylint.testutils.pyreverse import (
+    FunctionalPyreverseTestfile,
+    get_functional_test_files,
+)
 
 FUNCTIONAL_DIR = Path(__file__).parent / "functional"
 CLASS_DIAGRAM_TESTS = get_functional_test_files(FUNCTIONAL_DIR / "class_diagrams")
@@ -45,9 +26,13 @@ def test_class_diagrams(
     testfile: FunctionalPyreverseTestfile, tmpdir: LocalPath
 ) -> None:
     input_file = testfile.source
-    with pytest.raises(SystemExit) as sys_exit:
-        Run(["-o", "mmd", "-d", str(tmpdir), str(input_file)])
-    assert sys_exit.value.code == 0
-    assert testfile.expectation.read_text(encoding="utf8") == Path(
-        tmpdir / "classes.mmd"
-    ).read_text(encoding="utf8")
+    for output_format in testfile.options["output_formats"]:
+        with pytest.raises(SystemExit) as sys_exit:
+            args = ["-o", f"{output_format}", "-d", str(tmpdir)]
+            args.extend(testfile.options["command_line_args"])
+            args += [str(input_file)]
+            Run(args)
+        assert sys_exit.value.code == 0
+        assert testfile.source.with_suffix(f".{output_format}").read_text(
+            encoding="utf8"
+        ) == Path(tmpdir / f"classes.{output_format}").read_text(encoding="utf8")
