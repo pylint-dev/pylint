69c2f438d400b8f7bcceb13ccd318d635516fa99
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2022-04-24 11:53:38 +0200

    Include future import to resolve ``TypeError`` on Python 3.8 and below

--- tests/pyreverse/test_pyreverse_functional.py
+++ tests/pyreverse/test_pyreverse_functional.py
@@ -1,6 +1,8 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
+from __future__ import annotations
+
 from pathlib import Path
 from typing import NamedTuple
 
