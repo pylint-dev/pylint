444d60585e390e9c2f7e19caddaf6d7a016b0be6
MalanB <malan.kmu@gmail.com>
2022-04-29 11:26:42 +0530

    Added bad-format-string, consider-using-sys-exit, global-statement documentation examples
    Co-authored-by: Onkar Dixit <osdixit@gmail.com>
    Co-authored-by: Vladyslav Krylasov <vladyslav.krylasov@gmail.com>

---
+++ doc/data/messages/g/global-statement/bad.py
@@ -0,0 +1,9 @@
+var = 1
+
+def foo():
+    global var # [global-statement]
+    var = 10
+    print(var)
+
+foo()
+print(var)
---
+++ doc/data/messages/g/global-statement/good.py
@@ -0,0 +1,8 @@
+var = 1
+
+def foo(x):
+    print(var)
+    return 10
+
+var = foo()
+print(var)
