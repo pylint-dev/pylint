ff9c1556b09cdcac8276b1e84433c7beb9b241b2
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-04-30 15:43:59 +0200

    Update expand modules test

--- tests/lint/unittest_expand_modules.py
+++ tests/lint/unittest_expand_modules.py
@@ -69,6 +69,14 @@ test_pylinter = {
     "path": str(TEST_DIRECTORY / "lint/test_pylinter.py"),
 }
 
+test_namespace_packages = {
+    "basename": "lint",
+    "basepath": INIT_PATH,
+    "isarg": False,
+    "name": "lint.test_namespace_packages",
+    "path": str(TEST_DIRECTORY / "lint/test_namespace_packages.py"),
+}
+
 init_of_package = {
     "basename": "lint",
     "basepath": INIT_PATH,
@@ -98,6 +106,7 @@ class TestExpandModules(CheckerTestCase):
                 [str(Path(__file__).parent)],
                 [
                     init_of_package,
+                    test_namespace_packages,
                     test_pylinter,
                     test_utils,
                     this_file_from_init,
