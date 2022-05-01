e73767ada03ef3b5d7cdfe66683fd4a7104c5d6a
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-05-01 22:01:43 +0200

    oups

--- doc/data/messages/r/redundant-unittest-assert/bad.py
+++ doc/data/messages/r/redundant-unittest-assert/bad.py
@@ -4,8 +4,3 @@ import unittest
 class DummyTestCase(unittest.TestCase):
     def test_dummy(self):
         self.assertTrue("foo")  # [redundant-unittest-assert]
-
-
-def test_division():
-    a = 9 / 3
-    assert "No ZeroDivisionError were raised"  # assert-on-string-literal
--- doc/data/messages/r/redundant-unittest-assert/good.py
+++ doc/data/messages/r/redundant-unittest-assert/good.py
@@ -3,9 +3,5 @@ import unittest
 
 class DummyTestCase(unittest.TestCase):
     def test_dummy(self):
-        # Nothing, as an assert of a string literal will always pass
-        pass
-
-def test_division():
-    a = 9 / 3
-    assert a == 3
+        actual = "test_result"
+        self.assertEqual(actual, "expected")
