2f5d8c2cd8e58aefd8b5e8340694f2555a8f59e4
MalanB <malan.kmu@gmail.com>
2022-05-02 20:22:37 +0530

    Addressed PR #6472 comments for documentation issue #5953 > > Co-authored-by: Onkar Dixit <osdixit@gmail.com> Co-authored-by: Vladyslav Krylasov <vladyslav.krylasov@gmail.com>

--- doc/data/messages/c/consider-using-sys-exit/bad.py
+++ doc/data/messages/c/consider-using-sys-exit/bad.py
@@ -1,4 +1,4 @@
-if __name__ == '__main__':
-    user = input('Enter user name: ')
-    print(f'Hello, {user}')
-    exit(0) # [consider-using-sys-exit]
+if __name__ == "__main__":
+    user = input("Enter user name: ")
+    print(f"Hello, {user}")
+    exit(0)  # [consider-using-sys-exit]
