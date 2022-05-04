eb68655689233d55ed408feaa3b76987370c9a56
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-05-04 10:26:06 +0200

    Upgrade the message prefix

--- pylint/checkers/lambda_expressions.py
+++ pylint/checkers/lambda_expressions.py
@@ -19,14 +19,14 @@ class LambdaExpressionChecker(BaseChecker):
 
     name = "lambda-expressions"
     msgs = {
-        "C2901": (
+        "C3001": (
             "Lambda expression assigned to a variable. "
             'Define a function using the "def" keyword instead.',
             "unnecessary-lambda-assignment",
             "Used when a lambda expression is assigned to variable "
             'rather than defining a standard function with the "def" keyword.',
         ),
-        "C2902": (
+        "C3002": (
             "Lambda expression called directly. Execute the expression inline instead.",
             "unnecessary-direct-lambda-call",
             "Used when a lambda expression is directly called "
