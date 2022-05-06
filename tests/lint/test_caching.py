0912580a37c762e72e6a6ea4cafe3b857b2c1b58
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-05-06 16:39:19 +0200

    Fix saving of persistent data files on different drives

--- tests/lint/test_caching.py
+++ tests/lint/test_caching.py
@@ -5,6 +5,7 @@
 # Pytest fixtures work like this by design
 # pylint: disable=redefined-outer-name
 
+import sys
 from pathlib import Path
 
 import pytest
@@ -18,15 +19,67 @@ PYLINT_HOME_PATH = Path(PYLINT_HOME)
 
 
 @pytest.mark.parametrize(
-    "path,recur,expected",
+    "path,recur,pylint_home,expected",
     [
-        ["", 1, PYLINT_HOME_PATH / "_1.stats"],
-        ["", 2, PYLINT_HOME_PATH / "_2.stats"],
-        ["a/path", 42, PYLINT_HOME_PATH / "a_path_42.stats"],
+        ["", 1, PYLINT_HOME_PATH, PYLINT_HOME_PATH / "_1.stats"],
+        ["", 2, PYLINT_HOME_PATH, PYLINT_HOME_PATH / "_2.stats"],
+        ["a/path", 42, PYLINT_HOME_PATH, PYLINT_HOME_PATH / "a_path_42.stats"],
     ],
 )
-def test__get_pdata_path(path: str, recur: int, expected: Path) -> None:
-    assert _get_pdata_path(Path(path), recur) == expected
+def test__get_pdata_path(
+    path: str, recur: int, pylint_home: Path, expected: Path
+) -> None:
+    assert _get_pdata_path(Path(path), recur, pylint_home) == expected
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="Path type of *nix")
+@pytest.mark.parametrize(
+    "path,recur,pylint_home,expected",
+    [
+        [
+            "/workspace/MyDir/test.py",
+            1,
+            Path("/root/.cache/pylint"),
+            Path("/root/.cache/pylint") / "__workspace_MyDir_test.py_1.stats",
+        ],
+        [
+            "/workspace/MyDir/test.py",
+            1,
+            Path("//host/computer/.cache"),
+            Path("//host/computer/.cache") / "__workspace_MyDir_test.py_1.stats",
+        ],
+    ],
+)
+def test__get_pdata_path_nix(
+    path: str, recur: int, pylint_home: Path, expected: Path
+) -> None:
+    """test__get_pdata_path but specifically for *nix system paths."""
+    assert _get_pdata_path(Path(path), recur, pylint_home) == expected
+
+
+@pytest.mark.skipif(sys.platform != "win32", reason="Path type of windows")
+@pytest.mark.parametrize(
+    "path,recur,pylint_home,expected",
+    [
+        [
+            "D:\\MyDir\\test.py",
+            1,
+            Path("C:\\Users\\MyPylintHome"),
+            Path("C:\\Users\\MyPylintHome") / "D___MyDir_test.py_1.stats",
+        ],
+        [
+            "C:\\MyDir\\test.py",
+            1,
+            Path("C:\\Users\\MyPylintHome"),
+            Path("C:\\Users\\MyPylintHome") / "C___MyDir_test.py_1.stats",
+        ],
+    ],
+)
+def test__get_pdata_path_windows(
+    path: str, recur: int, pylint_home: Path, expected: Path
+) -> None:
+    """test__get_pdata_path but specifically for windows."""
+    assert _get_pdata_path(Path(path), recur, pylint_home) == expected
 
 
 @pytest.fixture
