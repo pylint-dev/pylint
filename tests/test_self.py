0af656bce6024e330615b6cae6687d74ca2fafeb
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-05-10 00:00:35 +0200

    Update tests/test_self.py

--- tests/test_self.py
+++ tests/test_self.py
@@ -1346,7 +1346,7 @@ class TestCallbackOptions:
         assert process.stdout == process_two.stdout
 
         # Check that the generated file is valid
-        with tempfile.NamedTemporaryFile(mode="w") as temp:`
+        with tempfile.NamedTemporaryFile(mode="w") as temp:
             temp.write(process.stdout)
             runner = Run(
                 [join(HERE, "regrtest_data", "empty.py"), f"--rcfile={temp.name}"],
