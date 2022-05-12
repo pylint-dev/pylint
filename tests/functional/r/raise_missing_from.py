366fd724152851f4e5941f0e140ebbfc3729d256
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-05-12 09:37:21 +0200

    [raise-missing-from] Clearer message and example in the documentation (#6576)
    * [raise-missing-from] Clearer message and example in the documentation
    
    Co-authored-by: cool-RR <ram@rachum.com>
    Co-authored-by: Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
    
    Refs #5953
    Closes #3707

--- tests/functional/r/raise_missing_from.py
+++ tests/functional/r/raise_missing_from.py
@@ -1,8 +1,10 @@
 # pylint:disable=missing-docstring, unreachable, using-constant-test, invalid-name, bare-except
 # pylint:disable=try-except-raise, undefined-variable, too-few-public-methods, superfluous-parens
 
-################################################################################
-# Positives:
+try:
+    1 / 0
+except:
+    raise ValueError('Invalid integer')  # [raise-missing-from]
 
 try:
     1 / 0
@@ -13,9 +15,8 @@ except ZeroDivisionError:
 try:
     1 / 0
 except ZeroDivisionError:
-    # Our algorithm doesn't have to be careful about the complicated expression below, because
-    # the exception above wasn't bound to a name.
-    # +1: [raise-missing-from]
+    # Our algorithm doesn't have to be careful about the complicated expression below,
+    # because the exception above wasn't bound to a name. # +1: [raise-missing-from]
     raise (foo + bar).baz
 
 try:
@@ -59,8 +60,26 @@ except ZeroDivisionError as e:
     raise KeyError(whatever, whatever=whatever)
 
 
-################################################################################
-# Negatives (Same cases as above, except with `from`):
+try:
+    1 / 0
+except (ZeroDivisionError, ValueError, KeyError):
+    if 1:
+        if 2:
+            pass
+        else:
+            with whatever:
+                # +1: [raise-missing-from]
+                raise KeyError(whatever, whatever=whatever)
+    else:
+        # +1: [raise-missing-from]
+        raise KeyError(whatever, overever=12)
+
+try:
+    # Taken from https://github.com/python/cpython/blob/3.10/Lib/plistlib.py#L459
+    pass
+except (OSError, IndexError, struct.error, OverflowError,
+        ValueError):
+    raise InvalidFileException()  # [raise-missing-from]
 
 try:
     1 / 0
@@ -106,10 +125,6 @@ try:
     1 / 0
 except ZeroDivisionError as e:
     raise KeyError(whatever, whatever=whatever) from foo
-
-
-################################################################################
-# Other negatives:
 
 try:
     1 / 0
