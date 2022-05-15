b22f7edcceda7cfc69190db0a2b14691f747c39f
nyabkun <nyabkun@gmail.com>
2022-05-16 06:38:03 +0900

    Add the functional test for #6612

---
+++ tests/pyreverse/functional/class_diagrams/attributes/_monkey.py
@@ -0,0 +1,11 @@
+# This is not a standalone test
+# Monkey class is called from Tree class in delayed_external_monkey_patching.py
+
+
+class Monkey:
+    def __init__(self, name):
+        from delayed_external_monkey_patching import Tree
+
+        self.name = name
+        self.tree = Tree()
+        self.tree.has_tasty_bananas = True  # This monkey patching will increase the number of items in instance_attrs for `Tree`
