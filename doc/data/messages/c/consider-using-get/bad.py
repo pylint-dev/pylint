75b3c21d4ba009596026aad6f4edfd204d3b0a62
Mark Byrne <mbyrnepr2@gmail.com>
2022-05-16 23:31:53 +0200

    Add documentation examples for `consider-using-get`.

---
+++ doc/data/messages/c/consider-using-get/bad.py
@@ -0,0 +1,4 @@
+knights = {"gallahad": "the pure", "robin": "the brave"}
+
+if "gallahad" in knights:  # [consider-using-get]
+    DESCRIPTION = knights["gallahad"]
---
+++ doc/data/messages/c/consider-using-get/good.py
@@ -0,0 +1,3 @@
+knights = {"gallahad": "the pure", "robin": "the brave"}
+
+description = knights.get("gallahad")
