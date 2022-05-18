16812855ba73fd670f471b3a8e4f1ca311e8ec5b
Mark Byrne <31762852+mbyrnepr2@users.noreply.github.com>
2022-05-18 09:08:19 +0200

    Update doc/data/messages/c/consider-using-join/bad.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/c/consider-using-join/bad.py
+++ doc/data/messages/c/consider-using-join/bad.py
@@ -1,6 +1,7 @@
-def fruits_to_string():
-    fruits = {"apple", "pear", "peach"}
+def fruits_to_string(fruits):
     formatted_fruit = ""
     for fruit in fruits:
         formatted_fruit += fruit  # [consider-using-join]
     return formatted_fruit
+
+print(fruits_to_string(["apple", "pear", "peach"]))
