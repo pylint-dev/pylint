fddaeebae0d21e192da210d13b75e06042953572
Mark Byrne <31762852+mbyrnepr2@users.noreply.github.com>
2022-05-18 09:31:39 +0200

    Add documentation examples for `consider-using-join`. (#6637)
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

---
+++ doc/data/messages/c/consider-using-join/bad.py
@@ -0,0 +1,7 @@
+def fruits_to_string(fruits):
+    formatted_fruit = ""
+    for fruit in fruits:
+        formatted_fruit += fruit  # [consider-using-join]
+    return formatted_fruit
+
+print(fruits_to_string(["apple", "pear", "peach"]))
---
+++ doc/data/messages/c/consider-using-join/good.py
@@ -0,0 +1,1 @@
+print("".join(["apple", "pear", "peach"]))
