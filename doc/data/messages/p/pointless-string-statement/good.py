9b8053947aa7e0898113b426a4f5eb23122d5ff8
Mark Byrne <31762852+mbyrnepr2@users.noreply.github.com>
2022-05-27 21:47:52 +0200

    Added documentation examples for pointless-?-statement (#6718)
    - `pointless-statement`
    - `pointless-string-statement`

--- doc/data/messages/p/pointless-string-statement/good.py
+++ doc/data/messages/p/pointless-string-statement/good.py
@@ -1,1 +1,3 @@
-# This is a placeholder for correct code for this message.
+"""This is a docstring which describes the module"""
+
+# This is comment which describes a particular part of the module.
