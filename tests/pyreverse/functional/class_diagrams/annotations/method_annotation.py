41b7ee23a04bfd5e7e40170a4aafbfbe987db20e
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2022-05-27 19:56:08 +0200

    Fix tests and add example for explicit `Union` type hint.

--- tests/pyreverse/functional/class_diagrams/annotations/method_annotation.py
+++ tests/pyreverse/functional/class_diagrams/annotations/method_annotation.py
@@ -1,14 +1,19 @@
+# pylint: disable=import-error,consider-alternative-union-syntax
 from __future__ import annotations
 
-# pylint: disable-next=import-error
-from lib import Banana, Coconut, Sound  # type: ignore[import]
+from typing import Optional, Union
+
+from lib import Banana, Coconut, Insect, Leaf, Sound  # type: ignore[import]
 
 
 class Monkey:
     def eat(self, food: Banana | Coconut) -> None:
         print(f"Monkey eats {food}")
 
-    def jump(self, height: int | None = 10) -> None:
+    def munch(self, food: Union[Leaf, Insect]) -> None:
+        print(f"Monkey munches {food}")
+
+    def jump(self, height: Optional[int] = 10) -> None:
         print(f"Monkey jumps {height}")
 
     def scream(self, volume: int | None) -> Sound:
