8734d92e1be1ab32d2c1ead75085f75558cd28e5
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-06-01 08:57:56 +0200

    Fix ``.git`` in package links in the primer

--- tests/primer/primer_tool.py
+++ tests/primer/primer_tool.py
@@ -240,8 +240,7 @@ class Primer:
                 filepath = str(message["path"]).replace(
                     str(package_data.clone_directory), ""
                 )
-                without_suffix = package_data.url.rsplit(".git")[0]
-                comment += f"{without_suffix}/blob/{package_data.branch}{filepath}#L{message['line']}\n"
+                comment += f"{package_data.url}/blob/{package_data.branch}{filepath}#L{message['line']}\n"
                 count += 1
                 print(message)
             if missing_messages:
