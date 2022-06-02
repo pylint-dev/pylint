a92d2ea64f13ae360d2fb1ebebaf52c5d97e4d1a
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-06-02 18:42:57 +0200

    Don't crash if we can't find the user's home directory

--- doc/whatsnew/2/2.14/full.rst
+++ doc/whatsnew/2/2.14/full.rst
@@ -5,6 +5,9 @@ What's New in Pylint 2.14.1?
 ----------------------------
 Release date: TBA
 
+* Don't crash if we can't find the user's home directory.
+
+  Closes #6802
 
 
 What's New in Pylint 2.14.0?
--- pylint/config/find_default_config_files.py
+++ pylint/config/find_default_config_files.py
@@ -63,8 +63,12 @@ def find_default_config_files() -> Iterator[Path]:
         if Path(os.environ["PYLINTRC"]).is_file():
             yield Path(os.environ["PYLINTRC"]).resolve()
     else:
-        user_home = Path.home()
-        if str(user_home) not in ("~", "/root"):
+        try:
+            user_home = Path.home()
+        except RuntimeError:
+            # If the home directory does not exist a RuntimeError will be raised
+            user_home = None
+        if user_home is not None and str(user_home) not in ("~", "/root"):
             home_rc = user_home / ".pylintrc"
             if home_rc.is_file():
                 yield home_rc.resolve()
