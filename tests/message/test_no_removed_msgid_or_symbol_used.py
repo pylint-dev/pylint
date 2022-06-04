f388437a11cd39350fcd738b6a93c128c2b8d92e
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-06-04 10:41:54 +0200

    [deleted messages] Add an exception for deleted messages in MessageIdStore

--- tests/message/test_no_removed_msgid_or_symbol_used.py
+++ tests/message/test_no_removed_msgid_or_symbol_used.py
@@ -3,7 +3,7 @@
 # Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
 
 from pylint.lint import PyLinter
-from pylint.message.deleted_message_ids import _DELETED_MESSAGES_IDS
+from pylint.message._deleted_message_ids import DELETED_MESSAGES_IDS
 
 
 def test_no_removed_msgid_or_symbol_used(linter: PyLinter) -> None:
@@ -12,7 +12,7 @@ def test_no_removed_msgid_or_symbol_used(linter: PyLinter) -> None:
     This could cause occasional bugs, but more importantly confusion and inconsistencies
     when searching for old msgids online. See https://github.com/PyCQA/pylint/issues/5729
     """
-    for deleted_messages in _DELETED_MESSAGES_IDS.values():
+    for deleted_messages in DELETED_MESSAGES_IDS.values():
         for msgid, symbol, old_names in deleted_messages:
             linter.msgs_store.message_id_store.register_message_definition(
                 msgid, symbol, old_names
