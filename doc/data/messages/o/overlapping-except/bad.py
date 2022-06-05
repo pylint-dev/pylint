d8065fe72d428b4768ccb66a036306b42938f760
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-06-05 20:57:50 +0900

    Apply suggestions from code review

--- doc/data/messages/o/overlapping-except/bad.py
+++ doc/data/messages/o/overlapping-except/bad.py
@@ -2,4 +2,4 @@ def divide_x_by_y(x: float, y: float):
     try:
         print(x / y)
     except (ArithmeticError, FloatingPointError) as e:  # [overlapping-except]
-        print(f"There was an issue : {e}")
+        print(f"There was an issue: {e}")
--- doc/data/messages/o/overlapping-except/good.py
+++ doc/data/messages/o/overlapping-except/good.py
@@ -13,4 +13,4 @@ def divide_x_by_y(x: float, y: float):
     try:
         print(x / y)
     except ArithmeticError as e:
-        print(f"There was an OverflowError, a ZeroDivisionError or a FloatingPointError : {e}")
+        print(f"There was an OverflowError, a ZeroDivisionError or a FloatingPointError: {e}")
