0772710bbf6c8ca5df1e91094ba7624bbad4e53f
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-06-05 17:42:42 +0900

    [doc] Add `super-init-not-called` example (#6840)
    Signed-off-by: harupy <hkawamura0130@gmail.com>
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/s/super-init-not-called/bad.py
+++ doc/data/messages/s/super-init-not-called/bad.py
@@ -5,5 +5,5 @@ class Fruit:
 
 
 class Apple(Fruit):
-    def __init__(self):
-        super().__init__("apple")
+    def __init__(self):  # [super-init-not-called]
+        print("Creating an apple")
