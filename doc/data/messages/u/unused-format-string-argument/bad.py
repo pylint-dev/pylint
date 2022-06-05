e73522dcd5f94ba16af8cb96b07a654bb3ef5c06
harupy <hkawamura0130@gmail.com>
2022-06-05 17:19:26 +0900

    Add unused-format-string-argument example
    Co-authored-by: Vladyslav Krylasov <vladyslav.krylasov@gmail.com>

---
+++ doc/data/messages/u/unused-format-string-argument/bad.py
@@ -0,0 +1,1 @@
+print("{x} {y}".format(x=1, y=2, z=3))  # [unused-format-string-argument]
--- doc/data/messages/u/unused-format-string-argument/good.py
+++ doc/data/messages/u/unused-format-string-argument/good.py
@@ -1,1 +1,3 @@
-# This is a placeholder for correct code for this message.
+print("{x} {y} {z}".format(x=1, y=2, z=3))
+# or
+print("{x} {y}".format(x=1, y=2))
