9fb3c1c4dd1285e0feb9399c5f61a703bbe95875
Mark Byrne <mbyrnepr2@gmail.com>
2022-06-06 16:58:01 +0200

    Added documentation examples for `nonlocal-and-global`.

--- doc/data/messages/n/nonlocal-and-global/good.py
+++ doc/data/messages/n/nonlocal-and-global/good.py
@@ -1,1 +1,19 @@
-# This is a placeholder for correct code for this message.
+NUMBER = 42
+
+
+def create_number_updater():
+    previous = None
+
+    def update_number(number):
+        global NUMBER
+        NUMBER = number
+        print(f"New global number is: {NUMBER}")
+
+        nonlocal previous
+        print(f"Previous global number was: {previous}")
+        previous = number
+    return update_number
+
+
+update_number = create_number_updater()
+update_number(24)
