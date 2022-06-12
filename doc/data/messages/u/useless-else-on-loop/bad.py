f64ae94fef9d3006aeb4a4caab58526fc1531418
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-06-12 17:40:15 +0200

    Apply suggestions from code review

--- doc/data/messages/u/useless-else-on-loop/bad.py
+++ doc/data/messages/u/useless-else-on-loop/bad.py
@@ -1,5 +1,5 @@
-def find_even_number(lst):
-    for x in lst:
+def find_even_number(numbers):
+    for x in numbers:
         if x % 2 == 0:
             return x
     else:  # [useless-else-on-loop]
--- doc/data/messages/u/useless-else-on-loop/good.py
+++ doc/data/messages/u/useless-else-on-loop/good.py
@@ -1,5 +1,5 @@
-def find_even_number(lst):
-    for x in lst:
+def find_even_number(numbers):
+    for x in numbers:
         if x % 2 == 0:
             return x
     print("Did not find an even number")
