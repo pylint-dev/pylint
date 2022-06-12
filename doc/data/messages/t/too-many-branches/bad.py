0ae10648e8240ec363360759e19d3864000448a7
harupy <hkawamura0130@gmail.com>
2022-06-12 14:29:52 +0900

    Add too-many-branches example

---
+++ doc/data/messages/t/too-many-branches/bad.py
@@ -0,0 +1,23 @@
+def num_to_word(x):  # [too-many-branches]
+    if x == 0:
+        return "zero"
+    elif x == 1:
+        return "one"
+    elif x == 2:
+        return "two"
+    elif x == 3:
+        return "three"
+    elif x == 4:
+        return "four"
+    elif x == 5:
+        return "five"
+    elif x == 6:
+        return "six"
+    elif x == 7:
+        return "seven"
+    elif x == 8:
+        return "eight"
+    elif x == 9:
+        return "nine"
+    else:
+        return None
