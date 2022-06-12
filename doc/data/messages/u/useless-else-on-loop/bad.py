15396045c7037a63419a8f3f2400acb0c76ac87c
harupy <hkawamura0130@gmail.com>
2022-06-13 00:11:00 +0900

    Add useless-else-on-loop example

--- doc/data/messages/u/useless-else-on-loop/bad.py
+++ doc/data/messages/u/useless-else-on-loop/bad.py
@@ -2,4 +2,5 @@ def find_even_number(lst):
     for x in lst:
         if x % 2 == 0:
             return x
-    print("Did not find an even number")
+    else:  # [useless-else-on-loop]
+        print("Did not find an even number")
