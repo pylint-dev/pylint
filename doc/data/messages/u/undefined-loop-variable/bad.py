913ed18cacd9ea81ddbd76c45dff651e8818ecde
Harutaka Kawamura <hkawamura0130@gmail.com>
2022-06-13 00:31:48 +0900

    Apply suggestions from code review
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/u/undefined-loop-variable/bad.py
+++ doc/data/messages/u/undefined-loop-variable/bad.py
@@ -1,6 +1,5 @@
-def find_even_number(lst):
-    for x in lst:
+def find_even_number(numbers):
+    for x in numbers:
         if x % 2 == 0:
             break
-
     return x  # [undefined-loop-variable]
