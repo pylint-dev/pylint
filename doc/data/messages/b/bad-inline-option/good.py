f94114b2ab5a024219b66c6cf310152f8a8c6d31
harupy <hkawamura0130@gmail.com>
2022-06-13 00:20:07 +0900

    Add bad-inline-option example

--- doc/data/messages/b/bad-inline-option/good.py
+++ doc/data/messages/b/bad-inline-option/good.py
@@ -1,1 +1,1 @@
-# This is a placeholder for correct code for this message.
+long_string = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  # pylint: disable=line-too-long
