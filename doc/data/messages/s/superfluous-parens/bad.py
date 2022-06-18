da82f3258642c3f1d2a64b3ffd2ed79ba3b8fa57
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-06-18 19:09:14 +0200

    Apply suggestions from code review

--- doc/data/messages/s/superfluous-parens/bad.py
+++ doc/data/messages/s/superfluous-parens/bad.py
@@ -1,9 +1,9 @@
 x = input()
 y = input()
-if (x == y):
+if (x == y):  # [superfluous-parens]
     pass
 
 i = 0
 exclude = []
-if (i - 0) in exclude:
+if (i - 0) in exclude:  # [superfluous-parens]
     pass
