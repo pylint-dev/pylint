25df881aa4eb9580888a85647efcc008fcb9898e
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-06-19 20:46:19 +0200

    [primer] Add a test for truncating comments

--- tests/testutils/_primer/test_primer.py
+++ tests/testutils/_primer/test_primer.py
@@ -3,6 +3,8 @@
 # Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
 
 """Test the primer commands. """
+from __future__ import annotations
+
 import sys
 from pathlib import Path
 from unittest.mock import patch
@@ -20,39 +22,70 @@ FIXTURES_PATH = HERE / "fixtures"
 
 PRIMER_CURRENT_INTERPRETER = (3, 10)
 
+DEFAULT_ARGS = ["python tests/primer/__main__.py", "compare", "--commit=v2.14.2"]
+
 
 @pytest.mark.skipif(
     sys.platform in {"win32", "darwin"},
-    reason="Primers are internal will never be run on costly github action (mac or windows)",
+    reason=(
+        "Primers are internal and will never be run on costly github action (mac or windows)"
+    ),
 )
 @pytest.mark.skipif(
     sys.version_info[:2] != PRIMER_CURRENT_INTERPRETER or IS_PYPY,
-    reason=f"Primers are internal will always be run for only one interpreter (currently {PRIMER_CURRENT_INTERPRETER})",
+    reason=(
+        "Primers are internal and will always be run for only one interpreter (currently"
+        f" {PRIMER_CURRENT_INTERPRETER})"
+    ),
 )
-@pytest.mark.parametrize(
-    "directory",
-    [
-        pytest.param(p, id=str(p.relative_to(FIXTURES_PATH)))
-        for p in FIXTURES_PATH.iterdir()
-        if p.is_dir()
-    ],
-)
-def test_compare(directory: Path) -> None:
-    main = directory / "main.json"
-    pr = directory / "pr.json"
-    expected_file = directory / "expected.txt"
-    new_argv = [
-        "python tests/primer/__main__.py",
-        "compare",
-        "--commit=v2.14.2",
-        f"--base-file={main}",
-        f"--new-file={pr}",
-    ]
-    with patch("sys.argv", new_argv):
-        Primer(PRIMER_DIRECTORY, PACKAGES_TO_PRIME_PATH).run()
-    with open(PRIMER_DIRECTORY / "comment.txt", encoding="utf8") as f:
-        content = f.read()
-    with open(expected_file, encoding="utf8") as f:
-        expected = f.read()
-    # rstrip so the expected.txt can end with a newline
-    assert content == expected.rstrip("\n")
+class TestPrimer:
+    @pytest.mark.parametrize(
+        "directory",
+        [
+            pytest.param(p, id=str(p.relative_to(FIXTURES_PATH)))
+            for p in FIXTURES_PATH.iterdir()
+            if p.is_dir()
+        ],
+    )
+    def test_compare(self, directory: Path) -> None:
+        """Test for the standard case.
+
+        Directory in 'fixtures/' with 'main.json', 'pr.json' and 'expected.txt'."""
+        self.__assert_expected(directory)
+
+    def test_truncated_compare(self) -> None:
+        """Test for the truncation of comments that are too long."""
+        max_comment_length = 500
+        directory = FIXTURES_PATH / "message_changed"
+        with patch(
+            "pylint.testutils._primer.primer_compare_command.MAX_GITHUB_COMMENT_LENGTH",
+            max_comment_length,
+        ):
+            content = self.__assert_expected(
+                directory, expected_file=directory / "expected_truncated.txt"
+            )
+        assert len(content) < max_comment_length
+
+    @staticmethod
+    def __assert_expected(
+        directory: Path,
+        main: Path | None = None,
+        pr: Path | None = None,
+        expected_file: Path | None = None,
+    ) -> str:
+        if main is None:
+            main = directory / "main.json"
+        if pr is None:
+            pr = directory / "pr.json"
+        if expected_file is None:
+            expected_file = directory / "expected.txt"
+        new_argv = DEFAULT_ARGS + [f"--base-file={main}", f"--new-file={pr}"]
+        with patch("sys.argv", new_argv):
+            Primer(PRIMER_DIRECTORY, PACKAGES_TO_PRIME_PATH).run()
+        with open(PRIMER_DIRECTORY / "comment.txt", encoding="utf8") as f:
+            content = f.read()
+        with open(expected_file, encoding="utf8") as f:
+            expected = f.read()
+        # rstrip so the expected.txt can end with a newline
+        assert content == expected.rstrip("\n")
+        return content
