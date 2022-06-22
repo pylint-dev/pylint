415af6deca139b3951605369de4a15bb4f318840
mpernigo <marco.pernigotti0@gmail.com>
2022-06-22 13:45:14 +0200

    Move ad hoc tests for python 3.8 to their own functional test

---
+++ tests/functional/ext/docparams/docparams_py38.py
@@ -0,0 +1,22 @@
+"""Fixture for testing missing documentation in docparams."""
+
+
+def differing_param_doc_pos_only(par1: int, /) -> int:  # [differing-param-doc]
+    """This is a test docstring documenting one non-existing param
+
+    :param par1: some param
+    :param param: some param
+    :return: the sum of the params
+    """
+
+    return par1
+
+
+def missing_type_doc_pos_only(par1, /) -> int:  # [missing-type-doc]
+    """This is a test docstring params where the type is not specified
+
+    :param par1: some param
+    :return: the param
+    """
+
+    return par1
