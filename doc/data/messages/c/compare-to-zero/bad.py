d7f17fadd4fa393540c80c9a1fc79a89d5d8aaf7
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-23 22:41:19 +0700

    Revise the examples
    Based on `compare-to-empty-string` examples

--- doc/data/messages/c/compare-to-zero/bad.py
+++ doc/data/messages/c/compare-to-zero/bad.py
@@ -1,2 +1,8 @@
-if int(input()) == 0:   # [compare-to-zero]
-    pass
+x = 0
+y = 1
+
+if x == 0:   # [compare-to-zero]
+    print("x is equal to zero")
+
+if y != 0:   # [compare-to-zero]
+    print("y is not equal to zero")
--- doc/data/messages/c/compare-to-zero/good.py
+++ doc/data/messages/c/compare-to-zero/good.py
@@ -1,2 +1,8 @@
-if bool(int(input())):
-    pass
+x = 0
+y = 1
+
+if not x:
+    print("x is equal to zero")
+
+if y:
+    print("y is not equal to zero")
