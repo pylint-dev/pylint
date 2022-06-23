ae3051ae575d7fd6ad3dbc78e2ae81d750d4b3ef
Jacob Walls <jacobtylerwalls@gmail.com>
2022-06-22 21:22:18 -0400

    add staticmethod decorators

--- doc/data/messages/o/order-dependent-resolution/bad.py
+++ doc/data/messages/o/order-dependent-resolution/bad.py
@@ -1,9 +1,11 @@
 class HarshParent:
+    @staticmethod
     def scold():
         return "Don't do that!"
 
 
 class KindParent:
+    @staticmethod
     def scold():
         return "It would be better for you in the long run if you wouldn't do that."
 
--- doc/data/messages/o/order-dependent-super-resolution/good.py
+++ doc/data/messages/o/order-dependent-super-resolution/good.py
@@ -1,13 +1,16 @@
 class HarshParent:
+    @staticmethod
     def scold():
         return "Don't do that!"
 
 
 class KindParent:
+    @staticmethod
     def scold():
         return "It would be better for you in the long run if you wouldn't do that."
 
 
 class Child(HarshParent, KindParent):
+    @staticmethod
     def scold():
         HarshParent.scold()
