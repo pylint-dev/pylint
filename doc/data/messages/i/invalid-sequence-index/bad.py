d33a2f34927f02d12600db5d7e47125638924002
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-25 01:06:55 +0700

    Update bad.py

--- doc/data/messages/i/invalid-sequence-index/bad.py
+++ doc/data/messages/i/invalid-sequence-index/bad.py
@@ -1,4 +1,4 @@
 fruits = ['apple', 'banana', 'orange']
-print(fruits["apple"])  # [invalid-sequence-index]
+print(fruits['apple'])  # [invalid-sequence-index]
 # but be careful with data[True], it will work since True is represnted as 1
 # and False as 0, so it will work
