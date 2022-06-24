1c3a17b540407a45553ac1a39d8bf5fb00bd3ca1
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-25 02:03:17 +0700

    Move warning from 'bad.py' to 'details.rst'

--- doc/data/messages/i/invalid-sequence-index/bad.py
+++ doc/data/messages/i/invalid-sequence-index/bad.py
@@ -1,4 +1,2 @@
 fruits = ['apple', 'banana', 'orange']
 print(fruits['apple'])  # [invalid-sequence-index]
-# but be careful with data[True], it will work since True is represnted as 1
-# and False as 0, so it will work
---
+++ doc/data/messages/i/invalid-sequence-index/details.rst
@@ -0,0 +1,1 @@
+Be careful with [True] or [False] as sequence index, since True is represnted as 1 and False as 0, so it will work.
