19c1e1116debef76cebccd6647552b6a5c97eeb5
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-25 10:21:35 +0700

    Update basic_checker.py

--- pylint/checkers/base/basic_checker.py
+++ pylint/checkers/base/basic_checker.py
@@ -166,10 +166,10 @@ class BasicChecker(_BasicChecker):
         "W0122": (
             "Use of exec",
             "exec-used",
-            'Used when you use the "exec" statement (function for Python '
-            "3), to discourage its usage. That doesn't "
-            "mean you cannot use it! "
-            "It's dangerous to use this function for a user input.",
+            'Raised when the "exec" statement is used. '
+            "It's dangerous to use this function for a user input, "
+            "and it's also slower than actual code in general. This doesn't "
+            "mean you should never use it, but you should consider alternatives first.",
         ),
         "W0123": (
             "Use of eval",
