2d0154a74165fe202a5d55c92b62adc432ce2295
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-25 13:40:23 +0700

    Update doc/data/messages/e/exec-used/good.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/e/exec-used/good.py
+++ doc/data/messages/e/exec-used/good.py
@@ -2,4 +2,6 @@ def get_user_code(username):
     return input(f'Enter code to be executed please, {username}: ')
 
 username = "Ada"
-exec(get_user_code(username))  # pylint: disable=exec-used
+globals = {'__builtins__' : None}
+locals = {'print': print}
+exec(get_user_code(username), globals, locals)  # pylint: disable=exec-used
