422adf109ccd8877f9476304538bb9fd6cbabe81
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-25 18:42:21 +0700

    Update doc/data/messages/n/non-iterator-returned/good.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/n/non-iterator-returned/good.py
+++ doc/data/messages/n/non-iterator-returned/good.py
@@ -1,15 +1,25 @@
-class Iterator:
-    def __init__(self, end, start=0):
-        self.n = start
-        self.end = end
+import random
+
+
+class GenericAstrology:
+    def __init__(self, signs, predictions):
+        self.signs = signs
+        self.predictions = predictions
 
     def __iter__(self):
+        self.index = 0
+        self.number_of_prediction = len(self.predictions)
         return self
 
     def __next__(self):
-        if self.n <= self.end:
-            n = self.n
-            self.n += 1
-            return n
+        if self.index == len(self.signs):
+            raise StopIteration
+        self.index += 1
+        prediction_index = random.randint(0, self.number_of_prediction - 1)
+        return self.signs[self.index - 1], self.predictions[prediction_index]
 
-        raise StopIteration
+
+SIGNS = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra"]
+PREDICTIONS = ["good things", "bad thing", "existential dread"]
+for sign, prediction in GenericAstrology(SIGNS, PREDICTIONS):
+    print(f"{sign} : {prediction} today")
