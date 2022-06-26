0838d07527cba9ec4f893df522dd26ff6ed39d6b
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-06-26 08:06:44 +0200

    Apply suggestions from code review

--- doc/data/messages/r/raising-bad-type/bad.py
+++ doc/data/messages/r/raising-bad-type/bad.py
@@ -1,5 +1,10 @@
-def div(x, y):
+class FasterThanTheSpeedOfLightError(ZeroDivisionError):
+    def __init__(self):
+        super().__init__("You can't go faster than the speed of light !")
+
+
+def calculate_speed(distance: float, time: float) -> float:
     try:
-        return x / y
-    except ZeroDivisionError:
+        return distance / time
+    except ZeroDivisionError as e:
         raise None  # [raising-bad-type]
