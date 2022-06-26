5bdc567aa8e2c38cdb7f966259553890558166fd
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-26 15:26:39 +0700

    Update doc/data/messages/t/too-many-instance-attributes/bad.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/t/too-many-instance-attributes/bad.py
+++ doc/data/messages/t/too-many-instance-attributes/bad.py
@@ -1,11 +1,13 @@
-class Foo:  # [too-many-instance-attributes]
+class Fruit:  # [too-many-instance-attributes]
     def __init__(self):
-        # max of 7 by default, can be configured
-        self.a = None
-        self.b = None
-        self.c = None
-        self.d = None
-        self.e = None
-        self.f = None
-        self.g = None
-        self.h = None
+        # max of 7 attributes by default, can be configured
+        self.worm_name = "Jimmy"
+        self.worm_type = "Codling Moths"
+        self.worm_color = "light brown"
+        self.fruit_name = "Little Apple"
+        self.fruit_color = "Bright red"
+        self.fruit_vitamins = ["A", "B1"]
+        self.fruit_antioxidants = None
+        self.secondary_worm_name = "Kim"
+        self.secondary_worm_type = "Apple maggot"
+        self.secondary_worm_color = "Whitish"        
