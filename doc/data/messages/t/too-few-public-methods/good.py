d75d1cde1b250737376f4b96101ef2e32962e0ce
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-26 15:00:43 +0700

    Update doc/data/messages/t/too-few-public-methods/good.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/t/too-few-public-methods/good.py
+++ doc/data/messages/t/too-few-public-methods/good.py
@@ -1,6 +1,27 @@
-class Worm:
-    def bore(self):
-        pass
+import dataclasses
 
-    def eat(self):
-        pass
+class Worm: 
+    def __init__(self, name: str, fruit_of_residence: Fruit):
+        self.name = name
+        self.fruit_of_residence = fruit_of_residence
+
+    def bore(self):
+        print(f"{self.name} is boring into {self.fruit_of_residence}")
+
+    def wiggle(self):
+        print(f"{self.name} wiggle around wormily.")
+
+# or
+
+@dataclasses.dataclass
+class Worm:
+    name:str
+    fruit_of_residence: Fruit
+
+def bore(worm: Worm):
+    print(f"{worm.name} is boring into {worm.fruit_of_residence}")
+
+# or
+
+def bore(fruit: Fruit, worm_name: str):
+    print(f"{worm_name} is boring into {fruit}")
