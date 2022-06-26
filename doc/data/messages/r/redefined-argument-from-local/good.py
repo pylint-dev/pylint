4a7c758466d10a0b9dd376ff46fbe30f69a9e279
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-06-26 06:23:44 +0200

    Update doc/data/messages/r/redefined-argument-from-local/good.py

--- doc/data/messages/r/redefined-argument-from-local/good.py
+++ doc/data/messages/r/redefined-argument-from-local/good.py
@@ -1,3 +1,3 @@
 def show(host_id=10.11):
-    for h_id, host in [[12.13, 'Venus'], [14.15, 'Mars']]:
-        print(host_id, h_id, host)
+    for inner_host_id, host in [[12.13, 'Venus'], [14.15, 'Mars']]:
+        print(host_id, inner_host_id, host)
