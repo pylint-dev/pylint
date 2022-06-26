97332b76ed5d11c8dbb7bcee3402adbd80c2113c
Julthep Nandakwang <julthep@nandakwang.com>
2022-06-26 15:26:58 +0700

    Update doc/data/messages/t/too-many-instance-attributes/good.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/t/too-many-instance-attributes/good.py
+++ doc/data/messages/t/too-many-instance-attributes/good.py
@@ -1,5 +1,20 @@
-class Foo:
+import dataclasses
+
+
+@dataclasses.dataclass
+class Worm:
+    name: str
+    type: str
+    color: str
+
+
+class Fruit:
     def __init__(self):
-        self.a = None
-        self.b = None
-        self.c = None
+        self.name = "Little Apple"
+        self.color = "Bright red"
+        self.vitamins = ["A", "B1"]
+        self.antioxidants = None
+        self.worms = [
+            Worm(name="Jimmy", type="Codling Moths", color="light brown"),
+            Worm(name="Kim", type="Apple maggot", color="Whitish"),
+        ]
