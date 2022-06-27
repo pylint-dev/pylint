497821ce140442cc44afd9fc6183b0878b3eb312
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-06-27 20:55:34 +0200

    Apply suggestions from code review

--- doc/data/messages/t/too-many-ancestors/good.py
+++ doc/data/messages/t/too-many-ancestors/good.py
@@ -1,11 +1,33 @@
-class A: ...
-class B: ...
-class C: ...
-class D: ...
-class E: ...
-class F: ...
-class G: ...
-class H: ...
+class Animal:
+    beaver_tailed: bool
+    can_swim: bool
+    has_beak: bool
+    has_fur: bool
+    has_vertebrae: bool
+    lays_egg: bool
+    protected_specie: bool
+    venomous: bool
 
-class LessComplex(A, B, C):
-    pass
+
+class Invertebrate(Animal):
+    has_vertebrae = False
+
+
+class Vertebrate(Animal):
+    has_vertebrae = True
+
+
+class Mammal(Vertebrate):
+    has_beak = False
+    has_fur = True
+    lays_egg = False
+    venomous = False
+
+
+class Playtypus(Mammal):
+    beaver_tailed = True
+    can_swim = True
+    has_beak = True
+    lays_egg = False
+    protected_specie = True
+    venomous = True
