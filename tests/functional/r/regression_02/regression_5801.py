05341c186256219888a1b1c2aa3541cd29770733
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-07-06 21:17:22 +0200

    Update tests/functional/r/regression_02/regression_5801.py

--- tests/functional/r/regression_02/regression_5801.py
+++ tests/functional/r/regression_02/regression_5801.py
@@ -1,4 +1,5 @@
-# pylint: disable=missing-module-docstring, missing-docstring
+# https://github.com/PyCQA/pylint/issues/5801
+# pylint: disable=missing-docstring
 
 import struct
 struct.unpack('h', b'\x00\x01')
