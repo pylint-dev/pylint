44164852a2dcadd34375eb5e4e977d6b2a9ef5bb
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-07-09 13:11:02 +0200

    Update doc/data/messages/c/consider-using-any-or-all/good.py

--- doc/data/messages/c/consider-using-any-or-all/good.py
+++ doc/data/messages/c/consider-using-any-or-all/good.py
@@ -1,4 +1,9 @@
 items = (1, 2, 3, 4, 5)
 
-any_even = any(item % 2 == 0 for item in items)
-all_even = all(item % 2 == 0 for item in items)
+def any_even(items):
+    """Return True if the list contains any even numbers"""
+    return any(item % 2 == 0 for item in items)
+
+def all_even(items):
+    """Return True if the list contains all even numbers"""
+    return all(item % 2 == 0 for item in items)
