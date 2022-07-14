88437827c2c9143d906257d04b6c5e2b7436d904
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-07-14 11:30:19 +0200

    [primer] Fail early instead of priming all the packages

--- pylint/testutils/_primer/primer_run_command.py
+++ pylint/testutils/_primer/primer_run_command.py
@@ -8,13 +8,13 @@ import json
 import sys
 import warnings
 from io import StringIO
-from itertools import chain
 
 from pylint.lint import Run
 from pylint.message import Message
 from pylint.reporters import JSONReporter
+from pylint.reporters.json_reporter import OldJsonExport
 from pylint.testutils._primer.package_to_lint import PackageToLint
-from pylint.testutils._primer.primer_command import PackageMessages, PrimerCommand
+from pylint.testutils._primer.primer_command import PrimerCommand
 
 GITHUB_CRASH_TEMPLATE_LOCATION = "/home/runner/.cache"
 CRASH_TEMPLATE_INTRO = "There is a pre-filled template"
@@ -22,49 +22,38 @@ CRASH_TEMPLATE_INTRO = "There is a pre-filled template"
 
 class RunCommand(PrimerCommand):
     def run(self) -> None:
-        packages: PackageMessages = {}
-
+        packages: dict[str, list[OldJsonExport]] = {}
         for package, data in self.packages.items():
-            packages[package] = self._lint_package(data)
-            print(f"Successfully primed {package}.")
+            packages[package] = self._lint_package(package, data)
+        path = (
+            self.primer_directory
+            / f"output_{'.'.join(str(i) for i in sys.version_info[:3])}_{self.config.type}.txt"
+        )
+        print(f"Writing result in {path}")
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(packages, f)
 
+    def _get_astroid_errors(
+        self, messages: list[OldJsonExport]
+    ) -> tuple[list[Message], list[Message]]:
         astroid_errors = []
         other_fatal_msgs = []
-        for msg in chain.from_iterable(packages.values()):
-            if msg.category == "fatal":
-                if GITHUB_CRASH_TEMPLATE_LOCATION in msg.msg:
+        for raw_message in messages:
+            message = JSONReporter.deserialize(raw_message)
+            if message.category == "fatal":
+                if GITHUB_CRASH_TEMPLATE_LOCATION in message.msg:
                     # Remove the crash template location if we're running on GitHub.
                     # We were falsely getting "new" errors when the timestamp changed.
-                    msg.msg = msg.msg.rsplit(CRASH_TEMPLATE_INTRO)[0]
-                if msg.symbol == "astroid-error":
-                    astroid_errors.append(msg)
+                    message.msg = message.msg.rsplit(CRASH_TEMPLATE_INTRO)[0]
+                if message.symbol == "astroid-error":
+                    astroid_errors.append(message)
                 else:
-                    other_fatal_msgs.append(msg)
+                    other_fatal_msgs.append(message)
+        return astroid_errors, other_fatal_msgs
 
-        with open(
-            self.primer_directory
-            / f"output_{'.'.join(str(i) for i in sys.version_info[:3])}_{self.config.type}.txt",
-            "w",
-            encoding="utf-8",
-        ) as f:
-            json.dump(
-                {
-                    p: [JSONReporter.serialize(m) for m in msgs]
-                    for p, msgs in packages.items()
-                },
-                f,
-            )
-
-        # Fail loudly (and fail CI pipelines) if any fatal errors are found,
-        # unless they are astroid-errors, in which case just warn.
-        # This is to avoid introducing a dependency on bleeding-edge astroid
-        # for pylint CI pipelines generally, even though we want to use astroid main
-        # for the purpose of diffing emitted messages and generating PR comments.
-        if astroid_errors:
-            warnings.warn(f"Fatal errors traced to astroid:  {astroid_errors}")
-        assert not other_fatal_msgs, other_fatal_msgs
-
-    def _lint_package(self, data: PackageToLint) -> list[Message]:
+    def _lint_package(
+        self, package_name: str, data: PackageToLint
+    ) -> list[OldJsonExport]:
         # We want to test all the code we can
         enables = ["--enable-all-extensions", "--enable=all"]
         # Duplicate code takes too long and is relatively safe
@@ -73,5 +62,31 @@ class RunCommand(PrimerCommand):
         arguments = data.pylint_args + enables + disables
         output = StringIO()
         reporter = JSONReporter(output)
-        Run(arguments, reporter=reporter, exit=False)
-        return [JSONReporter.deserialize(m) for m in json.loads(output.getvalue())]
+        print(f"Running 'pylint {', '.join(arguments)}'")
+        pylint_exit_code = -1
+        try:
+            Run(arguments, reporter=reporter)
+        except SystemExit as e:
+            pylint_exit_code = int(e.code)
+        readable_messages: str = output.getvalue()
+        messages: list[OldJsonExport] = json.loads(readable_messages)
+        if pylint_exit_code % 2 == 0:
+            print(f"Successfully primed {package_name}.")
+        else:
+            print(
+                f"Encountered fatal errors while priming {package_name} !\n{readable_messages}"
+            )
+            astroid_errors, other_fatal_msgs = self._get_astroid_errors(messages)
+            # Fail loudly (and fail CI pipelines) if any fatal errors are found,
+            # unless they are astroid-errors, in which case just warn.
+            # This is to avoid introducing a dependency on bleeding-edge astroid
+            # for pylint CI pipelines generally, even though we want to use astroid main
+            # for the purpose of diffing emitted messages and generating PR comments.
+            if astroid_errors:
+                warnings.warn(f"Fatal errors traced to astroid: {astroid_errors}")
+            plural = "s" if len(other_fatal_msgs) > 1 else ""
+            assert not other_fatal_msgs, (
+                f"We encountered {len(other_fatal_msgs)} fatal error message{plural}"
+                " that can't be attributed to bleeding edge astroid alone (see log)."
+            )
+        return messages
