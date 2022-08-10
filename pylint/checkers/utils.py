f3c08e5e34c1f6da6ae462318d5e1f07982cc7db
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-08-10 09:27:43 +0200

    Revert impossible import

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -22,6 +22,7 @@ import astroid.objects
 from astroid import TooManyLevelsError, nodes
 from astroid.context import InferenceContext
 from astroid.exceptions import AstroidError
+from astroid.nodes._base_nodes import ImportNode
 from astroid.typing import InferenceResult, SuccessfulInferenceResult
 
 if TYPE_CHECKING:
@@ -1652,7 +1653,7 @@ def get_subscript_const_value(node: nodes.Subscript) -> nodes.Const:
     return inferred
 
 
-def get_import_name(importnode: nodes.ImportNode, modname: str | None) -> str | None:
+def get_import_name(importnode: ImportNode, modname: str | None) -> str | None:
     """Get a prepared module name from the given import node.
 
     In the case of relative imports, this will return the
