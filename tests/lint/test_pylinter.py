673be3f2a1576c2cf30a3b5e0358e81cc73f62eb
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-08-15 20:28:13 +0200

    Add test

--- tests/lint/test_pylinter.py
+++ tests/lint/test_pylinter.py
@@ -3,6 +3,7 @@
 # Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
 
 from typing import Any, NoReturn
+from unittest import mock
 from unittest.mock import patch
 
 import pytest
@@ -41,3 +42,19 @@ def test_check_deprecation(linter: PyLinter, recwarn):
     linter.check("myfile.py")
     msg = recwarn.pop()
     assert "check function will only accept sequence" in str(msg)
+
+
+def test_crash_during_linting(
+    linter: PyLinter, capsys: CaptureFixture[str], tmpdir: LocalPath
+) -> None:
+    with mock.patch(
+        "pylint.lint.PyLinter.check_astroid_module", side_effect=RuntimeError
+    ):
+        linter.crash_file_path = str(tmpdir / "pylint-crash-%Y")
+        linter.check([__file__])
+        out, err = capsys.readouterr()
+        assert not out
+        assert not err
+        files = tmpdir.listdir()
+        assert len(files) == 1
+        assert "pylint-crash-20" in str(files[0])
