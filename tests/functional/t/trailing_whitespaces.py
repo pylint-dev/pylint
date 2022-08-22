e4d52f4832ceadd77a04c9be2071b1340f279c2d
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-08-22 10:04:41 +0200

    Don't emit ``trailing-whitespace`` twice for multi-line docstrings

--- tests/functional/t/trailing_whitespaces.py
+++ tests/functional/t/trailing_whitespaces.py
@@ -1,5 +1,5 @@
 """Regression test for trailing-whitespace (C0303)."""
-# pylint: disable=mixed-line-endings
+# pylint: disable=mixed-line-endings,pointless-string-statement
 
 # +1: [trailing-whitespace]
 print('some trailing whitespace')   
@@ -8,3 +8,13 @@ print('trailing whitespace does not count towards the line length limit')
 print('windows line ends are ok')
 # +1: [trailing-whitespace]
 print('but trailing whitespace on win is not')   
+
+# Regression test for https://github.com/PyCQA/pylint/issues/6936
+# +2: [trailing-whitespace]
+""" This module has the Board class.
+""" 
+
+# +3: [trailing-whitespace]
+""" This module has the Board class.
+It's a very nice Board.
+""" 
