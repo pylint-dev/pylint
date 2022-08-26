24eaec26f701cf8adddf27ffa8859e0bb5074a89
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-08-26 10:37:28 +0200

    Bump pylint to 2.15.0, update changelog (#7355)
    * Add Marc Byrne to the maintainer team

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -9,7 +9,7 @@ It's updated via tbump, do not modify.
 
 from __future__ import annotations
 
-__version__ = "2.15.0-a0"
+__version__ = "2.15.0"
 
 
 def get_numversion_from_version(v: str) -> tuple[int, int, int]:
