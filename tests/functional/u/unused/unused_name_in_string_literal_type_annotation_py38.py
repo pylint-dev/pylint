18522c1963fd0c283b14cef254603b20ba23df37
Levi Gruspe <mail.levig@gmail.com>
2022-09-02 23:18:29 +0800

    Test if unused-variable gets emitted if variable has the same name as typing.Literal arg

--- tests/functional/u/unused/unused_name_in_string_literal_type_annotation_py38.py
+++ tests/functional/u/unused/unused_name_in_string_literal_type_annotation_py38.py
@@ -5,4 +5,11 @@ from typing import Literal as Lit
 import typing as t
 
 # str inside Literal shouldn't be treated as names
-example: t.Literal["ArgumentParser", Lit["Namespace", "ArgumentParser"]]
+example1: t.Literal["ArgumentParser", Lit["Namespace", "ArgumentParser"]]
+
+
+def unused_variable_example():
+    hello = "hello" # [unused-variable]
+    world = "world" # [unused-variable]
+    example2: Lit["hello", "world"] = "hello"
+    return example2
