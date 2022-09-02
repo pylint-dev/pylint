0fbbddb9881613a6b8407b8c9d065bcc08acac4b
Levi Gruspe <mail.levig@gmail.com>
2022-09-02 23:09:50 +0800

    Fix more broken tests

--- tests/functional/u/unused/unused_name_in_string_literal_type_annotation.py
+++ tests/functional/u/unused/unused_name_in_string_literal_type_annotation.py
@@ -1,18 +1,12 @@
 """Test if pylint sees names inside string literal type annotations. #3299"""
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
-from typing import TypeAlias
-
-def unused_variable_should_not_be_emitted():
-    """unused-variable shouldn't be emitted for Example1."""
-    Example1: TypeAlias = int
-    result: set["Example1"] = set()
-    return result
+from typing import Set
 
 # unused-import shouldn't be emitted for Path
-example2: set["Path"] = set()
+example1: Set["Path"] = set()
 
-def example3(_: "ArgumentParser"):
+def example2(_: "ArgumentParser"):
     """unused-import shouldn't be emitted for ArgumentParser"""
 
 # pylint: disable=too-few-public-methods
