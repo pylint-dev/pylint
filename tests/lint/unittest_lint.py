b1e02187af55a892487dd65300ce6fd9102bb0d1
Drum Ogilvie <me@daogilvie.com>
2022-09-03 11:58:16 +0100

    Add test for bad plugin with duplicated load
    This is case 6 in issue #7264, and represents the other silent failure
    that is being made non-silent by this change.

--- tests/lint/unittest_lint.py
+++ tests/lint/unittest_lint.py
@@ -524,6 +524,71 @@ def test_load_plugin_command_line() -> None:
 
     sys.path.remove(dummy_plugin_path)
 
+@pytest.mark.usefixtures("pop_pylintrc")
+def test_load_plugin_command_line_and_config_with_inithook_in_config_bad_value() -> None:
+    dummy_plugin_path = abspath(join(REGRTEST_DATA_DIR, "dummy_plugin"))
+    with fake_home() as home_path:
+        # construct a basic rc file that just modifies the path
+        pylintrc_file = join(home_path, "pylintrc")
+        with open(pylintrc_file, "w", encoding="utf8") as out:
+            out.writelines(
+                [
+                    "[MASTER]\n",
+                    f"init-hook=\"import sys; sys.path.append('{home_path}')\"\n",
+                    "load-plugins=copy_dummy\n"
+
+                ]
+            )
+
+        copytree(dummy_plugin_path, join(home_path, "copy_dummy"))
+
+        # To confirm we won't load this module _without_ the init hook running.
+        assert home_path not in sys.path
+
+        run = Run(
+            [
+                "--rcfile",
+                pylintrc_file,
+                "--load-plugins",
+                "copy_dummy",
+                join(REGRTEST_DATA_DIR, "empty.py"),
+            ],
+            reporter=testutils.GenericTestReporter(),
+            exit=False,
+        )
+        assert run._rcfile == pylintrc_file
+        assert home_path in sys.path
+        # The module should not be loaded
+        assert (
+            len(
+                [ch.name for ch in run.linter.get_checkers() if ch.name == "copy_dummy"]
+            )
+            == 0
+        )
+
+        # There should be a bad-plugin-message for this module
+        assert len(run.linter.reporter.messages) == 1
+        assert run.linter.reporter.messages[0] == Message(
+            msg_id="E0013",
+            symbol="bad-plugin-value",
+            msg="Plugin 'copy_dummy' is impossible to load, is it installed ? ('No module named 'copy_dummy'')",
+            confidence=interfaces.Confidence(
+                name="UNDEFINED",
+                description="Warning without any associated confidence level.",
+            ),
+            location=MessageLocationTuple(
+                abspath="Command line or configuration file",
+                path="Command line or configuration file",
+                module="Command line or configuration file",
+                obj="",
+                line=1,
+                column=0,
+                end_line=None,
+                end_column=None,
+            ),
+        )
+
+        sys.path.remove(home_path)
 
 @pytest.mark.usefixtures("pop_pylintrc")
 def test_load_plugin_command_line_with_inithook_in_config_bad_value() -> None:
