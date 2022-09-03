84ce348b8d30a5114134dcb4047fbe95e19be7b2
Levi Gruspe <mail.levig@gmail.com>
2022-09-03 11:11:49 +0800

    Test if pylint sees names in return type annotation

--- tests/functional/u/unused/unused_name_in_string_literal_type_annotation.py
+++ tests/functional/u/unused/unused_name_in_string_literal_type_annotation.py
@@ -1,13 +1,15 @@
 """Test if pylint sees names inside string literal type annotations. #3299"""
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
-from typing import Set
+from typing import NoReturn, Set
 
 # unused-import shouldn't be emitted for Path
 example1: Set["Path"] = set()
 
-def example2(_: "ArgumentParser"):
-    """unused-import shouldn't be emitted for ArgumentParser"""
+def example2(_: "ArgumentParser") -> "NoReturn":
+    """unused-import shouldn't be emitted for ArgumentParser or NoReturn."""
+    while True:
+        pass
 
 # pylint: disable=too-few-public-methods
 class Class:
