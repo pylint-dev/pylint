2f766a1dcde49c76de1c826eb8df8cbbeb2e7c71
Levi Gruspe <mail.levig@gmail.com>
2022-09-04 19:10:41 +0800

    Fix #3299 false positives with names in string literal type annotations (#7400)
    Don't emit 'unused-variable' or 'unused-import' on names in string literal type annotations (#3299)
    Don't treat strings inside typing.Literal as names

---
+++ tests/functional/u/unused/unused_name_in_string_literal_type_annotation.py
@@ -0,0 +1,26 @@
+"""Test if pylint sees names inside string literal type annotations. #3299"""
+# pylint: disable=too-few-public-methods
+
+from argparse import ArgumentParser, Namespace
+import os
+from os import PathLike
+from pathlib import Path
+from typing import NoReturn, Set
+
+# unused-import shouldn't be emitted for Path
+example1: Set["Path"] = set()
+
+def example2(_: "ArgumentParser") -> "NoReturn":
+    """unused-import shouldn't be emitted for ArgumentParser or NoReturn."""
+    while True:
+        pass
+
+def example3(_: "os.PathLike[str]") -> None:
+    """unused-import shouldn't be emitted for os."""
+
+def example4(_: "PathLike[str]") -> None:
+    """unused-import shouldn't be emitted for PathLike."""
+
+class Class:
+    """unused-import shouldn't be emitted for Namespace"""
+    cls: "Namespace"
