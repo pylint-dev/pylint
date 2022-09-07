05a956f4fb1a54f8feeab37d87706f000c36a23e
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-09-07 11:16:37 +0200

    Spelling is hard

--- tests/lint/unittest_lint.py
+++ tests/lint/unittest_lint.py
@@ -1,6 +1,6 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# For details: https://GitHub.com/PyCQA/pylint/blob/main/LICENSE
-# Copyright (c) https://GitHub.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
+# For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
+# Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
 
 # pylint: disable=redefined-outer-name
 
