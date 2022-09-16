21db8db2030505f4a75f05a2df14775c5ced7a2d
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-09-16 10:20:43 +0200

    [invalid-class-object] Fix crash when __class__ is defined with a tuple
    Closes #7467

--- tests/functional/i/invalid/invalid_class_object.py
+++ tests/functional/i/invalid/invalid_class_object.py
@@ -1,11 +1,14 @@
 # pylint: disable=missing-docstring,too-few-public-methods,invalid-name
 from collections import defaultdict
 
+
 class A:
     pass
 
+
 class B:
     pass
+
 
 A.__class__ = B
 A.__class__ = str
@@ -30,3 +33,43 @@ class C:
         obj = C()
         obj.__class__ = self.__class__
         return obj
+
+
+class AnotherClass:
+    ...
+
+
+class Pylint7429Good:
+    """See https://github.com/PyCQA/pylint/issues/7467"""
+
+    def class_defining_function_good(self):
+        self.__class__, myvar = AnotherClass, "myvalue"
+        print(myvar)
+
+    def class_defining_function_bad(self):
+        self.__class__, myvar = 1, "myvalue"  # [invalid-class-object]
+        print(myvar)
+
+    def class_defining_function_good_inverted(self):
+        myvar, self.__class__ = "myvalue", AnotherClass
+        print(myvar)
+
+    def class_defining_function_bad_inverted(self):
+        myvar, self.__class__ = "myvalue", 1  # [invalid-class-object]
+        print(myvar)
+
+    def class_defining_function_complex_bad(self):
+        myvar, self.__class__, other = (  # [invalid-class-object]
+            "myvalue",
+            1,
+            "othervalue",
+        )
+        print(myvar, other)
+
+    def class_defining_function_complex_good(self):
+        myvar, self.__class__, other = (
+            "myvalue",
+            str,
+            "othervalue",
+        )
+        print(myvar, other)
