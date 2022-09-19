fe3436efb0ec10677ba1539ac02e26cb3f852cbb
Mark Byrne <31762852+mbyrnepr2@users.noreply.github.com>
2022-09-19 09:21:47 +0200

    False positive `global-variable-not-assigned` (#7479)
    * Fix false positive for ``global-variable-not-assigned`` when a global variable is re-assigned via a ``ImportFrom`` node.
    
    Closes #4809
    
    Co-authored-by: Jacob Walls <jacobtylerwalls@gmail.com>

--- tests/functional/g/globals.py
+++ tests/functional/g/globals.py
@@ -31,9 +31,15 @@ def define_constant():
 
 
 def global_with_import():
-    """should only warn for global-statement"""
+    """should only warn for global-statement when using `Import` node"""
     global sys  # [global-statement]
-    import sys  # pylint: disable=import-outside-toplevel
+    import sys
+
+
+def global_with_import_from():
+    """should only warn for global-statement when using `ImportFrom` node"""
+    global namedtuple  # [global-statement]
+    from collections import namedtuple
 
 
 def global_no_assign():
@@ -75,11 +81,6 @@ def override_func():
 
     FUNC()
 
-def func():
-    """Overriding a global with an import should only throw a global statement error"""
-    global sys  # [global-statement]
-
-    import sys
 
 def override_class():
     """Overriding a class should only throw a global statement error"""
