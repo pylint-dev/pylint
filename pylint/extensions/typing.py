f5e168e867799013fb380aa9fe8a0c1516a651c8
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-09-19 14:12:45 +0200

    Fix `undefined-loop-variable` with `NoReturn` and `Never` (#7476)
    Co-authored-by: detachhead <detachhead@users.noreply.github.com>
    Co-authored-by: Jacob Walls <jacobtylerwalls@gmail.com>

--- pylint/extensions/typing.py
+++ pylint/extensions/typing.py
@@ -17,6 +17,7 @@ from pylint.checkers.utils import (
     only_required_for_messages,
     safe_infer,
 )
+from pylint.constants import TYPING_NORETURN
 from pylint.interfaces import INFERENCE
 
 if TYPE_CHECKING:
@@ -75,12 +76,6 @@ DEPRECATED_TYPING_ALIASES: dict[str, TypingAlias] = {
 
 ALIAS_NAMES = frozenset(key.split(".")[1] for key in DEPRECATED_TYPING_ALIASES)
 UNION_NAMES = ("Optional", "Union")
-TYPING_NORETURN = frozenset(
-    (
-        "typing.NoReturn",
-        "typing_extensions.NoReturn",
-    )
-)
 
 
 class DeprecatedTypingAliasMsg(NamedTuple):
