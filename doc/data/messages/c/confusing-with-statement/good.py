ee7b47b89d50b76494104975d2c89b280879f6bf
clavedeluna <danalitovsky+git@gmail.com>
2022-10-04 15:27:29 -0300

    add docs for confusing-with-statement
    Co-authored-by: Vladyslav Krylasov <vladyslav.krylasov@gmail.com>

--- doc/data/messages/c/confusing-with-statement/good.py
+++ doc/data/messages/c/confusing-with-statement/good.py
@@ -1,1 +1,4 @@
-# This is a placeholder for correct code for this message.
+from contextlib import ExitStack
+with ExitStack() as stack:
+    fh1 = stack.enter_context(open('file1.txt', 'w'))
+    fh2 = stack.enter_context(open('file2.txt', 'w'))
