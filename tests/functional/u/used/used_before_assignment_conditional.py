2fc3015a57a0b28d918833a425d8b327461cd9ae
Or Bahari <or.ba402@gmail.com>
2022-10-04 20:07:57 +0300

    remove unnecessary backslash

--- tests/functional/u/used/used_before_assignment_conditional.py
+++ tests/functional/u/used/used_before_assignment_conditional.py
@@ -1,6 +1,6 @@
 """used-before-assignment cases involving IF conditions"""
 
-# pylint: disable=consider-using-augmented-assign\
+# pylint: disable=consider-using-augmented-assign
 
 if 1 + 1 == 2:
     x = x + 1  # [used-before-assignment]
