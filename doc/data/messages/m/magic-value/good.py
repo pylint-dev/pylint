64c3dbb16f649f6e89eaab7c06766dcea4ce3544
Or Bahari <or.ba402@gmail.com>
2022-10-05 13:16:36 +0300

    typo fixes magic value checker

--- doc/data/messages/m/magic-value/good.py
+++ doc/data/messages/m/magic-value/good.py
@@ -2,14 +2,14 @@ import random
 
 MAX_NUM_OF_ITERATIONS = 5
 THRESHOLD_VAL = 100
-MIN_MEASUREMNET_VAL = 0
-MAX_MEASUREMNET_VAL = 200
+MIN_MEASUREMENT_VAL = 0
+MAX_MEASUREMENT_VAL = 200
 
-measurement = random.randint(MIN_MEASUREMNET_VAL, MAX_MEASUREMNET_VAL)
+measurement = random.randint(MIN_MEASUREMENT_VAL, MAX_MEASUREMENT_VAL)
 above_threshold = False
 i = 0
 while i < MAX_NUM_OF_ITERATIONS:
     above_threshold = measurement > THRESHOLD_VAL
     if above_threshold:
         break
-    measurement = random.randint(MIN_MEASUREMNET_VAL, MAX_MEASUREMNET_VAL)
+    measurement = random.randint(MIN_MEASUREMENT_VAL, MAX_MEASUREMENT_VAL)
