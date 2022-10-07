c6e4fa0dadce0afee36c36533b49904974e50a44
clavedeluna <danalitovsky+git@gmail.com>
2022-10-07 08:54:02 -0300

    update confusing-with-statement example

--- doc/data/messages/c/confusing-with-statement/good.py
+++ doc/data/messages/c/confusing-with-statement/good.py
@@ -1,5 +1,3 @@
-from contextlib import ExitStack
-
-with ExitStack() as stack:
-    fh1 = stack.enter_context(open('file1.txt', 'w'))
-    fh2 = stack.enter_context(open('file2.txt', 'w'))
+with open('file.txt', 'w', encoding="utf8") as fh1:
+    with open('file.txt', 'w', encoding="utf8") as fh2:
+        pass
