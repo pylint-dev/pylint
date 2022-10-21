4d75e546141f46360115cfee226dc3380889b3e2
Or Bahari <or.ba402@gmail.com>
2022-10-21 13:29:51 +0300

    refactor checker name in bad.py for magic_value checker

--- doc/data/messages/m/magic-value/bad.py
+++ doc/data/messages/m/magic-value/bad.py
@@ -3,8 +3,8 @@ import random
 measurement = random.randint(0, 200)
 above_threshold = False
 i = 0
-while i < 5:   # [magic-value-compare]
-    above_threshold = measurement > 100   # [magic-value-compare]
+while i < 5:   # [magic-value-comparison]
+    above_threshold = measurement > 100   # [magic-value-comparison]
     if above_threshold:
         break
     measurement = random.randint(0, 200)
