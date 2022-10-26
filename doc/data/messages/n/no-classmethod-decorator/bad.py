1c94d979d9cabe3e1b9bd7e1777fb665b9e740b2
Dani Alcala <112832187+clavedeluna@users.noreply.github.com>
2022-10-26 14:28:07 -0300

    Add docs for non-* and other messages (#7672)
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/n/no-classmethod-decorator/bad.py
+++ doc/data/messages/n/no-classmethod-decorator/bad.py
@@ -4,7 +4,8 @@ class Fruit:
     def __init__(self, color):
         self.color = color
 
-    @classmethod
     def pick_colors(cls, *args):
         """classmethod to pick fruit colors"""
         cls.COLORS = args
+
+    pick_colors = classmethod(pick_colors)  # [no-classmethod-decorator]
