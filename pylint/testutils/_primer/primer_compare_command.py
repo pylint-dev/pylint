fe2d79de02838ff958a1ea4574af54c36df761ff
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-11-04 23:55:50 +0100

    Extract Comparator class from CompareCommand
    Move JSON loading, batching, and cross-referencing logic into a
    dedicated Comparator class. CompareCommand.run() now delegates to
    the Comparator and iterates over it to build the comment.

--- pylint/testutils/_primer/primer_compare_command.py
+++ pylint/testutils/_primer/primer_compare_command.py
@@ -3,79 +3,28 @@
 # Copyright (c) https://github.com/pylint-dev/pylint/blob/main/CONTRIBUTORS.txt
 from __future__ import annotations
 
-import json
-from pathlib import Path, PurePosixPath
+from pathlib import PurePosixPath
 
-from pylint.reporters.json_reporter import OldJsonExport
-from pylint.testutils._primer.primer_command import (
-    PackageData,
-    PackageMessages,
-    PrimerCommand,
-)
+from pylint.testutils._primer.comparator import Comparator
+from pylint.testutils._primer.primer_command import PackageData, PrimerCommand
 
 MAX_GITHUB_COMMENT_LENGTH = 65536
 
 
 class CompareCommand(PrimerCommand):
     def run(self) -> None:
-        if self.config.batches is None:
-            main_data = self._load_json(self.config.base_file)
-            pr_data = self._load_json(self.config.new_file)
-        else:
-            main_data = {}
-            pr_data = {}
-            for idx in range(self.config.batches):
-                main_data.update(
-                    self._load_json(
-                        self.config.base_file.replace("BATCHIDX", "batch" + str(idx))
-                    )
-                )
-                pr_data.update(
-                    self._load_json(
-                        self.config.new_file.replace("BATCHIDX", "batch" + str(idx))
-                    )
-                )
-
-        missing_messages_data, new_messages_data = self._cross_reference(
-            main_data, pr_data
+        comparator = Comparator(
+            self.config.base_file, self.config.new_file, self.config.batches
         )
-        comment = self._create_comment(missing_messages_data, new_messages_data)
+        comment = self._create_comment(comparator)
         with open(self.primer_directory / "comment.txt", "w", encoding="utf-8") as f:
             f.write(comment)
 
-    @staticmethod
-    def _cross_reference(
-        main_data: PackageMessages, pr_data: PackageMessages
-    ) -> tuple[PackageMessages, PackageMessages]:
-        missing_messages_data: PackageMessages = {}
-        for package, data in main_data.items():
-            package_missing_messages: list[OldJsonExport] = []
-            for message in data["messages"]:
-                try:
-                    pr_data[package]["messages"].remove(message)
-                except ValueError:
-                    package_missing_messages.append(message)
-            missing_messages_data[package] = PackageData(
-                commit=pr_data[package]["commit"], messages=package_missing_messages
-            )
-        return missing_messages_data, pr_data
-
-    @staticmethod
-    def _load_json(file_path: Path | str) -> PackageMessages:
-        with open(file_path, encoding="utf-8") as f:
-            result: PackageMessages = json.load(f)
-        return result
-
-    def _create_comment(
-        self, all_missing_messages: PackageMessages, all_new_messages: PackageMessages
-    ) -> str:
+    def _create_comment(self, comparator: Comparator) -> str:
         comment = ""
-        for package, missing_messages in all_missing_messages.items():
+        for package, missing_messages, new_messages in comparator:
             if len(comment) >= MAX_GITHUB_COMMENT_LENGTH:
                 break
-            new_messages = all_new_messages[package]
-            if not missing_messages["messages"] and not new_messages["messages"]:
-                continue
             comment += self._create_comment_for_package(
                 package, new_messages, missing_messages
             )
