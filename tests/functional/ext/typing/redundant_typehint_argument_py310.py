e76804b8886bee63a17e44aa596c4a381205c40d
Or Bahari <or.ba402@gmail.com>
2022-11-28 20:36:10 +0200

    merge test suites and rename news fragments checker name

--- tests/functional/ext/typing/redundant_typehint_argument_py310.py
+++
@@ -1,9 +0,0 @@
-""""Checks for redundant Union typehints in assignments - python 3.10"""
-from __future__ import annotations
-
-
-ANSWER_0: int | int = 0  # [redundant-typehint-argument]
-ANSWER_1: str | int | None | int | bool = 1   # [redundant-typehint-argument]
-ANSWER_2: dict | list[int] | float | str | int | bool = 2
-#  +1: [redundant-typehint-argument]
-ANSWER_3: list[int] | dict[int] | dict[list[int]] | list[str] | list[str] = 3  #
