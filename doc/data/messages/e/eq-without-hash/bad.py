dfb85c7600293e3600858dd09e2eaaeea1ee36e4
clavedeluna <danalitovsky+git@gmail.com>
2022-12-08 09:18:40 -0300

    add docs for eq-without-hash

--- doc/data/messages/e/eq-without-hash/bad.py
+++ doc/data/messages/e/eq-without-hash/bad.py
@@ -1,9 +1,6 @@
-class Fruit:
+class Fruit:  # [eq-without-hash]
     def __init__(self) -> None:
         self.name = "apple"
 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, Fruit) and other.x == self.x
-
-    def __hash__(self) -> int:
-        return hash(self.name)
