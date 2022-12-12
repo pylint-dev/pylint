f2bbe46f0fb20e3bc0594a0982eb70cfae36a146
David Lawson <dmrlawson@gmail.com>
2022-12-12 23:13:03 +0000

    pylint: add failing test when ambiguous abbreviated parameters are set in a config file
    This is confusing behaviour. The output is:
    
    ```
    usage: pylint [options]
    pylint: error: ambiguous option: --no could match --notes, --notes-rgx, --no-docstring-rgx
    ```
    
    The exit code is 2 which doesn't match the documentation: https://pylint.pycqa.org/en/latest/user_guide/usage/run.html#exit-codes

--- tests/lint/test_run_pylint.py
+++ tests/lint/test_run_pylint.py
@@ -2,6 +2,7 @@
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/pylint/blob/main/CONTRIBUTORS.txt
 
+from pathlib import Path
 
 import pytest
 from _pytest.capture import CaptureFixture
@@ -13,6 +14,23 @@ def test_run_pylint_with_invalid_argument(capsys: CaptureFixture[str]) -> None:
     """Check that appropriate exit code is used with invalid argument."""
     with pytest.raises(SystemExit) as ex:
         run_pylint(["--never-use-this"])
+    captured = capsys.readouterr()
+    assert captured.err.startswith("usage: pylint [options]")
+    assert ex.value.code == 32
+
+
+def test_run_pylint_with_invalid_argument_in_config(
+    capsys: CaptureFixture[str], tmp_path: Path
+) -> None:
+    """Check that appropriate exit code is used with an ambiguous
+    argument in a config file.
+    """
+    test_file = tmp_path / "testpylintrc"
+    with open(test_file, "w", encoding="utf-8") as f:
+        f.write("[MASTER]\nno=")
+
+    with pytest.raises(SystemExit) as ex:
+        run_pylint(["--rcfile", f"{test_file}"])
     captured = capsys.readouterr()
     assert captured.err.startswith("usage: pylint [options]")
     assert ex.value.code == 32
