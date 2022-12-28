f0b5a1ef25214c467fc340027d96cf113c781cd1
Nick Drozd <nicholasdrozd@gmail.com>
2022-12-28 11:59:22 -0600

    Cut assertions

--- pylint/extensions/docparams.py
+++ pylint/extensions/docparams.py
@@ -332,8 +332,7 @@ class DocstringParameterChecker(BaseChecker):
         if self.linter.config.accept_no_return_doc:
             return
 
-        func_node = node.frame(future=True)
-        assert isinstance(func_node, astroid.FunctionDef)
+        func_node: astroid.FunctionDef = node.frame(future=True)
 
         # skip functions that match the 'no-docstring-rgx' config option
         no_docstring_rgx = self.linter.config.no_docstring_rgx
@@ -359,8 +358,7 @@ class DocstringParameterChecker(BaseChecker):
         if self.linter.config.accept_no_yields_doc:
             return
 
-        func_node = node.frame(future=True)
-        assert isinstance(func_node, astroid.FunctionDef)
+        func_node: astroid.FunctionDef = node.frame(future=True)
 
         # skip functions that match the 'no-docstring-rgx' config option
         no_docstring_rgx = self.linter.config.no_docstring_rgx
