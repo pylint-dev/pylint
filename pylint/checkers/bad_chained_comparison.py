dc9f6c0981237fc38bf84d4dee64eb2413c68584
Zen <iarezenly@gmail.com>
2022-12-31 01:27:16 +0800

    Shorten long line length in bad chained comparisons

--- pylint/checkers/bad_chained_comparison.py
+++ pylint/checkers/bad_chained_comparison.py
@@ -23,8 +23,10 @@ class BadChainedComparisonChecker(BaseChecker):
         "W3501": (
             "Suspicious %s-part chained comparison using semantically incompatible operators (%s)",
             "bad-chained-comparison",
-            "Used when there is a chained comparison where one expression is part of two comparisons that belong to different semantic groups "
-            ' ("<" does not mean the same thing as "is", chaining them in "0 < x is None" is probably a mistake).',
+            "Used when there is a chained comparison where one expression is part "
+            "of two comparisons that belong to different semantic groups "
+            '("<" does not mean the same thing as "is", chaining them in ' 
+            '"0 < x is None" is probably a mistake).',
         )
     }
 
