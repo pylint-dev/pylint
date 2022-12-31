162ca0fccbc3c76911eafa02a9960917fa9cd6ff
sshane <shane@smiskol.com>
2022-12-31 03:21:27 -0800

    check type

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -1175,9 +1175,9 @@ def class_is_abstract(node: nodes.ClassDef) -> bool:
     # As well as directly inherited ABC class
     for base in node.bases:
         inferred_base = safe_infer(base)
-        if inferred_base.name == "abc" and inferred_base.root().name == "ABC":
-            # abc.ABC inheritance
-            return True
+        if isinstance(inferred_base, nodes.ClassDef):
+            if inferred_base.name == "abc" and inferred_base.root().name == "ABC":
+                return True
 
     for method in node.methods():
         if method.parent.frame(future=True) is node:
