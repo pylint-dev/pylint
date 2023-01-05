fbf7ac83a26cf7e73f00453ac8c1d761ff61cd17
Juan Toledo <toledo+github@lazaro.es>
2023-01-05 16:34:20 +0100

    Added cython import that also triggers false positive

--- tests/functional/r/regression_02/regression_cython_declare_8024.py
+++ tests/functional/r/regression_02/regression_cython_declare_8024.py
@@ -1,4 +1,13 @@
-"""Test for cython pure python cython.declare"""
+"""Test for cython pure python false positives"""
 import cython
 
+# cython allows any import in pure python mode. It can only
+# be determined correct at compile time
+# Check that it doesn't raise neither an import-error nor a
+# no-name-in-module error
+from cython.cimports.libc.math import sin
+
+# Same with type declarations
+# Check it doesn't generate a no-member error
 MY_VAR = cython.declare(cython.int, 0)
+print(sin(MY_VAR))
