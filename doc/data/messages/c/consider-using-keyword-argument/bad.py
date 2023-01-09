38f05652b1035a0c1a19608c1c89ecbb4cbc0448
clavedeluna <danalitovsky+git@gmail.com>
2023-01-09 09:15:36 -0300

    fix review suggestions

--- doc/data/messages/c/consider-using-keyword-argument/bad.py
+++ doc/data/messages/c/consider-using-keyword-argument/bad.py
@@ -2,4 +2,4 @@ def print_car(name, owner, title, color="red", year=2022):
     print(name)
 
 car = "a car"
-print_car(car, "name", "title", "green") # [consider-using-keyword-argument, consider-using-keyword-argument]
+print_car(car, "name", "title", "green")  # [consider-using-keyword-argument, consider-using-keyword-argument, consider-using-keyword-argument]
--- doc/data/messages/c/consider-using-keyword-argument/good.py
+++ doc/data/messages/c/consider-using-keyword-argument/good.py
@@ -2,4 +2,4 @@ def print_car(name, owner, title, color="red", year=2022):
     print(name)
 
 car = "a car"
-print_car(car, owner="your name", title="title", color="green")
+print_car(name=car, owner="your name", title="title", color="green")
