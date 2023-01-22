54ace7d850610c54c8007d9e58b083595ccae74f
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-01-22 13:53:47 +0100

    Update pylint/checkers/exceptions.py
    Co-authored-by: Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>

--- pylint/checkers/exceptions.py
+++ pylint/checkers/exceptions.py
@@ -173,7 +173,7 @@ MSGS: dict[str, MessageDefinitionTuple] = {
     "W0719": (
         "Raising too general exception: %s",
         "broad-exception-raised",
-        "Raising exceptions that are too generic force you to catch exception "
+        "Raising exceptions that are too generic force you to catch exceptions "
         "generically too. It will force you to use a naked ``except Exception:``"
         " clause. You might then end up catching exceptions other than the ones "
         "you expect to catch. This can hide bugs or make it harder to debug programs"
