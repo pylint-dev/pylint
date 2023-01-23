084e996f84990859b9d2a43b7891629646c3cdec
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-01-23 20:45:53 +0100

    Take review into account
    https://github.com/PyCQA/pylint/pull/8077\#discussion_r1083446366

--- pylint/checkers/exceptions.py
+++ pylint/checkers/exceptions.py
@@ -112,11 +112,11 @@ MSGS: dict[str, MessageDefinitionTuple] = {
     "W0702": (
         "No exception type(s) specified",
         "bare-except",
-        "A bare ``except:`` clause will catch ``SystemExit`` and ``KeyboardInterrupt``"
-        " exceptions, making it harder to interrupt a program with ``Control-C``, and "
-        "can disguise other problems. If you want to catch all exceptions that signal "
-        "program errors, use ``except Exception:`` (bare except is equivalent to"
-        " ``except BaseException:``).",
+        "A bare ``except:`` clause will catch ``SystemExit`` and "
+        "``KeyboardInterrupt`` exceptions, making it harder to interrupt a program "
+        "with ``Control-C``, and can disguise other problems. If you want to catch "
+        "all exceptions that signal program errors, use ``except Exception:`` (bare "
+        "except is equivalent to ``except BaseException:``).",
     ),
     "W0718": (
         "Catching too general exception %s",
