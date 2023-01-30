4036863950f8ad04d66b507ee3d851cec1dc066b
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-01-30 15:59:23 +0100

    [doc] Make sure that doc examples are of a reasonable length (#8135)
    And upgrade existing unreasonable length. Reading ease is drastically
    reduced on read the doc after 103 chars (Because of horizontal scrolling)

---
+++ doc/data/messages/n/no-self-use/details.rst
@@ -0,0 +1,2 @@
+If a function is not using any class attribute it can be a ``@staticmethod``,
+or a function outside the class.
--- doc/data/messages/t/too-many-format-args/bad.py
+++ doc/data/messages/t/too-many-format-args/bad.py
@@ -1,1 +1,2 @@
-print("Today is {0}, so tomorrow will be {1}".format("Monday", "Tuesday", "Wednesday"))  # [too-many-format-args]
+# +1: [too-many-format-args]
+print("Today is {0}, so tomorrow will be {1}".format("Monday", "Tuesday", "Wednesday"))
