506fa1fc93b1f92894489e3ad6846eeb2efb360b
saranti <tsarantis@proton.me>
2023-02-01 22:44:37 +1100

    use real working examples

--- doc/data/messages/t/too-complex/bad.py
+++ doc/data/messages/t/too-complex/bad.py
@@ -1,26 +1,54 @@
-def foo():
-    # McCabe rating is 11 here (by default 10)
-    # +1: [too-complex]
-    myint = 2
-    if myint == 5:
-        return myint
-    elif myint == 6:
-        return myint
-    elif myint == 7:
-        return myint
-    elif myint == 8:
-        return myint
-    elif myint == 9:
-        return myint
-    elif myint == 10:
-        if myint == 8:
-            while True:
-                return True
-        elif myint == 8:
-            with myint:
-                return 8
-    else:
-        if myint == 2:
-            return myint
-        return myint
-    return myint
+fruit = {'apple': 1.1,
+         'pear': 0.8,
+         'banana': 1.2,
+         'mango': 3.5,
+         'peach': 0.5,
+         'melon': 4.9,
+         'orange': 2.0,
+         'strawberry': 2.5,
+         'mandarin': 2.3,
+         'plum': 0.5,
+         'watermelon': 6.4
+         }
+
+discounted_fruit = ('banana', 'mango', 'orange', 'watermelon')
+shopping_list = []
+
+
+def fifty_percent_off(whole):
+    return (float(whole)) * 50 / 100
+
+
+for f, v in fruit.items():  # [too-complex]
+    # McCabe rating is 13 here (by default 10)
+    if f == 'apple':
+        shopping_list.append(v)
+    if f == 'pear':
+        shopping_list.append(v)
+    if f == 'banana':
+        v = fifty_percent_off(v)
+        shopping_list.append(v)
+    if f == 'mango':
+        v = fifty_percent_off(v)
+        shopping_list.append(v)
+    if f == 'peach':
+        shopping_list.append(v)
+    if f == 'melon':
+        shopping_list.append(v)
+    if f == 'orange':
+        v = fifty_percent_off(v)
+        shopping_list.append(v)
+    if f == 'strawberry':
+        shopping_list.append(v)
+    if f == 'mandarin':
+        shopping_list.append(v)
+    if f == 'plum':
+        shopping_list.append(v)
+    if f == 'watermelon':
+        v = fifty_percent_off(v)
+        shopping_list.append(v)
+
+    print(f'{f} ${v:.2f}')
+
+total = sum(shopping_list)
+print(f'Total: ${total:.2f}')
--- doc/data/messages/t/too-complex/good.py
+++ doc/data/messages/t/too-complex/good.py
@@ -1,1 +1,39 @@
 # split up to smaller functions
+
+fruit = {'apple': 1.1,
+         'pear': 0.8,
+         'banana': 1.2,
+         'mango': 3.5,
+         'peach': 0.5,
+         'melon': 4.9,
+         'orange': 2.0,
+         'strawberry': 2.5,
+         'mandarin': 2.3,
+         'plum': 0.5,
+         'watermelon': 6.4
+         }
+
+
+def fifty_percent_off(whole):
+    return (float(whole)) * 50 / 100
+
+
+def total_sum(list1):
+    total = 0
+    for i in list1:
+        total += i
+    return total
+
+
+discounted_fruit = ('banana', 'mango', 'orange', 'watermelon')
+shopping_list = []
+
+for f, v in fruit.items():
+    if f in discounted_fruit:
+        PRICE = fifty_percent_off(v)
+    else:
+        PRICE = v
+    shopping_list.append(PRICE)
+    print(f'{f} ${PRICE:.2f}')
+
+print(f'Total price is ${total_sum(shopping_list):.2f}')
