3ce238b21becf6f6f37f3ebfd3faab0272e437b4
Ollie <46904826+ollie-iterators@users.noreply.github.com>
2023-02-03 14:10:37 -0500

    Adding documentation for differing-type-doc

--- doc/data/messages/d/differing-type-doc/bad.py
+++ doc/data/messages/d/differing-type-doc/bad.py
@@ -1,6 +1,6 @@
 def leap_year(year):
     """ Function used to determine if a given year is a leap year
-        :param int year: a year to check is a leap year
+        :param str year: a year to check is a leap year
         :returns: a result.
         :rtype: boolean """
     if year % 400 == 0:
