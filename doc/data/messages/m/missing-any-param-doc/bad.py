ba2ab95687cf13c299adfae49f591a2090a12fd9
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-06 10:24:23 -0500

    Adding missing-any-param-doc

--- doc/data/messages/m/missing-any-param-doc/bad.py
+++ doc/data/messages/m/missing-any-param-doc/bad.py
@@ -1,10 +1,5 @@
 def add_two_numbers(x, y):
     """Function used to add two integers together.
-
-    :param int x: x value.
-    :param int y: y value.
-    :returns: a result.
-    :rtype: int
     """
 
     return x + y
