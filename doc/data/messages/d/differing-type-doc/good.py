3705b5797338c293bae37a7a379d3960347254cf
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-06 14:41:59 -0500

    Remove docstring errors

--- doc/data/messages/d/differing-type-doc/good.py
+++ doc/data/messages/d/differing-type-doc/good.py
@@ -1,10 +1,1 @@
-def foo(x, y):
-    """A dummy string.
-
-    :param int x: x value.
-    :param int y: y value.
-    :returns: a result.
-    :rtype: int
-    """
-
-    return x + y
+# This is a placeholder for correct code for this message.
