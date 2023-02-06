611c5529fce22d700909b0721f83fd1d9cc8d215
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2023-02-06 21:15:37 +0100

    Add ``docstring-first-line-empty`` doc example
    Co-authored-by: Ollie <46904826+ollie-iterators@users.noreply.github.com>

--- doc/data/messages/d/docstring-first-line-empty/bad.py
+++ doc/data/messages/d/docstring-first-line-empty/bad.py
@@ -1,5 +1,7 @@
-def foo():
-    """Lorem Ipsum is simply dummy text of the printing and typesetting
+def foo():  # [docstring-first-line-empty]
+    """
+
+    Lorem Ipsum is simply dummy text of the printing and typesetting
     industry.
 
     Lorem Ipsum has been the industry's standard dummy text ever since the
