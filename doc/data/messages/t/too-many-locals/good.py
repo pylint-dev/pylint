15cb92ea3c151c1958bcd4ecd8269cca2d883942
Ollie <46904826+ollie-iterators@users.noreply.github.com>
2023-02-06 17:35:03 -0500

    Fix code

--- doc/data/messages/t/too-many-locals/good.py
+++ doc/data/messages/t/too-many-locals/good.py
@@ -1,7 +1,5 @@
-def test_addition(num1, num2, num3, num4): # [too-many-locals]
+def test_addition(num1, num2):
     number1 = num1
     number2 = num2
-    number3 = num3
-    number4 = num4
 
-    return number1 + number2 + number3 + number4
+    return number1 + number2
