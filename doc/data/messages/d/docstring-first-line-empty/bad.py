9d1449be3d9d0bb8506f1ba1794a9fa6136a377c
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-06 15:10:51 -0500

    Fix docstring errors

--- doc/data/messages/d/docstring-first-line-empty/bad.py
+++ doc/data/messages/d/docstring-first-line-empty/bad.py
@@ -1,4 +1,4 @@
-def foo():
+def foo(): # [docstring-first-line-empty]
     """
 
     Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -7,4 +7,4 @@ def foo():
     Lorem Ipsum has been the industry's standard dummy text ever since the
     1500s, when an unknown printer took a galley of type and scrambled it
     to make a type specimen book
-    """ # [docstring-first-line-empty]
+    """
