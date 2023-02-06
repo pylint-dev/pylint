84695f65cc0f238ef0c986f4684a0528845d5ed5
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2023-02-06 21:14:13 +0100

    Add ``differing-param-doc`` doc example
    Co-authored-by: Ollie <46904826+ollie-iterators@users.noreply.github.com>

--- doc/data/messages/d/differing-param-doc/bad.py
+++ doc/data/messages/d/differing-param-doc/bad.py
@@ -1,8 +1,8 @@
-def foo(x, y):
+def foo(x, y):  # [differing-param-doc]
     """A dummy string.
 
     :param int x: x value.
-    :param int y: y value.
+    :param int z: z value.
     """
 
     return x + y
