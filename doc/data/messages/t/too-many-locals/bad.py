e28127eec0ce4a92bfa8f51976a8b0b575962c6f
Ollie <46904826+ollie-iterators@users.noreply.github.com>
2023-02-06 17:52:40 -0500

    Fix too-many-locals

--- doc/data/messages/t/too-many-locals/bad.py
+++ doc/data/messages/t/too-many-locals/bad.py
@@ -1,11 +1,31 @@
-def test_addition(num1, num2, num3, num4, num5, num6, num7, num8): # [too-many-locals]
-    number1 = num1
-    number2 = num2
-    number3 = num3
-    number4 = num4
-    number5 = num5
-    number6 = num6
-    number7 = num7
-    number8 = num8
+class TreeNode(object):
+    """ Class containing variables to be used in a Binary Search Tree """
+    def __init__(self, xVal):
+        self.val = xVal
+        self.left = None
+        self.right = None
 
-    return number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8
+def closestValue(root, target): # [too-many-locals]
+    """ Function used to fing the closest value of a given target value in a given
+        non-empty Binary Search Tree (BST) of unique values """
+    aVal = root.val
+    kid = root.left if target < aVal else root.right
+    if not kid:
+        return aVal
+    bVal = closestValue(kid, target)
+
+    result = min((aVal, bVal), key=lambda x: abs(target-x))
+    return result
+
+root = TreeNode(8)
+root.left = TreeNode(5)
+root.right = TreeNode(14)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(6)
+root.left.right.left = TreeNode(8)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(24)
+root.right.right.left = TreeNode(22)
+
+result = closestValue(root, 19)
+print(result)
--- doc/data/messages/t/too-many-locals/good.py
+++ doc/data/messages/t/too-many-locals/good.py
@@ -1,5 +1,30 @@
-def test_addition(num1, num2):
-    number1 = num1
-    number2 = num2
+class TreeNode(object):
+    """ Class containing variables to be used in a Binary Search Tree """
+    def __init__(self, xVal):
+        self.val = xVal
+        self.left = None
+        self.right = None
 
-    return number1 + number2
+def closestValue(root, target):
+    """ Function used to fing the closest value of a given target value in a given
+        non-empty Binary Search Tree (BST) of unique values """
+    aVal = root.val
+    kid = root.left if target < aVal else root.right
+    if not kid:
+        return aVal
+    bVal = closestValue(kid, target)
+
+    return min((aVal, bVal), key=lambda x: abs(target-x))
+
+root = TreeNode(8)
+root.left = TreeNode(5)
+root.right = TreeNode(14)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(6)
+root.left.right.left = TreeNode(8)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(24)
+root.right.right.left = TreeNode(22)
+
+result = closestValue(root, 19)
+print(result)
