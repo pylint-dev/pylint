613b6ee6f85f7b5f4989fd828f7fc8ad7a01a4d7
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-06 11:31:50 -0500

    Adding string for too-many-public-methods

--- doc/data/messages/t/too-many-public-methods/bad.py
+++ doc/data/messages/t/too-many-public-methods/bad.py
@@ -3,4 +3,4 @@ class TooManyPublicMethods:
     def meth2(self): pass
     def meth3(self): pass
     def meth4(self): pass
-    def meth5(self): pass
+    def meth5(self): pass # [too-many-public-methods]
