ca041feb3f202ca3c0a5df662bc0f0532706dd16
Ollie <46904826+ollie-iterators@users.noreply.github.com>
2023-02-05 20:44:07 -0500

    Revert "Merge changes"
    This reverts commit 4664487b80a5778fff0156049458652e2389afd3.

--- doc/data/messages/t/too-many-public-methods/good.py
+++ doc/data/messages/t/too-many-public-methods/good.py
@@ -1,11 +1,1 @@
-class Foo:
-    def a(self, a):
-        self.a = a
-    def b(self, b):
-        self.b = b
-    def c(self, c):
-        self.c = c
-    def d(self, d):
-        self.d = d
-    def e(self, e):
-        self.e = e
+# This is a placeholder for correct code for this message.
