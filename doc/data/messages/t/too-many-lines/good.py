30fafde75e02b3662cf84b81c0bf55a0682083da
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-07 14:27:41 -0500

    Fix good.py

--- doc/data/messages/t/too-many-lines/good.py
+++ doc/data/messages/t/too-many-lines/good.py
@@ -2,8 +2,8 @@ import datetime
 from datetime import datetime
 
 # Make a program to display the various Date Time formats
-dateStrings = ["Current date and time: ", "Current year: ", "Month of year: ", "Week number of the year: "]
-dateValues = [datetime.datetime.now(), datetime.date.today().strftime("%Y"), datetime.date.today().strftime("%B"), datetime.date.today().strftime("%W")]
+dateStrings = ["Current date and time: ", "Current year: "]
+dateValues = [datetime.datetime.now(), datetime.date.today().strftime("%Y")]
 
 for index in range(len(dateStrings)):
     print(dateStrings[index] + dateValues[index])
