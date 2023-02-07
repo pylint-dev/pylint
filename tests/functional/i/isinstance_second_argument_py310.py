4741057682f5392ec38e9543b656f1dc9247c552
Nick Drozd <nicholasdrozd@gmail.com>
2023-02-07 09:32:05 -0500

    Add docstring and more cases

--- tests/functional/i/isinstance_second_argument_py310.py
+++ tests/functional/i/isinstance_second_argument_py310.py
@@ -1,5 +1,29 @@
-# pylint: disable = missing-docstring
+'''Tests for isinstance with compound types'''
+
+# True negatives
 isinstance(0, int | str)
 isinstance(0, int | int | int)
 isinstance(0, int | str | list | float)
 isinstance(0, (int | str) | (list | float))
+
+# True positives
+isinstance(0, int | 5)  # [isinstance-second-argument-not-valid-type]
+isinstance(0, str | 5 | int)  # [isinstance-second-argument-not-valid-type]
+
+
+# FALSE POSITIVES
+
+# Type aliases cannot be inferred
+IntOrStr = int | str
+isinstance(0, IntOrStr)  # [isinstance-second-argument-not-valid-type]
+ListOrDict = list | dict
+isinstance(0, (float | ListOrDict) | IntOrStr)  # [isinstance-second-argument-not-valid-type]
+
+
+# FALSE NEGATIVES
+
+# Parameterized generics will raise type errors at runtime.
+# Warnings should be raised, but aren't (yet).
+isinstance(0, list[int])
+ListOfInts = list[int]
+isinstance(0, ListOfInts)
