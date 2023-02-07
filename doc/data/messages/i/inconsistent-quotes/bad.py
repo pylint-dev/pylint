89149dd12066b0108535ba93b5ca227a1f0c9fb4
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-07 14:25:00 -0500

    Add changes

--- doc/data/messages/i/inconsistent-quotes/bad.py
+++ doc/data/messages/i/inconsistent-quotes/bad.py
@@ -5,4 +5,4 @@ from datetime import datetime
 # Exercise 1:
 # Make a program to display the various Date Time formats
 print("Current date and time: " , datetime.datetime.now())
-print("Current year: ", datetime.date.today().strftime("%Y"))
+print('Current year: ', datetime.date.today().strftime("%Y")) # [inconsistent-quotes]
