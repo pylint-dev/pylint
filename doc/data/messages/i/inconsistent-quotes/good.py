d2695b8631e9dcbad5690ea44a2c448c7789639b
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-02-07 21:36:31 +0100

    Apply suggestions from code review

--- doc/data/messages/i/inconsistent-quotes/good.py
+++ doc/data/messages/i/inconsistent-quotes/good.py
@@ -1,8 +1,3 @@
-# Imports
 import datetime
-from datetime import datetime
 
-# Exercise 1:
-# Make a program to display the various Date Time formats
-print("Current date and time: " , datetime.datetime.now())
 print("Current year: ", datetime.date.today().strftime("%Y"))
