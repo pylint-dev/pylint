d370c600656019407369c366b7de80002e20e7ae
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-07 14:02:11 -0500

    Add too-many-lines

---
+++ doc/data/messages/t/too-many-lines/bad.py
@@ -0,0 +1,13 @@
+# [too-many-lines]
+import datetime
+from datetime import datetime
+
+# Make a program to display the various Date Time formats
+print("Current date and time: " , datetime.datetime.now())
+print("Current year: ", datetime.date.today().strftime("%Y"))
+print("Month of year: ", datetime.date.today().strftime("%B"))
+print("Week number of the year: ", datetime.date.today().strftime("%W"))
+print("Weekday of the week: ", datetime.date.today().strftime("%w"))
+print("Day of year: ", datetime.date.today().strftime("%j"))
+print("Day of the month : ", datetime.date.today().strftime("%d"))
+print("Day of week: ", datetime.date.today().strftime("%A"))
--- doc/data/messages/t/too-many-lines/good.py
+++ doc/data/messages/t/too-many-lines/good.py
@@ -1,1 +1,9 @@
-# This is a placeholder for correct code for this message.
+import datetime
+from datetime import datetime
+
+# Make a program to display the various Date Time formats
+dateStrings = ["Current date and time: ", "Current year: ", "Month of year: ", "Week number of the year: "]
+dateValues = [datetime.datetime.now(), datetime.date.today().strftime("%Y"), datetime.date.today().strftime("%B"), datetime.date.today().strftime("%W")]
+
+for index in range(len(dateStrings)):
+    print(dateStrings[index] + dateValues[index])
