1b634da4c7d7a92a2748b636a392a8bdf5e1a540
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-08 10:13:45 -0500

    Change example

--- doc/data/messages/t/too-many-lines/bad.py
+++ doc/data/messages/t/too-many-lines/bad.py
@@ -1,16 +1,3 @@
-def leap_year(year): # [too-many-lines]
-    """ Function used to determine whether a given year is a leap year """
-    if year % 400 == 0:
-        return True
-    if year % 100 == 0:
-        return False
-    if year % 4 == 0:
-        return True
-    else:
-        return False
-
-print(leap_year(1990))
-print(leap_year(1991))
-print(leap_year(1992))
-print(leap_year(1993))
-print(leap_year(1994))
+print(1) # [too-many-lines]
+print(2)
+print(3)
--- doc/data/messages/t/too-many-lines/good.py
+++ doc/data/messages/t/too-many-lines/good.py
@@ -1,13 +1,2 @@
-def leap_year(year):
-    """ Function used to determine whether a given year is a leap year """
-    if year % 400 == 0:
-        return True
-    if year % 100 == 0:
-        return False
-    if year % 4 == 0:
-        return True
-    else:
-        return False
-
-for year in range(1990, 1994):
-    print(leap_year(year))
+print(1)
+print(2)
