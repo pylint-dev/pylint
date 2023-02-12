0a4027655c9fbf64470f9d83e9f0ee52e5dcdda7
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-02-12 22:37:34 +0100

    Add regression test for #8109

---
+++ tests/functional/r/regression_02/regression_8109.py
@@ -0,0 +1,15 @@
+"""Regression test for https://github.com/PyCQA/pylint/issues/8109."""
+
+# pylint: disable=missing-docstring, unsupported-binary-operation
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Number:
+    amount: int | float
+    round: int = 2
+
+    def __str__(self):
+        number_format = "{:,.%sf}" % self.round  # [consider-using-f-string]
+        return number_format.format(self.amount).rstrip("0").rstrip(".")
