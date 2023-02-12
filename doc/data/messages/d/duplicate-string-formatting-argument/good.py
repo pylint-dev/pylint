fadd9dc4a8f9b7f69cea8b55fb4a317a3d97b725
Ollie <46904826+ollie-iterators@users.noreply.github.com>
2023-02-12 17:55:28 -0500

    Add documentation for duplicate-string-formatting-argument and bidirectional-unicode (#8269)
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/d/duplicate-string-formatting-argument/good.py
+++ doc/data/messages/d/duplicate-string-formatting-argument/good.py
@@ -1,1 +1,14 @@
-# This is a placeholder for correct code for this message.
+# pylint: disable=missing-docstring, consider-using-f-string
+
+SEE = "see 👀"
+SEA = "sea 🌊"
+
+# +1: [duplicate-string-formatting-argument,duplicate-string-formatting-argument]
+CONST = """
+A sailor went to {sea}, {sea}, {sea}
+To {see} what he could {see}, {see}, {see}
+But all that he could {see}, {see}, {see}
+Was the bottom of the deep blue {sea}, {sea}, {sea}!
+""".format(
+    sea=SEA, see=SEE
+)
