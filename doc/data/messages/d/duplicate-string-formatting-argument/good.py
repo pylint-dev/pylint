2ea59dd0d53f4b2fec2cc5a2fbb9b01baa8b5815
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-12 17:55:49 -0500

    Removing unnecessary string

--- doc/data/messages/d/duplicate-string-formatting-argument/good.py
+++ doc/data/messages/d/duplicate-string-formatting-argument/good.py
@@ -3,7 +3,6 @@
 SEE = "see 👀"
 SEA = "sea 🌊"
 
-# +1: [duplicate-string-formatting-argument,duplicate-string-formatting-argument]
 CONST = """
 A sailor went to {sea}, {sea}, {sea}
 To {see} what he could {see}, {see}, {see}
