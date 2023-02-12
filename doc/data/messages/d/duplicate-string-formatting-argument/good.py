67eaa0bdb9f513d5f501dee5b4efd6f90a9e1bdf
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-12 16:30:11 -0500

    Add error for duplicate-string-formatting-argument

--- doc/data/messages/d/duplicate-string-formatting-argument/good.py
+++ doc/data/messages/d/duplicate-string-formatting-argument/good.py
@@ -1,1 +1,7 @@
-# This is a placeholder for correct code for this message.
+# pylint: disable=missing-docstring
+
+NAME = 42
+OTHER_NAME = 24
+OTHER_OTHER_NAME = 2
+
+CONST = f"some value {NAME} some other value {OTHER_NAME} {OTHER_OTHER_NAME}"
