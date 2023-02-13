10356bfd77c1204a093adf4fd8941fe0b6556674
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-13 12:10:03 -0500

    Add documentation for deprecated-method

--- doc/data/messages/d/deprecated-method/bad.py
+++ doc/data/messages/d/deprecated-method/bad.py
@@ -1,4 +1,4 @@
 import unittest
 from importlib.machinery import SourceFileLoader, SourcelessFileLoader
 
-SourceFileLoader('unittest', unittest.__file__).exec_module()
+SourceFileLoader('unittest', unittest.__file__).load_module() # [deprecated-method]
