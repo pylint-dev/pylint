01d0876f9a2a711c27210e024a4fa4dae765c992
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-13 18:57:08 -0500

    Adding missing-any-param-doc

---
+++ doc/data/messages/m/missing-any-param-doc/bad.py
@@ -0,0 +1,4 @@
+def puppies(head, tail): #[missing-any-param-doc]
+    """Puppy function ...
+    """
+    print(head, tail)
--- doc/data/messages/m/missing-any-param-doc/good.py
+++ doc/data/messages/m/missing-any-param-doc/good.py
@@ -1,1 +1,6 @@
-# This is a placeholder for correct code for this message.
+def puppies(head: str, tail: str):
+    """ Puppy function
+        :param head: string information about the head of the dog
+        :param tail: string information about the tail of the dog ...
+    """
+    print(head, tail)
