9b5b2ac965d3741a1f6fac3772e9aa383c3373d9
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-13 12:04:09 -0500

    Adding documentation for deprecated-class

--- doc/data/messages/d/deprecated-class/bad.py
+++ doc/data/messages/d/deprecated-class/bad.py
@@ -1,5 +1,8 @@
 import collections
+from collections import Iterable  # [deprecated-class]
 from collections import Counter
+
+_ = collections.Awaitable()  # [deprecated-class]
 
 # With sequence of items
 print(Counter(['B','B','A','B','C','A','B',
