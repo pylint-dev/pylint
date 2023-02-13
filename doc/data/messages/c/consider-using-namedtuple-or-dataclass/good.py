b5d5551f7ce8234bd41b46d9d6fd793dfe68d0f3
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-13 10:20:18 -0500

    Change to new style for consider-using-namedtuple ...

--- doc/data/messages/c/consider-using-namedtuple-or-dataclass/good.py
+++ doc/data/messages/c/consider-using-namedtuple-or-dataclass/good.py
@@ -1,8 +1,8 @@
-from collections import namedtuple
+from typing import NamedTuple
 
-MAPPING_1 = namedtuple("MAPPING_1", {"key_1", "key_2", "key_diff_1", "key_diff_2"})
-entry_1 = {"key_1": 1, "key_2": 4, "key_diff_1": 4}
-entry_2 = {"key_1": 1, "key_2": 4, "key_diff_2": 3}
 
-MAPPING_1(1, 4, 4, 0)
-MAPPING_1(1, 4, 0, 3)
+class MAPPING_1(NamedTuple):
+    key_1: str
+    key_2: str
+    key_diff_1: str
+    key_diff_2: str
