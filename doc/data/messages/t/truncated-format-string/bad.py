358731b997edac8f05867de6041980e0f276d85b
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-14 10:51:21 -0500

    Removing truncated-format-string

--- doc/data/messages/t/truncated-format-string/bad.py
+++
@@ -1,3 +0,0 @@
-PARG_1 = PARG_2 = PARG_3 = 1
-
-print("strange format %2" % PARG_2) # [truncated-format-string]
--- doc/data/messages/t/truncated-format-string/good.py
+++ doc/data/messages/t/truncated-format-string/good.py
@@ -1,3 +1,1 @@
-PARG_1 = PARG_2 = PARG_3 = 1
-
-print(f"strange format {PARG_2}")
+# This is a placeholder for correct code for this message.
