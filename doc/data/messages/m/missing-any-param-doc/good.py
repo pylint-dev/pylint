8b897cf73f743dfa8cc7485eea1ca2713d4a3e05
Ollie <46904826+ollie-iterators@users.noreply.github.com>
2023-02-14 08:44:14 -0500

    Update doc/data/messages/m/missing-any-param-doc/good.py
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- doc/data/messages/m/missing-any-param-doc/good.py
+++ doc/data/messages/m/missing-any-param-doc/good.py
@@ -1,6 +1,6 @@
 def puppies(head: str, tail: str):
     """ Puppy function
-        :param head: string information about the head of the dog
-        :param tail: string information about the tail of the dog ...
+        :param head: description of the head of the dog
+        :param tail: description of the tail of the dog
     """
     print(head, tail)
