eb63065cc06dce1488d55a1672acd340570f87bb
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-02-14 09:27:27 +0100

    Streamline deprecated class example

--- doc/data/messages/d/deprecated-class/bad.py
+++ doc/data/messages/d/deprecated-class/bad.py
@@ -1,6 +1,1 @@
-import collections
-from collections import Counter, Iterable  # [deprecated-class]
-
-# With sequence of items
-print(Counter(['B','B','A','B','C','A','B',
-               'B','A','C']))
+from collections import Iterable  # [deprecated-class]
--- doc/data/messages/d/deprecated-class/good.py
+++ doc/data/messages/d/deprecated-class/good.py
@@ -1,6 +1,1 @@
-import collections
-from collections import Counter
-
-# With sequence of items
-print(Counter(['B','B','A','B','C','A','B',
-               'B','A','C']))
+from collections.abc import Iterable
