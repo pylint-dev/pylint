897f6e96ed94558b4b2be29ed8fa6915aaeebe1a
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-02-21 23:07:01 +0100

    simplify code in good.py

--- doc/data/messages/t/too-many-lines/good/is_palindrome.py
+++ doc/data/messages/t/too-many-lines/good/is_palindrome.py
@@ -1,10 +1,2 @@
 def is_palindrome(string):
-    left_pos = 0
-    right_pos = len(string) - 1
-
-    while right_pos >= left_pos:
-        if not string[left_pos] == string[right_pos]:
-            return False
-        left_pos += 1
-        right_pos -= 1
-    return True
+    return string == string[::-1]
