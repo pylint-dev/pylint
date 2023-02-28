da32e98c71c38d091e8495043bb0f06642baa78c
Ollie Willcox <owillcox@iteratorstesting.com>
2023-02-28 16:06:20 -0500

    Staging changes

--- tests/functional/s/super/super_init_not_called.py
+++ tests/functional/s/super/super_init_not_called.py
@@ -26,7 +26,7 @@ class UninferableChild(UninferableParent):  # [undefined-variable]
 # Tests for not calling the init of a parent that does not define one
 # but inherits it.
 class GrandParentWithInit:
-    def __init__(self):
+    def __init__(self) -> None:
         print(self)
 
 
@@ -37,24 +37,24 @@ class ParentWithoutInit(GrandParentWithInit):
 class ChildOne(ParentWithoutInit, GrandParentWithInit):
     """Since ParentWithoutInit calls GrandParentWithInit it doesn't need to be called."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         GrandParentWithInit.__init__(self)
 
 
 class ChildTwo(ParentWithoutInit):
-    def __init__(self):
+    def __init__(self) -> None:
         ParentWithoutInit.__init__(self)
 
 
 class ChildThree(ParentWithoutInit):
-    def __init__(self):  # [super-init-not-called]
+    def __init__(self) -> None:  # [super-init-not-called]
         ...
 
 
 # Regression test as reported in
 # https://github.com/PyCQA/pylint/issues/6027
 class MyUnion(ctypes.Union):
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
 
