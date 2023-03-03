81bb644a82fcbc7c3d53d7a4faa297c57925f1c7
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-03-03 19:48:39 +0100

    [doc] Add an example for 'missing-format-string-key' (#8379)

--- doc/data/messages/m/missing-format-string-key/bad.py
+++ doc/data/messages/m/missing-format-string-key/bad.py
@@ -1,7 +1,7 @@
+# +1: [missing-format-string-key]
 fruit_prices = """
 Apple: %(apple_price)d ¤
 Orange: %(orange_price)d ¤
 """ % {
-    "apple_price": 42,
-    "orange_price": 87,
+    "apple_price": 42
 }
