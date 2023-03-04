56bb787a095944a6cea5dc17b9f0e414bcdd02e0
Ollie Willcox <owillcox@iteratorstesting.com>
2023-03-04 18:45:10 -0500

    Fix code

--- pylint/checkers/base/docstring_checker.py
+++ pylint/checkers/base/docstring_checker.py
@@ -145,7 +145,10 @@ class DocStringChecker(_BasicChecker):
                         overridden = True
                         break
                 self._check_docstring(
-                    ftype, node, report_missing=not overridden, confidence=confidence  # type: ignore[arg-type]
+                    ftype,
+                    node,
+                    report_missing=not overridden,  # type: ignore[arg-type]
+                    confidence=confidence,
                 )
             elif isinstance(node.parent.frame(future=True), nodes.Module):
                 self._check_docstring(ftype, node)  # type: ignore[arg-type]
