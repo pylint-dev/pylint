9a6039b955889f3e9c6b8fdb9c1e399c95c57740
Jacob Walls <jacobtylerwalls@gmail.com>
2023-03-11 18:22:46 -0500

    Add pragmas

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -1803,7 +1803,7 @@ def is_typing_guard(node: nodes.If) -> bool:
         "This method will be removed in pylint 3.0; use in_type_checking_block() instead.",
         DeprecationWarning,
         stacklevel=2,
-    )
+    )  # pragma: no cover
     return isinstance(
         node.test, (nodes.Name, nodes.Attribute)
     ) and node.test.as_string().endswith("TYPE_CHECKING")
@@ -1815,7 +1815,7 @@ def is_node_in_typing_guarded_import_block(node: nodes.NodeNG) -> bool:
         "This method will be removed in pylint 3.0; use in_type_checking_block() instead.",
         DeprecationWarning,
         stacklevel=2,
-    )
+    )  # pragma: no cover
     return isinstance(node.parent, nodes.If) and is_typing_guard(node.parent)
 
 
@@ -1828,7 +1828,7 @@ def is_node_in_guarded_import_block(node: nodes.NodeNG) -> bool:
         "This method will be removed in pylint 3.0; use in_type_checking_block() instead.",
         DeprecationWarning,
         stacklevel=2,
-    )
+    )  # pragma: no cover
     return isinstance(node.parent, nodes.If) and (
         is_sys_guard(node.parent) or is_typing_guard(node.parent)
     )
