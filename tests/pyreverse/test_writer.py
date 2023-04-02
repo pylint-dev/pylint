0f7861526beb59c7fe652dcc455c57bc22d47f6c
Alvaro Frias Garay <alvaro.frias@eclypsium.com>
2023-04-01 23:05:38 -0300

    Add tests
    Signed-off-by: Alvaro Frias Garay <alvaro.frias@eclypsium.com>

--- tests/pyreverse/test_writer.py
+++ tests/pyreverse/test_writer.py
@@ -207,3 +207,17 @@ def test_color_for_stdlib_module(default_config: PyreverseConfig) -> None:
     obj.node = Mock()
     obj.node.qname.return_value = "collections"
     assert writer.get_shape_color(obj) == "grey"
+
+# test to check the names of the generated files are corrected when using an incorrect character like "/" in the package name
+def test_package_name_with_slash(default_config: PyreverseConfig) -> None:
+    writer = DiagramWriter(default_config)
+    obj = Mock()
+
+    obj.objects = []
+    obj.get_relationships.return_value = []
+    obj.title = "test/package/name/with/slash/"
+    writer.write([obj])
+
+    assert os.path.exists("test_package_name_with_slash_.dot")
+    # remove the generated file
+    os.remove("test_package_name_with_slash_.dot")
