6e4d89ffba19ca3237a0cbda63a19e698a879769
Mark Byrne <mbyrnepr2@gmail.com>
2023-04-09 00:16:32 +0200

    Fix false positive for ``positional-only-arguments-expected`` when a function contains both a positional-only parameter that has a default value, and ``**kwargs``.
    Closes #8555

--- tests/functional/p/positional_only_arguments_expected.py
+++ tests/functional/p/positional_only_arguments_expected.py
@@ -16,3 +16,21 @@ cake.nihon(1, 2, i=3)  # [positional-only-arguments-expected]
 cake.nihon(1, r=2, i=3)  # [positional-only-arguments-expected]
 cake.nihon(a=1, r=2, i=3)  # [positional-only-arguments-expected]
 cake.nihon(1, r=2, i=3, cheese=True)  # [positional-only-arguments-expected]
+
+
+def function_with_kwargs(apple, banana="Yellow banana", /, **kwargs):
+    """
+    Calling this function with the `banana` keyword should not emit
+    `positional-only-arguments-expected` since it is added to `**kwargs`.
+
+    >>> function_with_kwargs("Red apple", banana="Green banana")
+    >>> "Red apple"
+    >>> "Yellow banana"
+    >>> {"banana": "Green banana"}
+    """
+    print(apple)
+    print(banana)
+    print(kwargs)
+
+
+function_with_kwargs("Red apple", banana="Green banana")
