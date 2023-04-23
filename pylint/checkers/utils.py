7826795cb68dbc484f954d9a29101041ebf62119
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-04-23 22:45:30 +0200

    Fix `FunctionDef` isinstance checks (#8607)

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -356,9 +356,10 @@ def is_defined_before(var_node: nodes.Name) -> bool:
         if defnode is None:
             continue
         defnode_scope = defnode.scope()
-        if isinstance(defnode_scope, (*COMP_NODE_TYPES, nodes.Lambda)):
+        if isinstance(
+            defnode_scope, (*COMP_NODE_TYPES, nodes.Lambda, nodes.FunctionDef)
+        ):
             # Avoid the case where var_node_scope is a nested function
-            # FunctionDef is a Lambda until https://github.com/pylint-dev/astroid/issues/291
             if isinstance(defnode_scope, nodes.FunctionDef):
                 var_node_scope = var_node.scope()
                 if var_node_scope is not defnode_scope and isinstance(
