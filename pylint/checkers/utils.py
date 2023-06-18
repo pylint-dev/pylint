d6c02b63896517e91e91077699a898c81d490a20
Jacob Walls <jacobtylerwalls@gmail.com>
2023-06-18 16:03:20 -0400

    Reduce indentation

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -1411,28 +1411,25 @@ def function_arguments_are_ambiguous(
 ) -> bool:
     if func1.argnames() != func2.argnames():
         return True
-    if func1.args.args is not None and func2.args.args is not None:
-        # Check ambiguity among function default values
-        pairs_of_defaults = [
-            (func1.args.defaults, func2.args.defaults),
-            (func1.args.kw_defaults, func2.args.kw_defaults),
-        ]
-        for zippable_default in pairs_of_defaults:
-            if len(zippable_default[0]) != len(zippable_default[1]):
-                return True
-            for default1, default2 in zip(*zippable_default):
-                if isinstance(default1, nodes.Const) and isinstance(
-                    default2, nodes.Const
-                ):
-                    if default1.value != default2.value:
-                        return True
-                elif isinstance(default1, nodes.Name) and isinstance(
-                    default2, nodes.Name
-                ):
-                    if default1.name != default2.name:
-                        return True
-                else:
+    # Check ambiguity among function default values
+    pairs_of_defaults = [
+        (func1.args.defaults, func2.args.defaults),
+        (func1.args.kw_defaults, func2.args.kw_defaults),
+    ]
+    for zippable_default in pairs_of_defaults:
+        if None in zippable_default:
+            continue
+        if len(zippable_default[0]) != len(zippable_default[1]):
+            return True
+        for default1, default2 in zip(*zippable_default):
+            if isinstance(default1, nodes.Const) and isinstance(default2, nodes.Const):
+                if default1.value != default2.value:
                     return True
+            elif isinstance(default1, nodes.Name) and isinstance(default2, nodes.Name):
+                if default1.name != default2.name:
+                    return True
+            else:
+                return True
     return False
 
 
