b6119317f8c4bf2f162097e5d18b16e2ec8e6b47
Jacob Walls <jacobtylerwalls@gmail.com>
2023-07-04 08:12:17 -0400

    Simplify

--- pylint/config/config_initialization.py
+++ pylint/config/config_initialization.py
@@ -156,13 +156,13 @@ def _config_initialization(
     )
 
 
-def _order_all_first(config_args: list[str], *, joined: bool = True) -> list[str]:
+def _order_all_first(config_args: list[str], *, joined: bool) -> list[str]:
     """Reorder config_args such that --enable=all or --disable=all comes first.
 
     Raise if both are given.
 
-    If joined is True, expect args in the form `--enable=all,for-any-all`.
-    If joined is False, expect args in the form '--enable', 'all,for-any-all`.
+    If joined is True, expect args in the form '--enable=all,for-any-all'.
+    If joined is False, expect args in the form '--enable', 'all,for-any-all'.
     """
     indexes_to_prepend = []
     all_action = ""
@@ -184,18 +184,17 @@ def _order_all_first(config_args: list[str], *, joined: bool = True) -> list[str
                 "--enable=all and --disable=all are incompatible."
             )
         all_action = arg
+
         indexes_to_prepend.append(i)
+        if not joined:
+            indexes_to_prepend.append(i + 1)
 
     returned_args = []
     for i in indexes_to_prepend:
         returned_args.append(config_args[i])
-        if not joined:
-            returned_args.append(config_args[i + 1])
 
     for i, arg in enumerate(config_args):
         if i in indexes_to_prepend:
-            continue
-        if not joined and i - 1 in indexes_to_prepend:
             continue
         returned_args.append(arg)
 
