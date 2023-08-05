03329f1736c90bea9f67ee7c31ad3a0dc2175198
Mark Byrne <mbyrnepr2@gmail.com>
2023-08-05 09:36:21 +0200

    Use a different example for the functional test.

--- tests/functional/i/invalid/invalid_name/invalid_name_enum.py
+++ tests/functional/i/invalid/invalid_name/invalid_name_enum.py
@@ -1,4 +1,3 @@
-
 """ Tests for invalid-name checker in the context of enums. """
 # pylint: disable=too-few-public-methods
 
@@ -6,16 +5,26 @@
 from enum import Enum
 
 
-class JustARegularClass:
-    """ No `invalid-name` by default for class attributes
-    """
-    apple = 42
-    orange = 24
+class Color(Enum):
+    """Represents colors as (red, green, blue) tuples."""
 
+    YELLOW      = 250, 250,   0
+    KHAKI       = 250, 250, 125
+    MAGENTA     = 250,   0, 250
+    VIOLET      = 250, 125, 250
+    CYAN        =   0, 250, 250
+    aquamarine  = 125, 250, 250  # [invalid-name]
 
-class EnumClass(Enum):
-    """ Members of a subclass of `enum.Enum` are expected to be UPPERCASE.
-    """
-    apple = 42  # [invalid-name]
-    orange = 24  # [invalid-name]
-    PEAR = 1
+    red: int
+    green: int
+    blue: int
+
+    def __init__(self, red: int, green: int, blue: int) -> None:
+        self.red   = red
+        self.green = green
+        self.blue  = blue
+
+    @property
+    def as_hex(self) -> str:
+        """Get hex 'abcdef' representation for a color."""
+        return f'{self.red:0{2}x}{self.green:0{2}x}{self.blue:0{2}x}'
