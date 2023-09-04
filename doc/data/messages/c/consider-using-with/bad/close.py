9c1c6637b3ea19bcd53a4efff7df9e178323f51c
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-09-04 21:56:13 +0200

    [consider-using-with] Use the template allowing multiple files

--- doc/data/messages/c/consider-using-with/bad/close.py
+++ doc/data/messages/c/consider-using-with/bad/close.py
@@ -1,5 +1,3 @@
 file = open("apple.txt", "r", encoding="utf8")  # [consider-using-with]
 contents = file.read()
 file.close()
-
-worst = open("banana.txt", "r", encoding="utf8").read()  # [consider-using-with]
