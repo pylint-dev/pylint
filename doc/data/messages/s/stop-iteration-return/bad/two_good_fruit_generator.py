6eca2c1528d1b6dd7dd61734e0c401ea522ff5f4
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-09-12 22:20:51 +0200

    [stop-iteration-return] Use the multiple files template

--- doc/data/messages/s/stop-iteration-return/bad/two_good_fruit_generator.py
+++ doc/data/messages/s/stop-iteration-return/bad/two_good_fruit_generator.py
@@ -1,14 +1,3 @@
-def fruit_generator():
-    for fruit in ["apple", "banana"]:
-        yield fruit
-    raise StopIteration  # [stop-iteration-return]
-
-
-def two_fruits_generator(fruits):
-    for fruit in fruits:
-        yield fruit, next(fruits)  # [stop-iteration-return]
-
-
 def two_good_fruits_generator(fruits):
     for fruit in fruits:
         if not fruit.is_tasty():
