f503f2c9a7b2a8edbcb721dd99f50656e6663925
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-09-12 21:25:00 +0200

    [overlapping-except] Use the multiple files template

--- doc/data/messages/o/overlapping-except/good/less_generic_first.py
+++ doc/data/messages/o/overlapping-except/good/less_generic_first.py
@@ -6,15 +6,3 @@ def divide_x_by_y(x: float, y: float):
     except ArithmeticError as e:
         # FloatingPointError  were already caught at this point
         print(f"There was an OverflowError or a ZeroDivisionError: {e}")
-
-
-# Or:
-
-
-def divide_x_by_y(x: float, y: float):
-    try:
-        print(x / y)
-    except ArithmeticError as e:
-        print(
-            f"There was an OverflowError, a ZeroDivisionError or a FloatingPointError: {e}"
-        )
