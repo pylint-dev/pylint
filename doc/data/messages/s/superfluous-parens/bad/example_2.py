717353531e8a66dd03fcc3232090d5fd6a9e510c
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-09-12 22:28:26 +0200

    [superfluous-parens] Use the multiple files template

--- doc/data/messages/s/superfluous-parens/bad/example_2.py
+++ doc/data/messages/s/superfluous-parens/bad/example_2.py
@@ -1,8 +1,3 @@
-x = input()
-y = input()
-if (x == y):  # [superfluous-parens]
-    pass
-
 i = 0
 exclude = []
 if (i - 0) in exclude:  # [superfluous-parens]
--- doc/data/messages/s/superfluous-parens/good/example_2.py
+++ doc/data/messages/s/superfluous-parens/good/example_2.py
@@ -1,8 +1,3 @@
-x = input()
-y = input()
-if x == y:
-    pass
-
 i = 0
 exclude = []
 if i - 0 in exclude:
