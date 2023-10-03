fc62a9da9c0a69678bcc1c3740a0500de06c0a17
Mark Byrne <31762852+mbyrnepr2@users.noreply.github.com>
2023-10-03 22:54:20 +0200

    Update pylint/checkers/utils.py
    Co-authored-by: Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -2285,5 +2285,4 @@ def is_enum_member(node: nodes.AssignName) -> bool:
     # A dataclass is one known case for when `members` can be `None`
     if members is None:
         return False
-    enum_member_objects = members[0].items
-    return node.name in [name_obj.name for value, name_obj in enum_member_objects]
+    return node.name in [name_obj.name for value, name_obj in members[0].items]
