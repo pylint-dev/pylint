b6774f6224f99f012aeb819e4f4898cf74dab240
Ryan Ozawa <ryan.ozawa21@gmail.com>
2023-10-08 13:45:22 -0700

    add: test cases for new checker
    original example case included
    a slightly modified positive case
    2 negative cases where exception is handled properly

---
+++ tests/functional/c/contextmanager_generator_missing_cleanup.py
@@ -0,0 +1,67 @@
+# pylint: disable = disallowed-name, missing-docstring, useless-return, invalid-name
+# pylint: disable = line-too-long, comparison-of-constants, broad-exception-raised
+# pylint: disable = unused-variable
+import contextlib
+from contextlib import contextmanager
+import weakref
+
+# Positive
+
+
+@contextlib.contextmanager
+def cm():  # [contextmanager-generator-missing-cleanup]
+    print("cm enter")
+    a = yield
+    print("cm exit")
+
+
+@contextmanager
+def bad_error_handler_cm():  # [contextmanager-generator-missing-cleanup]
+    print("cm enter")
+    try:
+        a = yield
+    except ValueError:
+        pass
+    print("cm exit")
+
+
+def genfunc():
+    with cm():
+        print("stepping")
+        yield
+
+
+def main():
+    gen = genfunc()
+    ref = weakref.ref(gen, print)
+    next(gen)
+
+
+# Negative
+
+
+@contextlib.contextmanager
+def good_contextmanager():
+    print("good cm enter")
+    try:
+        a = yield
+    finally:
+        print("good cm exit")
+
+
+@contextmanager
+def other_good_contextmanager():
+    print("good cm enter")
+    try:
+        a = yield
+    finally:
+        print("good cm exit")
+
+
+@contextmanager
+def good_cm_except():
+    print("good cm enter")
+    try:
+        a = yield
+    except GeneratorExit:
+        print("good cm exit")
