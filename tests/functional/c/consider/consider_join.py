db882dc478b83e484e113bd3711d21c02c98dd3d
theirix <theirix@gmail.com>
2023-10-09 12:48:01 +0300

    Updates test cases for option defaulted to true

--- tests/functional/c/consider/consider_join.py
+++ tests/functional/c/consider/consider_join.py
@@ -19,19 +19,19 @@ for number in ['1', '2', '3']:
 
 result = 'a'
 for number in ['1', '2', '3']:
-    result += f'b{number}'
+    result += f'b{number}'  # [consider-using-join]
 assert result == 'ab1b2b3'
 assert result == 'b'.join(['a', '1', '2', '3'])
 
 result = 'a'
 for number in ['1', '2', '3']:
-    result += f'{number}c'
+    result += f'{number}c'  # [consider-using-join]
 assert result == 'a1c2c3c'
 assert result == 'a' + 'c'.join(['1', '2', '3']) + 'c'
 
 result = 'a'
 for number in ['1', '2', '3']:
-    result += f'b{number}c'
+    result += f'b{number}c'  # [consider-using-join]
 assert result == 'ab1cb2cb3c'
 assert result == 'ab' + 'cb'.join(['1', '2', '3']) + 'c'
 
@@ -41,7 +41,7 @@ for number in ['1', '2', '3']:
 
 result = ''
 for number in ['1', '2', '3']:
-    result += f"{number}, "
+    result += f"{number}, "  # [consider-using-join]
 result = result[:-2]
 
 result = 0  # result is not a string
--- tests/functional/c/consider/consider_join_for_non_empty_separator.py
+++ tests/functional/c/consider/consider_join_for_non_empty_separator.py
@@ -2,23 +2,23 @@
 
 result = 'a'
 for number in ['1', '2', '3']:
-    result += f'b{number}'  # [consider-using-join]
+    result += f'b{number}'
 assert result == 'ab1b2b3'
 assert result == 'b'.join(['a', '1', '2', '3'])
 
 result = 'a'
 for number in ['1', '2', '3']:
-    result += f'{number}c'  # [consider-using-join]
+    result += f'{number}c'
 assert result == 'a1c2c3c'
 assert result == 'a' + 'c'.join(['1', '2', '3']) + 'c'
 
 result = 'a'
 for number in ['1', '2', '3']:
-    result += f'b{number}c'  # [consider-using-join]
+    result += f'b{number}c'
 assert result == 'ab1cb2cb3c'
 assert result == 'ab' + 'cb'.join(['1', '2', '3']) + 'c'
 
 result = ''
 for number in ['1', '2', '3']:
-    result += f"{number}, "  # [consider-using-join]
+    result += f"{number}, "
 result = result[:-2]
