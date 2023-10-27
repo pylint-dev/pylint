fa173283a4ba2b35e0fe63ef228d479effdef4ea
azinneck0485 <123660683+azinneck0485@users.noreply.github.com>
2023-10-27 17:11:27 -0400

    update functional testing for broad-exception-caught to include try-except*

--- tests/functional/b/broad_exception_caught.py
+++ tests/functional/b/broad_exception_caught.py
@@ -37,3 +37,33 @@ try:
     __revision__ += 1
 except CustomNarrowException:
     print('error')
+
+
+try:
+    __revision__ += 1
+except* Exception: # [broad-exception-caught]
+    print('error')
+
+
+try:
+    __revision__ += 1
+except* BaseException: # [broad-exception-caught]
+    print('error')
+
+
+try:
+    __revision__ += 1
+except* ValueError:
+    print('error')
+
+
+try:
+    __revision__ += 1
+except* CustomBroadException: # [broad-exception-caught]
+    print('error')
+
+
+try:
+    __revision__ += 1
+except* CustomNarrowException:
+    print('error')
