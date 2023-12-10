6884b2efa88cc0e9b3c303f78de16493ec2098bf
Jacob Walls <jacobtylerwalls@gmail.com>
2023-12-10 16:12:09 -0500

    [wrong-exception-operation] Fix FP for tuple concatenation of exception types (#9289)
    (cherry picked from commit 8d4c6c1f300414abad0d4f5cd81e592e8743b6f0)

--- tests/functional/w/wrong_exception_operation.py
+++ tests/functional/w/wrong_exception_operation.py
@@ -16,3 +16,24 @@ try:
     1/0
 except (ValueError < TypeError): # [wrong-exception-operation]
     pass
+
+
+# Concatenation of exception type tuples
+DIVISION_BY_ZERO = (ZeroDivisionError,)
+VALUE_ERROR = (ValueError,)
+UNINFERABLE = DIVISION_BY_ZERO | VALUE_ERROR
+
+try:
+    1/0
+except (ValueError, ) + DIVISION_BY_ZERO:
+    pass
+
+try:
+    1/0
+except (ValueError, ) | DIVISION_BY_ZERO:  # [wrong-exception-operation]
+    pass
+
+try:
+    1/0
+except (ValueError, ) + UNINFERABLE:
+    pass
