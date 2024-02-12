de65a5c470cf6a02246f964d3585eb1c80910bfd
Aleksey Petryankin <al.petryankin@gmail.com>
2024-02-12 05:06:17 +0000

    Increase test coverage for local configs
    - plus minor changes for github spellchecker
    - plus fix for root directory on Windows

--- tests/config/test_per_directory_config.py
+++ tests/config/test_per_directory_config.py
@@ -32,28 +32,34 @@ def test_fall_back_on_base_config(tmp_path: Path) -> None:
 
 
 @pytest.fixture
-def _create_subconfig_test_fs(tmp_path: Path) -> tuple[Path, Path, Path, Path]:
+def _create_subconfig_test_fs(tmp_path: Path) -> tuple[Path, ...]:
     level1_dir = tmp_path / "level1_dir"
     level1_init = level1_dir / "__init__.py"
     conf_file1 = level1_dir / "pylintrc"
     test_file1 = level1_dir / "a.py"
     test_file3 = level1_dir / "z.py"
+    level1_dir_without_config = tmp_path / "level1_dir_without_config"
+    level1_init2 = level1_dir_without_config / "__init__.py"
+    test_file4 = level1_dir_without_config / "aa.py"
     subdir = level1_dir / "sub"
     level2_init = subdir / "__init__.py"
     conf_file2 = subdir / "pylintrc"
     test_file2 = subdir / "b.py"
+    os.makedirs(level1_dir_without_config)
     os.makedirs(subdir)
     level1_init.touch()
+    level1_init2.touch()
     level2_init.touch()
     test_file_text = "#LEVEL1\n#LEVEL2\n#ALL_LEVELS\n#TODO\n"
     test_file1.write_text(test_file_text)
     test_file2.write_text(test_file_text)
     test_file3.write_text(test_file_text)
+    test_file4.write_text(test_file_text)
     conf1 = "[MISCELLANEOUS]\nnotes=LEVEL1,ALL_LEVELS"
     conf2 = "[MISCELLANEOUS]\nnotes=LEVEL2,ALL_LEVELS"
     conf_file1.write_text(conf1)
     conf_file2.write_text(conf2)
-    return level1_dir, test_file1, test_file2, test_file3
+    return level1_dir, test_file1, test_file2, test_file3, test_file4
 
 
 # check that use-parent-configs doesn't break anything
@@ -61,10 +67,12 @@ def _create_subconfig_test_fs(tmp_path: Path) -> tuple[Path, Path, Path, Path]:
     "local_config_args",
     [["--use-local-configs=y"], ["--use-local-configs=y", "--use-parent-configs=y"]],
 )
-# check files and configs from top-level package or subpackage
+# check modules and use of configuration files from top-level package or subpackage
 @pytest.mark.parametrize("test_file_index", [0, 1, 2])
 # check cases when cwd contains pylintrc or not
-@pytest.mark.parametrize("start_dir_modificator", [".", ".."])
+@pytest.mark.parametrize(
+    "start_dir_modificator", [".", "..", "../level1_dir_without_config"]
+)
 def test_subconfig_vs_root_config(
     _create_subconfig_test_fs: tuple[Path, ...],
     capsys: CaptureFixture,
@@ -80,7 +88,7 @@ def test_subconfig_vs_root_config(
     start_dir = (level1_dir / start_dir_modificator).resolve()
 
     orig_cwd = os.getcwd()
-    output = [f"{start_dir = }, {test_file = }"]
+    output = [f"{start_dir = }\n{test_file = }\n"]
     os.chdir(start_dir)
     for _ in range(2):
         # _Run adds --rcfile, which overrides config from cwd, so we need original Run here
@@ -90,28 +98,70 @@ def test_subconfig_vs_root_config(
         test_file = test_file.parent
     os.chdir(orig_cwd)
 
-    expected_note = f"LEVEL{(test_file_index%2)+1}"
-    assert (
-        expected_note in output[1]
-    ), f"readable debug output:\n{output[0]}\n{output[1]}"
-    assert (
-        expected_note in output[2]
-    ), f"readable debug output:\n{output[0]}\n{output[2]}"
+    expected_note = "LEVEL1"
+    if test_file_index == 1:
+        expected_note = "LEVEL2"
+    assert_message = f"Wrong note after checking FILE. Readable debug output:\n{output[0]}\n{output[1]}"
+    assert expected_note in output[1], assert_message
+    assert_message = f"Wrong note after checking DIRECTORY. Readable debug output:\n{output[0]}\n{output[2]}"
+    assert expected_note in output[2], assert_message
 
     if test_file_index == 0:
         # 'pylint level1_dir/' should use config from subpackage when checking level1_dir/sub/b.py
-        assert (
-            "LEVEL2" in output[2]
-        ), f"readable debug output:\n{output[0]}\n{output[2]}"
+        assert_message = f"Wrong note after checking DIRECTORY. Readable debug output:\n{output[0]}\n{output[2]}"
+        assert "LEVEL2" in output[2], assert_message
     if test_file_index == 1:
         # 'pylint level1_dir/sub/b.py' and 'pylint level1_dir/sub/' should use
         # level1_dir/sub/pylintrc, not level1_dir/pylintrc
-        assert (
-            "LEVEL1" not in output[1]
-        ), f"readable debug output:\n{output[0]}\n{output[1]}"
-        assert (
-            "LEVEL1" not in output[2]
-        ), f"readable debug output:\n{output[0]}\n{output[2]}"
+        assert_message = f"Wrong note after checking FILE. Readable debug output:\n{output[0]}\n{output[1]}"
+        assert "LEVEL1" not in output[1], assert_message
+        assert_message = f"Wrong note after checking DIRECTORY. Readable debug output:\n{output[0]}\n{output[2]}"
+        assert "LEVEL1" not in output[2], assert_message
+
+
+# check that use-parent-configs doesn't break anything
+@pytest.mark.parametrize(
+    "local_config_args",
+    [["--use-local-configs=y"], ["--use-local-configs=y", "--use-parent-configs=y"]],
+)
+# check cases when test_file without local config belongs to cwd subtree or not
+@pytest.mark.parametrize(
+    "start_dir_modificator", [".", "..", "../level1_dir_without_config"]
+)
+def test_missing_local_config(
+    _create_subconfig_test_fs: tuple[Path, ...],
+    capsys: CaptureFixture,
+    local_config_args: list[str],
+    start_dir_modificator: str,
+) -> None:
+    """Test that when checked file or module doesn't have config
+    in its own directory, it uses default config or config from cwd.
+    """
+    level1_dir, *tmp_files = _create_subconfig_test_fs
+    # file from level1_dir_without_config
+    test_file = tmp_files[3]
+    start_dir = (level1_dir / start_dir_modificator).resolve()
+
+    orig_cwd = os.getcwd()
+    output = [f"{start_dir = }\n{test_file = }\n"]
+    os.chdir(start_dir)
+    for _ in range(2):
+        # _Run adds --rcfile, which overrides config from cwd, so we need original Run here
+        LintRun([*local_config_args, str(test_file)], exit=False)
+        output.append(capsys.readouterr().out.replace("\\n", "\n"))
+
+        test_file = test_file.parent
+    os.chdir(orig_cwd)
+
+    # from default config
+    expected_note = "TODO"
+    if start_dir_modificator == ".":
+        # from config in level1_dir
+        expected_note = "LEVEL1"
+    assert_message = f"Wrong note after checking FILE. Readable debug output:\n{output[0]}\n{output[1]}"
+    assert expected_note in output[1], assert_message
+    assert_message = f"Wrong note after checking DIRECTORY. Readable debug output:\n{output[0]}\n{output[2]}"
+    assert expected_note in output[2], assert_message
 
 
 @pytest.mark.parametrize("test_file_index", [0, 1])
@@ -120,7 +170,7 @@ def test_subconfig_vs_cli_arg(
     capsys: CaptureFixture,
     test_file_index: int,
 ) -> None:
-    """Test that cli args have priority over subconfigs."""
+    """Test that CLI arguments have priority over subconfigs."""
     test_root, *tmp_files = _create_subconfig_test_fs
     test_file = tmp_files[test_file_index]
     orig_cwd = os.getcwd()
@@ -129,9 +179,9 @@ def test_subconfig_vs_cli_arg(
     output = capsys.readouterr().out.replace("\\n", "\n")
     os.chdir(orig_cwd)
 
-    # check that cli arg overrides default value
+    # check that CLI argument overrides default value
     assert "TODO" not in output
-    # notes=FIXME in cli should override all pylintrc configs
+    # notes=FIXME in arguments should override all pylintrc configs
     assert "ALL_LEVELS" not in output
 
 
@@ -162,3 +212,18 @@ def test_subconfig_in_parent(tmp_path: Path, capsys: CaptureFixture) -> None:
     # check that file is linted with config from ../, which is not a cwd
     assert "TODO" not in output1
     assert "LEVEL1" in output1
+
+
+def test_register_local_config_accepts_directory(
+    _create_subconfig_test_fs: tuple[Path, ...]
+) -> None:
+    """Test that register_local_config can handle directory as argument."""
+    level1_dir, *tmp_files = _create_subconfig_test_fs
+    # init linter without local configs
+    linter = LintRun([str(tmp_files[0])], exit=False).linter
+    assert level1_dir not in linter._directory_namespaces
+
+    # call register_local_config with directory as argument
+    assert level1_dir.is_dir()
+    linter.register_local_config(str(level1_dir))
+    assert level1_dir in linter._directory_namespaces
