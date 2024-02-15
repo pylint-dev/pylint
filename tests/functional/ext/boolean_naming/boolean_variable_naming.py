abcb09fe08fd918c430e029d4228df939a85a1a7
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-02-15 08:55:21 -0800

    add: test cases for code coverage

--- tests/functional/ext/boolean_naming/boolean_variable_naming.py
+++ tests/functional/ext/boolean_naming/boolean_variable_naming.py
@@ -1,6 +1,6 @@
 """Tests that variables assigned as constants with boolean values are checked."""
 
-# pylint: disable=invalid-name
+# pylint: disable=invalid-name,missing-function-docstring
 a: bool = True  # [invalid-boolean-variable-name]
 
 # +1: [invalid-boolean-variable-name]
@@ -14,3 +14,13 @@ can_fly = False
 is_flying: bool = False
 
 flew = True  # [invalid-boolean-variable-name]
+
+
+def hello() -> str:
+    return "Hello, World!"
+
+
+my_str = hello()
+
+another_str: str
+another_str = "Hello, World!"
