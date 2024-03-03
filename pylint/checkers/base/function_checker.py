6c950356d4b008441f7dc967b9c958e6eca627f0
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-03-03 13:43:02 -0800

    change: new checker logic for refined 'contextmanager_generator_missing_cleanup' error

--- pylint/checkers/base/function_checker.py
+++ pylint/checkers/base/function_checker.py
@@ -6,10 +6,13 @@
 
 from __future__ import annotations
 
+from itertools import chain
+
 from astroid import nodes
 
 from pylint.checkers import utils
 from pylint.checkers.base.basic_checker import _BasicChecker
+from pylint.lint.pylinter import PyLinter
 
 
 class FunctionChecker(_BasicChecker):
@@ -24,113 +27,189 @@ class FunctionChecker(_BasicChecker):
         )
     }
 
+    def __init__(self, linter: PyLinter) -> None:
+        """Initialize the checker and container to store contextmanagers."""
+        super().__init__(linter)
+        # as this walks FunctionDef nodes, if it is wrapped in a contextmanager
+        # add it here for checking in generator functions
+        # if encounter generator FunctionDef, then check if it uses a contextmanager and conditionally add here
+        # TODO: should this string be function name or module import name (foo v mod.bar.foo)
+        self._contextmanagers: set[str] = set()
+        # as this walks FunctionDef nodes, if it is a generator that uses a With/AsyncWith node
+        # add it here for checking against contextmanager, in case order of visiting is reversed
+        # key is the contextmanager string name, value is the generator FunctionDef node to be used in the message
+        self._generator_with_contextmanager: dict[str, nodes.FunctionDef] = {}
+
+    def _add_node_to_contextmanagers(self, node: nodes.FunctionDef) -> None:
+        """Add a node to the internal contextmanager set.
+
+        Checks if the node was previously added and if so, adds a message.
+
+        :param node: FunctionDef node to add
+        :type node: nodes.FunctionDef
+        """
+        simple_name = node.name
+        if simple_name not in self._contextmanagers:
+            self._contextmanagers.add(simple_name)
+        else:
+            err_node = self._generator_with_contextmanager[simple_name]
+            self.add_message(
+                "contextmanager-generator-missing-cleanup",
+                node=err_node,
+                args=(err_node.lineno,),
+            )
+
+    def _add_node_to_generators(
+        self, with_node: nodes.With | nodes.AsyncWith, node: nodes.FunctionDef
+    ) -> None:
+        """Adds node to the internal generator set.
+
+        If the used contextmanager for this generator has already been added, add a message.
+
+        :param with_node: The With or AsyncWith node to add that the generator function uses
+        :type with_node: nodes.With | nodes.AsyncWith
+        :param node: The function node to possible add messages about
+        :type node: nodes.FunctionDef
+        """
+        # with statements can have multiple items, so we will just add all for now
+        # TODO: clean up this logic, probably have to change the input params to be the contextManager call
+        for item in with_node.items:
+            used_contextmanager = item[0]
+            contextmgr_name = self._get_name(used_contextmanager)
+            if contextmgr_name in self._contextmanagers:
+                self.add_message(
+                    "contextmanager-generator-missing-cleanup",
+                    node=node,
+                    args=(node.lineno,),
+                )
+            else:
+                self._generator_with_contextmanager[contextmgr_name] = node
+                self._contextmanagers.add(contextmgr_name)
+
     @utils.only_required_for_messages("contextmanager-generator-missing-cleanup")
     def visit_functiondef(self, node: nodes.FunctionDef) -> None:
+        self._check_contextmanager_generator_missing_cleanup(node)
+
+    @utils.only_required_for_messages("contextmanager-generator-missing-cleanup")
+    def visit_asyncfunctiondef(self, node: nodes.AsyncFunctionDef) -> None:
         self._check_contextmanager_generator_missing_cleanup(node)
 
     def _check_contextmanager_generator_missing_cleanup(
         self, node: nodes.FunctionDef
     ) -> None:
-        """Check a FunctionDef to see if it uses `contextmanager` on a generator
-        function.
-
-        The following code sample will never print the message **cm exit**::
-
-            from contextlib import contextmanager
-
-            @contextmanager
-            def cm():
-                print("cm enter")
-                a = yield
-                print('cm exit')
-
-            def genfunc():
-                with cm():
-                    print("stepping")
-                    yield
-
-            def main():
-                gen = genfunc()
-                ref = weakref.ref(gen, print)
-                next(gen)
-
-        The function ``cm`` needs to include error handling for ``GeneratorExit``
-        to properly finish the function execution.
+        """Check a FunctionDef to find contextmanagers and generators used in a no
+        cleanup pattern.
 
         :param node: FunctionDef node to check
         :type node: nodes.FunctionDef
         """
-        contextmanager_str = "contextmanager"
-        if node.decorators is None:
+        # if function does not use a Yield statement, it cant be a generator or a contextmanager
+        # TODO: should this also capture YieldFrom?
+        yield_nodes = list(node.nodes_of_class(nodes.Yield))
+        if not yield_nodes:
             return
 
-        decorator_list = node.decorators.nodes
-        if not any(
-            contextmanager_str == self._get_name(deco) for deco in decorator_list
-        ):
-            # No contextmanager decorator
-            return
-
-        # A ``yield`` statement can either be Expr or Assign, but the value is always of Yield
-        # Additionally a good function could be input where the yield is already wrapped by a Try
-        # but not have the required Handler or Finally
-        body_nodes: list[nodes.Yield | nodes.Try] = list(
-            node.nodes_of_class((nodes.Yield, nodes.Try))
+        # check if function has a contextmanager decorator
+        contextmanager_str = "contextlib.contextmanager"
+        should_be_added_to_contextmanagers = any(
+            decorator
+            # this is fully qualified import name
+            for decorator in node.decoratornames()
+            if decorator == contextmanager_str
         )
-        yield_nodes: list[nodes.Yield] = []
-        for n in body_nodes:
-            if not isinstance(n, nodes.Try):
-                yield_nodes.append(n)
-                continue
-            if not n.nodes_of_class(nodes.Yield):
-                # if Try has no Yield inside it, ignore
-                continue
-            # This Try Node has a Yield inside of it, making this a generator function wrapped
-            # by contextlib.contextmanager. See if handlers or finally is present
-            if (
-                any(
-                    self._get_name(handler.type) == "GeneratorExit"
-                    for handler in n.handlers
-                    if handler.type is not None
-                )
-                or n.finalbody
-            ):
-                continue
-
-            # this Try block doesn't properly handle GeneratorExit, add warning
-            self.add_message(
-                "contextmanager-generator-missing-cleanup",
-                node=node,
-                args=(node.end_lineno,),
-            )
+        # cant be a contextmanager and a erroring generator at the same time
+        if (
+            should_be_added_to_contextmanagers
+            and self._node_fails_contextmanager_cleanup(node, yield_nodes=yield_nodes)
+        ):
+            self._add_node_to_contextmanagers(node)
             return
 
-        # if there are yield nodes in function and nodes that could be cleanup after it
-        # add the warning
-        # if the last statement is the yield, there is no cleanup to be done
-        if yield_nodes and not self._check_node_has_yield(node.body[-1]):
-            self.add_message(
-                "contextmanager-generator-missing-cleanup",
-                node=node,
-                args=(node.end_lineno,),
-            )
+        # otherwise could be a generator that uses a contextmanager
+        with_nodes = list(node.nodes_of_class(nodes.With))
+        async_with_nodes = list(node.nodes_of_class(nodes.AsyncWith))
+        if not with_nodes and not async_with_nodes:
+            # no inner contextmanager, so no need to check
+            return
+        all_nodes = chain(with_nodes, async_with_nodes)
+        for with_node in all_nodes:
+            if self._node_fails_generator_checks(with_node):
+                self._add_node_to_generators(with_node, node)
 
     @staticmethod
-    def _check_node_has_yield(node: nodes.NodeNG) -> bool:
-        """Return whether a given node has a Yield node in it.
+    def _node_fails_contextmanager_cleanup(
+        node: nodes.FunctionDef, yield_nodes: list[nodes.Yield]
+    ) -> bool:
+        """Check if a node fails contextmanager cleanup.
+
+        Current checks for a contextmanager:
+            - only if the context manager yields something other than None
+            - only if the context manager lacks a finally, or does not catch GeneratorExit
 
         :param node: Node to check
-        :type node: nodes.NodeNG
-        :return: True if Yield contained, False otherwise
+        :type node: nodes.FunctionDef
+        :return: True if fails, False otherwise
+        :param yield_nodes: List of Yield nodes in the function body
+        :type yield_nodes: list[nodes.Yield]
         :rtype: bool
         """
-        return any(node.nodes_of_class(nodes.Yield))
+        # if context manager yields a non-constant value, then continue checking
+        if any(
+            yield_node.value is None or isinstance(yield_node.value, nodes.Const)
+            for yield_node in yield_nodes
+        ):
+            return False
+        # if function body has multiple Trys, filter down to the ones that have a yield node
+        try_with_yield_nodes = [
+            try_node
+            for try_node in node.nodes_of_class(nodes.Try)
+            if any(
+                try_body_node.value in yield_nodes for try_body_node in try_node.body
+            )
+        ]
+        if not try_with_yield_nodes:
+            # no try blocks at all, so checks after this line do not apply
+            return True
+        # if the contextmanager has a finally block, then it is fine
+        if all(try_node.finalbody for try_node in try_with_yield_nodes):
+            return False
+        # if the contextmanager catches GeneratorExit, then it is fine
+        if all(
+            any(
+                isinstance(handler.type, nodes.Name)
+                and handler.type.name == "GeneratorExit"
+                for handler in try_node.handlers
+            )
+            for try_node in try_with_yield_nodes
+        ):
+            return False
+        return True
+
+    @staticmethod
+    def _node_fails_generator_checks(node: nodes.With | nodes.AsyncWith) -> bool:
+        """Check if a node fails generator checks.
+
+        Current checks for generator functions:
+            - only if a with/async with statement is used
+            - only if the val in `with cm() as val` is not discarded, e.g. not for uses like with cm():
+
+        :param node: Node to check
+        :type node: nodes.With | nodes.AsyncWith
+        :return: True if fails, False otherwise
+        :rtype: bool
+        """
+        # if the with/async with statement discards the value, then return False
+        if all(item[1] is None for item in node.items):
+            return False
+
+        return True
 
     @staticmethod
     def _get_name(node: nodes.NodeNG) -> str:
-        """Helper function to get the name from a node.
+        """Helper function to get the final name component of a node.
 
         Helpful when possible to have a Name or Attribute node.
+        If Attribute, returns the attrname, otherwise returns the name.
 
         :param node: Node to get name of
         :type node: nodes.NodeNG
@@ -141,4 +220,6 @@ class FunctionChecker(_BasicChecker):
             return node.name  # type: ignore[no-any-return]
         if isinstance(node, nodes.Attribute):
             return node.attrname  # type: ignore[no-any-return]
+        if isinstance(node, nodes.Call):
+            return node.func.repr_name()  # type: ignore[no-any-return]
         return node.as_string()  # type: ignore[no-any-return]
