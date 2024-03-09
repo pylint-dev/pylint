54979847b5a369cf033882bac728710d0f1129a5
Dennis Keck <26092524+fellhorn@users.noreply.github.com>
2024-03-09 11:07:01 +0100

    Cleanup tests
    Signed-off-by: Dennis Keck <26092524+fellhorn@users.noreply.github.com>

--- tests/test_self.py
+++ tests/test_self.py
@@ -215,8 +215,13 @@ class TestRunTC:
 
     def test_disable_all(self) -> None:
         out = StringIO()
-        self._runtest([UNNECESSARY_LAMBDA, "--disable=all"], out=out, code=32)
-        assert "No files to lint: exiting." in out.getvalue().strip()
+        self._runtest([UNNECESSARY_LAMBDA, "--disable=all"], out=out, code=0)
+        assert "Your code has been rated at 10.00/10" in out.getvalue().strip()
+
+    def test_disable_all_enable_invalid(self) -> None:
+        out = StringIO()
+        self._runtest([UNNECESSARY_LAMBDA, "--disable=all", "--enable=foo"], out=out, code=0)
+        assert "W0012: Unknown option value for '--enable', expected a valid pylint message and got 'foo'" in out.getvalue().strip()
 
     def test_output_with_verbose(self) -> None:
         out = StringIO()
