1ad19d4744530bf20eab1d2977c63255ec0c3b44
Mark Byrne <mbyrnepr2@gmail.com>
2024-05-22 14:28:10 +0200

    Fix a false negative for ``duplicate-argument-name`` by including ``positional-only``, ``*args`` and ``**kwargs`` arguments in the check.
    Closes #9669

--- tests/functional/d/duplicate/duplicate_argument_name.py
+++ tests/functional/d/duplicate/duplicate_argument_name.py
@@ -1,14 +1,23 @@
 """Check for duplicate function arguments."""
 
+# pylint: disable=missing-docstring, line-too-long
+
 
 def foo1(_, _): # [duplicate-argument-name]
-    """Function with duplicate argument name."""
+    ...
 
 def foo2(_abc, *, _abc): # [duplicate-argument-name]
-    """Function with duplicate argument name."""
+    ...
 
 def foo3(_, _=3): # [duplicate-argument-name]
-    """Function with duplicate argument name."""
+    ...
 
 def foo4(_, *, _): # [duplicate-argument-name]
-    """Function with duplicate argument name."""
+    ...
+
+def foo5(_, *_, _=3): # [duplicate-argument-name, duplicate-argument-name]
+    ...
+
+# +1: [duplicate-argument-name, duplicate-argument-name, duplicate-argument-name, duplicate-argument-name]
+def foo6(_, /, _, *_, _="_", **_):
+    ...
