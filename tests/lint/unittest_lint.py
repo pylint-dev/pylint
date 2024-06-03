7e593167e363278903ac72bbe4d685e2cb50f2d7
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2024-06-04 01:01:05 +0200

    Hopefully fix the last test pollution

--- tests/lint/unittest_lint.py
+++ tests/lint/unittest_lint.py
@@ -20,6 +20,7 @@ from pathlib import Path
 from shutil import copy, rmtree
 from unittest import mock
 
+import astroid
 import platformdirs
 import pytest
 from astroid import nodes
@@ -1130,6 +1131,9 @@ def test_recursive_ignore(ignore_parameter: str, ignore_parameter_value: str) ->
     ):
         module = os.path.abspath(join(REGRTEST_DATA_DIR, *regrtest_data_module))
     assert module in linted_file_paths
+    # We lint the modules in `regrtest` in other tests as well. Prevent test polluting by
+    # explictly clearing the astroid caches.
+    astroid.MANAGER.clear_cache()
 
 
 def test_source_roots_globbing() -> None:
