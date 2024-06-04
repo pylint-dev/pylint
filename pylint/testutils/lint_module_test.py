5ff5496ce06e13538356b9d0ade56de47b5325f1
Shane Smiskol <shane@smiskol.com>
2024-06-04 00:08:55 -0700

    other PR

--- pylint/testutils/lint_module_test.py
+++ pylint/testutils/lint_module_test.py
@@ -253,12 +253,12 @@ class LintModuleTest:
         self._linter.check(modules_to_check)
         expected_messages, expected_output = self._get_expected()
         actual_messages, actual_output = self._get_actual()
-        self._check_output_text(expected_messages, expected_output, actual_output)
         assert (
             expected_messages == actual_messages
         ), self.error_msg_for_unequal_messages(
             actual_messages, expected_messages, actual_output
         )
+        self._check_output_text(expected_messages, expected_output, actual_output)
 
     def error_msg_for_unequal_messages(
         self,
