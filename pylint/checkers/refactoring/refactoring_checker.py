9cf4d1fa872064745563553002e58094ab1a3ff7
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2024-06-06 23:15:18 +0200

    [unnecessary-list-index-lookup] Fix crashes for uninferrable 'start' value in 'enumerate' (#9704)
    ---------
    
    Co-authored-by: Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
    (cherry picked from commit 9f8dcbd6a9d37d56a14b747b83d97d6b8527a2ab)

--- pylint/checkers/refactoring/refactoring_checker.py
+++ pylint/checkers/refactoring/refactoring_checker.py
@@ -2454,7 +2454,9 @@ class RefactoringChecker(checkers.BaseTokenChecker):
             and isinstance(node.operand, (nodes.Attribute, nodes.Name))
         ):
             inferred = utils.safe_infer(node)
-            start_val = inferred.value if inferred else None
+            # inferred can be an astroid.base.Instance as in 'enumerate(x, int(y))' or
+            # not correctly inferred (None)
+            start_val = inferred.value if isinstance(inferred, nodes.Const) else None
             return start_val, INFERENCE
         if isinstance(node, nodes.UnaryOp):
             return node.operand.value, HIGH
