96592c916290f26011da98b65b7f26016b625ccb
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2024-06-14 10:21:54 +0200

    same treatment for functional test update

--- tests/functional/r/regression_02/regression_8736.py
+++ tests/functional/r/regression_02/regression_8736.py
@@ -1,2 +1,3 @@
-"""This does not crash in the functional tests, but it does when called directly"""
-assert "\U00010000" == "\ud800\udc00"
+"""This does not crash in the functional tests, but it did when called directly."""
+
+assert "\U00010000" == "\ud800\udc00"  # [comparison-of-constants]
