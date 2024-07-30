3015491625d3c80c50ce3fb06deff856920897c9
Jacob Walls <jacobtylerwalls@gmail.com>
2024-07-29 21:58:03 -0400

    cosmetic

--- doc/data/messages/t/too-many-positional/bad.py
+++ doc/data/messages/t/too-many-positional/bad.py
@@ -1,6 +1,5 @@
 class FiveArgumentMethods:
     """The max positional arguments default is 5."""
 
-    # +1: [too-many-positional]]
-    def take_five_args(self, a, b, c, d, e):
+    def take_five_args(self, a, b, c, d, e):  # [too-many-positional]
         pass
