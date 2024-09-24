01c559ba630618e1adbaa551c134419137df9b7b
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2024-09-24 12:53:28 +0200

    [too-many-positional-arguments doc] Add the main option to use, real example

--- doc/data/messages/t/too-many-positional-arguments/bad.py
+++ doc/data/messages/t/too-many-positional-arguments/bad.py
@@ -1,5 +1,6 @@
-class FiveArgumentMethods:
-    """The max positional arguments default is 5."""
+# +1: [too-many-positional-arguments]
+def calculate_drag_force(velocity, area, density, drag_coefficient):
+    return 0.5 * drag_coefficient * density * area * velocity**2
 
-    def take_five_args(self, a, b, c, d, e):  # [too-many-positional-arguments]
-        pass
+
+drag_force = calculate_drag_force(30, 2.5, 1.225, 0.47)
--- doc/data/messages/t/too-many-positional-arguments/good.py
+++ doc/data/messages/t/too-many-positional-arguments/good.py
@@ -1,5 +1,7 @@
-class FiveArgumentMethods:
-    """The max positional arguments default is 5."""
+def calculate_drag_force(*, velocity, area, density, drag_coefficient):
+    return 0.5 * drag_coefficient * density * area * velocity**2
 
-    def take_five_args(self, a, b, c, d, *, e=False):
-        pass
+
+drag_force = calculate_drag_force(
+    velocity=30, area=2.5, density=1.225, drag_coefficient=0.47
+)
