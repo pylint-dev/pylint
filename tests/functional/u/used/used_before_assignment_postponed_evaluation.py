74c49b2a58f78526c6fc86f250e6ffca91afac8d
Nick Drozd <nicholasdrozd@gmail.com>
2024-10-01 04:39:55 -0400

    Add tests for #8192, #8893, #9642, #9689, #9815, #9941 (#9981)

--- tests/functional/u/used/used_before_assignment_postponed_evaluation.py
+++ tests/functional/u/used/used_before_assignment_postponed_evaluation.py
@@ -1,5 +1,5 @@
 """Tests for used-before-assignment when postponed evaluation of annotations is enabled"""
-# pylint: disable=missing-function-docstring, invalid-name
+# pylint: disable=missing-function-docstring, invalid-name, missing-class-docstring, too-few-public-methods
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
@@ -11,3 +11,29 @@ print(var)  # [used-before-assignment]
 
 def function_one(m: math):  # no error for annotations
     return m
+
+# https://github.com/pylint-dev/pylint/issues/8893
+if TYPE_CHECKING:
+    import datetime
+
+def f():
+    return datetime.datetime.now()  # [used-before-assignment]
+
+def g() -> datetime.datetime:
+    return datetime.datetime.now()  # FALSE NEGATIVE
+
+if TYPE_CHECKING:
+    class X:
+        pass
+
+def h():
+    return X()  # FALSE NEGATIVE
+
+def i() -> X:
+    return X()  # FALSE NEGATIVE
+
+if TYPE_CHECKING:
+    from mod import Y
+
+def j():
+    return {Y() for _ in range(1)}  # FALSE NEGATIVE
