8e46d69adb3e493a38cb879e132763b531aca4d2
Dominic Lavery <dom@circleci.com>
2024-11-21 12:37:58 +0000

    Calculate linter.config.jobs in cgroupsv2 environments. Fixes #10103

--- tests/test_pylint_runners.py
+++ tests/test_pylint_runners.py
@@ -18,6 +18,7 @@ from unittest.mock import MagicMock, mock_open, patch
 import pytest
 
 from pylint import run_pylint, run_pyreverse, run_symilar
+from pylint.lint.run import _query_cpu
 from pylint.testutils import GenericTestReporter as Reporter
 from pylint.testutils._run import _Run as Run
 from pylint.testutils.utils import _test_cwd
@@ -90,6 +91,8 @@ def test_pylint_run_jobs_equal_zero_dont_crash_with_cpu_fraction(
     def _mock_path(*args: str, **kwargs: Any) -> pathlib.Path:
         if args[0] == "/sys/fs/cgroup/cpu/cpu.shares":
             return MagicMock(is_file=lambda: True)
+        if args[0] == "/sys/fs/cgroup/cpu.max":
+            return MagicMock(is_file=lambda: False)
         return pathlib_path(*args, **kwargs)
 
     filepath = os.path.abspath(__file__)
@@ -100,3 +103,90 @@ def test_pylint_run_jobs_equal_zero_dont_crash_with_cpu_fraction(
                 with patch("pylint.lint.run.Path", _mock_path):
                     Run(testargs, reporter=Reporter())
         assert err.value.code == 0
+
+
+@pytest.mark.parametrize(
+    "contents",
+    [
+        "1 2",
+        "max 100000",
+    ],
+)
+def test_pylint_run_jobs_equal_zero_dont_crash_with_cgroupv2(
+    tmp_path: pathlib.Path,
+    contents: str,
+) -> None:
+    """Check that the pylint runner does not crash if `pylint.lint.run._query_cpu`
+    determines only a fraction of a CPU core to be available.
+    """
+    builtin_open = open
+
+    def _mock_open(*args: Any, **kwargs: Any) -> BufferedReader:
+        if args[0] == "/sys/fs/cgroup/cpu.max":
+            return mock_open(read_data=contents)(*args, **kwargs)  # type: ignore[no-any-return]
+        return builtin_open(*args, **kwargs)  # type: ignore[no-any-return]
+
+    pathlib_path = pathlib.Path
+
+    def _mock_path(*args: str, **kwargs: Any) -> pathlib.Path:
+        if args[0] == "/sys/fs/cgroup/cpu/cpu.shares":
+            return MagicMock(is_file=lambda: False)
+        if args[0] == "/sys/fs/cgroup/cpu/cfs_quota_us":
+            return MagicMock(is_file=lambda: False)
+        if args[0] == "/sys/fs/cgroup/cpu.max":
+            return MagicMock(is_file=lambda: True)
+        return pathlib_path(*args, **kwargs)
+
+    filepath = os.path.abspath(__file__)
+    testargs = [filepath, "--jobs=0"]
+    with _test_cwd(tmp_path):
+        with pytest.raises(SystemExit) as err:
+            with patch("builtins.open", _mock_open):
+                with patch("pylint.lint.run.Path", _mock_path):
+                    Run(testargs, reporter=Reporter())
+        assert err.value.code == 0
+
+
+@pytest.mark.parametrize(
+    "contents,expected",
+    [
+        ("50000 100000", 1),
+        ("100000 100000", 1),
+        ("200000 100000", 2),
+        ("299999 100000", 2),
+        ("300000 100000", 3),
+        # Unconstrained cgroup
+        ("max 100000", None),
+    ],
+)
+def test_query_cpu_cgroupv2(
+    tmp_path: pathlib.Path,
+    contents: str,
+    expected: int,
+) -> None:
+    """Check that `pylint.lint.run._query_cpu` generates realistic values in cgroupsv2
+    systems.
+    """
+    builtin_open = open
+
+    def _mock_open(*args: Any, **kwargs: Any) -> BufferedReader:
+        if args[0] == "/sys/fs/cgroup/cpu.max":
+            return mock_open(read_data=contents)(*args, **kwargs)  # type: ignore[no-any-return]
+        return builtin_open(*args, **kwargs)  # type: ignore[no-any-return]
+
+    pathlib_path = pathlib.Path
+
+    def _mock_path(*args: str, **kwargs: Any) -> pathlib.Path:
+        if args[0] == "/sys/fs/cgroup/cpu/cpu.shares":
+            return MagicMock(is_file=lambda: False)
+        if args[0] == "/sys/fs/cgroup/cpu/cfs_quota_us":
+            return MagicMock(is_file=lambda: False)
+        if args[0] == "/sys/fs/cgroup/cpu.max":
+            return MagicMock(is_file=lambda: True)
+        return pathlib_path(*args, **kwargs)
+
+    with _test_cwd(tmp_path):
+        with patch("builtins.open", _mock_open):
+            with patch("pylint.lint.run.Path", _mock_path):
+                cpus = _query_cpu()
+                assert cpus == expected
