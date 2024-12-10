b7740a214e297cabd1e48b14911e6ca3ceff9301
Caroline Choi <cchoi1@iris-hp-z8.stanford.edu>
2024-12-10 14:39:57 -0800

    Model patch for bug pylint__pylint7738057978676344711

--- pylint/config/exceptions.py
+++ pylint/config/exceptions.py
@@ -10,16 +10,15 @@ class UnrecognizedArgumentAction(Exception):
     action is not recognized.
     """
 
-
 class _UnrecognizedOptionError(Exception):
     """Raised if an ArgumentManager instance tries to parse an option that is
     unknown.
     """
-
     def __init__(self, options: list[str], *args: object) -> None:
         self.options = options
         super().__init__(*args)
-
-
 class ArgumentPreprocessingError(Exception):
     """Raised if an error occurs during argument pre-processing."""
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(message)
