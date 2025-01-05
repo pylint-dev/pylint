35c062cdf46dd07e8256dd0d3c5749e2415110ac
Julfried <51880314+Julfried@users.noreply.github.com>
2025-01-05 20:27:55 +0100

    Refactor test_depth_limited_write to set max depth in default config

--- tests/pyreverse/test_writer.py
+++ tests/pyreverse/test_writer.py
@@ -344,11 +344,13 @@ def test_depth_limited_write(
     default_config: PyreverseConfig, get_project: GetProjectCallable
 ) -> None:
     """Test package diagram generation with a depth limit of 1."""
+    # Set max depth in default config instead of the writer instance
+    default_config.max_depth = 1
+
     writer = DiagramWriter(default_config)
     project = get_project(TEST_DATA_DIR, name="depth_limited")
     linker = Linker(project)
     handler = DiadefsHandler(default_config)
     dd = DefaultDiadefGenerator(linker, handler).visit(project)
-    writer.max_depth = 1
     writer.write(dd)
     _assert_files_are_equal("packages_depth_limited.dot")
