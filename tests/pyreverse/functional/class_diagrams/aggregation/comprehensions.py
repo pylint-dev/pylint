1bf86ff96165a3c2374ea79e890b84c592c559e8
Julfried <51880314+Julfried@users.noreply.github.com>
2025-03-29 03:04:16 +0100

    Add a test case for comprehensions

---
+++ tests/pyreverse/functional/class_diagrams/aggregation/comprehensions.py
@@ -0,0 +1,28 @@
+# Test for https://github.com/pylint-dev/pylint/issues/10236
+
+class Component:
+    """A component class."""
+    def __init__(self, name: str):
+        self.name = name
+
+
+class Container:
+    """A container class that uses comprehension to create components."""
+    def __init__(self):
+        self.components = [Component(f"component_{i}") for i in range(3)] # list
+        self.component_dict = {i: Component(f"dict_component_{i}") for i in range(2)} # dict
+        self.components_set = {Component(f"set_component_{i}") for i in range(2)} # set
+        self.lazy_components = (Component(f"lazy_{i}") for i in range(2)) # generator
+
+
+class MultiContainer:
+    """A container with multiple component types."""
+    def __init__(self):
+        # Test mixed component types in a list
+        self.mixed = [Component("first"), Container()]
+
+
+if __name__ == "__main__":
+    container = Container()
+
+    print(type(container.components_set))
