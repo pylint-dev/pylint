dc13f1bd075e1424d193d6b8122eeff6a3f0063e
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-05-03 22:13:00 +0200

    black

--- tests/functional/u/unidiomatic_typecheck.py
+++ tests/functional/u/unidiomatic_typecheck.py
@@ -61,7 +61,8 @@ def parameter_shadowing_inference_negatives(type):
     type(42) in [int]
     type(42) not in [int]
 
-def deliberate_subclass_check_negatives(a,b):
+
+def deliberate_subclass_check_negatives(a, b):
     type(42) is type(b)
     type(42) is not type(b)
     type(42) == type(b)
@@ -71,8 +72,9 @@ def deliberate_subclass_check_negatives(a,b):
     type(a) == type(b)
     type(a) != type(b)
 
+
 def type_of_literals_positives(a):
-    type(a) is type([])   # [unidiomatic-typecheck]
+    type(a) is type([])  # [unidiomatic-typecheck]
     type(a) is not type([])  # [unidiomatic-typecheck]
     type(a) is type({})  # [unidiomatic-typecheck]
     type(a) is not type({})  # [unidiomatic-typecheck]
