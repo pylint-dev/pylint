f45bc2c6d2ff818c8db8bf1c569918106cd669e5
Julfried <julian.grimm8@gmail.com>
2025-05-19 00:16:47 +0200

    Introduce composition

--- pylint/pyreverse/diagrams.py
+++ pylint/pyreverse/diagrams.py
@@ -234,15 +234,22 @@ class ClassDiagram(Figure, FilterMixIn):
                 except KeyError:
                     continue
 
-            # associations & aggregations links
+            # Composition links
+            for name, values in list(node.compositions_type.items()):
+                for value in values:
+                    self.assign_association_relationship(
+                        value, obj, name, "composition"
+                    )
+
+            # Aggregation links
             for name, values in list(node.aggregations_type.items()):
                 for value in values:
                     self.assign_association_relationship(
                         value, obj, name, "aggregation"
                     )
 
+            # Association links
             associations = node.associations_type.copy()
-
             for name, values in node.locals_type.items():
                 if name not in associations:
                     associations[name] = values
