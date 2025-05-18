c555bf46853e279b9b6feb8104fd25bf9c1c60ca
Julfried <julian.grimm8@gmail.com>
2025-05-18 23:36:44 +0200

    Correct the test output to follow UML semantics

--- tests/pyreverse/functional/class_diagrams/aggregation/fields.py
+++ tests/pyreverse/functional/class_diagrams/aggregation/fields.py
@@ -4,24 +4,24 @@ class P:
     pass
 
 class A:
-    x: P
+    x: P  # can't tell, so default to aggregation
 
 class B:
     def __init__(self, x: P):
-        self.x = x
+        self.x = x  # not instantiated, so aggregation
 
 class C:
     x: P
 
     def __init__(self, x: P):
-        self.x = x
+        self.x = x  # not instantiated, so aggregation
 
 class D:
     x: P
 
     def __init__(self):
-        self.x = P()
+        self.x = P()  # instantiated, so composition
 
 class E:
     def __init__(self):
-        self.x = P()
+        self.x = P()  # instantiated, so composition
