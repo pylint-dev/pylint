045f179c0e86ff3a31f7b9e329caea0976c59757
Jacob Walls <jacobtylerwalls@gmail.com>
2025-05-18 15:35:50 -0400

    Resolve `possibly-used-before-assignment` false positives from `match` block assignments (#10393)
    (cherry picked from commit ad14b5bf9f2eaaf9b69a0daef09d1d944e1b11cf)

--- tests/functional/u/used/used_before_assignment_py310.py
+++ tests/functional/u/used/used_before_assignment_py310.py
@@ -5,3 +5,104 @@ match ("example", "one"):
         print("x used to cause used-before-assignment!")
     case _:
         print("good thing it doesn't now!")
+
+
+# pylint: disable = missing-function-docstring, redefined-outer-name, missing-class-docstring
+
+# https://github.com/pylint-dev/pylint/issues/9668
+from enum import Enum
+from pylint.constants import PY311_PLUS
+if PY311_PLUS:
+    from typing import assert_never  # pylint: disable=no-name-in-module
+else:
+    from typing_extensions import assert_never
+
+class Example(Enum):
+    FOO = 1
+    BAR = 2
+
+def check_value_if_then_match_return(example: Example, should_check: bool) -> str | None:
+    if should_check:
+        result = None
+    else:
+        match example:
+            case Example.FOO:
+                result = "foo"
+            case Example.BAR:
+                result = "bar"
+            case _:
+                return None
+
+    return result
+
+def check_value_if_then_match_raise(example: Example, should_check: bool) -> str | None:
+    if should_check:
+        result = None
+    else:
+        match example:
+            case Example.FOO:
+                result = "foo"
+            case Example.BAR:
+                result = "bar"
+            case _:
+                raise ValueError("Not a valid enum")
+
+    return result
+
+def check_value_if_then_match_assert_never(example: Example, should_check: bool) -> str | None:
+    if should_check:
+        result = None
+    else:
+        match example:
+            case Example.FOO:
+                result = "foo"
+            case Example.BAR:
+                result = "bar"
+            case _:
+                assert_never(example)
+
+    return result
+
+def g(x):
+    if x is None:
+        y = 0
+    else:
+        match x:
+            case int():
+                y = x
+            case _:
+                raise TypeError(type(x))
+
+    return y
+
+def check_value_if_then_match_nested(
+    example: Example, example_inner: Example, should_check: bool
+) -> str | None:
+    if should_check:
+        result = None
+    else:
+        match example:
+            case Example.FOO:
+                match example_inner:
+                    case Example.BAR:
+                        result = "bar"
+                    case _:
+                        return None
+            case _:
+                return None
+
+    return result
+
+def check_value_if_then_match_non_exhaustive(example: Example, should_check: bool) -> str | None:
+    if should_check:
+        result = None
+    else:
+        match example:
+            case Example.FOO:
+                result = "foo"
+            case Example.BAR:
+                pass
+            case _:
+                return None
+
+    return result  # [possibly-used-before-assignment]
