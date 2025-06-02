ddfe7a383ad7fbcd6ac01709d1b7eb214c7e45c3
pavan-msys <pyeddula@msystechnologies.com>
2025-06-02 18:33:31 +0530

    modified test file

--- tests/test_import_module_shadowing.py
+++ tests/test_import_module_shadowing.py
@@ -1,4 +1,6 @@
-# pylint: disable=missing-docstring,consider-using-with,trailing-whitespace,unused-import
+# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# For details: https://github.com/pylint-dev/pylint/blob/main/LICENSE
+# Copyright (c) https://github.com/pylint-dev/pylint/blob/main/CONTRIBUTORS.txt
 
 import os
 import tempfile
