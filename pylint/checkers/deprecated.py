52cad2e9189675112e05a96d7f6dadf80c12530c
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-06-07 09:29:12 +0200

    [mypy] Fix expected 'str' in 'check_deprecated_class'

--- pylint/checkers/deprecated.py
+++ pylint/checkers/deprecated.py
@@ -142,14 +142,11 @@ class DeprecatedMixin(BaseChecker):
         if qname in self.deprecated_decorators():
             self.add_message("deprecated-decorator", node=node, args=qname)
 
-    @utils.only_required_for_messages(
-        "deprecated-module",
-        "deprecated-class",
-    )
+    @utils.only_required_for_messages("deprecated-module", "deprecated-class")
     def visit_importfrom(self, node: nodes.ImportFrom) -> None:
         """Triggered when a from statement is seen."""
-        basename = node.modname
-        basename = get_import_name(node, basename)
+        basename = get_import_name(node, node.modname)
+        assert basename is not None, "Module name should not be None"
         self.check_deprecated_module(node, basename)
         class_names = (name for name, _ in node.names)
         self.check_deprecated_class(node, basename, class_names)
