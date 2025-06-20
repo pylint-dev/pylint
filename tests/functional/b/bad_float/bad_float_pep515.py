96628ea04f6322a9bae621aea3f37e6c0bf2a93e
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-06-20 23:54:05 +0200

    Fix the strict notation functional tests

--- tests/functional/b/bad_float/bad_float_pep515.py
+++ tests/functional/b/bad_float/bad_float_pep515.py
@@ -3,6 +3,7 @@
 not_grouped_by_three = 1_23_456_7_89  # [bad-float-notation]
 mixing_with_exponent = 1_23_4_5_67_8e9  # [bad-float-notation]
 above_threshold_without_grouping = 123456789  # [bad-float-notation]
+scientific_notation = 1.2345678e16 # [bad-float-notation]
+engineering_notation = 12.345678e15 # [bad-float-notation]
+
 proper_grouping = 123_456_789
-scientific_notation = 1.2345678e16
-engineering_notation = 12.345678e15
