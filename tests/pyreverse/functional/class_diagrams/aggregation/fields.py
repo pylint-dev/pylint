0a4488d71954fcd5f088d1f35360ff3a4b20be66
Julfried <51880314+Julfried@users.noreply.github.com>
2025-06-28 16:47:55 +0200

    Update comments in test file

--- tests/pyreverse/functional/class_diagrams/aggregation/fields.py
+++ tests/pyreverse/functional/class_diagrams/aggregation/fields.py
@@ -4,24 +4,22 @@ class P:
     pass
 
 class A:
-    x: P  # just type hint, no ownership, so association
+    x: P  # just type hint, no ownership → Association
 
 class B:
     def __init__(self, x: P):
-        self.x = x  # not instantiated, so aggregation
+        self.x = x  # receives object, not created → Aggregation
 
 class C:
     x: P
-
     def __init__(self, x: P):
-        self.x = x  # not instantiated, so aggregation
+        self.x = x  # receives object, not created → Aggregation
 
 class D:
     x: P
-
     def __init__(self):
-        self.x = P()  # instantiated, so composition
+        self.x = P()  # creates object → Composition
 
 class E:
     def __init__(self):
-        self.x = P()  # instantiated, so composition
+        self.x = P()  # creates object → Composition
