97f760322a8b89a4a34f45f3a21b2cda90a01cde
Florent Gallaire <fgallaire@gmail.com>
2025-07-01 02:54:31 +0200

    Fix max_positional_arguments, fix tests

--- tests/functional/t/too/too_many_positional_arguments.py
+++ tests/functional/t/too/too_many_positional_arguments.py
@@ -1,9 +1,9 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 class FiveArgumentMethods:
-    """The max positional arguments default is 5."""
-    def fail1(self, a, b, c, d, e):  # [too-many-arguments, too-many-positional-arguments]
+    """The max positional arguments default is 5, so 6 for a method because of self."""
+    def fail1(self, a, b, c, d, e, f):  # [too-many-arguments, too-many-positional-arguments]
         pass
-    def fail2(self, a, b, c, d, /, e):  # [too-many-arguments, too-many-positional-arguments]
+    def fail2(self, a, b, c, d, e, /, f):  # [too-many-arguments, too-many-positional-arguments]
         pass
-    def okay1(self, a, b, c, d, *, e=True):  # [too-many-arguments]
+    def okay1(self, a, b, c, d, e, *, f=True):  # [too-many-arguments]
         pass
