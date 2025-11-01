dc85a9e932bef2964193fad5413bdb55516795f3
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-11-01 08:09:01 +0100

    Better naming

--- tests/functional/i/invalid/invalid_name/invalid_name_with_final_typing.py
+++ tests/functional/i/invalid/invalid_name/invalid_name_with_final_typing.py
@@ -1,24 +1,26 @@
-""" Regression test for https://github.com/pylint-dev/pylint/issues/10711. """
+"""Regression test for https://github.com/pylint-dev/pylint/issues/10711."""
 
 # pylint: disable=missing-class-docstring, missing-function-docstring
 
 from dataclasses import dataclass
 from typing import Final
 
-module_snake_case_param: Final[int] = 42  # [invalid-name]
-MODULE_UPPER_CASE_PARAM: Final[int] = 42
+module_snake_case_constant: Final[int] = 42  # [invalid-name]
+MODULE_UPPER_CASE_CONSTANT: Final[int] = 42
+
 
 def example_function() -> None:
-    function_snake_case_param: Final[int] = 42 # [invalid-name]
-    FUNCTION_UPPER_CASE_PARAM: Final[int] = 42
-    print(function_snake_case_param, FUNCTION_UPPER_CASE_PARAM)
+    function_snake_case_constant: Final[int] = 42  # [invalid-name]
+    FUNCTION_UPPER_CASE_CONSTANT: Final[int] = 42
+    print(function_snake_case_constant, FUNCTION_UPPER_CASE_CONSTANT)
+
 
 @dataclass
 class ExampleClass:
-    class_snake_case_param: Final[int] = 42 # [invalid-name]
-    CLASS_UPPER_CASE_PARAM: Final[int] = 42
+    class_snake_case_constant: Final[int] = 42  # [invalid-name]
+    CLASS_UPPER_CASE_CONSTANT: Final[int] = 42
 
     def method(self) -> None:
-        method_snake_case_param: Final[int] = 42 # [invalid-name]
-        METHOD_UPPER_CASE_PARAM: Final[int] = 42
-        print(method_snake_case_param, METHOD_UPPER_CASE_PARAM)
+        method_snake_case_constant: Final[int] = 42  # [invalid-name]
+        METHOD_UPPER_CASE_CONSTANT: Final[int] = 42
+        print(method_snake_case_constant, METHOD_UPPER_CASE_CONSTANT)
