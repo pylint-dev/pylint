5fc003cdf4d4550464bea77d9f2e0a0e16b0df36
Jacob Walls <jacobtylerwalls@gmail.com>
2025-11-07 07:46:00 -0500

    Fix behavior

--- tests/functional/i/invalid/invalid_name/invalid_name_with_final_typing.py
+++ tests/functional/i/invalid/invalid_name/invalid_name_with_final_typing.py
@@ -10,8 +10,8 @@ MODULE_UPPER_CASE_CONSTANT: Final[int] = 42
 
 
 def function() -> None:
-    function_snake_case_constant: Final[int] = 42  # [invalid-name]
-    FUNCTION_UPPER_CASE_CONSTANT: Final[int] = 42
+    function_snake_case_constant: Final[int] = 42
+    FUNCTION_UPPER_CASE_CONSTANT: Final[int] = 42  # [invalid-name]
     print(function_snake_case_constant, FUNCTION_UPPER_CASE_CONSTANT)
 
 
@@ -21,6 +21,6 @@ class Class:
     CLASS_UPPER_CASE_CONSTANT: Final[int] = 42
 
     def method(self) -> None:
-        method_snake_case_constant: Final[int] = 42  # [invalid-name]
-        METHOD_UPPER_CASE_CONSTANT: Final[int] = 42
+        method_snake_case_constant: Final[int] = 42
+        METHOD_UPPER_CASE_CONSTANT: Final[int] = 42  # [invalid-name]
         print(method_snake_case_constant, METHOD_UPPER_CASE_CONSTANT)
