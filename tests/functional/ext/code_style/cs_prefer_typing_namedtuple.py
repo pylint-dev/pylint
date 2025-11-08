be9701c324763c515347883b82f47418e6bde984
Zen Lee <zenlyj97@gmail.com>
2025-11-08 23:54:17 +0800

    Fix crash when a slice object is called

--- tests/functional/ext/code_style/cs_prefer_typing_namedtuple.py
+++ tests/functional/ext/code_style/cs_prefer_typing_namedtuple.py
@@ -7,3 +7,8 @@ class SearchMatch(
     namedtuple('SearchMatch', ['els', 'index', 'iterator'])  # [prefer-typing-namedtuple]
 ):
     """Adapted from primer package `music21`."""
+
+
+# Regression test for https://github.com/pylint-dev/pylint/issues/10708
+x = slice(42)
+x()  # pylint: disable=not-callable
