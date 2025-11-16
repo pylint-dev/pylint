a4e83616412c21b7e210ab77811f16b22b8a3874
Alvaro Frias <alvaro.frias@eclypsium.com>
2025-11-16 19:51:32 -0300

    update docs
    Signed-off-by: Alvaro Frias <alvaro.frias@eclypsium.com>

--- doc/data/messages/m/missing-param-type-annotation/bad.py
+++ doc/data/messages/m/missing-param-type-annotation/bad.py
@@ -2,9 +2,9 @@ def greet(name):  # [missing-param-type-annotation]
     return f"Hello, {name}!"
 
 
-def add(x, y) -> int:  # [missing-param-type-annotation]
+def add(x, y) -> int:  # [missing-param-type-annotation, missing-param-type-annotation]
     return x + y
 
 
-def process(*args, **kwargs):  # [missing-param-type-annotation]
+def process(*args, **kwargs):  # [missing-param-type-annotation, missing-param-type-annotation]
     return combine(args, kwargs)
