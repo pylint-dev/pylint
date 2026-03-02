fdab22daa154fbc923c06a2eb0f0061dcddd5eef
Roman Valov <roman.valov@gmail.com>
2026-03-02 23:54:14 +0000

    Implement too-many-positional-arguments test for static and class methods

--- tests/functional/t/too/too_many_positional_arguments.py
+++ tests/functional/t/too/too_many_positional_arguments.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 class RegularMethods:
-    """The max positional arguments default is 5. Regular methods doesn't count `self`."""
+    """The max positional arguments default is 5. Regular methods don't count `self`."""
+
     # +1: [too-many-arguments, too-many-positional-arguments]
     def regular_fail1(self, a, b, c, d, e, f):
         pass
@@ -11,4 +12,46 @@ class RegularMethods:
     def regular_okay1(self, a, b, c, d, e, *, f=True):
         pass
     def regular_okay2(self, a, b, c, d, e):
+        pass
+
+
+# pylint: disable=missing-function-docstring, missing-module-docstring
+class StaticMethods:
+    """The max positional arguments default is 5. Static methods count them all."""
+
+    @staticmethod
+    # +1: [too-many-arguments, too-many-positional-arguments]
+    def static_fail1(a, b, c, d, e, f):
+        pass
+    @staticmethod
+    # +1: [too-many-arguments, too-many-positional-arguments]
+    def static_fail2(a, b, c, d, e, /, f):
+        pass
+    @staticmethod
+    # +1: [too-many-arguments]
+    def static_okay1(a, b, c, d, e, *, f=True):
+        pass
+    @staticmethod
+    def static_okay2(a, b, c, d, e):
+        pass
+
+
+# pylint: disable=missing-function-docstring, missing-module-docstring
+class ClassMethods:
+    """The max positional arguments default is 5. Class methods don't count `cls`."""
+
+    @classmethod
+    # +1: [too-many-arguments, too-many-positional-arguments]
+    def class_fail1(cls, a, b, c, d, e, f):
+        pass
+    @classmethod
+    # +1: [too-many-arguments, too-many-positional-arguments]
+    def class_fail2(cls, a, b, c, d, e, /, f):
+        pass
+    @classmethod
+    # +1: [too-many-arguments]
+    def class_okay1(cls, a, b, c, d, e, *, f=True):
+        pass
+    @classmethod
+    def class_okay2(cls, a, b, c, d, e):
         pass
