862e3e9e48234da3e07fed92cd44b06b154e8986
Julfried <julian.grimm8@gmail.com>
2026-03-05 22:41:24 +0100

    Revert "Also use augmented syspath when parallel execution is requested"
    This reverts commit f82dcad9e1eb6f3a67bb5254d07aecee6b652e76.

--- pylint/lint/pylinter.py
+++ pylint/lint/pylinter.py
@@ -719,13 +719,12 @@ class PyLinter(
         # TODO: Move the parallel invocation into step 3 of the checking process
         if not self.config.from_stdin and self.config.jobs > 1:
             original_sys_path = sys.path[:]
-            with augmented_sys_path(extra_packages_paths):
-                check_parallel(
-                    self,
-                    self.config.jobs,
-                    self._iterate_file_descrs(files_or_modules),
-                    extra_packages_paths,
-                )
+            check_parallel(
+                self,
+                self.config.jobs,
+                self._iterate_file_descrs(files_or_modules),
+                extra_packages_paths,
+            )
             sys.path = original_sys_path
             return
 
