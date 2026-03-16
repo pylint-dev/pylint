a40c536eba4598c5d2a11e3c4e81774ff06a40d5
Roman Valov <roman.valov@gmail.com>
2026-03-16 10:35:02 +0000

    Add tests for interference between first argument and ignored arguments

--- tests/functional/t/too/too_many_positional_arguments.py
+++ tests/functional/t/too/too_many_positional_arguments.py
@@ -20,11 +20,25 @@ class RegularMethods:
     # +1: [too-many-arguments]
     def regular_soso3(self, /, a, b, c, d, e, *, f=True):
         pass
+    # +1: [too-many-arguments]
+    def regular_soso4(self, a, b, c, d, e, /, _f, *, g=True):
+        pass
+    # +1: [too-many-arguments]
+    def regular_soso5(self, /, _a, b, c, d, e, f, *, g=True):
+        pass
     def regular_okay1(self, a, b, c, d, e):
         pass
     def regular_okay2(self, a, b, c, d, e, /):
         pass
     def regular_okay3(self, *, a, b, c, d, e):
+        pass
+    def regular_okay4(self, a, b, c, d, e, /, _f):
+        pass
+    def regular_okay5(self, _a, b, c, d, e, /, f):
+        pass
+    def regular_okay6(self, /, _a, b, c, d, e, f):
+        pass
+    def regular_okay7(self, /, a, b, c, d, e, _f):
         pass
 
 
@@ -49,6 +63,10 @@ class StaticMethods:
     def static_soso2(a, b, c, d, e, /, *, f=True):
         pass
     @staticmethod
+    # +1: [too-many-arguments]
+    def static_soso3(a, b, c, d, e, /, _f, *, g=True):
+        pass
+    @staticmethod
     def static_okay1(a, b, c, d, e):
         pass
     @staticmethod
@@ -56,6 +74,12 @@ class StaticMethods:
         pass
     @staticmethod
     def static_okay3(*, a, b, c, d, e):
+        pass
+    @staticmethod
+    def static_okay4(a, b, c, d, e, /, _f):
+        pass
+    @staticmethod
+    def static_okay5(_a, b, c, d, e, /, f):
         pass
 
 
@@ -88,6 +112,14 @@ class ClassMethods:
     def class_soso3(cls, /, a, b, c, d, e, *, f=True):
         pass
     @classmethod
+    # +1: [too-many-arguments]
+    def class_soso4(cls, a, b, c, d, e, /, _f, *, g=True):
+        pass
+    @classmethod
+    # +1: [too-many-arguments]
+    def class_soso5(cls, /, _a, b, c, d, e, f, *, g=True):
+        pass
+    @classmethod
     def class_okay1(cls, a, b, c, d, e):
         pass
     @classmethod
@@ -95,4 +127,16 @@ class ClassMethods:
         pass
     @classmethod
     def class_okay3(cls, *, a, b, c, d, e):
+        pass
+    @classmethod
+    def class_okay4(cls, a, b, c, d, e, /, _f):
+        pass
+    @classmethod
+    def class_okay5(cls, _a, b, c, d, e, /, f):
+        pass
+    @classmethod
+    def class_okay6(cls, /, _a, b, c, d, e, f):
+        pass
+    @classmethod
+    def class_okay7(cls, /, a, b, c, d, e, _f):
         pass
