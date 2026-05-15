371933bfbfb3da19264f8f1add87c0c7e35dc771
Douglas Needham <cinnion@gmail.com>
2026-05-15 15:43:45 -0400

    Fix pylint skipping similarly named project directory (#10970)
    Because _discover_files() will falsely ignore a with a name like 
    `applications_api` after walking across one named `applications`, add
    the os directory separator to the end of what is a known directory
    (because of the fact that it has a file named `__init__.py` in it).
    
    Closes #10969

--- pylint/lint/pylinter.py
+++ pylint/lint/pylinter.py
@@ -681,11 +681,11 @@ class PyLinter(
                         self.config.ignore_patterns,
                         self.config.ignore_paths,
                     ):
-                        skip_subtrees.append(root)
+                        skip_subtrees.append(root + os.sep)
                         continue
 
                     if "__init__.py" in files:
-                        skip_subtrees.append(root)
+                        skip_subtrees.append(root + os.sep)
                         yield root
                     else:
                         yield from (
