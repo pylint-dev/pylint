90c385e5f2ab457aeca0f8bc7f7980d65b2c8302
Millefeuille <millefeuille42@proton.me>
2026-05-31 16:47:42 +0200

    fix: use metaclass __call__ signature on class instantiation (#11055)
    Fixes a E1121 (too-many-function-args),
    a E1120 (no-value-for-parameter) and probaly other false positives
    related to the function signature,
    that triggers when a class uses a metaclass
    that overrides `__call__` with a different signature.
    
    This was caused by `_determine_callable`
    in `pylint/checkers/typecheck.py` that was not checking for the
    potential metaclass and their potential `__call__` override
    and rather solely checked `__init__` and `__new__`.
    Adding a check for it's existence and returning it
    solved the false positives.
    
    Signed-off-by: Millefeuille <millefeuille42@proton.me>
    (cherry picked from commit d6eb4812f4a02c24d4ab986489304048618852a8)

--- tests/functional/t/too/too_many_function_args.py
+++ tests/functional/t/too/too_many_function_args.py
@@ -22,3 +22,22 @@ def main(param):
 # Negative case, see `_check_isinstance_args` in `./pylint/checkers/typecheck.py`
 isinstance(1, int, int) # [too-many-function-args]
 isinstance(1, 1, int) # [too-many-function-args, isinstance-second-argument-not-valid-type]
+
+# Check if respecting Python's actual runtime MRO when using metaclasses
+class Meta(type):
+    """Metaclass with a __call__ method that takes more arguments than the class it creates."""
+    def __call__(cls, a: int, b: int, c: int) -> "MyClass":
+        """Calls the class __call__ with the first argument."""
+        return super().__call__(a)
+
+
+class MyClass(metaclass=Meta): # pylint: disable=too-few-public-methods
+    """Class with a __init__ method that takes fewer arguments than the metaclass __call__."""
+    def __init__(self, a: str) -> None:
+        """Initializes the class."""
+        self.a = a
+        self.b = None
+
+
+MyClass(1, 2, 3, 4) # [too-many-function-args]
+MyClass(1, 2, 3)
