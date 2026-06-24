f7dc94090a437c1277f1f5b161e3d125d608af5d
Julfried <julian.grimm8@gmail.com>
2026-06-24 09:14:06 +0200

    Share primer configuration

--- pylint/testutils/_primer/pyreverse_primer.py
+++ pylint/testutils/_primer/pyreverse_primer.py
@@ -4,12 +4,13 @@
 
 from __future__ import annotations
 
-import argparse
-import json
-import sys
 from pathlib import Path
 
 from pylint.testutils._primer import PackageToLint
+from pylint.testutils._primer.primer_configuration import (
+    get_argument_parser,
+    get_packages_to_prime_from_json,
+)
 from pylint.testutils._primer.primer_prepare_command import PrepareCommand
 from pylint.testutils._primer.pyreverse_primer_command import PyreversePrimerCommand
 from pylint.testutils._primer.pyreverse_primer_compare_command import CompareCommand
@@ -24,64 +25,16 @@ class PyreversePrimer:
         self,
         primer_directory: Path,
         packages_path: Path,
-        targets_path: Path,
     ) -> None:
         self.primer_directory = primer_directory
-        self._argument_parser = argparse.ArgumentParser(prog="Pylint Pyreverse Primer")
-        self._subparsers = self._argument_parser.add_subparsers(
-            dest="command", required=True
-        )
-
-        prepare_parser = self._subparsers.add_parser("prepare")
-        prepare_parser.add_argument(
-            "--clone", help="Clone all packages.", action="store_true", default=False
-        )
-        prepare_parser.add_argument(
-            "--check",
-            help="Check consistencies and commits of all packages.",
-            action="store_true",
-            default=False,
-        )
-        prepare_parser.add_argument(
-            "--make-commit-string",
-            help="Get latest commit string.",
-            action="store_true",
-            default=False,
-        )
-        prepare_parser.add_argument(
-            "--read-commit-string",
-            help="Print latest commit string.",
-            action="store_true",
-            default=False,
-        )
-
-        run_parser = self._subparsers.add_parser("run")
-        run_parser.add_argument(
-            "--type", choices=["main", "pr"], required=True, help="Type of primer run."
-        )
-
-        compare_parser = self._subparsers.add_parser("compare")
-        compare_parser.add_argument(
-            "--base-file",
-            required=True,
-            help="Location of output file of the base run.",
-        )
-        compare_parser.add_argument(
-            "--new-file",
-            required=True,
-            help="Location of output file of the new run.",
-        )
-        compare_parser.add_argument(
-            "--commit",
-            required=True,
-            help="Commit hash of the PR commit being checked.",
+        self._argument_parser = get_argument_parser(
+            "Pylint Pyreverse Primer", with_batches=False
         )
 
         self.config = self._argument_parser.parse_args()
-        self.targets = self._get_targets_from_json(targets_path)
-        self.packages = self._get_packages_to_prime_from_json(
-            packages_path, self.targets
-        )
+        packages_to_prime = get_packages_to_prime_from_json(packages_path)
+        self.targets = self._get_targets_from_packages(packages_to_prime)
+        self.packages = self._filter_packages_for_pyreverse(packages_to_prime)
 
         if self.config.command == "prepare":
             self.command: PrepareCommand | PyreversePrimerCommand = PrepareCommand(
@@ -100,38 +53,21 @@ class PyreversePrimer:
         self.command.run()
 
     @staticmethod
-    def _minimum_python_supported(package_data: dict[str, str]) -> bool:
-        min_python_str = package_data.get("minimum_python", None)
-        if not min_python_str:
-            return True
-        min_python_tuple = tuple(int(n) for n in min_python_str.split("."))
-        return min_python_tuple <= sys.version_info[:2]
-
-    @staticmethod
-    def _get_targets_from_json(
-        json_path: Path,
+    def _get_targets_from_packages(
+        packages: dict[str, PackageToLint],
     ) -> dict[str, PyreversePrimerTarget]:
-        with open(json_path, encoding="utf-8") as stream:
-            return {
-                name: PyreversePrimerTarget(**target_data)
-                for name, target_data in json.load(stream).items()
-            }
+        return {
+            target_name: PyreversePrimerTarget(package=package_name, **target_data)
+            for package_name, package in packages.items()
+            for target_name, target_data in package.pyreverse_targets.items()
+        }
 
     @staticmethod
-    def _get_packages_to_prime_from_json(
-        json_path: Path,
-        targets: dict[str, PyreversePrimerTarget],
+    def _filter_packages_for_pyreverse(
+        packages: dict[str, PackageToLint],
     ) -> dict[str, PackageToLint]:
-        with open(json_path, encoding="utf-8") as stream:
-            packages_to_prime = {
-                name: PackageToLint(**package_data)
-                for name, package_data in json.load(stream).items()
-                if PyreversePrimer._minimum_python_supported(package_data)
-            }
-
-        package_names = {target.package for target in targets.values()}
         return {
-            package_name: packages_to_prime[package_name]
-            for package_name in package_names
-            if package_name in packages_to_prime
+            package_name: package
+            for package_name, package in packages.items()
+            if package.pyreverse_targets
         }
