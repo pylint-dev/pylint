400a4116fa5b05dab169f3ef1ec7472773ca70ff
Kotesh Kumar Yelamati <yelamatikoteshkumar@gmail.com>
2026-07-06 22:41:06 -0400

    Cover variadic branches of parameter kind summary in tests

--- tests/functional/a/arguments_differ_issue5793.py
+++ tests/functional/a/arguments_differ_issue5793.py
@@ -20,3 +20,19 @@ class Child(Base):
 
     def positional_only(self, param1, param2):  # [arguments-differ]
         return param1 - param2
+
+
+class VariadicBase:
+    def variadic_positional(self, *args):
+        print(args)
+
+    def variadic_keyword(self, **kwargs):
+        print(kwargs)
+
+
+class VariadicChild(VariadicBase):
+    def variadic_positional(self, arg):  # [arguments-differ, arguments-differ]
+        print(arg)
+
+    def variadic_keyword(self, *, arg):  # [arguments-differ, arguments-differ]
+        print(arg)
