a3a7485db262fcda4ad8103b216a7bcad8720e98
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2026-07-27 22:01:50 +0200

    Fix too-many-lines being reported at another module's pragma line
    (cherry picked from commit 9f6657a0b862543f0e0c2b951361edddfc901a7f)

--- pylint/lint/message_state_handler.py
+++ pylint/lint/message_state_handler.py
@@ -30,6 +30,9 @@ if TYPE_CHECKING:
     from pylint.lint.pylinter import PyLinter
 
 
+CONTROL_PRAGMAS = frozenset({"disable", "disable-next", "enable"})
+
+
 class _MessageStateHandler:
     """Class that handles message disabling & enabling and processing of inline
     pragma's.
@@ -350,7 +353,7 @@ class _MessageStateHandler:
 
         See func_block_disable_msg.py test case for expected behaviour.
         """
-        control_pragmas = {"disable", "disable-next", "enable"}
+        self._pragma_lineno = {}
         prev_line = None
         saw_newline = True
         seen_newline = True
@@ -395,7 +398,7 @@ class _MessageStateHandler:
                         )
                     for msgid in pragma_repr.messages:
                         # Add the line where a control pragma was encountered.
-                        if pragma_repr.action in control_pragmas:
+                        if pragma_repr.action in CONTROL_PRAGMAS:
                             self._pragma_lineno[msgid] = start[0]
 
                         if (pragma_repr.action, msgid) == ("disable", "all"):
