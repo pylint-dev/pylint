2e3d033b0de3e9152599943ba2007e74927aeff9
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2026-08-09 20:37:57 +0200

    Bump pylint to 4.0.7, update changelog

--- pylint/__pkginfo__.py
+++ pylint/__pkginfo__.py
@@ -9,7 +9,7 @@ It's updated via tbump, do not modify.
 
 from __future__ import annotations
 
-__version__ = "4.0.6"
+__version__ = "4.0.7"
 
 
 def get_numversion_from_version(v: str) -> tuple[int, int, int]:
