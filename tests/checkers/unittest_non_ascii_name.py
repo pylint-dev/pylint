fc2f9e1d52011a8c4f6482533a0b204deeb5055b
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2026-08-17 12:11:16 +0200

    Enable ruff's PT014 (pytest-duplicate-parametrize-test-cases)
    Both duplicates were copy-paste leftovers; one of them even reused the same test id twice.

--- tests/checkers/unittest_non_ascii_name.py
+++ tests/checkers/unittest_non_ascii_name.py
@@ -238,11 +238,6 @@ class TestNonAsciiChecker(pylint.testutils.CheckerTestCase):
                 id="from_okay_module_import_bad_as_good_and_bad",
             ),
             pytest.param(
-                "from foo.bar import functiøn as good, bäd",
-                "bäd",
-                id="from_okay_module_import_bad_as_good_and_bad",
-            ),
-            pytest.param(
                 "from foo.bar import functiøn as good, *",
                 # We still have functiøn within our namespace and could detect this
                 # But to do this properly we would need to check all `*` imports
