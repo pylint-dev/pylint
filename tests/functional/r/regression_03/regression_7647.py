1b324f986c6bd60f295d52d8d4ad16eafeb84297
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2026-09-12 22:40:46 +0200

    Restore the functional test for issue 7647 from main
    Main grew its own regression test at this exact path while this branch was
    open, so merging main conflicted on it. The resolution kept this branch's copy
    and dropped main's; main's is the one to keep.

--- tests/functional/r/regression_03/regression_7647.py
+++ tests/functional/r/regression_03/regression_7647.py
@@ -1,27 +1,22 @@
-# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# For details: https://github.com/pylint-dev/pylint/blob/main/LICENSE
-# Copyright (c) https://github.com/pylint-dev/pylint/blob/main/CONTRIBUTORS.txt
-
-"""Regression test for https://github.com/pylint-dev/pylint/issues/7647
-
-lambda referencing conditional kwargs dict should not trigger unnecessary-lambda.
-
-Reproducible up to pylint 3.0; fixed in pylint 3.1.0.
-"""
-
-# pylint: disable=missing-docstring
+# Regression test for https://github.com/pylint-dev/pylint/issues/7647
+# `unnecessary-lambda` was wrongly emitted when the lambda called a function
+# with `**kwargs` built from an if-else expression.
+"""No `unnecessary-lambda` when kwargs come from an if-else expression."""
 from contextlib import ExitStack
 
 
-def fun(**kwargs):
+def eat(**kwargs):
+    """Print the meal."""
     print(kwargs)
 
 
-def run_in_context(callable_):
+def run_in_context(callback):
+    """Run the callback inside a context."""
     with ExitStack():
-        callable_()
+        callback()
 
 
-def main(omit_arg: bool):
-    kwargs = {} if omit_arg else {"arg": 2}
-    run_in_context(lambda: fun(**kwargs))
+def main(hungry: bool):
+    """Build kwargs conditionally and run."""
+    kwargs = {} if hungry else {"fruit": "apple"}
+    run_in_context(lambda: eat(**kwargs))
