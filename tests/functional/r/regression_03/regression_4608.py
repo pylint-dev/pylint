099a79a5cebf77ddb6881c8765f2e4a461c75808
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2026-09-13 07:20:50 +0200

    Add regression test for issue 4608

---
+++ tests/functional/r/regression_03/regression_4608.py
@@ -0,0 +1,18 @@
+# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# For details: https://github.com/pylint-dev/pylint/blob/main/LICENSE
+# Copyright (c) https://github.com/pylint-dev/pylint/blob/main/CONTRIBUTORS.txt
+
+"""Regression test for https://github.com/pylint-dev/pylint/issues/4608
+
+'-x if x is not None else None' should not trigger invalid-unary-operand-type.
+"""
+
+# pylint: disable=missing-docstring,invalid-name
+import random
+
+if random.choice([0, 1]):
+    x = None
+else:
+    x = 15
+
+y = (-x if x is not None else None)
