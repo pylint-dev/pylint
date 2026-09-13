6d837069a53b3dd003698985b0c35c6149d612c6
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2026-09-13 07:20:51 +0200

    Add regression test for issue 9497

---
+++ tests/functional/r/regression_03/regression_9497.py
@@ -0,0 +1,12 @@
+# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# For details: https://github.com/pylint-dev/pylint/blob/main/LICENSE
+# Copyright (c) https://github.com/pylint-dev/pylint/blob/main/CONTRIBUTORS.txt
+
+"""Regression test for https://github.com/pylint-dev/pylint/issues/9497
+
+datetime.datetime non-existent member should be detected by no-member.
+"""
+
+import datetime
+
+print(datetime.datetime.not_a_member)  # [no-member]
