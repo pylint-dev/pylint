f5d53f4f6100c87a1e02ab06879e2468125201d2
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2026-09-25 09:33:12 +0200

    Explain generator vs list in the looping-through-iterator example

--- doc/data/messages/l/looping-through-iterator/bad.py
+++ doc/data/messages/l/looping-through-iterator/bad.py
@@ -1,3 +1,4 @@
+# A generator: it can only be looped over once, then it is empty
 fruits = (name.upper() for name in ["apple", "banana"])
 for basket in ["red basket", "blue basket"]:
     for fruit in fruits:  # [looping-through-iterator]
--- doc/data/messages/l/looping-through-iterator/good.py
+++ doc/data/messages/l/looping-through-iterator/good.py
@@ -1,3 +1,4 @@
+# A list: it can be looped over again and again
 fruits = [name.upper() for name in ["apple", "banana"]]
 for basket in ["red basket", "blue basket"]:
     for fruit in fruits:
