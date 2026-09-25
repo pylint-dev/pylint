47d7cdc00c7376ea0912a7ce7c377a79f85f579f
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2026-09-25 09:33:24 +0200

    Use a tuple in the looping-through-iterator good example

--- doc/data/messages/l/looping-through-iterator/bad.py
+++ doc/data/messages/l/looping-through-iterator/bad.py
@@ -1,4 +1,4 @@
-# A generator: it can only be looped over once, then it is empty
+# Looks like a tuple, but it is a generator: it can only be looped over once
 fruits = (name.upper() for name in ["apple", "banana"])
 for basket in ["red basket", "blue basket"]:
     for fruit in fruits:  # [looping-through-iterator]
--- doc/data/messages/l/looping-through-iterator/good.py
+++ doc/data/messages/l/looping-through-iterator/good.py
@@ -1,5 +1,5 @@
-# A list: it can be looped over again and again
-fruits = [name.upper() for name in ["apple", "banana"]]
+# A real tuple: it can be looped over again and again
+fruits = tuple(name.upper() for name in ["apple", "banana"])
 for basket in ["red basket", "blue basket"]:
     for fruit in fruits:
         print(basket, fruit)
